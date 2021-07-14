aac376f53e46ccdb7f3e676f7b8abdf4a69754d8
dongwu-kim <alduk7797@naver.com>
2021-07-14 21:14:33 +0900

    MODIFY : localstorage 저장 및 정보전달방식 개선 - 기존 click event, keyup event form 전달로 개선

--- students/23th/westagram/js/login.js
+++ students/23th/westagram/js/login.js
@@ -1,3 +1,16 @@
+function localStorageIdClear() {
+  localStorage.clear();
+}
+
+function setIdLocalStorage() {
+  const idInput = document.querySelector(`#loginIdInput`),
+    loginForm = document.querySelector(`.login-block__form`);
+
+  loginForm.addEventListener(`submit`, () => {
+    localStorage.setItem(`id`, `${idInput.value.split(`@`)[0]}`);
+  });
+}
+
 function inputTextEvent() {
   const idInput = document.querySelector(`#loginIdInput`),
     pwInput = document.querySelector(`#loginPwInput`);
@@ -18,70 +31,8 @@ function inputTextEvent() {
   });
 }
 
-function loginBtnClick() {
-  // const url = window.location.href;
-  // let nowUrl = url.split(`/`);
-  // if (nowUrl[3] === `login.html`) {
-  const idInput = document.querySelector(`#loginIdInput`),
-    pwInput = document.querySelector(`#loginPwInput`);
-  const loginBtn = document.querySelector(`#loginBtn`);
-
-  loginBtn.addEventListener(`click`, () => {
-    const url = `./main.html?`;
-    const id = idInput.value.split(`@`)[0];
-    history.pushState(``, ``, url);
-    localStorage.setItem(`id`, `${id}`);
-  });
-  // }
-}
-
-// function loginEnterPress() {
-//   const idInput = document.querySelector(`#loginIdInput`),
-//     pwInput = document.querySelector(`#loginPwInput`);
-//   idInput.addEventListener(`keyup`, (event) => {
-//     if (event.code === `Enter`) {
-//       if (idInput.value === `` && pwInput.value === ``) {
-//         alert(`ID, Password를 입력해주세요.`);
-//       } else if (idInput.value === `` && pwInput.value !== ``) {
-//         alert(`ID를 입력해주세요.`);
-//       } else if (idInput.value !== `` && pwInput.value === ``) {
-//         alert(`Password를 입력해주세요.`);
-//       } else {
-//         const url = `./main.html?`;
-//         const id = idInput.value.split(`@`)[0];
-//         history.pushState(``, ``, url);
-//         localStorage.setItem(`id`, `${id}`);
-//       }
-//     }
-//   });
-//   pwInput.addEventListener(`keyup`, (event) => {
-//     if (event.code === `Enter`) {
-//       if (idInput.value === `` && pwInput.value === ``) {
-//         alert(`ID, Password를 입력해주세요.`);
-//       } else if (idInput.value === `` && pwInput.value !== ``) {
-//         alert(`ID를 입력해주세요.`);
-//       } else if (idInput.value !== `` && pwInput.value === ``) {
-//         alert(`Password를 입력해주세요.`);
-//       } else {
-//         const url = `./main.html?`;
-//         const id = idInput.value.split(`@`)[0];
-//         history.pushState(``, ``, url);
-//         localStorage.setItem(`id`, `${id}`);
-//       }
-//     }
-//   });
-// }
-
-// function mainReload() {
-//   const url = `http://127.0.0.1:5500/login.html`;
-//   console.log(window.location.href);
-//   if (window.location.href === url) {
-//     location.reload();
-//   }
-// }
-
 (() => {
-  // mainReload();
+  localStorageIdClear();
+  setIdLocalStorage();
   inputTextEvent();
-  loginBtnClick();
 })();
