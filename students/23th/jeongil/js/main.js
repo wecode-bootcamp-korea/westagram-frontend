363aa2354ce1d191c9757dbb30a0fcd2b1041c0a
201steve <jeongillee201@gmail.com>
2021-07-14 17:08:31 +0900

    add: 로그인 페이지 레이아웃 완료 -로그인 페이지 레이아웃 작성

---
+++ students/23th/jeongil/js/main.js
@@ -0,0 +1,5 @@
+const idbutton = document.getElementsByClassName("idButton")
+const passwordButton = document.getElementsByClassName("passwordButton")[0];
+const logInButton = document.getElementsByClassName("logInButton")[0]
+
+console.log(idbutton)
