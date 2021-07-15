d6af4b9a13f555c472e6472f4ea3108c2f389649
lnhyen43@gmail.com <lnhyen43@gmail.com>
2021-07-15 20:49:51 +0900

    Add: 메인 페이지 레이아웃 완료 로그인 페이지, 메인페이지 기능 완료

--- students/23th/nahyunlee/westagram/login.js
+++ students/23th/nahyunlee/westagram/login.js
@@ -1,23 +1,34 @@
 const userId = document.querySelector('.id');
-//const userPassword = document.querySelector('.password');
+const userPassword = document.querySelector('.password');
 const loginButton = document.querySelector('.loginButton');
 
 //id/password에 한 글자 이상 입력 시, 버튼 활성화 
 // id/password가 공백일 때는 버튼 비활성화 
-userId.addEventListener('keyup', function () {
+userPassword.addEventListener('keyup', function () {
   //현재 keyup만 되는 상태
-  const userIdValue = document.querySelector('.id').value;
-  const userPasswordValue = document.querySelector('.password').value;
-  if (userIdValue === "") {
-    return loginButton.style.backgroundColor = "blue";
+  const userIdValue = userId.value;
+  const userPasswordValue = userPassword.value;
+  if (userIdValue !== "") {
+    if (userPasswordValue !== "") {
+      loginButton.style.backgroundColor = "blue";
+    }
   } else {
-    return loginButton.style.backgroundColor = "blue";
   }
 });
 
 
-
 //로그인 성공 시, 페이지 이동
+
+userPassword.addEventListener('keydown', function (e) {
+  if (e.code === 'Enter') {
+    alert('로그인에 성공하셨습니다.')
+    document.querySelector('.id').value = '';
+    location.href = "main.html";
+  }
+});
+
 loginButton.addEventListener('click', function () {
+  alert('로그인에 성공하셨습니다.')
+  document.querySelector('.id').value = '';
   location.href = "main.html";
-})
+});
---
+++ students/23th/nahyunlee/westagram/main.js
@@ -0,0 +1,42 @@
+const reply = document.querySelector('.reply');
+const replyButton = document.querySelector('.replybutton');
+
+//enter를 칠 때, 댓글을 추가한다.  
+reply.addEventListener('keydown', function (e) {
+  //enter를 쳤을 때, 
+  if (e.code === 'Enter') {
+    //input의 값을 받아온다.
+    let newReply = reply.value;
+    //값을 넣을 공간을 만든다.
+    let commentBox = document.createElement("p");
+    //값이 있다면 프로세스 진행
+    if (newReply) {
+      commentBox.classList.add('.newComment'); //클래스명 추가
+      commentBox.innerText = newReply; //작성한 값을 공간에 넣기
+      //부모 노드에 자식노드 추가 
+      document.querySelector('.desContainer').appendChild(commentBox);
+      document.querySelector('.reply').value = '';
+    } else {
+
+    }
+  }
+
+})
+
+document.querySelector('.reply').value = "";
+
+
+replyButton.addEventListener('click', function () {
+  let newReply = reply.value;
+  let commentBox = document.createElement("p");
+  if (newReply) {
+    commentBox.classList.add('.newComment');
+    commentBox.innerText = newReply;
+    document.querySelector('.desContainer').appendChild(commentBox);
+    document.querySelector('.reply').value = '';
+
+  } else {
+
+  }
+  document.querySelector('.reply').value = "";
+})
