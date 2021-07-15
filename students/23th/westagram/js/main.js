0f8e9f15a9be6d3a8f8fa3ba97f05aa51be2a501
dongwu-kim <alduk7797@naver.com>
2021-07-15 15:32:33 +0900

    MODIFY : 댓글정보 저장방식 수정 - 기존 id, text 1:1 대응 방식 -> 한 번에 처리 - button, input.value -> form 변경 - localStorage 내부 데이터 구조 변경

--- students/23th/westagram/js/main.js
+++ students/23th/westagram/js/main.js
@@ -1,3 +1,4 @@
+// viewport 해상도 -> style 바꿈
 function screenInit() {
   const feedContainer = document.querySelector(`.feed-container`),
     recommendedBlock = document.querySelector(`.recommended-block`);
@@ -10,10 +11,9 @@ function screenInit() {
       }px`),
       (feedContainer.style.marginRight = `361px`))
     : (recommendedBlock.style.visibility = `hidden`);
-
-  console.log(viewportWidth);
 }
 
+// 댓글에 해당하는 div 생성 후 배치
 function createComments(idString, textString) {
   const photoComments = document.querySelector(`.photo__comments`);
   const commentsIdText = [idString, textString];
@@ -32,51 +32,69 @@ function createComments(idString, textString) {
   photoComments.appendChild(commentsDesc);
 }
 
+// form submit 할 때, 댓글 localStorage에 저장
+function saveComments(commentsArr, idString, textString) {
+  let comment = { id: idString, text: textString };
+
+  commentsArr.push(comment);
+
+  commentsArr = JSON.stringify(commentsArr);
+  localStorage.setItem(`comment`, commentsArr);
+}
+
+// localStorage 내부에 존재하는 댓글 id, 내용 가져오기
+function loadComments() {
+  let commentsArr = localStorage.getItem(`comment`);
+
+  commentsArr = JSON.parse(commentsArr);
+
+  if (commentsArr !== null) {
+    for (let i = 0; i < commentsArr.length; i++) {
+      createComments(commentsArr[i][`id`], commentsArr[i][`text`]);
+    }
+  }
+  return commentsArr;
+}
+
+// 댓글 DOM event, saveComments, 이전 댓글정보 유무 확인
 function addComments() {
   const commentsInput = document.querySelector(`#commentsInput`),
-    commentsBtn = document.querySelector(`#addCommentsBtn`),
-    commentsPlaceholder = document.querySelector(`#commentsPlaceholder`);
+    commentsPlaceholder = document.querySelector(`#commentsPlaceholder`),
+    commentsForm = document.querySelector(`.comments-form`);
 
   let comments = ``;
-  commentsInput.addEventListener(`keyup`, (event) => {
+
+  commentsInput.addEventListener(`keyup`, () => {
     comments = commentsInput.value;
     comments === ``
       ? (commentsPlaceholder.style.visibility = `visible`)
       : (commentsPlaceholder.style.visibility = `hidden`);
-    const newCommentsId = localStorage.getItem(`id`),
-      newCommentsText = comments;
-    if (event.code === `Enter`) {
-      comments !== ``
-        ? (createComments(newCommentsId, newCommentsText),
-          (commentsInput.value = ``))
-        : alert(`댓글 내용을 입력해주세요`);
-    }
   });
 
-  // commentsInput.addEventListener(`click`, () => {
-  //   comments === ``
-  //     ? (commentsPlaceholder.style.visibility = `visible`)
-  //     : (commentsPlaceholder.style.visibility = `hidden`);
-  // });
+  let commentsArr = [];
 
-  commentsBtn.addEventListener(`click`, () => {
+  localStorage.getItem(`comment`) !== null
+    ? (commentsArr = JSON.parse(localStorage.getItem(`comment`)))
+    : (commentsArr = []);
+
+  commentsForm.addEventListener(`submit`, () => {
     const newCommentsId = localStorage.getItem(`id`),
       newCommentsText = comments;
-
     comments !== ``
       ? (createComments(newCommentsId, newCommentsText),
         (commentsInput.value = ``))
-      : alert(`댓글 내용을 입력해주세요`);
+      : alert(`댓글 내용을 입력해주세요.`);
+    saveComments(commentsArr, newCommentsId, newCommentsText);
   });
 }
 
-function init() {
+// initialize
+(() => {
   screenInit();
+  loadComments();
   window.addEventListener(`resize`, () => {
     screenInit();
     location.reload();
   });
   addComments();
-}
-
-init();
+})();
