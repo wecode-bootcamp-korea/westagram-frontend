2560e31d0bf57db8b9c738363ba71142a2010f27
mateo <materokatti@gmail.com>
2021-07-15 20:46:50 +0900

    ADD: 댓글 좋아요, 삭제 기능 추가

--- students/23th/doyun/instargram_clone/js/main.js
+++ students/23th/doyun/instargram_clone/js/main.js
@@ -1,62 +1,60 @@
 const commentInput = document.getElementsByClassName('comment')[0];
 const commentBtn = document.getElementsByClassName('fa-check')[0];
+const likeButton = document.getElementsByClassName('commentLine')[0];
+
+likeButton.addEventListener('click', (e) => {
+   console.log(e.target);
+   const item = e.target;
+   //delete
+   if (item.classList[0]==='fas'){
+    const commentCon = document.getElementsByClassName('commentContainer')[0]
+    commentCon.remove()
+   } 
+   //like
+   if (item.classList[0]==='fa') {
+     item.parentElement.classList.toggle('active');
+   }
+ })
 
 const makeComment = () => {
-    const container = document.createElement('div'); 
-    const userImg = document.createElement('img');
-    const innerContainer = document.createElement('div');
-    const userId = document.createElement('span');
-    const comment = document.createElement('span');
-    container.className = 'commentContainer';
-    userImg.src = './img/profile.png';
-    userImg.className = 'profile';
-    userId.innerHTML = 'Doyun';
-    userId.className = 'name';
-    comment.innerHTML = commentInput.value;
-    comment.className = 'comValue';
+    
+  const container = document.createElement('div'); 
+  container.className = 'commentContainer';
 
-    innerContainer.appendChild(userId);
-    innerContainer.appendChild(comment);
-    container.appendChild(userImg);
-    container.appendChild(innerContainer);
+  const userImg = document.createElement('img');
+  userImg.src = './img/profile.png';
+  userImg.className = 'profile';
+  container.appendChild(userImg);
 
-    document.getElementsByClassName('commentLine')[0].appendChild(container);
+  const innerContainer = document.createElement('div');
+  container.appendChild(innerContainer);
 
-    commentInput.value = ''; 
+  const userId = document.createElement('span');
+  userId.innerHTML = 'Doyun';
+  userId.className = 'name';
+  innerContainer.appendChild(userId);
+
+  const comment = document.createElement('span');
+  comment.innerHTML = commentInput.value;
+  comment.className = 'comValue';
+  innerContainer.appendChild(comment);
+
+  const imoBox = document.createElement('span')
+  innerContainer.appendChild(imoBox);
+
+  const likeBtn = document.createElement('button');
+  likeBtn.className = 'like';
+  likeBtn.innerHTML = '<i class="fa fa-thumbs-up"></i>'
+  imoBox.appendChild(likeBtn);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'delete';
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  imoBox.appendChild(deleteBtn);
+  
+  document.getElementsByClassName('commentLine')[0].appendChild(container);
+
+  commentInput.value = ''; 
 }
 
 commentBtn.addEventListener('click', makeComment)
-
-addArray = arr => {
-    const newArr = arr;
-    for(let i=0; i<arr.length; i++){
-      newArr[i] = this.arr[i]
-    }
-    console.log(arr)
-    return arr;
-  }
-  
-addArray([1,2])
-
-
-
-// for (j = 0; j < commentBtn.length; j++) {
-//     const commentValue = []
-//     commentValue.push(document.querySelectorAll('.articleComment input')[j].value)
-//     commentBtn[j].addEventListener('click', () => {
-//         console.log(commentValue);
-//     })
-// }
-
-
-
-// for (i = 0; i < commentInput.length; i++) {
-//     commentInput[i].addEventListener("keyup", (e) => {
-//         //const commentValue = e.target.value;
-//         const comment = document.createElement('p');
-
-
-        
-
-//     });
-// }
