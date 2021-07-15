2a84f7a17c96854e356ba2febf6373da9d1274d5
ddodam <80012897+ddodam@users.noreply.github.com>
2021-07-15 14:13:34 +0900

    로그인 페이지 기능 수정

--- students/23th/jiseon(3577)/login/js/login.js
+++ students/23th/jiseon(3577)/login/js/login.js
@@ -9,18 +9,32 @@ userName.addEventListener('keyup',inputValue);
 passWord.addEventListener('keyup',inputValue);
 
 
-function inputValue() {
-  const hasClass = loginBtn.classList.contains("active");
-  if(userName||passWord){
+function inputValue(){
   loginBtn.classList.add("active");
-  }
-  if(hasClass){
-  loginBtn.classList.remove("active");
+
+  const userNameLength = userName.value.length;
+  const passWordLength = passWord.value.length;
+  
+  if((userNameLength||passWordLength)===0)
+  {
+    loginBtn.classList.remove("active");
   }
 }
 
-//input.value값이 필요할 것이라고 생각했는데 그냥 input만 조건으로 줘도 
-// 함수가 먹힌다. 왜지? 
+
+
+// function inputValue() {
+//   const hasClass = loginBtn.classList.contains("active");
+//   if(userName||passWord){
+//   loginBtn.classList.add("active");
+//   }
+//   if(hasClass){
+//   loginBtn.classList.remove("active");
+//   }
+// }
+
+// //input.value값이 필요할 것이라고 생각했는데 그냥 input만 조건으로 줘도 
+// 함수가 먹힌다. 왜지? keyup이벤트가 일어나면 함수가 호출되기때문에!
 
 
 
