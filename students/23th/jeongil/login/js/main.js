274319e5451c5a0c9bfecffc11795fa635d04b89
201steve <jeongillee201@gmail.com>
2021-07-16 21:21:11 +0900

    ADD : ID,PW창에 1글자씩 입력되면 로그인 버튼 활성화 -id로그인,pw로그인 입력창에 put 이벤트 발생시 -로그인 버튼 컬러 활성화 완료

--- students/23th/jeongil/login/js/main.js
+++ students/23th/jeongil/login/js/main.js
@@ -1,5 +1,17 @@
 const idButton = document.getElementsByClassName("idBtn")[0]
-const logInBtn = document.getElementsByClassName("logInButton")[0]
+const pwButton = document.getElementsByClassName("passwordBtn")[0]
+const logIn = document.getElementsByClassName("logInButton")[0]
 
+function handleInput(){
+  const idValue = idButton.value;
+  const pwValue = pwButton.value;
+  
+  if(idValue.length>0 && pwValue.length>0){
+    logIn.style.backgroundColor = "#0095f6"
+  }else{
+    logIn.style.backgroundColor = "#c0dffd"
+  }
+}
 
-
+idButton.addEventListener("input",handleInput)
+pwButton.addEventListener("input",handleInput)
