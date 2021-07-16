bbf5064ac0b7c17a71918815c996bbd85bcd7d2d
jungmin <minmin93240@google.com>
2021-07-16 16:03:57 +0900

    modify : 로그인 페이지 , 메인페이지 변수명 수정,bottom-up 수정 - 메인페이지 -  필요없는 태그 삭제 진행중 -메인페이지 - 변수명 수정 중 -메인페이지 - 필요없는 css속성 삭제 중 -메인페이지 - css순서 수정 중 -로그인페이지 - 변수명 수정, 필요없는 css수정, 일부  태그 hight 속성 삭제, css 순서수정  완료 t message for your changes. Lines starting

--- students/23th/jungmin/login.js
+++ students/23th/jungmin/login.js
@@ -1,25 +1,20 @@
-let password = document.getElementById('password');
-let id = document.getElementById('id');
-let button = document.querySelector("Button");
-
+const password = document.getElementById('password');
+const id = document.getElementById('id');
+const button = document.querySelector("Button");
+let test = function(){
+  let pwValue = password.value, idValue = id.value;
+    pwValue&&idValue?(
+      button.disabled=false,
+      button.style.backgroundColor="#0095f6",
+      button.style.color="#fff"
+    ) : (
+      button.disabled=true,
+      button.style.backgroundColor="rgba(var(--d69,0,149,246),.3)",
+      button.style.color="rgba(var(--eca,255,255,255),1)"
+    );
+}
 button.addEventListener('click',function(){
   alert("아이디와 비밀번호가 맞을까요 ? :)")
   })
-  
-function test(){
-  let pwValue = password.value;
-  let idValue = id.value;
-  if(pwValue&&idValue){
-    button.disabled=false;
-    button.style.backgroundColor="#0095f6";
-    button.style.color="#fff";
-  }
-  else{
-    button.disabled=true;
-    button.style.backgroundColor="rgba(var(--d69,0,149,246),.3)";
-    button.style.color="rgba(var(--eca,255,255,255),1)";
-  }
-}
-
 password.addEventListener("keyup", test);
 id.addEventListener("keyup",test);
--- students/23th/jungmin/main.js
+++ students/23th/jungmin/main.js
@@ -1,42 +1,44 @@
 let message = document.getElementsByClassName("textMessage")[0];
-let post=document.getElementsByClassName("postButton")[0];
-let messVal = ""
-let count = 0;
-function text(e){
-    messVal=message.value;
-if(messVal){
-    post.style.opacity=1;
-    post.disabled=false;
-    message.style.color="black";
-}
-else {
-    post.style.opacity=0.5;
-    post.disabled=true;
-}
+let post = document.getElementsByClassName("postButton")[0];
+let messVal = "";
 
-if(post.disabled==false&&e.code=="Enter"){
+function text(e) {
+  messVal = message.value;
+  if (messVal) {
+    post.style.opacity = 1;
+    post.disabled = false;
+    message.style.color = "black";
+  } else {
+    post.style.opacity = 0.5;
+    post.disabled = true;
+  }
+
+  if (post.disabled == false && e.code == "Enter") {
     postfuc();
-}
+  }
 }
 
-function postfuc(){
-    messVal= message.value;
-     let posted = document.getElementsByClassName("posted")[0];
-     let alldiv=document.createElement('div');
-     let textdiv=document.createElement('div');
-     let id=document.createElement("a");
-     let img = document.createElement("img");
-     alldiv.className="circle messagebox topmargin";
-     id.innerHTML="mango_9324 ";
-     img.className="heart";
-    img.setAttribute("src","https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png")
-    textdiv.appendChild(id);
-    textdiv.innerHTML+=messVal;
-    posted.appendChild(alldiv);
-    alldiv.appendChild(textdiv);
-    alldiv.appendChild(img);
-    message.value="";
+function postfuc() {
+  messVal = message.value;
+  let posted = document.getElementsByClassName("posted")[0];
+  let alldiv = document.createElement("div");
+  let textdiv = document.createElement("div");
+  let id = document.createElement("a");
+  let img = document.createElement("img");
+  alldiv.className = "circle messagebox topmargin";
+  id.innerHTML = "mango_9324 ";
+  img.className = "heart";
+  img.setAttribute(
+    "src",
+    "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+  );
+  textdiv.appendChild(id);
+  textdiv.innerHTML += messVal;
+  posted.appendChild(alldiv);
+  alldiv.appendChild(textdiv);
+  alldiv.appendChild(img);
+  message.value = "";
 }
 
 message.addEventListener("keyup", text);
-post.addEventListener("click",postfuc);
+post.addEventListener("click", postfuc);
