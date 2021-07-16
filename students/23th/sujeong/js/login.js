c59d3d2e0d414821b87df0e3d665458c1f82f684
crystalee <79251654+eoskandlv@users.noreply.github.com>
2021-07-16 18:00:28 +0900

    Refactor : 로그인 페이지 추가 리팩토링

--- students/23th/sujeong/js/login.js
+++ students/23th/sujeong/js/login.js
@@ -1,6 +1,7 @@
 const thisIsId = document.querySelector('.id');
 const thisIsPw = document.querySelector('.pw');
 const thisIsButton = document.getElementsByClassName('btn_login')[0];
+const thisIsForm = document.getElementsByClassName('loginform')[0];
 
 function thisIsLogin (e){
     if(thisIsId.value && thisIsPw.value){
@@ -11,10 +12,10 @@ function thisIsLogin (e){
         thisIsButton.style.backgroundColor = '#b2dffc';
     }
     if (thisIsButton.disabled==false && e.code == 'Enter') {
-        thisIsLogin();   
-        document.getElementById('form').reset();
+        thisIsLogin();  
     }
 }
 
 thisIsId.addEventListener('keyup', thisIsLogin);
 thisIsPw.addEventListener('keyup', thisIsLogin);
+thisIsForm.addEventListener('keyup', thisIsLogin);
