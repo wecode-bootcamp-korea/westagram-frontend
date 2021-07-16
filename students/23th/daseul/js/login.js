c9be084ffa858b7ccd244cda6ad3ee8a66bea360
cocacollllla <cocacollllla@gmail.com>
2021-07-16 16:03:56 +0900

    Add:메인페이지 레이아웃 완료 / 기능 구현 완료

--- students/23th/daseul/js/login.js
+++ students/23th/daseul/js/login.js
@@ -5,4 +5,12 @@ const loginBtn = document.querySelector('.login-btn button');
 
 inputIdPw.addEventListener('keyup', function(e){
   (pwInput.value && idInput.value) ? loginBtn.classList.add('btnColorToBlue') : loginBtn.classList.remove('btnColorToBlue');
+});
+
+loginBtn.addEventListener('click', function(e){
+  if(idInput.value.includes('@') == false) {
+    alert('@가 포함되어야 합니다.');
+  } else if (pwInput.value.length <= 5) {
+    alert('비밀번호는 5글자 이상이어야 합니다.');
+  }
 })
