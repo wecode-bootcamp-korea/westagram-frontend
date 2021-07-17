19e9a81c7e0f1ecc1c93221ddfa66255dfca730a
JaeHyun <nam2350@gmail.com>
2021-07-17 17:48:53 +0900

    Add : 메인페이지 기능     - 메인페이지 기능완성

--- students/23th/jaehyun/scripts/main.js
+++ students/23th/jaehyun/scripts/main.js
@@ -2,6 +2,11 @@ const searchInput = document.getElementsByClassName("searchInput")[0];
 const searchIcon = document.getElementsByClassName('searchIcon')[0];
 const searchText = document.getElementsByClassName('searchText')[0];
 
+const replyInput = document.getElementsByClassName('replyInput')[0];
+const postButton = document.getElementsByClassName('postButton')[0];
+const replies = document.getElementById('reply');
+const deleteButton = document.getElementById('delete');
+
 function home() {
     window.location.reload();
 }
@@ -14,3 +19,41 @@ function inputPostionMove() {
 searchInput.addEventListener('input', e => {
     searchText.style.visibility = 'hidden';
 })
+
+function addComment(commentValue) {
+    const newComment = document.createElement('li');
+    const newName = document.createElement('span');
+    const newContent = document.createElement('span');
+    const deleteButton = document.createElement('p');
+
+    newName.classList.add('name');
+    deleteButton.classList.add('delete');
+    newName.innerText = 'Test ';
+    newContent.innerText = commentValue;
+    deleteButton.innerText = 'x';
+
+    newComment.appendChild(newName);
+    newComment.appendChild(newContent);
+    newComment.appendChild(deleteButton);
+    replies.appendChild(newComment);
+
+    deleteButton.addEventListener('click', () => {
+        newComment.remove();
+    })
+
+    replyInput.value = '';
+}
+
+function validateComment() {
+    const commentValue = replyInput.value;
+    if (commentValue.length > 0) {
+        addComment(commentValue);
+        return;
+    }
+    else {
+        alert('댓글을 입력해주세요')
+        return;
+    }
+}
+
+postButton.addEventListener('click', validateComment);
