ac27f2d367369d1f0e3f922b903bdf419948699e
차예은 <chayeeun@chayeeun-ui-MacBookAir.local>
2021-07-21 14:02:28 +0900

    Add: -로그인, 메인페이지 레이아웃 완성 -로그인 페이지 기능 구현

--- students/23th/yeeun/Westagram-frontend/js/login.js
+++ students/23th/yeeun/Westagram-frontend/js/login.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const loginForm = document.getElementsByClassName('loginForm')[0];
+const idInput = document.getElementById("id");
+const pwInput = document.getElementById("pw");
+const loginBtn = document.getElementById("loginBtn");
+
+function handleInput() {
+  const idValue = idInput.value;
+  const pwValue = pwInput.value;
+  if(idValue.length > 0 && pwValue.length > 0) {
+    loginBtn.disabled = false;
+    loginBtn.style.opacity = 1;
+    loginBtn.style.cursor = "pointer";
+  } else {
+    loginBtn.disabled = true;
+    loginBtn.style.opacity = 0.5;
+    loginBtn.style.cursor = "default";
+  }
+}
+
+function goToMain () {
+  
+  location.replace('http://127.0.0.1:5500/students/23th/yeeun/Westagram-frontend/main.html');
+}
+
+function init() {
+  loginForm.addEventListener('input', handleInput);
+  loginBtn.addEventListener('click', goToMain);
+}
+
+init();
