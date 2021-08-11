22778ecbf1016971dc12d5ab4ced2c9cb72c8d1e
orosy <hanei7632@gmail.com>
2021-08-11 17:03:41 +0900

    Add: 로그인 페이지 기능 완성 및 메인 페이지 댓글 기능 완성 - login email, password 유효성 검사 로직 - login form 레이아웃 작성 완료 - main form 레이아웃  작성 완료 - main 댓글 작성 기능 구현 완료 - main 댓글 삭제 기능 진행중

---
+++ students/24th/taesookim/westagram/login/js/login.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("keyup", () => {
+  const loginBtn = document.querySelector("button");
+  const usernameVal = document.querySelector(".username").value;
+  const passwordVal = document.querySelector(".password").value;
+  if (usernameVal.indexOf("@") >= 0 && passwordVal.length >= 5) {
+    loginBtn.style.backgroundColor = "#0095F6";
+  } else if (!usernameVal || !passwordVal) {
+    loginBtn.style.backgroundColor = "#b2dffc";
+  }
+});
---
+++ students/24th/taesookim/westagram/main/js/main.js
@@ -0,0 +1,44 @@
+const searchInput = document.querySelector(".search-bar");
+const commentInput = document.querySelector(".comment-post");
+const commentForm = document.querySelector(".comment-form");
+const deleteBtn = document.querySelector(".far.fa-trash-alt");
+const lis = document.querySelectorAll(".comments li");
+
+searchInput.addEventListener("input", () => {
+  const search = document.querySelector(".search");
+  const searchIcon = document.querySelector(".fas.fa-search");
+  searchInput.value
+    ? ((searchIcon.style.display = "none"), (search.style.display = "none"))
+    : ((searchIcon.style.display = "block"), (search.style.display = "block"));
+});
+
+commentInput.addEventListener("input", () => {
+  const btnPost = document.querySelector(".btn-post");
+  commentInput.value
+    ? (btnPost.style.color = "#0095f6")
+    : (btnPost.style.color = "#b2dffc");
+});
+
+commentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addComment();
+  commentInput.value = "";
+});
+
+const addComment = () => {
+  const username = "ttsone_ss";
+  const commentList = document.querySelector(".comments");
+  const spanTag = document.createElement("span");
+  const pTag = document.createElement("p");
+  const iTag1 = document.createElement("i");
+  const iTag2 = document.createElement("i");
+  const liTag = document.createElement("li");
+  spanTag.append(username);
+  pTag.append(spanTag, commentInput.value, iTag1, iTag2);
+  iTag1.setAttribute("class", "far fa-heart");
+  iTag2.setAttribute("class", "far fa-trash-alt");
+  liTag.append(pTag);
+  commentList.append(liTag);
+};
+
+const likeComment = () => {};
