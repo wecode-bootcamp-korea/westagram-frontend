4ccea0d1785686a66f32ca39c945c3c788dbee54
Casey <hjkdw9506@gmail.com>
2021-08-12 08:32:11 +0900

    ⚡️ add: 로그인 기능 id email 기재여부 확인 로직 추가 - 로그인 화면에서 id 입력시 "@"기재여부 확인하는 로직 추가하여 email로만 어플리케이션 접근 가능하도록 구현

--- students/24th/hyunjaekim/js/login.js
+++ students/24th/hyunjaekim/js/login.js
@@ -1,11 +1,25 @@
 const loginContainer = document.querySelector('.loginContainer');
+const userID = document.querySelector('.userID');
+const userPW = document.querySelector('.userPW');
+const loginBtn = document.querySelector(".loginBtn");
 
-loginContainer.addEventListener("keyup",()=>{
-    const userID = document.querySelector('.userID').value;
-    const userPW = document.querySelector('.userPW').value;
-    const loginBtn = document.querySelector(".loginBtn");
+function validateEmail() {
+    const idArray = userID.value.split(""); // 받아온 id를 array화
+    for(let i = 0; i < idArray.length; i++) {
+        if(idArray[i] === "@") {
+            activeLoginBtn();
+        }
+    }
+}
 
-    userID.length > 4 && userPW.length > 4 
+function activeLoginBtn() {
+    userID.value.length > 4 && userPW.value.length > 4 
         ? loginBtn.classList.add('active') 
         : loginBtn.classList.remove('active');
-});
+}
+
+function init() {
+    loginContainer.addEventListener("keyup",()=> validateEmail());   
+}
+
+init();
