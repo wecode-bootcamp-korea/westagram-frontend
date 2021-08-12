4b7193f5b94d1714659aec1eceb73828a5ca505d
박은정 <eunjeong@bag-eunjeong-ui-MacBookAir.local>
2021-08-12 18:04:57 +0900

    Add : login 완료, main 레이아웃 진행중 - login 레이아웃 완료 - login inout tag에서 한 글자 이상 입력해야 버튼태그 작성가능 - 버튼 활성화되면 파란색으로 색 바뀜 - main 레이아웃 우측 사이드 진행중

---
+++ students/24th/eunjeongPark/westagram/js/login.js
@@ -0,0 +1,25 @@
+const inputId = document.querySelector("#login-id");
+const inputPassword = document.querySelector("#login-pwd");
+const button = document.querySelector(".login-btn");
+
+function loginBtn() {
+    let idValue = inputId.value;
+    let passwordValue = inputPassword.value;
+
+    if(idValue.length > 0 && passwordValue.length > 0) {
+        button.disabled = false;
+        button.style.cursor = "pointer";
+        button.style.backgroundColor = "#1c7ed6";
+    }else {
+        button.disabled = true;
+        button.style.cursor = "default";
+        button.style.backgroundColor = "#C4E1FB";
+    }
+}
+
+function loginSuccess() {
+  alert('로그인 성공!');
+}
+
+inputId.addEventListener('keyup', loginBtn);
+inputPassword.addEventListener('keyup', loginBtn);
