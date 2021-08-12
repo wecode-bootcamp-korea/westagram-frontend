6d339ad0b44fc208c4bafb2eee4d785a2d04ad77
BobaeKeum <aann0221@gmail.com>
2021-08-12 17:40:54 +0900

    Add: 로그인 페이지 구현 -로그인 버튼 활성화 완료 -메인페이지 네브바 구현 완료

---
+++ students/24th/bobaekeum/westargram/main.js
@@ -0,0 +1,11 @@
+let idElement =document.querySelector('.id');
+let passElement =document.querySelector('.password');
+let btn = document.querySelector('.btn');
+passElement.addEventListener('keyup', function(e) {
+    if(idElement.value.length>3 && passElement.value.length> 4){
+        btn.style.backgroundColor= "rgb(0,150,252)";
+    }
+    else if(idElement.value.length<4 || passElement.value.length <5){
+        btn.style.backgroundColor='rgb(201, 224, 249)';
+    }
+});
