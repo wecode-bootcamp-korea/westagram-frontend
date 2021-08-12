59501e75e631e1cd700c786e46ee787c96c2a4bd
donghee <dongdh02076@gmail.com>
2021-08-12 13:48:54 +0900

    [Mission 1~4완료] login 페이지 css수정, main 페이지 기능완성 - login 페이지 : class명 수정 위주 코드 리팩토링 - main 페이지 : css구현 - main 페이지 : 댓글 입력 후 엔터입력/버튼 클릭시 새로운 댓글 생성 기능 구현

--- students/24th/dongheeKim/login/js/login.js
+++ students/24th/dongheeKim/login/js/login.js
@@ -3,19 +3,18 @@ const loginInfo = document.querySelector(".loginBox");
 const thisIsBtn = document.querySelector('.loginBtn');
 
 thisIsBtn.disabled= true;
-thisIsBtn.style.backgroundColor = "lightskyblu"; 
+thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),.3)"; 
 
 loginInfo.addEventListener('keyup',function(){
 
     const thisIsID = document.querySelector('.id').value;
     const thisIsPW = document.querySelector('.password').value;
 
-    if((!thisIsID === false) && (!thisIsPW ===false)){
-        thisIsBtn.disabled = false;
-        thisIsBtn.style.backgroundColor = "blue";
-    } else {
-        thisIsBtn.disabled= true;
-        thisIsBtn.style.backgroundColor = "lightskyblue"; 
-    } 
-    }
-)
+    ((!thisIsID === false) && (!thisIsPW ===false)) ? (
+        thisIsBtn.disabled = false,
+        thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),1)") : (
+            thisIsBtn.disabled= true,
+            thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),.3)",
+            thisIsBtn.style.cursor = "Pointer"
+        )
+})
