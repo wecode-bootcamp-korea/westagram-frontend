4e30c3426fe442c9eef47b1728593fa6543a05ca
youngho <youngho8762@gamil.com>
2021-08-12 11:19:36 +0900

    Add : 로그인, 메인페이지 완료 - 1~6 ,9 번 미션 완료 - 리팩토링

---
+++ students/24th/kimyoungho/js/login.js
@@ -0,0 +1,32 @@
+const id = document.getElementById("id");
+const password = document.getElementById('password');
+const loginButton = document.getElementById("loginButton");
+const loginForm = document.getElementsByClassName("loginForm")[0];
+
+loginForm.addEventListener("keyup", () => {
+   if(id.value.length >= 1 && password.value.length >= 1) {
+      loginButton.style.backgroundColor = "#0095F6";
+      loginButton.disabled = false;
+      loginButton.style.cursor= "pointer"
+   } else {
+      loginButton.style.backgroundColor = "#C5E1FC";
+      loginButton.style.cursor= "default"
+      }
+});
+
+loginButton.addEventListener("click", (event) => {
+   event.preventDefault();
+   loginCheck();
+});
+
+function loginCheck() {
+   if (id.value.length <= 4 && id.value.length >= 15) {
+      alert("아이디는 5자~15자 이내로 입력해주세요");
+      id.focus();
+   } else if (password.value.length <= 4 && password.value.length >= 15) {
+         alert("비밀번호는 5자~15자 이내로 입력해주세요");
+         password.focus();
+   }
+}
+
+
---
+++ students/24th/kimyoungho/js/main.js
@@ -0,0 +1,48 @@
+const postingButton = document.querySelector(".postingButton");
+const comment = document.querySelector(".comment");
+const writeComment = document.querySelector(".writeComment");
+const commentHeart = document.getElementsByClassName("commentHeart");
+
+postingButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addComment();
+});
+
+writeComment.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addComment();
+    }
+});
+
+function addComment() {
+    if(writeComment.value.length >= 1) {
+        const makeTag = document.createElement("div");
+        comment.appendChild(makeTag);
+        makeTag.innerHTML=`<span class="boldFont">bbangho</span> ${writeComment.value} <img class="commentHeart" src="./img/garbage.png"> <img class="commentHeart" src="./img/heart.png">`
+        }
+    writeComment.value = ""
+}
+
+comment.addEventListener("click", (event) => {
+    const src = event.target.getAttribute("src")
+    const heart = "./img/heart.png"
+    const redHeart = "./img/redHeart.png"
+    const deleteIcon = "./img/garbage.png"
+
+    if (src === heart) {
+        event.target.src=redHeart;
+    } else if (src === redHeart) {
+        event.target.src=heart;
+    }
+
+     if (src === deleteIcon) {
+        event.target.parentNode.remove()
+     }
+});
+
+
+
+
+
+
