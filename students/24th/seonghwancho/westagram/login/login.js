534e15b15221b3f16e77973bf45fea589f85352c
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-08-12 21:52:38 +0900

    Add: 댓글 기능 추가, self refactoring - 댓글 기능 추가 - self refactoring

--- students/24th/seonghwancho/westagram/login/login.js
+++ students/24th/seonghwancho/westagram/login/login.js
@@ -1,4 +1,18 @@
+//기본 설정
 'use strict';
+
+// Id, Password 1글자 이상 입력시 로그인 버튼 활설화
+// const loginBtn = document.querySelector('.loginBtn');
+// const inputBox = document.querySelectorAll('.inputBox');
+// const inputEmail = document.querySelector('#inputEmail');
+// const inputPw = document.querySelector('#inputPw');
+// for (let i = 0; i < inputBox.length; i++) {
+//   inputBox[i].addEventListener('input', () => {
+//     return ((inputEmail.value && inputPw.value) ? loginBtn.style.backgroundColor = '#2196f3' : loginBtn.style.backgroundColor = '#c4e1fb');
+//   })
+// };
+
+//Id에 @가 들어가는지?, Password가 5자 이상인지? 확인하는 유효성 검사 기능
 const loginBtn = document.querySelector('.loginBtn');
 const inputBox = document.querySelectorAll('.inputBox');
 const inputEmail = document.querySelector('#inputEmail');
--- students/24th/seonghwancho/westagram/main/main.js
+++ students/24th/seonghwancho/westagram/main/main.js
@@ -1,14 +1,38 @@
+// 내프로필 정보
+const myProfile = {
+  image : 'profile.jpeg',
+  id : 'choseonghwan',
+  idDescrip : '조성환-wecode24'
+}
+
+// 댓글_댓글 추가 기능 구현
 const commentBarContent = document.querySelector('.commentBarContent');
 const commentBarPostbtn = document.querySelector('.commentBarPostbtn');
+const commentWindow = document.querySelector('.commentWindow');
 const comment = document.querySelector('.comment');
-const articlediv = document.querySelector('.article');
-const descriptionBar = document.querySelector('.descriptionBar')
 
 commentBarPostbtn.addEventListener('click', addComment);
+commentBarContent.addEventListener('keypress', function (e) {
+  // 엔터키 누르는 이벤트 설정 방법 2가지
+  if (e.key === 'Enter') {
+    addComment();
+  }
+  // if (e.keyCode === 13) {
+  //   addComment();
+  // }
+});
 
 function addComment() {
-  console.log('hello');
   const addCommentPtag = document.createElement('p');
-  addCommentPtag.innerHTML = commentBarContent.value;
-  comment.appendChild(addCommentPtag);
+  addCommentPtag.className = 'comment';
+  const addCommentId = document.createElement('span');
+  addCommentId.className = 'commentId'
+  const addCommentContent = document.createElement('span');
+  addCommentContent.className = 'commentContent'
+  
+  commentWindow.appendChild(addCommentPtag);
+  addCommentPtag.appendChild(addCommentId);
+  addCommentPtag.appendChild(addCommentContent);
+  addCommentId.innerHTML = myProfile.id;
+  addCommentContent.innerHTML = commentBarContent.value;
 };
