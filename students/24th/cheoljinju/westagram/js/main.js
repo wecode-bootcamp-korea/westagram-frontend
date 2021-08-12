b284209984afe56fbc47f0a40c18a8fe6fbfe665
jin <toto006521@gmail.com>
2021-08-12 10:29:03 +0900

    Add : 댓글 좋아요 및 삭제 기능 추가 - 댓글 좋아요 기능 추가 - 댓글 삭제 기능 추가 - 필요 없는 console 및 주석 삭제 - 관련 html 및 css 수정

--- students/24th/cheoljinju/westagram/js/main.js
+++ students/24th/cheoljinju/westagram/js/main.js
@@ -4,17 +4,23 @@ const commentBtn = document.querySelector(".comment-button");
 const input = document.querySelector(".comment-input");
 const aside = document.querySelector(".aside");
 const clientSize = window.innerWidth;
+const feeds = document.querySelector(".main__feeds");
+const feed = document.querySelector(".main__feed");
 
+// 코멘트 생성하는 함수
 const createComment = (inputValue) => {
   const commentRow = document.createElement("div");
   commentRow.setAttribute("class", "feed__comment");
   commentRow.innerHTML = `
     <span>wecode_24th</span>
     <span>${inputValue}</span>
+    <button type="button" class="comment__delete"><i class="far fa-trash-alt"></i></button>
+    <button type="button" class="comment__heart"><i class="far fa-heart"></i></button>
   `;
   return commentRow;
 };
 
+// 코멘트 추가하는 함수
 const addComment = () => {
   const inputValue = input.value;
   if (input.value == false) {
@@ -34,7 +40,6 @@ commentBtn.addEventListener("click", addComment);
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     addComment();
-    console.log(input.value);
     return;
   }
 });
@@ -55,7 +60,6 @@ window.addEventListener("resize", () => {
   if(clientSize < 1500){
     aside.style.right = `${280 - resizing/ 1.1}px`
   }
-  console.log(resizing);
 });
 
 // aside 큰 화면 로드 시에 위치 조정
@@ -64,3 +68,39 @@ window.addEventListener("load", () => {
     aside.style.right = `${500}px`;
   }
 })
+
+// 좋아요 버튼 on/off 함수
+function onHeartClick(event){
+  if(event.target.className === "far fa-heart"){
+    event.target.classList.remove("far");
+    event.target.classList.remove("fa-heart");
+    event.target.classList.add("fas");
+    event.target.classList.add("fa-heart");
+  } else if(event.target.className === "fas fa-heart"){
+    event.target.classList.remove("fas");
+    event.target.classList.remove("fa-heart");
+    event.target.classList.add("far");
+    event.target.classList.add("fa-heart");
+  }
+}
+
+// 삭제 버튼 함수
+function onDelete(event){
+  let target = event.target;
+  if(event.target.className === "far fa-trash-alt"){
+    target.parentNode.parentNode.remove(); // 재사용성 고민필요
+  }
+}
+
+// 댓글 아이콘 버튼 기능 구현
+feeds.addEventListener("click", (event) => {
+  if(event.target.tagName !== "I"){
+    return;
+  }
+  onHeartClick(event);
+  onDelete(event);
+})
+
+
+
+
