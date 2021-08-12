947c2ca2743b94ac4256394fd5666f3f3bf6ac52
donghee <dongdh02076@gmail.com>
2021-08-12 19:26:15 +0900

    Refactor: 로그인 페이지, 메인 페이지 리팩토링 - login page : 중복코드 삭제 - main page : 중복코드 삭제, 불필요한 div삭제, class명 정리

--- students/24th/dongheeKim/login/js/login.js
+++ students/24th/dongheeKim/login/js/login.js
@@ -7,14 +7,15 @@ thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),.3)";
 
 loginInfo.addEventListener('keyup',function(){
 
-    const thisIsID = document.querySelector('.id').value;
-    const thisIsPW = document.querySelector('.password').value;
+  const thisIsID = document.querySelector('.id').value;
+  const thisIsPW = document.querySelector('.password').value;
 
-    ((!thisIsID === false) && (!thisIsPW ===false)) ? (
-        thisIsBtn.disabled = false,
-        thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),1)") : (
-            thisIsBtn.disabled= true,
-            thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),.3)",
-            thisIsBtn.style.cursor = "Pointer"
-        )
-})
+  ((!thisIsID === false) && (!thisIsPW ===false)) ? (
+    thisIsBtn.disabled = false,
+    thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),1)"
+    ) : (
+    thisIsBtn.disabled= true,
+    thisIsBtn.style.backgroundColor = "rgba(var(--d69,0,149,246),.3)",
+    thisIsBtn.style.cursor = "Pointer"
+    )
+  })
