1e75e67a66a07d9a788fd69e938274867557c8be
horizonn03 <horizonn03@gmail.com>
2021-08-13 21:13:38 +0900

    Add: 댓글삭제 구현

--- students/24th/doyoungjung/js/login.js
+++ students/24th/doyoungjung/js/login.js
@@ -1,15 +1,17 @@
+"use strict";
+
 const inputId = document.querySelector(".loginId");
 const inputPassword = document.querySelector(".loginPassword");
 const loginButton = document.querySelector(".loginBtn");
-  
-  function countValue(event) { 
-    let idValue = inputId.value;
-    let passwordValue = inputPassword.value;  
-    (idValue.length > 0 && passwordValue.length > 0) ?
+
+function countValue(event) {
+  let idValue = inputId.value;
+  let passwordValue = inputPassword.value;
+  (idValue.length > 0 && passwordValue.length >= 5 && idValue.indexOf("@") !== -1) ?
     (loginButton.style.opacity = "1",
-    loginButton.style.cursor = "pointer") :
-    loginButton.style.opacity = "0.2" 
-  }
+      loginButton.style.cursor = "pointer") :
+    loginButton.style.opacity = "0.2"
+}
 
 inputId.addEventListener("keyup", countValue);
 inputPassword.addEventListener("keyup", countValue);
--- students/24th/doyoungjung/js/main.js
+++ students/24th/doyoungjung/js/main.js
@@ -1,49 +1,71 @@
+"use strict";
+
 const newInput = document.querySelector(".inputText");
 const clickBtn = document.querySelector(".inputBtn");
 const form = document.querySelector(".addReply")
 
-function moreReply(){
+function moreReply() {
   let replySpace = document.querySelector(".feedReply");
   let inputText = newInput.value;
-  let newDiv = document.createElement('div');
+  let newLi = document.createElement('li');
   let newSpan1 = document.createElement('span');
   let newSpan2 = document.createElement('span');
+  let newSpan3 = document.createElement('span');
+  let newI = document.createElement('i');
 
-  newDiv.classList.add('friendsReply');
+  newLi.classList.add('friendsReply');
   newSpan1.innerHTML = "doyboy03";
   newSpan2.innerHTML = inputText;
+  newSpan3.classList.add('trashCan');
+  newI.setAttribute("class", "fas fa-trash-alt");
 
-  replySpace.appendChild(newDiv);
-  newDiv.appendChild(newSpan1);
-  newDiv.appendChild(newSpan2);
+  replySpace.appendChild(newLi);
+  newLi.appendChild(newSpan1);
+  newLi.appendChild(newSpan2);
+  newLi.appendChild(newSpan3);
+  newSpan3.appendChild(newI);
+
+  newSpan3.addEventListener("click", () => {
+    replySpace.removeChild(newLi);
+  })
 
   newInput.value = "";
 }
 
-clickBtn.addEventListener("mousedown",moreReply);
+clickBtn.addEventListener("mousedown", moreReply);
 
-function moreReply2(event){
-    if(event.keyCode == 13){
+function moreReply2() {
+  if (event.keyCode == 13) {
     let replySpace = document.querySelector(".feedReply");
     let inputText = newInput.value;
-    let newDiv = document.createElement('div');
+    let newLi = document.createElement('li');
     let newSpan1 = document.createElement('span');
     let newSpan2 = document.createElement('span');
-  
-    newDiv.classList.add('friendsReply');
+    let newSpan3 = document.createElement('span');
+    let newI = document.createElement('i');
+
+    newLi.classList.add('friendsReply');
     newSpan1.innerHTML = "doyboy03";
     newSpan2.innerHTML = inputText;
-  
-    replySpace.appendChild(newDiv);
-    newDiv.appendChild(newSpan1);
-    newDiv.appendChild(newSpan2);
-  
+    newSpan3.classList.add('trashCan');
+    newI.setAttribute("class", "fas fa-trash-alt");
+
+    replySpace.appendChild(newLi);
+    newLi.appendChild(newSpan1);
+    newLi.appendChild(newSpan2);
+    newLi.appendChild(newSpan3);
+    newSpan3.appendChild(newI);
+
+    newSpan3.addEventListener("click", () => {
+      replySpace.removeChild(newLi);
+    })
+
     newInput.value = "";
-    }
   }
+};
 
-  form.addEventListener("submit" , (e)=>{
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-newInput.addEventListener("keyup", moreReply2);
+newInput.addEventListener("keydown", moreReply2);
