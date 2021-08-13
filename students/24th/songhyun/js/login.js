56f3924b91fc4054eea6ce73f5ec0e45f2dd289f
vi2920va <vi2920va@gmail.com>
2021-08-13 16:24:15 +0900

    Add: ID, PW 유효성 검사  - id '@' 포함 유효성 검사 로직  - pw 5글자 이상 검사 로직

--- students/24th/songhyun/js/login.js
+++ students/24th/songhyun/js/login.js
@@ -1,12 +1,36 @@
-const userName = document.querySelector('.user-name');
+const userEmail = document.querySelector('.user-name');
 const userPw = document.querySelector('.user-pw');
 const loginBtn = document.querySelector('.login-btn');
 
 loginBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  if (userName.value !== '' && userPw.value !== '') {
-    loginBtn.classList.add('active');
+
+  if (
+    userEmail.getAttribute('required') === 'true' &&
+    userPw.value.length > 5
+  ) {
+    return loginBtn.classList.add('active');
   } else {
-    loginBtn.classList.remove('active');
+    return loginBtn.classList.remove('active');
   }
 });
+
+userEmail.addEventListener('keydown', (e) => {
+  let regExp = new RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  if (!regExp.test(e.target.value)) {
+    return userEmail.setAttribute('required', false);
+  } else {
+    return userEmail.setAttribute('required', true);
+  }
+});
+
+// const emailCheck = (email) => {
+
+//   if (regExp.test(email)) {
+//     console.log('i');
+//   } else {
+//     console.log('no');
+//   }
+// };
