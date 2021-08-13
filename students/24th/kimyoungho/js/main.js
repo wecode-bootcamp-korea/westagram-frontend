fe8735fd2783c3f245cab0bd98ed37cd4a53ed42
youngho <youngho8762@gamil.com>
2021-08-13 12:54:58 +0900

    Refactor: 프리티어 적용

--- students/24th/kimyoungho/js/main.js
+++ students/24th/kimyoungho/js/main.js
@@ -1,56 +1,50 @@
-const postingButton = document.querySelector(".postingButton");
-const comment = document.querySelector(".comment");
-const writeComment = document.querySelector(".writeComment");
-const commentHeart = document.getElementsByClassName("commentHeart");
+const postingButton = document.querySelector('.postingButton');
+const comment = document.querySelector('.comment');
+const writeComment = document.querySelector('.writeComment');
+const commentHeart = document.getElementsByClassName('commentHeart');
 
-postingButton.addEventListener("click", (event) => {
+postingButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  addComment();
+});
+
+writeComment.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
     event.preventDefault();
     addComment();
+  }
 });
 
-writeComment.addEventListener("keydown", (event) => {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        addComment();
-    }
-});
-
-writeComment.addEventListener("keyup", () => {
-    if (writeComment.value.length >= 1) {
-        postingButton.style.color ="#3797F0"
-        postingButton.style.cursor ="pointer"
-    }
+writeComment.addEventListener('keyup', () => {
+  if (writeComment.value.length >= 1) {
+    postingButton.style.color = '#3797F0';
+    postingButton.style.cursor = 'pointer';
+  }
 });
 
 function addComment() {
-    if(writeComment.value.length >= 1) {
-        const makeTag = document.createElement("div");
-        comment.appendChild(makeTag);
-        makeTag.innerHTML=`<span class="boldFont">bbangho</span> ${writeComment.value} <img class="commentHeart" src="./img/garbage.png"> <img class="commentHeart" src="./img/heart.png">`
-        }
-    writeComment.value = ""
-    postingButton.style.color ="#D4E9FD"
+  if (writeComment.value.length >= 1) {
+    const makeTag = document.createElement('div');
+    comment.appendChild(makeTag);
+    makeTag.innerHTML = `<span class="boldFont">bbangho</span> ${writeComment.value} <img class="commentHeart" src="./img/garbage.png"> <img class="commentHeart" src="./img/heart.png">`;
+  }
+  writeComment.value = '';
+  postingButton.style.color = '#D4E9FD';
 }
 
-comment.addEventListener("click", (event) => {
-    const src = event.target.getAttribute("src")
-    const heart = "./img/heart.png"
-    const redHeart = "./img/redHeart.png"
-    const deleteIcon = "./img/garbage.png"
+comment.addEventListener('click', (event) => {
+  const src = event.target.getAttribute('src');
+  const heart = './img/heart.png';
+  const redHeart = './img/redHeart.png';
+  const deleteIcon = './img/garbage.png';
 
-    if (src === heart) {
-        event.target.src=redHeart;
-    } else if (src === redHeart) {
-        event.target.src=heart;
-    }
+  if (src === heart) {
+    event.target.src = redHeart;
+  } else if (src === redHeart) {
+    event.target.src = heart;
+  }
 
-     if (src === deleteIcon) {
-        event.target.parentNode.remove()
-     }
+  if (src === deleteIcon) {
+    event.target.parentNode.remove();
+  }
 });
-
-
-
-
-
-
