7b707e8394f42385eda0d13ba12f88e97355b8ad
orosy <hanei7632@gmail.com>
2021-08-13 11:25:14 +0900

    Add: main form 검색 기능 구현 - 배열 객체, filter, forEach 등을 활용한 검색 기능 구현 - 검색창 활성화 시에 삭제 버튼 활성화 구현 - 삭제 기능 진행중

--- students/24th/taesookim/westagram/main/js/main.js
+++ students/24th/taesookim/westagram/main/js/main.js
@@ -2,7 +2,10 @@ const searchInput = document.querySelector(".search-bar");
 const commentInput = document.querySelector(".comment-post");
 const commentForm = document.querySelector(".comment-form");
 const commentList = document.querySelector(".comments");
+const resultContainer = document.querySelector(".suggestions-container");
+const filteredList = document.querySelector(".suggestions-list");
 
+// 검색창 포커싱에 따른 레이아웃 변화 기능
 searchInput.addEventListener("focus", () => {
   const search = document.querySelector(".search");
   const searchIcon = document.querySelector(".fas.fa-search");
@@ -16,11 +19,14 @@ searchInput.addEventListener("blur", () => {
   const search = document.querySelector(".search");
   const searchIcon = document.querySelector(".fas.fa-search");
   const searchDelete = document.querySelector(".fas.fa-times-circle");
-  search.classList.remove("focus");
-  searchIcon.classList.remove("focus");
-  searchDelete.classList.remove("focus");
+  if (!searchInput.value) {
+    search.classList.remove("focus");
+    searchIcon.classList.remove("focus");
+    searchDelete.classList.remove("focus");
+  }
 });
 
+// 댓글 input value에 따른 버튼 색깔 변화
 commentInput.addEventListener("input", () => {
   const btnPost = document.querySelector(".btn-post");
   commentInput.value
@@ -28,6 +34,7 @@ commentInput.addEventListener("input", () => {
     : (btnPost.style.color = "#b2dffc");
 });
 
+// 댓글 작성 기능 구현
 commentForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addComment();
@@ -50,6 +57,7 @@ const addComment = () => {
   commentList.append(liTag);
 };
 
+// 댓글 좋아요 및 삭제 기능 구현
 commentList.addEventListener("click", (e) => {
   const comment = document.querySelector(".comments li");
   e.target.className === "far fa-trash-alt" && comment.remove();
@@ -59,3 +67,62 @@ commentList.addEventListener("click", (e) => {
     e.target.className = "far fa-heart";
   }
 });
+
+// 검색창 검색 기능 구현
+const accountArray = [
+  {
+    id: "wecode_bootcamp",
+    nickname: ">wecode | 위코드",
+    image: "/images/wecode.jpeg",
+  },
+  {
+    id: "wecode_socialclub",
+    nickname: ">wecode | 위코드",
+    image: "/images/wecode_socialclub.jpeg",
+  },
+  {
+    id: "wecode_fullstack_bootcamp",
+    nickname: "Wecode Full Stack Bootcamp",
+    image: "/images/wecode_fullstack.jpeg",
+  },
+  {
+    id: "orosy.ts",
+    nickname: "김태수",
+    image: "/images/81681164.jpg",
+  },
+];
+
+searchInput.addEventListener("keyup", () => {
+  filteredList.innerHTML = "";
+  resultContainer.style.display = "none";
+  if (searchInput.value) {
+    const filteredAccount = accountArray.filter((x) => searchFunc(x.id));
+    if (filteredAccount) {
+      filteredAccount.forEach((li) => showFilteredAccount(li));
+    }
+  }
+});
+
+searchInput.addEventListener("focusout", () => {
+  const containerCap = document.querySelector(".suggestions-cap");
+  resultContainer.style.display = "none";
+  containerCap.style.display = "none";
+});
+
+const searchFunc = (objId) => {
+  const searchId = searchInput.value;
+  return objId.indexOf(searchId) !== -1;
+};
+
+const showFilteredAccount = (account) => {
+  const containerCap = document.querySelector(".suggestions-cap");
+  resultContainer.style.display = "block";
+  containerCap.style.display = "block";
+  const filteredOne = document.createElement("li");
+  filteredOne.innerHTML = `<img src=${account.image} alt=${account.id} 사진 />
+  <div class="suggest-container">
+    <p class="suggest-username">${account.id}</p>
+    <p class="suggest-description">${account.nickname}</p>
+  </div>`;
+  filteredList.append(filteredOne);
+};
