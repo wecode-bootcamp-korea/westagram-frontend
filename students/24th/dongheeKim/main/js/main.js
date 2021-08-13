130edc3b1a55f0877ea759bfa0fe4cd652cf8a4c
donghee <dongdh02076@gmail.com>
2021-08-14 00:15:00 +0900

    ADD: 메인페이지 댓글 좋아요/취소 기능 완료 - main page 하트 클릭시 붉은 색으로 변경 - main page 붉은 하트 클릭시 다시 빈 하트로 변경 - main page 댓글 삭제 버튼 변경

--- students/24th/dongheeKim/main/js/main.js
+++ students/24th/dongheeKim/main/js/main.js
@@ -7,12 +7,12 @@ const commentText = document.querySelector(".feeds-comment_box");
 function plusComment() {
   const newComment = document.createElement('li');
   newComment.innerHTML = `<span><span class="accountName">donghee</span> ${commentText.value}</span>` 
-                       + `<span class="heart"><i class="far fa-heart"></i><span>`
-                       + `<span class="delete">X</span>`;
+                       + `<div><span class="heart"><i class="far fa-heart"></i></span>
+                       <span class="delete"><i class="far fa-trash-alt"></i></span></div>`;
   feedsText.appendChild(newComment);
   commentText.value = null;
+  likeComment(newComment);
   deleteComment(newComment);
-
 }
 
 // 함수 선언 : 댓글 삭제 함수
@@ -22,7 +22,13 @@ function deleteComment(newComment) {
   deleteBtn.style.cursor = "Pointer";
 }
 
-
+// 함수 선언 : 댓글 좋아요/취소 함수
+function likeComment(newComment) {
+  const likeBtn = newComment.querySelector(".heart");
+  likeBtn.style.cursor = "Pointer";
+  likeBtn.addEventListener("click",function() {
+    event.target.className = event.target.className === "far fa-heart" ? "fas fa-heart" : "far fa-heart";
+})}
 
 // 이벤트 추가
 commentBtn.addEventListener("click",plusComment)
