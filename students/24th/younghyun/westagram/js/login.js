f368b590a187e250ac17a0c4f1bde667e9b0c790
rhfovk <rhfovk@gmail.com>
2021-08-13 13:47:32 +0900

    Add : 로그인 시 'alert' - login button 조건으로 게시 버튼이 활성화 -> enter or 게시버튼 click 이벤트로 로그인 'alert'반환

--- students/24th/younghyun/westagram/js/login.js
+++ students/24th/younghyun/westagram/js/login.js
@@ -6,8 +6,20 @@ loginBox.addEventListener("keyup",()=>{
     let instaId = document.querySelector('.id').value;
     let instaPw = document.querySelector('.pw').value;
     (instaId.includes('@')) && (instaId.length > 4) && ( instaPw.length > 4) ? loginBtn.classList.add('active') : loginBtn.classList.remove('active');
-
 });
+
+loginBtn.addEventListener("click",checkLogin);
+window.addEventListener("keypress",(e)=>{
+    if(e.keyCode === 13){
+        checkLogin();
+    }
+});
+
+function checkLogin(){
+    if(loginBtn.classList.contains('active')){
+        alert("로그인 성공!");
+    }
+}
 
 
 
