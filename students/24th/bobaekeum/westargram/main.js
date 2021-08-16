d8c1e67e3da1f937f2eff3c4f1fa5d5c42a9a56b
BobaeKeum <aann0221@gmail.com>
2021-08-16 17:52:55 +0900

    ADD: 필수 구현 사항 완료 - 메인 화면 구현 및 댓글 기능 구현 - 로그인 화면 구현 및 버튼 활성화 기능 구현

--- students/24th/bobaekeum/westargram/main.js
+++ students/24th/bobaekeum/westargram/main.js
@@ -1,11 +1,55 @@
-let idElement =document.querySelector('.id');
-let passElement =document.querySelector('.password');
-let btn = document.querySelector('.btn');
-passElement.addEventListener('keyup', function(e) {
-    if(idElement.value.length>3 && passElement.value.length> 4){
-        btn.style.backgroundColor= "rgb(0,150,252)";
+// 메인페이지 댓글 기능
+let addTextElement = document.querySelector('.post');
+let textElement = document.querySelector('.typingText');
+let commentBtn = document.querySelector('.post');
+
+
+textElement.addEventListener('keyup', function() {
+    if(textElement != 0 ){
+        commentBtn.style.color="rgb(0,150,252)";
+    }else if(textElement=0){
+        commentBtn.style.color='rgb(201, 224, 249)';
     }
-    else if(idElement.value.length<4 || passElement.value.length <5){
-        btn.style.backgroundColor='rgb(201, 224, 249)';
+})
+
+textElement.addEventListener('keydown', (e)=>{
+    if(e.keyCode === 13){
+        let div = document.createElement('div');
+        let spanId = document.createElement('span');
+        let spanText = document.createElement('span');
+        let othersComments = document.querySelector(".othersComments")
+
+        div.classList.add('thatgle');
+        spanId.classList.add('uName');
+        spanText.classList.add("othersComment")
+        spanId.textContent = 'nugunga';
+        spanText.textContent= textElement.value;
+        
+        div.appendChild(spanId);
+        div.appendChild(spanText);
+        othersComments.appendChild(div);
+        textElement.value="";  
     }
-});
+        
+    })
+
+    textElement.addEventListener('click', ()=>{
+        if(textElement.value){
+            let div = document.createElement('div');
+            let spanId = document.createElement('span');
+            let spanText = document.createElement('span');
+            let othersComments = document.querySelector(".othersComments")
+    
+            div.classList.add('thatgle');
+            spanId.classList.add('uName');
+            spanText.classList.add("othersComment")
+            spanId.textContent = 'nugunga';
+            spanText.textContent= textElement.value;
+            
+            div.appendChild(spanId);
+            div.appendChild(spanText);
+            othersComments.appendChild(div);
+            textElement.value="";    
+        }    
+    })
+    
