6ab872dfa7abc9e67c68e5c85b3c74625a34b275
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-09-08 17:17:24 +0900

    Add Login page complete

---
+++ students/25th/seyeon/weStaram/js/login.js
@@ -0,0 +1,9 @@
+const loginButton = document.getElementsByClassName('button')[0];
+// document.getElementsByClassName('button')[0].disabled = true;
+
+// loginButton.addEventListener('keyup', function(){}
+//     if () {loginButton.style.backgroundColor = 'blue';}
+// });
+
+const inputId = document.getElementById('id');
+const inputPw = document.getElementById('pw'); 
