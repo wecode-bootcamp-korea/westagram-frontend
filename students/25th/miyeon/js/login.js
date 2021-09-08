0aaf240521bb211296488f9c8fbbcba2e9bb0f0b
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-08 17:07:58 +0900

    Add : 로그인 페이지 완료, 메인 페이지 만드는 중 - JS 미구현

---
+++ students/25th/miyeon/js/login.js
@@ -0,0 +1,10 @@
+// 로그인 인풋값에 값이 있을시 색상 변환
+let idInput = document.getElementById('idInput')
+let pwInput = document.getElementById('pwInput')
+let loginBtn = document.getElementById('loginClick')
+
+// 특정 아이디와 비번 값이어야만 로그인이 되도록(메인페이지로 이동 가능하게)
+
+
+
+// 비밀번호 잊으셨나요 버튼 클릭시 prompt창으로 핸드폰 번호 기입 d
