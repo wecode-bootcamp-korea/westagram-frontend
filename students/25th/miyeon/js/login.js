ab58b060cf2280e93e4ac1f02f9b2cef835520a5
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-09 18:11:28 +0900

    Add: 로그인 페이지, 메인 페이지 완성 / js 미구현

--- students/25th/miyeon/js/login.js
+++ students/25th/miyeon/js/login.js
@@ -1,7 +1,17 @@
 // 로그인 인풋값에 값이 있을시 색상 변환
-let idInput = document.getElementById('idInput')
-let pwInput = document.getElementById('pwInput')
-let loginBtn = document.getElementById('loginClick')
+let idInput = document.getElementById('idInput');
+let pwInput = document.getElementById('pwInput');
+let loginBtn = document.getElementById('loginClick');
+
+idInput.addEventListener('click', function() {
+    let idval = document.getElementById('idInput').value;
+
+    if (!idval) {
+        loginBtn.style.backgroundColor = 'black';
+    } else {
+        loginBtn.style.backgroundColor = 'white';
+    }
+});
 
 // 특정 아이디와 비번 값이어야만 로그인이 되도록(메인페이지로 이동 가능하게)
 
