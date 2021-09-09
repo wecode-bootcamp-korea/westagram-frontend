6e5e70f5ec94993783ab1c126fbaa62a17f925a1
xodid157 <xodid157@gmail.com>
2021-09-10 02:45:46 +0900

    add: 로그인,메인 레이아웃및 필수 기술구현 완료 - 로그인 화면 레이아웃 완료 - 로그인 화면 id,ps 입력 및 유효성 검사 enter 치면 버튼활성화 구현완료 -메인 화면 레이아웃 구현완료 -댓글 추가 기능 구현 완료

--- students/25th/taeyang/westagram/js/login.js
+++ students/25th/taeyang/westagram/js/login.js
@@ -1,25 +1,30 @@
 const button = document.getElementById('login-button');
 const idIn = document.getElementById('id-email');
 const passwordIn = document.getElementById('password');
+const validity = document.getElementsByClassName("validity")[0]
 
-
+// id에 @ password 5자이상 유효성 검사 및 버튼활성화
 function login() {
   const id= idIn.value
   const password = passwordIn.value
-  if (id && password) {
+  if (id.includes("@") && (password.length >= 5)) {
     button.classList.remove("button-off")
     button.disabled = false;
+    validity.style.display = 'none';
     } else {
     button.classList.add("button-off")
     button.disabled = true;
+    validity.style.display = 'block';
+  }
+}
+// 엔터 치면 버튼 클릭
+function enter(e) {
+  if(e.keyCode == 13) {
+    button.click();
   }
 }
 
-  function enter(e) {
-    if(e.keyCode == 13) {
-      button.click();
-    }
-  }
+
 
 passwordIn.addEventListener('keyup',login);
 idIn.addEventListener('keyup',login);
--- students/25th/taeyang/westagram/js/main.js
+++ students/25th/taeyang/westagram/js/main.js
@@ -1,7 +1,11 @@
 const input = document.getElementsByClassName("comment-text")[0];
+const inputTwo = document.getElementsByClassName("comment-text")[1];
 const btn = document.getElementsByClassName("btn")[0];
+const btnTwo = document.getElementsByClassName("btn")[1];
 const comment = document.getElementsByClassName("comment-second")[0]
+const commentTwo = document.getElementsByClassName("comment-second")[1]
 const emptyComment = document.getElementsByClassName("empty-comment")[0] 
+const emptyCommentTwo = document.getElementsByClassName("empty-comment")[1] 
 
 const onAdd = () => {
   const text = input.value;
@@ -29,7 +33,6 @@ const onAdd = () => {
   heartLike.setAttribute("class", "heart-like");
   heartLike.setAttribute("alt", "heart-like");
   heartLike.setAttribute("src", "imgs/heart.png");
-
   
   yous.appendChild(youName);
   yous.appendChild(commentWrit);
@@ -51,3 +54,49 @@ input.addEventListener('keypress', (event) => {
   return;
 })
 
+const onAddTwo = () => {
+  const text = inputTwo.value;
+    if (inputTwo.value ==="") {
+      inputTwo.focus();
+      return
+    }
+  
+  const commentSecond = document.createElement("div");
+  commentSecond.setAttribute("class", "comment-second");
+  
+  const yous = document.createElement("div");
+  yous.setAttribute("class", "yous");
+  
+  const youName = document.createElement("a");
+  youName.setAttribute("class", "you-name");
+  youName.setAttribute("href", "#");
+  youName.innerHTML = "ty_050";
+  
+  const commentWrit = document.createElement("span");
+  commentWrit.setAttribute("class", "comment-writ");
+  commentWrit.innerHTML =" " + text;
+
+  const heartLike = document.createElement("img")
+  heartLike.setAttribute("class", "heart-like");
+  heartLike.setAttribute("alt", "heart-like");
+  heartLike.setAttribute("src", "imgs/heart.png");
+  
+  yous.appendChild(youName);
+  yous.appendChild(commentWrit);
+  commentSecond.appendChild(yous);
+  commentSecond.appendChild(heartLike);
+  emptyCommentTwo.appendChild(commentSecond)
+  inputTwo.value ="";
+  inputTwo.focus();
+};
+
+btnTwo.addEventListener('click', () => {
+  onAddTwo();
+});
+
+inputTwo.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAddTwo();
+  }
+  return;
+})
