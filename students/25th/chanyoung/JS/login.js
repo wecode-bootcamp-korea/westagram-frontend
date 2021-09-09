93a1578901440b23f1fe9e4bcc5e746d18f78918
jcy <poiuytasr@gmail.com>
2021-09-10 07:47:57 +0900

    Refactor: 해당 css & js 파일 리팩토링 - login.css & main.css | 코드 간격 추가 - login.css & main.css | 속성 작성 순서에 따라 코드 순서 변경 - main.js | 한 줄 짜리 if문 블록 제거 - main.js | e.target[1] 등 알아보기 힘든 코드 변수에 할당 후 사용으로 변경 - main.js | 순서정리 DOM -> 전역변수(const -> let) -> function -> eventListener - main.js | 중복 코드 정리 및 코드 간격 추가 - main.js | innerHTML 값 정리 - main.js | 주석 정리 - 공통    | Prettier 설정 변경 (한 줄  문자 수 증가, 주석 색상 변경)

--- students/25th/chanyoung/JS/login.js
+++ students/25th/chanyoung/JS/login.js
@@ -1,26 +1,26 @@
-const id = document.querySelector(".login__id");
-const passwrod = document.querySelector(".login__password");
-const btn = document.querySelector(".login__button");
+const id = document.querySelector('.login__id');
+const password = document.querySelector('.login__password');
+const btn = document.querySelector('.login__button');
 
 let isId = false,
   isPassword = false;
 
 const getInput = (e) => {
   // 아이디 & 비번 값 체크
-  if (e.target.className === "login__id") {
+  if (e.target.className === 'login__id') {
     isId = e.target.value ? true : false;
   }
-  if (e.target.className === "login__password") {
+  if (e.target.className === 'login__password') {
     isPassword = e.target.value ? true : false;
   }
-  // 활성화 및 배경 컬러 변경
+  // 활성화 & 배경 컬러 변경
   if (isId && isPassword) {
     btn.disabled = false;
-    btn.style.backgroundColor = "#376088";
+    btn.style.backgroundColor = '#376088';
   } else {
-    btn.disabled = "disabled";
-    btn.style.backgroundColor = "#c4e1fb";
+    btn.disabled = 'disabled';
+    btn.style.backgroundColor = '#c4e1fb';
   }
 };
-id.addEventListener("input", getInput);
-passwrod.addEventListener("input", getInput);
+id.addEventListener('input', getInput);
+password.addEventListener('input', getInput);
--- students/25th/chanyoung/JS/main.js
+++ students/25th/chanyoung/JS/main.js
@@ -1,159 +1,115 @@
-const commentBtn = document.querySelector(".feeds-form");
-const commentHTML = document.querySelector(".feeds-main__comments ul");
-const searchInput = document.querySelector(".header__search input");
-const searchUserContainer = document.querySelector(
-  ".header__search__user-container"
-);
-const feedHeartBtn = document.querySelector(
-  ".feeds-main__icons--left .fa-heart"
-);
-const commentTime = document.querySelector(".feeds-main__comment-time");
-const menuProfile = document.querySelector(".menu-profile");
-const menuProfileBox = document.querySelector(".menu-profile-box");
-let commentHearts = document.querySelectorAll(".feeds-main__comments__heart");
-let commentDeleteBtn = document.querySelectorAll(
-  ".feeds-main__comments__delete"
-);
-const USER_NAME = "JeungChanYoung";
+const commentBtn = document.querySelector('.feeds-form');
+const commentHTML = document.querySelector('.feeds-main__comments ul');
+const searchInput = document.querySelector('.header__search input');
+const searchUserContainer = document.querySelector('.header__search__user-container');
+const feedHeartBtn = document.querySelector('.feeds-main__icons--left .fa-heart');
+const commentTime = document.querySelector('.feeds-main__comment-time');
+const menuProfile = document.querySelector('.menu-profile');
+const menuProfileBox = document.querySelector('.menu-profile-box');
+let commentHearts = document.querySelectorAll('.feeds-main__comments__heart');
+let commentDeleteBtn = document.querySelectorAll('.feeds-main__comments__delete');
+
+const MY_NAME = 'JeungChanYoung';
 const users = [
   {
-    name: "DUNA",
-    img: "images/duna1.jpg",
+    name: 'DUNA',
+    img: 'images/duna2.jpg',
   },
   {
-    name: "duNa_origin",
-    img: "images/duna2.jpg",
+    name: 'duNNyaa',
+    img: 'images/duna2.jpg',
   },
   {
-    name: "duna_o",
-    img: "images/duna2.jpg",
+    name: 'dunyong',
+    img: 'images/duna2.jpg',
   },
   {
-    name: "duNA-ori",
-    img: "images/duna2.jpg",
+    name: 'dunaisgood',
+    img: 'images/duna2.jpg',
   },
   {
-    name: "Duna_orion",
-    img: "images/duna2.jpg",
-  },
-  {
-    name: "good_dog",
-    img: "images/dog1.jpg",
+    name: 'Dunaisdog',
+    img: 'images/duna2.jpg',
   },
 ];
+// 찾아온 유저 관리용 배열
+let searchUsers = [];
 
 const addComment = (e) => {
   event.preventDefault();
+  const input = e.target[0];
 
-  if (e.target[0].value) {
-    let newComment = document.createElement("li");
-    newComment.className = "feeds-main__comments__content";
-    newComment.innerHTML = `<div class="comment-wrapper">
-    <span class="username">
-    ${USER_NAME}</span>
-    ${e.target[0].value}
-    <i class="fas fa-backspace feeds-main__comments__delete"></i>
-  </div>
-  <i class="fas fa-heart feeds-main__comments__heart" aria-hidden="true"></i>`;
+  if (input.value) {
+    let newComment = document.createElement('li');
+    newComment.className = 'feeds-main__comments__content';
+    newComment.innerHTML = `<div class="comment-wrapper"> 
+    <span class="username">${MY_NAME}</span> ${e.target[0].value}
+      <i class="fas fa-backspace feeds-main__comments__delete"></i>
+    </div>
+    <i class="fas fa-heart feeds-main__comments__heart" aria-hidden="true"></i>`;
     commentHTML.appendChild(newComment);
 
+    // 댓글 작성 시간 추가
     addCommentTime();
 
-    // 생성된 버튼에 이벤트 추가
-    newComment.querySelector(".feeds-main__comments__heart").onclick =
-      changeHeartColor;
-    newComment.querySelector(".feeds-main__comments__delete").onclick =
-      deleteComment;
+    newComment.querySelector('.feeds-main__comments__heart').onclick = changeHeartColor;
+    newComment.querySelector('.feeds-main__comments__delete').onclick = deleteComment;
 
-    // input 값 초기화!
-    e.target[0].value = null;
+    // 댓글 입력 창 초기화
+    input.value = null;
   }
 };
-const addCommentTime = () => (commentTime.innerText = "1분 전"); // 로컬 스토리지 이용해야 하는 걸로 알고 있기 때문에 패스
 
-const changeHeartColor = (e) => {
-  const color = e.target.style.color;
-  e.target.style.color = color ? "" : "red";
-};
+const addCommentTime = () => (commentTime.innerText = '1분 전');
+
+const changeHeartColor = (e) => (e.target.style.color = e.target.style.color ? '' : 'red');
 
 const deleteComment = (e) => {
-  const commentElementIdx = 2;
-  e.path[commentElementIdx].remove();
+  const target = e.path[2];
+  target.remove();
 };
 
-commentBtn.addEventListener("submit", addComment);
-// 인자를 사용하려면 익명함수를 써라
-feedHeartBtn.addEventListener("click", changeHeartColor);
-
-const addHeartBtnListener = () => {
-  commentHearts.forEach((e) => e.addEventListener("click", changeHeartColor));
-};
-const addDeleteBtnListener = () => {
-  commentDeleteBtn.forEach((e) => e.addEventListener("click", deleteComment));
-};
+// nav 검색창
 const searchText = (e) => {
   const text = e.path[0].value;
-  // 돋보기 아이콘 지우기
-  if (text) {
-    e.path[1].childNodes[1].style.visibility = "hidden";
-  } else {
-    e.path[1].childNodes[1].style.visibility = "visible";
-  }
-  // 유저 검색
-  console.log(searchUserContainer);
+  const searchIcon = e.path[1].childNodes[1];
 
-  // USER 라는 배열 안에 있는 오브젝트의 name에 접근을 해서
+  if (text) {
+    if (searchIcon.style.visibility !== 'hidden') searchIcon.style.visibility = 'hidden';
+  } else searchIcon.style.visibility = 'visible';
+
   matchUsername(text);
-  //유저 오브젝트에서 -> 유저들을 받아옴 -> 앞부터 3글자가 일치 할 경우 -> 유저컨테이너 생성 및 찾은 유저 표시
-  //
 };
 
-let searchUsers = [];
+// 검색 값에 따라 유저 판별
 const matchUsername = (text) => {
-  // 유저 목록 생성?
-
   if (text.match(new RegExp(`.{3,}`))) {
     if (!searchUsers.length) {
-      // 배열에 값이 없으면
-      // 3글자 이상인지 체크하고
       for (user of users) {
-        if (
-          // 유저들의 이름 일치 여부 체크
-          user.name.slice(0, text.length).toUpperCase() ===
-          text.slice(0, text.length).toUpperCase()
-        ) {
-          //찾은 유저의 컨테이너를 만들어서
-          let userContainer = document.createElement("div");
-          userContainer.className = "header__search__user-container__user";
+        // 유저들의 이름 일치 여부 체크
+        if (user.name.slice(0, text.length).toUpperCase() === text.slice(0, text.length).toUpperCase()) {
+          // 찾은 유저의 컨테이너 생성
+          let userContainer = document.createElement('div');
+          userContainer.className = 'header__search__user-container__user';
           userContainer.innerHTML = `<img class="user-img" src="${user.img}" alt=""></img>
-    <span class="username">${user.name}</span>`;
-          // 그리고 관리 할 배열에 붙여
+          <span class="username">${user.name}</span>`;
           searchUsers.push({ user: user, userContainer: userContainer });
-
-          //유저 컨테이너가 담길 공간에 붙여
           searchUserContainer.appendChild(userContainer);
         }
       }
     } else {
       let searchUserIdx = 0;
-      let tempSearchUsers = [];
       for (searchUser of searchUsers) {
-        console.log("테스트  " + searchUser.user.name);
         if (
           // 유저들의 이름 일치 여부 체크
-          searchUser.user.name.slice(0, text.length).toUpperCase() !==
-          text.slice(0, text.length).toUpperCase()
+          searchUser.user.name.slice(0, text.length).toUpperCase() !== text.slice(0, text.length).toUpperCase()
         ) {
-          console.log(`삭제되는 : ${searchUser.user.name}`);
-
-          searchUserContainer.removeChild(searchUser.userContainer);
+          searchUser.userContainer.style.display = 'none';
         } else {
-          tempSearchUsers.push(searchUser);
+          searchUser.userContainer.style.display = 'flex';
         }
         searchUserIdx++;
       }
-      console.log(searchUsers);
-      searchUsers = tempSearchUsers;
     }
   } else {
     // 찾은 유저들 삭제 및 배열 비우기
@@ -163,17 +119,20 @@ const matchUsername = (text) => {
     searchUsers.length = 0;
   }
 };
-// 프로필 메뉴 박스 생성
-const createMenuProfileBox = (e) => {
-  e.target.nextElementSibling.style.display = "flex";
-};
-const deleteMenuProfileBox = (e) => {
-  e.target.style.display = "none";
-};
 
-searchInput.addEventListener("input", searchText);
-menuProfile.addEventListener("click", createMenuProfileBox);
-menuProfileBox.addEventListener("mouseleave", deleteMenuProfileBox);
+// 프로필 메뉴 박스 생성
+const createMenuProfileBox = (e) => (e.target.nextElementSibling.style.display = 'flex');
+
+// 프포필 메뉴 박스 제거
+const deleteMenuProfileBox = (e) => (e.target.style.display = 'none');
+
+searchInput.addEventListener('input', searchText);
+feedHeartBtn.addEventListener('click', changeHeartColor);
+commentBtn.addEventListener('submit', addComment);
+menuProfile.addEventListener('click', createMenuProfileBox);
+menuProfileBox.addEventListener('mouseleave', deleteMenuProfileBox);
+const addHeartBtnListener = () => commentHearts.forEach((e) => e.addEventListener('click', changeHeartColor));
+const addDeleteBtnListener = () => commentDeleteBtn.forEach((e) => e.addEventListener('click', deleteComment));
 
 addHeartBtnListener();
 addDeleteBtnListener();
