890ac3443846c74856d3aed16258826d42abb573
Jetom88 <shinhro3o@gmail.com>
2021-09-10 00:08:39 +0900

    login.js 기능 구현

--- students/25th/hyeri/tomstagram/js/login.js
+++ students/25th/hyeri/tomstagram/js/login.js
@@ -3,11 +3,16 @@
     const loginPasswordInput = document.querySelector(".password");
     const loginBtn = document.querySelector(".login_btn");
 
-    loginPasswordInput.addEventListener('keydown', () => {
-        const loginIdInputValue = loginIdInput.value;
-        const loginPasswordInputValue = loginPasswordInput.value;
+    const loginPasswordInputValue = loginPasswordInput.value;
+    const loginIdInputValue = loginIdInput.value;
 
-        loginIdInputValue && loginPasswordInputValue ? loginBtn.disabled = true : loginBtn.disabled = false;
+    const loginInfo = loginPasswordInputValue && loginIdInputValue;
+
+    // loginBtn.disabled = true : 
+    // 
+    
+    loginPasswordInput.addEventListener('keyup', ()=> {
+        (loginInfo === true) ? loginBtn.disabled = true : loginBtn.disabled = false;
         loginBtn.style.backgroundColor = "#0095f6";
     })
 
