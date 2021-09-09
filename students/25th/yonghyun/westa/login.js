94191e2fa87a7a9151e21437133b6a7b1abedb14
yonghyun <dydgus0421@gmail.com>
2021-09-10 08:42:54 +0900

    refactoring 진행중

--- students/25th/yonghyun/westa/login.js
+++ students/25th/yonghyun/westa/login.js
@@ -1,49 +1,13 @@
 const instaId = document.querySelector('.iId')
 const instaPw = document.querySelector('.iPw')
 const loginBtnn = document.querySelector('.loginBtn')
-// const someinput = document.querySelector('.id');
 
-function asd () {
+function canLogin () {
     const user_id = instaId.value;
     const user_pw = instaPw.value;
         user_id.indexOf('@') !== -1 && user_pw.length > 4 ? loginBtnn.disabled = false : loginBtnn.disabled = true;
     }
 
-instaId.addEventListener('keyup',asd)
-instaPw.addEventListener('keyup',asd)
-
-// instaPw.addEventListener('keyup', function () {
-//     const user_id = instaId.value;
-//     const user_pw = instaPw.value;
-//         user_id.length !==0 && user_pw.length >= 5 ?loginBtnn.disabled = false : loginBtnn.disabled = true;
-//         })
-
-// instaId.addEventListener('keyup', function () {
-//     const user_id = instaId.value;
-//     const user_pw = instaPw.value;
-//         user_id.length !==0 && user_pw.length >= 5 ? loginBtnn.disabled = false : loginBtnn.disabled = true;
-//         })
-
-// someinput.addEventListener('keyup', function () {
-//     const user_id = instaId.value;
-//     const user_pw = instaPw.value;
-//         if(user_id.length !==0 && user_pw.length !==0){
-//             loginBtnn.disabled = false;
-//         } else {
-//             loginBtnn.disabled = true;
-//         }
-//     }
-// )
-
-// instaPw.addEventListener('keyup', function () {
-//     const user_id = instaId.value;
-//     const user_pw = instaPw.value;
-//         user_id.indexOf('@') !== -1 && user_pw.length > 4 ?loginBtnn.disabled = false : loginBtnn.disabled = true;
-//         })
-
-// instaId.addEventListener('keyup', function () {
-//     const user_id = instaId.value;
-//     const user_pw = instaPw.value;
-//         user_id.indexOf('@') !== -1 && user_pw.length > 4 ? loginBtnn.disabled = false : loginBtnn.disabled = true;
-//         })
+instaId.addEventListener('keyup', canLogin)
+instaPw.addEventListener('keyup', canLogin)
 
