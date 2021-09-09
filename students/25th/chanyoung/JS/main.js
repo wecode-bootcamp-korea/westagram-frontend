2728cade8be454b146b06cda8808a10835f946ab
jcy <poiuytasr@gmail.com>
2021-09-09 09:59:00 +0900

    Add: 아이디 검색기능 추가 - 검색될 temp user object 추가 - 검색 리스트 컨테이너 추가 - 3개 이상 문자 입력시 &  user name과 input value가 같을시 검색 되도록 조건 추가 - 검색된 유저 목록 관리를 위한 searchUsers array 추가 zsh:1: command not found: wq

--- students/25th/chanyoung/JS/main.js
+++ students/25th/chanyoung/JS/main.js
@@ -1,6 +1,9 @@
 const commentBtn = document.querySelector(".feeds-form");
 const commentHTML = document.querySelector(".feeds-main__comments ul");
 const searchInput = document.querySelector(".header__search input");
+const searchUserContainer = document.querySelector(
+  ".header__search__user-container"
+);
 const feedHeartBtn = document.querySelector(
   ".feeds-main__icons--left .fa-heart"
 );
@@ -9,12 +12,33 @@ let commentHearts = document.querySelectorAll(".feeds-main__comments__heart");
 let commentDeleteBtn = document.querySelectorAll(
   ".feeds-main__comments__delete"
 );
-
 const USER_NAME = "JeungChanYoung";
-const USER = {
-  name: "DUNA",
-  img: "images/duna2.jpg",
-};
+const users = [
+  {
+    name: "DUNA",
+    img: "images/duna1.jpg",
+  },
+  {
+    name: "duNa_origin",
+    img: "images/duna2.jpg",
+  },
+  {
+    name: "duna_o",
+    img: "images/duna2.jpg",
+  },
+  {
+    name: "duNA-ori",
+    img: "images/duna2.jpg",
+  },
+  {
+    name: "Duna_orion",
+    img: "images/duna2.jpg",
+  },
+  {
+    name: "good_dog",
+    img: "images/dog1.jpg",
+  },
+];
 
 const addComment = (e) => {
   event.preventDefault();
@@ -34,15 +58,10 @@ const addComment = (e) => {
     addCommentTime();
 
     // 생성된 버튼에 이벤트 추가
-    const newHeartBtn = document.querySelectorAll(
-      ".feeds-main__comments__heart"
-    );
-    const newDeleteBtn = document.querySelectorAll(
-      ".feeds-main__comments__delete"
-    );
-
-    newHeartBtn[newHeartBtn.length - 1].onclick = changeHeartColor;
-    newDeleteBtn[newDeleteBtn.length - 1].onclick = deleteComment;
+    newComment.querySelector(".feeds-main__comments__heart").onclick =
+      changeHeartColor;
+    newComment.querySelector(".feeds-main__comments__delete").onclick =
+      deleteComment;
 
     // 리스너 추가
     // input 값 초기화!
@@ -71,16 +90,83 @@ const addHeartBtnListener = () => {
 const addDeleteBtnListener = () => {
   commentDeleteBtn.forEach((e) => e.addEventListener("click", deleteComment));
 };
-const searchKeyword = (e) => {
-  const keyword = e.path[0].value;
+const searchText = (e) => {
+  const text = e.path[0].value;
   // 돋보기 아이콘 지우기
-  if (keyword) {
+  if (text) {
     e.path[1].childNodes[1].style.visibility = "hidden";
   } else {
     e.path[1].childNodes[1].style.visibility = "visible";
   }
   // 유저 검색
+  console.log(searchUserContainer);
+
+  // USER 라는 배열 안에 있는 오브젝트의 name에 접근을 해서
+  matchUsername(text);
+  //유저 오브젝트에서 -> 유저들을 받아옴 -> 앞부터 3글자가 일치 할 경우 -> 유저컨테이너 생성 및 찾은 유저 표시
+  //
 };
-searchInput.addEventListener("input", searchKeyword);
+
+let searchUsers = [];
+const matchUsername = (text) => {
+  // 유저 목록 생성?
+
+  if (text.match(new RegExp(`.{3,}`))) {
+    if (!searchUsers.length) {
+      // 배열에 값이 없으면
+      // 3글자 이상인지 체크하고
+      for (user of users) {
+        if (
+          // 유저들의 이름 일치 여부 체크
+          user.name.slice(0, text.length).toUpperCase() ===
+          text.slice(0, text.length).toUpperCase()
+        ) {
+          //찾은 유저의 컨테이너를 만들어서
+          let userContainer = document.createElement("div");
+          userContainer.className = "header__search__user-container__user";
+          userContainer.innerHTML = `<img class="user-img" src="${user.img}" alt=""></img>
+    <span class="username">${user.name}</span>`;
+          // 그리고 관리 할 배열에 붙여
+          searchUsers.push({ user: user, userContainer: userContainer });
+
+          //유저 컨테이너가 담길 공간에 붙여
+          searchUserContainer.appendChild(userContainer);
+
+          // 찾은 유저 컨테이너 만드는 init
+
+          // 찾은 유저를 배열에 담아 관리
+        }
+      }
+    } else {
+      let searchUserIdx = 0;
+      let tempSearchUsers = [];
+      for (searchUser of searchUsers) {
+        console.log("테스트  " + searchUser.user.name);
+        if (
+          // 유저들의 이름 일치 여부 체크
+          searchUser.user.name.slice(0, text.length).toUpperCase() !==
+          text.slice(0, text.length).toUpperCase()
+        ) {
+          console.log(`삭제되는 : ${searchUser.user.name}`);
+
+          searchUserContainer.removeChild(searchUser.userContainer);
+        } else {
+          tempSearchUsers.push(searchUser);
+        }
+        searchUserIdx++;
+      }
+      console.log(searchUsers);
+      searchUsers = tempSearchUsers;
+    }
+  } else {
+    // 찾은 유저들 삭제 및 배열 비우기
+    searchUsers.forEach((e) => {
+      searchUserContainer.removeChild(e.userContainer);
+    });
+    searchUsers.length = 0;
+  }
+};
+
+searchInput.addEventListener("input", searchText);
 addHeartBtnListener();
 addDeleteBtnListener();
