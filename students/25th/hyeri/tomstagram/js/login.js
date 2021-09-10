d50d85541817119b89d7c423d4c742ebbe64c4ff
Jetom88 <jetom.shin@gmail.com>
2021-09-10 18:39:18 +0900

    Fixed: css 수정

--- students/25th/hyeri/tomstagram/js/login.js
+++ students/25th/hyeri/tomstagram/js/login.js
@@ -3,22 +3,24 @@
     const loginPasswordInput = document.querySelector(".password");
     const loginBtn = document.querySelector(".login_btn");
 
-    loginPasswordInput.addEventListener('keydown', () => {
+    function activeHandler() {
         const loginIdInputValue = loginIdInput.value;
         const loginPasswordInputValue = loginPasswordInput.value;
 
-        if (loginIdInputValue.length && loginPasswordInputValue.length) {
-            //
-            loginBtn.disabled = false
-        } else {
-            //
-            loginBtn.disabled = true
+        const loginSuccess = loginIdInputValue && loginPasswordInputValue;
+
+        if (loginSuccess === true) {
+            loginBtn.classList.add("active");
+            loginBtn.disabled = false;
         }
+    }
 
-        // const test = loginBtn.attr('disabled');
-        // test = false;
-
-        // (loginPasswordInputValue == null && loginIdInputValue == null) ? loginBtn.style.backgroundColor = "#ddd" : test;
-    })
-    console.logZ
+    loginPasswordInput.addEventListener('keydown', activeHandler());
 })();
+
+
+// if (loginIdInputValue.length && loginPasswordInputValue.length) {
+//     loginBtn.disabled = false
+// } else {
+//     loginBtn.disabled = true
+// }
