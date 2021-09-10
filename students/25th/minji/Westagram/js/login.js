2d9beba60da67ae0dc575d1f564c91f5405f1ec3
Jeong-minji <alswl4152@naver.com>
2021-09-10 23:58:51 +0900

    Modify: 로그인 유효성검사 if문 삼항연산자로 변경

--- students/25th/minji/Westagram/js/login.js
+++ students/25th/minji/Westagram/js/login.js
@@ -5,13 +5,9 @@ const btn_login = document.querySelector(".btn_login");
 
 // 유효성검사 통과시 버튼 활성화하고 메인으로 이동
 const activateButton = () => {
-  if (login_id.value.includes("@") && login_pw.value.length >= 5) {
-    btn_login.disabled = false;
-    btn_login.style.opacity = 1;
-  } else {
-    btn_login.disabled = true;
-    btn_login.style.opacity = 0.4;
-  }
+  login_id.value.includes("@") && login_pw.value.length >= 5
+    ? ((btn_login.disabled = false), (btn_login.style.opacity = 1))
+    : ((btn_login.disabled = true), (btn_login.style.opacity = 0.4));
 };
 
 login_form.addEventListener("keyup", activateButton);
