7c713f2c5e92afada49c43353d6386b6774dd33d
xodid157 <xodid157@gmail.com>
2021-09-10 15:59:45 +0900

    메인 댓글인풋창 변경

--- students/25th/taeyang/westagram/js/main.js
+++ students/25th/taeyang/westagram/js/main.js
@@ -7,13 +7,16 @@ const commentTwo = document.getElementsByClassName("comment-second")[1]
 const emptyComment = document.getElementsByClassName("empty-comment")[0] 
 const emptyCommentTwo = document.getElementsByClassName("empty-comment")[1] 
 
+
+// 첫번째 피드
 const onAdd = () => {
   const text = input.value;
     if (input.value ==="") {
       input.focus();
       return
     }
-  
+
+// HTML 요소를 생성해서 클래스 명,속성 넣기
   const commentSecond = document.createElement("div");
   commentSecond.setAttribute("class", "comment-second");
   
@@ -34,19 +37,27 @@ const onAdd = () => {
   heartLike.setAttribute("alt", "heart-like");
   heartLike.setAttribute("src", "imgs/heart.png");
   
+
+  //생성한 요소를 묶기
   yous.appendChild(youName);
   yous.appendChild(commentWrit);
   commentSecond.appendChild(yous);
   commentSecond.appendChild(heartLike);
   emptyComment.appendChild(commentSecond)
+
+  // 값이 비어있으면 enter 안쳐지게
   input.value ="";
+
+  //아이디나 비밀번호를 버튼이나 enter 를 쳤을때도 포커스 되게 하기
   input.focus();
 };
 
+// 버튼을 클릭했을때 버튼활성화 및 색 변하는 함수 실행
 btn.addEventListener('click', () => {
   onAdd();
 });
 
+// 아이디나 검색창 Enter 치면 버튼 클릭 구현
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
@@ -54,6 +65,7 @@ input.addEventListener('keypress', (event) => {
   return;
 })
 
+//두번째 피드 시작
 const onAddTwo = () => {
   const text = inputTwo.value;
     if (inputTwo.value ==="") {
@@ -61,6 +73,7 @@ const onAddTwo = () => {
       return
     }
   
+// HTML 요소를 생성해서 클래스 명,속성 넣기
   const commentSecond = document.createElement("div");
   commentSecond.setAttribute("class", "comment-second");
   
@@ -81,19 +94,26 @@ const onAddTwo = () => {
   heartLike.setAttribute("alt", "heart-like");
   heartLike.setAttribute("src", "imgs/heart.png");
   
+  //생성한 요소를 묶기
   yous.appendChild(youName);
   yous.appendChild(commentWrit);
   commentSecond.appendChild(yous);
   commentSecond.appendChild(heartLike);
   emptyCommentTwo.appendChild(commentSecond)
+  
+  // 값이 비어있으면 enter 안쳐지게
   inputTwo.value ="";
+
+  //아이디나 비밀번호를 버튼이나 enter 를 쳤을때도 포커스 되게 하기
   inputTwo.focus();
 };
 
+// 버튼을 클릭했을때 버튼활성화 및 색 변하는 함수 실행
 btnTwo.addEventListener('click', () => {
   onAddTwo();
 });
 
+// 아이디나 검색창 Enter 치면 버튼 클릭 구현
 inputTwo.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAddTwo();
