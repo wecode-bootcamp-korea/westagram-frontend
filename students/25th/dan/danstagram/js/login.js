c5a590fa2538c6baff51e2b7d485d5301f67aee4
kangdan <kangdanne@gmail.com>
2021-09-10 10:23:27 +0900

    login.js 사용하지 않는 코드 제거

--- students/25th/dan/danstagram/js/login.js
+++ students/25th/dan/danstagram/js/login.js
@@ -1,12 +1,9 @@
 const isId = document.getElementById('user_id');
 const isPw = document.getElementById('user_pw');
 const letsLogin = document.getElementById('btn_login');
-const isInput = document.getElementsByTagName('input')[0]
 
 isId.addEventListener('keyup',activeBtn)
 isPw.addEventListener('keyup',activeBtn)
-
-console.log(isInput)
 
 function activeBtn(){
  
