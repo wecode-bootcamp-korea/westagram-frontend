39dc4b6237a336906d9057b0795e75a209f31ad2
하상영 <sangyouh@usc.edu>
2021-09-10 15:20:12 +0900

    필수구현사항 모두 완료

--- students/25th/sangyoung-westa/login.js
+++ students/25th/sangyoung-westa/login.js
@@ -4,12 +4,15 @@ let inputArray= document.querySelectorAll("input");
 inputArray.forEach(function(e) {
     e.addEventListener("keyup", function() {
         const pwBar = document.getElementsByTagName("input")[1].value;
+        // 비밀번호 입력창의 value 를 저장하는 변수
         const idBar = document.getElementsByTagName("input")[0].value;
-        
+        // 아이디 입력창의 value 를 저장하는 변수
         if(idBar.length >=1 && pwBar.length >= 1){
-    
+        //각 입력창에 뭐라도 하나 이상 입력될시 아래 코드가 수행됨
             let title = document.getElementsByTagName('button')[0];
+            // 기존 로그인창의 태그를 title 이라는 변수에 저장
             title.className = 'button-change';
+            //미리 css 에 입력해둔 button-change 라는 class 명을 상단 태그에 지정해줌.
          }
          return;
 
--- students/25th/sangyoung-westa/main.js
+++ students/25th/sangyoung-westa/main.js
@@ -1,5 +1,24 @@
-let inputArray= document.querySelectorAll("button");
 
-inputArray.addEventListener("click", function(e){
-    
+document.getElementById("event-handler").addEventListener("keypress",function(e){
+// enter 키 입력시 댓글을 comment-insert div 안에 삽입시켜주는 함수
+    if(e.key === "Enter"){
+        let getComment = document.getElementById("event-handler").value;
+        //event-handler 라는 아이디를 가지고있는  input창에 입력된 값을 getComment 라는 변수에 저장
+        let inputComment = document.createElement("P");
+        //createElement 를 통해 paragraph 를 생성
+        inputComment.innerHTML = getComment;
+        //innterHTML 을 통해 생성된 pragraph에 getComment 의 내용을 추가
+        const location = document.getElementById("comment-insert");
+        // 삽입할 위치인 comment-insert div 의 위치를 변수에 저장 
+        location.appendChild(inputComment);
+        // appendChild 를 통해 해당 로케이션에 추가
+    }
+});
+document.getElementById("button-id").addEventListener("click",function(){
+// 게시버튼 클릭시 댓글을 comment-insert div 안에 삽입시켜주는 함수
+        let getComment = document.getElementById("event-handler").value;
+        let inputComment = document.createElement("P");
+        inputComment.innerHTML = getComment;
+        const location = document.getElementById("comment-insert");
+        location.appendChild(inputComment);
 });
