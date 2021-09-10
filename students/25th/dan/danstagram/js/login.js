2c9f1013b93ec87952a34aaf23452088dbf24897
kangdan <kangdanne@gmail.com>
2021-09-10 09:39:30 +0900

    로그인 & 메인페이지 리팩토링 중

--- students/25th/dan/danstagram/js/login.js
+++ students/25th/dan/danstagram/js/login.js
@@ -3,24 +3,17 @@ const isPw = document.getElementById('user_pw');
 const letsLogin = document.getElementById('btn_login');
 const isInput = document.getElementsByTagName('input')[0]
 
-// id에 키업 때 한 글자 이상 있는지 체크
-// pw에 키업 때 한 글자 이상 있는지 체크
-// 두 다 글자 있는지 체크
-// 둘 다 한 글자 이상 있으면 login버튼 active
-// 둘 중 하나 비었으면 있으면 login버튼 active XXX
-
 isId.addEventListener('keyup',activeBtn)
 isPw.addEventListener('keyup',activeBtn)
 
 console.log(isInput)
-//isInput.addEventListener('keyup',activeBtn)
 
 function activeBtn(){
  
-    if (0 < isId.value.length && 0 < isPw.value.length){
-        letsLogin.classList.add('active');
-    } else {
-        letsLogin.classList.remove('active');
-    }
+  if (0 < isId.value.length && 0 < isPw.value.length){
+    letsLogin.classList.add('active');
+  } else {
+    letsLogin.classList.remove('active');
+  }
 }
 
--- students/25th/dan/danstagram/js/main.js
+++ students/25th/dan/danstagram/js/main.js
@@ -1,43 +1,37 @@
-
-// 댓글 input 창에 엔터치거나 
-// "게시" 누르면
-// 댓글 추가되도록 createElement로 요소 생성해서, input에 입력한 값이 추가 되어야합니다
-
 const getComment = document.getElementById('list_comment');
 const getNewComment = document.getElementById('new_comment');
 const postComment = document.getElementById('btn_post');
 
 postComment.addEventListener('click', addComment);
 getNewComment.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        addComment()
-    }
+  if (event.keyCode === 13) {
+     event.preventDefault();
+    addComment()
+  }
 });
 
 function addComment(){
-    const listComment = document.getElementById('list_comment')
+  const listComment = document.getElementById('list_comment')
 
-    // 새 코멘트 공간만들어서 코멘트 리스트에 추가
-    const newCommentTxt = document.createElement('p');
-    newCommentTxt.classList.add('txt_comment');
+  // 새 코멘트 공간만들어서 코멘트 리스트에 추가
+  const newCommentTxt = document.createElement('p');
+  newCommentTxt.classList.add('txt_comment');
 
-    //위치 중요)input에 적힌 값 할당
-    const getNewCommentTxt = getNewComment.value;
+  //input에 적힌 값 할당
+  const getNewCommentTxt = getNewComment.value;
 
-    // 코멘트 안에 출력될 ID 공간
-    newCommentTxt.innerHTML = '<a href="#n" class="txt_id">follow ID</a>';
+  // 코멘트 안에 출력될 ID 공간
+  newCommentTxt.innerHTML = '<a href="#n" class="txt_id">follow ID</a>';
 
-    // 코멘트 안에 출력될 내용
-    let span = document.createElement("span")
-    newCommentTxt.append(span)
-    newCommentTxt.lastChild.innerText = getNewCommentTxt;
+  // 코멘트 안에 출력될 내용
+  let span = document.createElement("span")
+  newCommentTxt.append(span)
+  newCommentTxt.lastChild.innerText = getNewCommentTxt;
 
-    // 코멘트 리스트에 추가
-    getComment.appendChild(newCommentTxt)
-    console.log(getNewComment)
+  // 코멘트 리스트에 추가
+  getComment.appendChild(newCommentTxt)
+  console.log(getNewComment)
 
-    // 코멘트 리스트에 추가되면 input비움
-    getNewComment.value = ''
-    //document.getElementById("list_comment").innerText = getNewComment;
+  // 코멘트 리스트에 추가되면 input비움
+  getNewComment.value = ''
 }
