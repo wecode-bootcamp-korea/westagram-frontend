c3bb0ed805ab2c19e7aaf2cb4e1f154460a85f67
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-10 16:53:56 +0900

    Fix: 로그인, 메인 JS 기능 수정

--- students/25th/miyeon/js/login.js
+++ students/25th/miyeon/js/login.js
@@ -1,5 +1,5 @@
 // 로그인 인풋값에 값이 있을시 색상 변환
-const inputTag = document.getElementsByTagName('input')[0];
+const inputTag = document.getElementsByTagName('input');
 const idInput = document.getElementById('idInput');
 const pwInput = document.getElementById('pwInput');
 const loginBtn = document.getElementsByClassName('loginGo')[0];
@@ -7,22 +7,26 @@ const loginBtn = document.getElementsByClassName('loginGo')[0];
 idInput.addEventListener('keyup', btnClick);
 pwInput.addEventListener('keyup', btnClick);
 
-// 백스페이스로 지운 다음, Id값과 pw값이 0이면 색상 비활성화
-idInput.addEventListener('keyup', function(event) {
-    if (`${event.key}` == 'Backspace') {
-        if (idInput.value.length == 0 && pwInput.value.length == 0) {
-        loginBtn.style.backgroundColor = "#b9def7";
-        }
-    }
+// 아이디 인풋값에 키보드를 눌렀다 뗄 때 실행
+inputTag.addEventListener('keyup', function(event) {
+        // 아이디만 입력하고, 비번을 입력하지 않았을 때 비활성화
         if (idInput.value.length > 0 && pwInput.value.length == 0) {
             loginBtn.style.backgroundColor = "#b9def7";
         }
 
+        // 아이디는 입력하지 않고, 비번만 입력했을 때 비활성화
         if (idInput.value.length == 0 && pwInput.value.length > 0) {
             loginBtn.style.backgroundColor = "#b9def7";
         }
-    
+
+        // 인풋창에서 백스페이스로 값을 지워웠을 때
+        if (`${event.key}` == 'Backspace') {
+            if (idInput.value.length == 0 && pwInput.value.length == 0) {
+            loginBtn.style.backgroundColor = "#b9def7";
+            }
+        }
 })
+
 
 function btnClick() {
     if (idInput.value.length > 0 && pwInput.value.length > 0) {
--- students/25th/miyeon/js/main.js
+++ students/25th/miyeon/js/main.js
@@ -1,12 +1,39 @@
-const commentList = document.getElementsByClassName('commentList')[1];
-const inputVal = document.getElementsByClassName('input')[0];
-const uploadClick = document.getElementById('textUpload');
+const commentList = document.querySelector('.commentList');
+const inputEl = document.querySelector('.comment');
+const uploadClick = document.querySelector('#textUpload');
 
-
-let createEl = document.createElement("span");
-
-uploadClick.addEventListener('click', function() {
-    if (inputVal.value.length > 0) {
-     commentList.appendChild('inputVal');
+inputEl.addEventListener('keyup', btnColorChange)
+uploadClick.addEventListener('click', btnClick);
+document.addEventListener('keyup', function(e) {
+    if (e.key === 'Enter') {
+        const createEl = document.createElement("li");
+        createEl.innerHTML = `<b>buzzi_nyang</b>&nbsp; ` + inputEl.value;
+        commentList.appendChild(createEl);
+        inputEl.value = null;
+    } else if (!inputEl.value) {
+        const notActive = `{$uploadClick}`.attr('disabled');
+        notActive = false;
     }
-});
+}); // 엔터 치면 댓글 업로드하는 함수
+
+// 댓글창에 값이 있을 때와 없을 때의 색상 변경 함수
+function btnColorChange() {
+    if (inputEl.value) {
+        uploadClick.style.color = "#0095f6";
+    } else if (!inputEl.value) {
+        uploadClick.style.color = "#b9def7";
+    }
+}
+
+// 댓글 입력 버튼을 클릭했을 때 댓글 업로드 함수
+function btnClick() {
+    if (inputEl.value) {
+        const createEl = document.createElement("li");
+        createEl.innerHTML = `<b>buzzi_nyang</b>&nbsp; ` + inputEl.value;
+        commentList.appendChild(createEl);
+        inputEl.value = null;
+    } else if (!inputEl.value) {
+        const notActive = `{$uploadClick}`.attr('disabled');
+        notActive = false;
+    }
+}
