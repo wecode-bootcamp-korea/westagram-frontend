59fef84552f0bb67b77b50f989581d4677b8eb40
71summernight <71summernight@naver.com>
2021-09-10 12:38:27 +0900

    add login disable

--- students/25th/hyunjin/js/login.js
+++ students/25th/hyunjin/js/login.js
@@ -8,7 +8,9 @@ id.addEventListener("keyup", checkIdPassword);
 function checkIdPassword() {
   if (id.value.indexOf(checkString) != -1 && password.value.length > 5) {
     login.style.backgroundColor = "#0095F6";
+    login.disabled = false;
   } else {
     login.style.backgroundColor = "#c4e1fb";
+    login.disabled = true;
   }
 }
