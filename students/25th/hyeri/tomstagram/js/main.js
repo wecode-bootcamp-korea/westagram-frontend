b6febfd7b73bdbb608002c2a14bc94274804275b
Jetom88 <shinhro3o@gmail.com>
2021-09-11 13:51:40 +0900

    Fixed: 댓글 기능 구현 완료!

--- students/25th/hyeri/tomstagram/js/main.js
+++ students/25th/hyeri/tomstagram/js/main.js
@@ -1,8 +1,7 @@
 // 댓글 입력 후 엔터키를 누르거나 게시 버튼 클릭 시 댓글이 추가될 수 있도록 구현해주세요.
 (()=> {
     const commentForm = document.querySelector(".comment_input > form");
-    const commentBtn = document.querySelector(".comment_btn");
-
+    
     function commentHandler(e){
         e.preventDefault();
         const commentInput = document.querySelector(".comment_input_txt");
