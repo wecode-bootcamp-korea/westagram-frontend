1ca510b5419e53b9f4eb379c95465050baf20837
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-12 00:35:31 +0900

    fix : 추가 기능 구현 : 헤더 검색창에 글자 입력시 돋보기 아이콘 사라짐 기능, 미디어쿼리 기능

--- students/25th/miyeon/js/main.js
+++ students/25th/miyeon/js/main.js
@@ -1,14 +1,7 @@
-const commentList = document.querySelector('.commentList');
-const inputEl = document.querySelector('.comment');
-const uploadClick = document.querySelector('#textUpload');
-
-inputEl.addEventListener('keyup', btnColorChange)
-uploadClick.addEventListener('click', btnClick);
-document.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-        btnClick();
-    }
-}); // 엔터 치면 댓글 업로드하는 함수
+const commentList = document.querySelector('.commentList'); // 댓글 목록
+const inputEl = document.querySelector('.comment'); // 댓글 입력 인풋창
+const uploadClick = document.querySelector('#textUpload'); // 댓글 입력 버튼
+const searchInput = document.querySelector('.searchBar'); // 헤더 상단 검색창
 
 // 댓글창에 값이 있을 때와 없을 때의 색상 변경 함수
 function btnColorChange() {
@@ -32,3 +25,23 @@ function btnClick() {
     btnColorChange();
     }
 }
+
+// 필수 기능 구현
+inputEl.addEventListener('keyup', btnColorChange)
+uploadClick.addEventListener('click', btnClick);
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        btnClick();
+    }
+}); // 엔터 치면 댓글 업로드하는 함수
+
+// 추가 기능 구현
+searchInput.addEventListener('keyup', () => {
+    const searchIcon = document.querySelector('.searchIcon');
+    const centerBar = document.querySelector('.centerBar');
+    if (searchInput.value.length > 0) {
+        searchIcon.style.display = 'none'; // 비활성화
+    } else if (searchInput.value.length == 0) {
+        searchIcon.style.display = '';     // 활성화
+        }
+}) // 헤더의 검색창에 입력시 돋보기 아이콘 활성화, 미입력시 비활성화
