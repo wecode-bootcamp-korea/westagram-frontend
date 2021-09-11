f711f5cf857d353a6d4b755bc6ffc00b8c018eec
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-11 16:08:20 +0900

    Fixed : 로그인 JS 구현 및 리팩토링 완료

--- students/25th/miyeon/js/login.js
+++ students/25th/miyeon/js/login.js
@@ -1,45 +1,46 @@
 // 로그인 인풋값에 값이 있을시 색상 변환
-const inputTag = document.getElementsByTagName('input');
 const idInput = document.getElementById('idInput');
 const pwInput = document.getElementById('pwInput');
 const loginBtn = document.getElementsByClassName('loginGo')[0];
 
-idInput.addEventListener('keyup', btnClick);
-pwInput.addEventListener('keyup', btnClick);
+idInput.addEventListener('keyup', inputKeyboardEvent);
+pwInput.addEventListener('keyup', inputKeyboardEvent);
+loginBtn.addEventListener('click', loginBtnClick);
+document.addEventListener('keyup', (e) => {
+    if (`${e.key}` == 'Enter') {
+        loginBtnClick();
+    }
+}) // enter 입력시 loginBtnClick() 호출
 
-// 아이디 인풋값에 키보드를 눌렀다 뗄 때 실행
-inputTag.addEventListener('keyup', function(event) {
-        // 아이디만 입력하고, 비번을 입력하지 않았을 때 비활성화
-        if (idInput.value.length > 0 && pwInput.value.length == 0) {
-            loginBtn.style.backgroundColor = "#b9def7";
-        }
-
-        // 아이디는 입력하지 않고, 비번만 입력했을 때 비활성화
-        if (idInput.value.length == 0 && pwInput.value.length > 0) {
-            loginBtn.style.backgroundColor = "#b9def7";
-        }
-
-        // 인풋창에서 백스페이스로 값을 지워웠을 때
-        if (`${event.key}` == 'Backspace') {
-            if (idInput.value.length == 0 && pwInput.value.length == 0) {
-            loginBtn.style.backgroundColor = "#b9def7";
-            }
-        }
-})
-
-
-function btnClick() {
-    if (idInput.value.length > 0 && pwInput.value.length > 0) {
-        loginBtn.style.backgroundColor = "#0095f6";
+// 로그인 버튼 클릭 이벤트
+function loginBtnClick() {
+    if (idInput.value === "buzzi_nyang" && pwInput.value === "1234") {
+        location.replace("./main.html");
+    } else if(idInput.value.length == 0 && pwInput.value.length == 0) {
+        alert("id와 pw 번호를 입력해 주세요.");
+    } else {
+        alert("캣스타그램 계정 주인만 로그인 할 수 있습니다.");
     }
 }
 
+// id, pw 입력값에 따른 버튼 활성화 메서드
+function inputKeyboardEvent() {
+    // id값과 pw값이 있으면 버튼 활성화
+    if (idInput.value.length > 0 && pwInput.value.length > 0) {
+        loginBtn.style.backgroundColor = "#0095f6";
+    }
+    // id값은 없지만, pw값이 있으면 버튼 비활성화
+    if (idInput.value.length == 0 && pwInput.value.length > 0) {
+        loginBtn.style.backgroundColor = "#b9def7";
+    }
+    // id값은 있지만, pw값이 없음녀 버튼 비활성화
+    if (idInput.value.length > 0 && pwInput.value.length == 0) {
+        loginBtn.style.backgroundColor = "#b9def7";
+    }
+    
+}
 
-// 특정 아이디와 비번 값이어야만 로그인이 되도록(메인페이지로 이동 가능하게)
-
-
-
-// 비밀번호 잊으셨나요 버튼 클릭시 prompt창으로 핸드폰 번호 기입 
-
-
-// (idWhat && pwWhat) ? cka : 거짓
+// 비밀번호 잊으셨나요 버튼 클릭시 메서드 
+function phoneNumberCheck() {
+    alert("잘 떠올려 보세요! 계정 주인만 가입할 수 있습니다!");
+}
