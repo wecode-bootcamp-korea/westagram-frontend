d95dd8be3d449512d0459deca3896c5fb99257b5
이선호 <brown@iseonhoui-MacBook-Air.local>
2021-09-13 09:57:49 +0900

    메인페이지 댓글기능 수정중

--- students/25th/sunho/westagram/js/login.js
+++ students/25th/sunho/westagram/js/login.js
@@ -1,11 +1,15 @@
+"use strict"
+
 const id = document.getElementById("userId");
 const pw = document.getElementById("password")
-const btn = document.getElementsByTagName("button")[0];
+const button = document.getElementsByTagName("button")[0];
 const inputTag = document.getElementsByTagName("input")[0];
 
 id.addEventListener('keyup',checkPassword);
 password.addEventListener('keyup',checkPassword);
 
 function checkPassword() {
-    id.value.indexOf('@') >= 0 && password.value.length >= 5 ? btn.disabled= false : btn.disabled=true;
+    id.value.indexOf('@') >= 0 && password.value.length >= 5 ? button.disabled= false : button.disabled=true;
 }
+
+
--- students/25th/sunho/westagram/js/main.js
+++ students/25th/sunho/westagram/js/main.js
@@ -1,6 +1,29 @@
-const search = document.getElementById('search');
+"use strict"
 
-function inputTxt(){
-    document.getElementById('searchTxt').value = 'search';
+const commentInput = document.getElementsByClassName('post')[0];
+const submit = document.getElementsByClassName('submit')[0];
+
+commentInput.addEventListener('keyup',submitBtn);
+
+function submitBtn(){
+    commentInput.value.length !== 0 ? submit.disabled = false : submit.disabled = true;
 }
-search.addEventListener('click',inputTxt )
+/* ---------------------------------------- */
+/* 댓글 생성 */
+/* ---------------------------------------- */
+
+function enterComment() {
+    const comment = document.getElementsByClassName('comment')[0];
+    const newComment = document.createElement('li');
+    newComment.innerHTML = `<b>dltjsgho</b>`+ commentInput.value;
+
+    comment.appendChild(newComment);
+    commentInput.value= null;
+}
+
+commentInput.addEventListener('keyup',function(e){
+    if(e.code === 'Enter') {
+        enterComment();
+    }
+})
+
