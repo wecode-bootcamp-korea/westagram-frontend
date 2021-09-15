011b42b26a108950f989fe4066a2fb6f5f244ba7
yonghyun <dydgus0421@gmail.com>
2021-09-15 09:28:47 +0900

    메인 페이지 추가 기능 구현중

--- students/25th/yonghyun/westa/main.js
+++ students/25th/yonghyun/westa/main.js
@@ -1,11 +1,39 @@
+const WriteBtn = document.getElementById('writeBtn');
+const EnterBtn = document.getElementById('writeComment');
+const addR = document.getElementById('addR');
+
 function addMessage() {
-    const makeP = document.createElement('p');
+    if(EnterBtn.value){
+    const makeP = document.createElement('li');
     const message = document.getElementById('writeComment').value;
-    const firstP = '<a href="" class="re_id">wecode_bootcamp</a><span class="re_p">';
-    const lastP = '</span><span class="deletespan"><button class="deleteBtn"><i class="fas fa-ellipsis-h"></i></button></span><span><button class="re_heart"><i class="far fa-heart"></i></button></span>'
+    const firstP = `<a href="" class="re_id">wecode_bootcamp</a>
+    <span class="re_p">`;
+    const lastP = `</span>
+    <span class="deletespan">
+    <button class="deleteBtn"><i class="fas fa-ellipsis-h"></i></button>
+    </span>
+    <span class="delete">x</span><button class="re_heart"><i class="far fa-heart"></i></button>`
 
     makeP.innerHTML = `${firstP}${message}${lastP}`;
-    document.getElementById('other_re').appendChild(makeP);
+    addR.appendChild(makeP);
+    reset();   
+    }
+    
+}
+
+function keyEnter() {
+    if(window.event.keyCode==13){
+        addMessage();
+    }
+}
+
+WriteBtn.addEventListener('click',addMessage);
+EnterBtn.addEventListener('keydown',keyEnter);
+
+
+function reset() {
+    const message = document.getElementById('writeComment');
+    message.value = null;
 }
 
 function heartChange() {
@@ -13,14 +41,19 @@ function heartChange() {
     const emptyH = document.querySelector('#emptyheart');
     const redH = document.querySelector('#redheart');
     
-    // if(emptyH.style.color = 'transparent'){
-        // emptyH.style.color = 'black';
-        // redH.style.color = 'transparent';
-    // }else if(emptyH.style.color = 'red'){
         emptyH.style.color = 'transparent';
         redH.style.color = 'red';
-    // }
 }
+
+const deleteIcon = document.querySelector('.delete')
+const manyLine = document.querySelector('.feedComment')
+
+function eraseLine() {
+    const thisLine = manyLine.querySelector('li');
+    thisLine.remove();
+}
+
+deleteIcon.addEventListener('click', eraseLine);
 
 // const clickE = document.querySelector('.erase');
 // const asd = document.getElementsByClassName('re_p');
@@ -30,3 +63,26 @@ function heartChange() {
 // }
 
 // clickE.addEventListener('click', removeP);
+
+// const makeBtn = document.querySelector('.erase');
+// const comment = document.querySelector('.deleteLine');
+
+// makeBtn.addEventListener("click", (event) => {
+       
+//     const btn = event.target; 
+//     console.log(btn);  //span erase
+    
+//     const deleteAll = btn.parentNode; 
+//     console.log(deleteAll); //p asdf
+//     // comment.removeChild(deleteAll); 
+// })
+
+// function deletesome(){
+//     const btn = document.querySelector('.erase'); 
+//     const comment = document.querySelector('.deleteLine');
+//     // console.log(btn);  //span erase
+    
+//     const deleteAll = btn.parentNode; 
+//     console.log(deleteAll); //p asdf
+//     // comment.removeChild(deleteAll); 
+// }
