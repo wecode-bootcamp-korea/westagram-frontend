6c0e6f98562128b5caa6b91216deeaaa86ef7b7e
Younsoo Tom Lim <tomandlim@gmail.com>
2021-10-14 20:44:30 +0900

    [미션1/2/3] 완료 - 로그인페이지 레이아웃 완료 - ID, PW 입력 시 로그인 버튼 활성화 기능 완료 - 메인페이지 레이아웃 완료
    Please enter the commit message for your changes. Lines starting

--- students/26th/younsoo26/assets/js/main.js
+++ students/26th/younsoo26/assets/js/main.js
@@ -2,15 +2,36 @@ const loginBttn = document.getElementById('submit');
 const thisIsUser = document.getElementById('username');
 const thisIsPassword = document.getElementById('password');
 
-const activateBttn = () => {
-    if(thisIsPassword.value && thisIsUser.value){
-        loginBttn.classList.add("login-activate");
-    }else{
-        loginBttn.classList.remove("login-activate");
-    }
+let inputUserID;
+let inputPasswords;
+
+
+const userIDChecker = e => {
+    inputUserID = e.target.value;
+    activateSubmit();
 }
 
-thisIsUser.onkeyup = activateBttn;
-thisIsPassword.onkeyup = activateBttn;
+const passwordChecker = e => {
+    inputPasswords = e.target.value;
+    activateSubmit();
+}
+
+const activateSubmit = () => {
+    inputUserID&&inputPasswords ? loginBttn.classList.add("login-activate") : loginBttn.classList.remove("login-activate");
+}
+
+thisIsUser.oninput = userIDChecker;
+thisIsPassword.oninput = passwordChecker;
+
+// const activateBttn = () => {
+//     if(thisIsPassword.value && thisIsUser.value){
+//         loginBttn.classList.add("login-activate");
+//     }else{
+//         loginBttn.classList.remove("login-activate");
+//     }
+// }
+
+// thisIsUser.onkeyup = activateBttn;
+// thisIsPassword.onkeyup = activateBttn;
 
 // console.log(thisIsPassword)
