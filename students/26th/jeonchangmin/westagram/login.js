21d4b743b5d0635ccddaaf68f5f1e35f9a95c0a4
ChangMin <cckdals222@gmail.com>
2021-10-14 15:22:58 +0900

    Add: [Misson 1, 2] 로그인 페이지 완료 - 로그인 페이지 레이아웃 작성완료 - 로그인 페이지 유효성 작성완료 - 로그인 버튼 로직 작성완료

---
+++ students/26th/jeonchangmin/westagram/login.js
@@ -0,0 +1,25 @@
+const inputId = document.querySelector('.loginId');
+const inputPassword = document.querySelector('.loginPassword');
+const button = document.querySelector('button');
+const inputs = document.querySelectorAll('input');
+
+// loginId.addEventListener('keyup', checkInput);
+// loginPassword.addEventListener('keyup', checkInput);
+inputs.forEach((inputTag) => {
+    inputTag.addEventListener('keyup', checkInput);
+});
+
+function checkInput () {
+    let IdLength = inputId.value.length;
+    let passwordLength = inputPassword.value.length;
+
+    if(IdLength > 0 && passwordLength > 0) {
+        button.style.opacity = 1;
+        button.style.cursor = 'pointer';
+    } else {
+        button.style.opacity = 0.5;
+        button.style.cursor = 'default';
+    }
+    // console.log(`IdLength:${IdLength}`);
+    // console.log(`passwordLength:${passwordLength}`);
+}
