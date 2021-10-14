51d1cedb380dc250cc3adf51b79f95382a6aaf7d
saeyoung <syoungbb25@gmail.com>
2021-10-14 15:38:02 +0900

    arecord

---
+++ students/26th/Jangsaeyoung/wewe.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const thisUser = document.getElementById('num');
+const thisPwd = document.getElementById('pwd');
+const thisBtn = documnet.getElementsByClass('Btn');
+
+thisUser.addEventListener('change', function(event){
+   const inUser = document.getElementById('num').Value;
+   const inPwd = document.getElementById('pwd').Value;
+
+   constinUser = document.getElementById("Button").disabled = true;)
+
+
+
+
+
+
+})
