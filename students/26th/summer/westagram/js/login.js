02f8b1b9f9f77e52325aa06997d1e6e89ac8c87d
Summer Kim <sunmer6@gmail.com>
2021-10-14 15:23:38 +0900

    Add: [Mission 1,2] 로그인 페이지 완료 - 로그인 페이지 레이아웃 작성 완료 - 로그인 페이지 ID, PW 입력 시 로그인 버튼 활성화 기능 완료

---
+++ students/26th/summer/westagram/js/login.js
@@ -0,0 +1,32 @@
+const idTag = document.getElementById('id');
+const pwTag = document.getElementById('pw');
+const loginBtn = document.getElementById('loginBtn')
+
+let id = '';
+let pw = '';
+
+const onInputId = (e) => {
+  id = e.target.value;
+}
+
+const onInputPw = (e) => {
+  pw = e.target.value;
+  if (!id || !pw) {
+    loginBtn.style.opacity = "0.4"
+    return;
+  }
+  loginBtn.style.opacity = "1"
+}
+
+const onClickButton = () => {
+  if (!id || !pw) {
+    alert('id 와 pw를 입력해주세요')
+    return;
+  }
+  alert('로그인 되었습니다.')
+}
+
+idTag.addEventListener('input', onInputId);
+pwTag.addEventListener('input', onInputPw);
+loginBtn.addEventListener('click', onClickButton);
+
