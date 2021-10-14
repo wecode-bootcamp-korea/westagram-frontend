b9f508a74fd44478cde9e6235e56cc8f4b7d3ae5
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-10-15 02:44:33 +0900

    [Mission 4]댓글 내용 입력 후 Enter press, 혹은 게시 버튼 클릭 시 댓글 추가 - click 및 keyup 이벤트 후속 처리 - 정해진 댓글 갯수만큼 랜더링 처리

---
+++ students/26th/byeonghyeon/js/login.js
@@ -0,0 +1,19 @@
+const inputContainer = document.querySelector('.login-input-container');
+const inputs = document.querySelectorAll('.login-input');
+const loginBtn = document.querySelector('.login-btn');
+
+(function app() {
+    let isValue = false;
+
+    inputContainer.addEventListener('keyup', (event) => {
+        if(!event.target.classList.contains('login-input')) return
+        inputs.forEach(elem => {
+            elem.value !== '' ? isValue = true : isValue = false
+            // if(elem.value !== '') isValue = true;
+            // else isValue = false;
+        })
+        if(isValue) loginBtn.classList.add('active');
+        else loginBtn.classList.remove('active');
+    })    
+})();
+
---
+++ students/26th/byeonghyeon/js/main.js
@@ -0,0 +1,71 @@
+const inputContainer = document.querySelector('.comment-input-container');
+const comments = document.querySelector('.comment-list');
+const input = document.querySelector('.comment-input');
+const isTime = document.querySelector('.feed-bottom-date');
+const commentBtn = document.querySelector('.post-comment-btn');
+
+const users = ['apple_90', 'orange_97', 'grape_30', 'melon_30', 'mango_78'];
+let id = 0;
+
+function onAdd(target) {
+    if(target !== 'BUTTON' && target !== 'INPUT') return;
+    if(id === 2) onUpload();
+
+    const text = input.value;
+
+    if(text !== '') {
+        const item = createItem(text);
+        comments.append(item);
+
+    } else return;
+
+    input.value = '';
+    input.focus();
+    commentBtn.classList.remove('active');
+}
+
+function createItem(text) {
+    const itemRow = document.createElement('li');
+    itemRow.classList.add('comment-item');
+    itemRow.setAttribute('data-id', id);
+
+    let index = Math.floor(Math.random() * users.length);
+
+    itemRow.innerHTML = `
+    <div class="comment">
+        <a href="#" class="feed-bottom-account profile-name">${users[index]}</a>
+        <span class="comment-content">${text}</span>
+    </div>
+        <button><img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" alt="heart" class="comment-heart"></button>
+    `
+
+    id++;
+
+    return itemRow;
+}
+
+function onUpload() {
+    let items = Array.from(comments.children);
+    items.forEach(elem => {
+        if(elem.dataset.id === '0') comments.removeChild(elem);
+        else elem.setAttribute('data-id', 0);
+    })
+
+    id--;
+}
+
+inputContainer.addEventListener('click', (event) => {
+    let target = event.target.nodeName;
+    onAdd(target);
+});
+
+inputContainer.addEventListener('keyup', (event) => {
+    let target = event.target.nodeName;
+    let checkText = event.target.value;
+
+    if(checkText !== '') commentBtn.classList.add('active');
+    else commentBtn.classList.remove('active');
+    
+    const keycode = event.keyCode;
+    if(keycode === 13) onAdd(target);
+})
