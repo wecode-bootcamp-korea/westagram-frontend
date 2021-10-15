18c57caf0c179035d4c9e12be9cf853738c3e37c
standup8 <borapark13@gmail.com>
2021-10-15 22:46:56 +0900

    [modified] 메인페이지 필수구현사항 구현 완료 1. 메인페이지 CSS 레이아웃 수정 완료 2. 메인페이지 button javascript 구현 및 유효성검사 완료

--- students/26th/bora/westagram/js/main.js
+++ students/26th/bora/westagram/js/main.js
@@ -1,1 +1,38 @@
 //main.js
+
+const input = document.querySelector('.chatBox');
+const msg = document.querySelector('.msg');
+const btn = document.querySelector('.uploadBtn');
+
+//1. 게시 버튼 활성화
+const writeChat = e => {
+    btn.style.opacity = 0.4;
+
+    if(input){    
+        btn.style.opacity = 1;
+    } else {
+        btn.style.opacity = 0.4;
+    }
+}
+input.addEventListener("input",writeChat);
+
+//2. chatBox에 입력해서 게시버튼 클릭하면 요소가 추가되게 하기
+const addChat = e => {
+    e.preventDefault();
+
+    const newCommentId = document.createElement('p');
+    newCommentId.classList.add('userId');
+    newCommentId.innerHTML = `bori9412`;
+    
+    const newCommentTxt = document.createElement('p');
+    newCommentTxt.classList.add('msg');
+    newCommentTxt.innerHTML = input.value;
+    
+    document.querySelector('.new').appendChild(newCommentId);
+    document.querySelector('.new').appendChild(newCommentTxt);
+
+    //삭제버튼 추가하기
+
+}
+
+btn.addEventListener("click", addChat);
