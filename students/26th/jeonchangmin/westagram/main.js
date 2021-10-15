651a331f44c43e12018eb574afb9433096efb657
ChangMin <cckdals222@gmail.com>
2021-10-15 17:33:36 +0900

    Misson6 삭제기능 구현은 했지만 상당히 코드가 복잡함 댓글 좋아요 기능은 구현하지 못함

--- students/26th/jeonchangmin/westagram/main.js
+++ students/26th/jeonchangmin/westagram/main.js
@@ -4,7 +4,7 @@ const posting = document.querySelector('.upload');
 
 input.addEventListener('keyup', e => {
     const textLength = input.value.length;
-    if(e.keyCode === 13) {
+    if(e.keyCode === 13 && textLength > 0) {
         addComment();
         return;
     }
@@ -28,9 +28,34 @@ function addComment() {
     posting.style.opacity = 0.3;
     posting.style.cursor = 'default';
     const text = input.value;
+    const span = document.createElement('span');
+    span.innerHTML = text;
+    const i = document.createElement('i');
+    i.className = 'far fa-heart commentHeart';
+    const div = document.createElement('div');
+    div.className = 'commentFirst';
+    div.appendChild(span);
+    div.appendChild(i);
+    const button = document.createElement('button');
+    button.className = 'deleteButton'
+    button.innerHTML = '삭제';
     const li = document.createElement('li');
-    li.innerHTML = text;
     li.className = 'comment';
+    li.appendChild(div);
+    li.appendChild(button);
     ul.appendChild(li);
+    // const commentHearts = document.querySelectorAll('.commentHeart');
+    // commentHearts.forEach(commentHeart => {
+    //     commentHeart.addEventListener('click', () => {
+    //         commentHeart.style.color = 'red';
+    //         console.log('heart red is done');
+    //     })
+    // });
+    button.addEventListener('click', () => {
+        const comment = button.closest('.comment');
+        comment.remove();
+    });
+
     input.value = '';
 }
+
