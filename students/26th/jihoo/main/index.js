e93fccd3beee425f9bed6e0d9fb3381f4e7ff165
정지후 <loveadsadf1976@gmail.com>
2021-10-17 22:46:40 +0900

    main.js에서input에 값을 넣으면 게시 버튼이 활성화되기를 추가했다.

--- students/26th/jihoo/main/index.js
+++ students/26th/jihoo/main/index.js
@@ -10,11 +10,10 @@ function checkInput() {
         registerComment(commentInput.value);
     }
 }
-
 function registerComment(value){
     const commentLists = document.getElementById("commentLists");
     const newCommentList = document.createElement("li");
-    const newComment = `<span class="name">we-dohdkjfd</span><span>${value}</span><span class="delete">x</span>`
+    const newComment = `<span class="name">we-dohdkjfd</span><span>${value}</span><span class="delete">X</span>`
     
     newCommentList.innerHTML = newComment;
     deleteComment(newCommentList);
@@ -33,3 +32,30 @@ const init = () => {
 };
 
 init();
+
+const inputId = document.querySelector("#name");
+const button = document.querySelector('.upload');
+
+function commentBtn() {
+    let idValu = inputId.value;
+
+    if(idValu.length > 0) {
+        button.disabled = false;
+        button.style.cursor = 'pointer';
+        button.style.color = '#1c7ed6';
+    } else {
+        button.disabled = true;
+        button.style.cursor = 'default';
+    }
+}
+
+inputId.addEventListener('keyup', commentBtn);
+// const postBtn = document.querySelector(".upload")[0];
+// const postInput = document.querySelector("#name")[0];
+// postInput.addEventListener("input", function() {
+//   const commentPost = postInput.value;
+//   commentPost.length > 0 ?
+//   postBtn.disabled = false :
+//   postBtn.disabled = true;
+// });
+
