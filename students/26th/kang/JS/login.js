1faa3cc78d6c754e375b9ea6feff02934ebb0124
betterplaywon <kkgi1234@gmail.com>
2021-10-18 10:14:01 +0900

    좋아요 버튼 기능 구현 중

--- students/26th/kang/JS/login.js
+++ students/26th/kang/JS/login.js
@@ -9,7 +9,7 @@ function loginColor() {
   const idValue = document.getElementById('idBox').value;
   const pwValue = document.getElementById('pwBox').value;
   const userValue = idValue.indexOf('@') !== -1 && pwValue.length >= 5;
-  userValue ? (loginBtn.style.opacity = 1) : (loginBtn.style.opacity = 0.3);
+  userValue ? (loginBtn.style.background = '#0098fd') : (loginBtn.style.background = '#8fd2ff');
 }
 
 user.addEventListener('input', loginColor);
--- students/26th/kang/JS/main.js
+++ students/26th/kang/JS/main.js
@@ -4,6 +4,8 @@ const commentInput = document.getElementById('comment');
 const commentBtn = document.getElementById('posting-btn');
 const commentRipple = document.getElementById('area');
 
+// 1. 엔터키 || 게시 버튼 클릭으로 댓글 달기 기능 구현
+
 commentBtn.addEventListener('click', function () {
   if (commentInput.value !== '') {
     let newComment = document.createElement('li');
@@ -22,3 +24,16 @@ function enterPress() {
   }
 }
 commentInput.addEventListener('keyup', enterPress);
+
+// 2. 좋아요 클릭으로 색 넣고 빼기 구현중
+
+const heart = document.getElementById('heart-bck');
+
+function redHeart() {
+  if ((heart.style.background = 'none')) {
+    heart.style.background = 'tomato';
+  }
+  return (heart.style.background = 'none');
+}
+
+heart.addEventListener('click', redHeart);
