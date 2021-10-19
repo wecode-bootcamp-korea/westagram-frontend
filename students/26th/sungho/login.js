6ae2d2a4a2fc7dbbd45077230073a294eec5d624
양성호 <sungho@yangseonghoui-MacBookAir.local>
2021-10-19 12:48:17 +0900

    Add : 셀프 리팩토링 완료

--- students/26th/sungho/login.js
+++ students/26th/sungho/login.js
@@ -2,7 +2,7 @@ let id1 = document.querySelector(".id")
 let pw1 = document.querySelector(".pw")
 let butt = document.querySelector(".button")
 const loginForm = document.getElementById('loginForm');
-//
+
 loginForm.addEventListener('input' ,function() {
     const idVal = id1.value;
     const pwVal = pw1.value;
--- students/26th/sungho/main.js
+++ students/26th/sungho/main.js
@@ -2,15 +2,12 @@ const commentIn = document.querySelector(".commentIn");
 const commentOut = document.querySelector(".commentOut");
 const first = document.querySelector(".firstComment");
 
-
 commentIn.addEventListener('keydown',function () {
     if (event.keyCode==13){
-        event.preventDefault(); //기본 이벤트 발생을 막아주는 메소드
+        event.preventDefault(); 
         const comment = document.createElement("p");
         const comment99 = commentIn.value;
         comment.innerHTML=comment99;
         commentOut.appendChild(comment);
-        
     }
-
 })
