92ab89d0cc79882db7cfee6fd446ad8088c091e8
양성호 <sungho@yangseonghoui-MacBookAir.local>
2021-10-19 14:01:25 +0900

    Add : 6번 삭제까지 구현

--- students/26th/sungho/main.js
+++ students/26th/sungho/main.js
@@ -1,13 +1,23 @@
 const commentIn = document.querySelector(".commentIn");
 const commentOut = document.querySelector(".commentOut");
-const first = document.querySelector(".firstComment");
+const commentWrite = document.querySelector(".commentWrite")
 
 commentIn.addEventListener('keydown',function () {
     if (event.keyCode==13){
         event.preventDefault(); 
-        const comment = document.createElement("p");
-        const comment99 = commentIn.value;
-        comment.innerHTML=comment99;
-        commentOut.appendChild(comment);
+        const comment = document.createElement("li");
+        const commentValue = commentIn.value;
+        comment.innerHTML= `<span><strong>성호님&nbsp&nbsp&nbsp&nbsp</strong></span>` + commentValue;
+        const button = document.createElement('button');
+        button.innerHTML = "x"
+        comment.appendChild(button);
+        deletebutton(button, comment);
+        commentWrite.appendChild(comment);
     }
 })
+
+function deletebutton(button, comment) {
+    button.addEventListener('click', () => {
+        comment.remove();
+    })
+}
