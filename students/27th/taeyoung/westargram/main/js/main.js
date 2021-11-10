34f79c51093da722709c6e67ed5f0fccd885e319
Moro-yong <leta@Taeyoungui-MacBookPro.local>
2021-11-10 15:57:31 +0900

    add : westargram main page

---
+++ students/27th/taeyoung/westargram/main/js/main.js
@@ -0,0 +1,21 @@
+const getRecomment = document.querySelector('.recommnetInput');
+const recommentButton = document.querySelector('.recommnetBt');
+const commentBox = document.querySelector('.feedCommentLeft');
+
+
+function getComment() {
+    
+};
+
+
+
+
+
+//버튼, 인풋, 댓글 작성칸, 
+
+//키보드 input의 value를 받아오기
+// 게시버튼 또는 엔터를 눌렀을 때 
+//value의 값을 특정 위치에 추가
+
+// 댓글의 value가 없다면
+// 아무런 기능을 하지 않는다.
