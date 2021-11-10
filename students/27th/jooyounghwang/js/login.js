00e2780179032484ec6f5e695ba2fb4d74b10205
JooyoungHwnag <71796608+JooyoungHwnag@users.noreply.github.com>
2021-11-10 15:17:01 +0900

    Add: [Misson 1] 로그인 페이지 추가   -로그인 페이지 레이아웃 작성   -로그인 페이지 유효성 검사 기능 작성

---
+++ students/27th/jooyounghwang/js/login.js
@@ -0,0 +1,14 @@
+const loginWrap = document.querySelector('.loginWrap');
+const loginInput = document.querySelector('.loginInput');
+const passwordInput = document.querySelector('.passwordInput');
+const buttonDefault = document.querySelector('.btnDefalut');
+
+function handleLogin(){
+  const loginValue = loginInput.value;  
+  const passwordValue = passwordInput.value;
+  loginValue.length > 0 && passwordValue.length > 0 
+  ? buttonDefault.classList.add('btnChageBlue')
+  : buttonDefault.classList.remove('btnChageBlue');
+}
+
+loginWrap.addEventListener('keyup', handleLogin);
