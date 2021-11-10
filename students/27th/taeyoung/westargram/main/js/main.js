848a7e009fbfcf719f97726997f2afc0f8e053c2
Moro-yong <leta@Taeyoungui-MacBookPro.local>
2021-11-10 22:50:47 +0900

    Add : main.js - 댓글 달기 기능 추가 (button click and Enter key press)
    Modify : main.html, style.css
    - 댓글 기능을 위해 태그 및 스타일 변경

--- students/27th/taeyoung/westargram/main/js/main.js
+++ students/27th/taeyoung/westargram/main/js/main.js
@@ -1,14 +1,67 @@
 const getRecomment = document.querySelector('.recommnetInput');
 const recommentButton = document.querySelector('.recommnetBt');
-const commentBox = document.querySelector('.feedCommentLeft');
+const commentBox = document.querySelector('.feedComment');
+
+// 엔터 눌렀을 때 댓글 달기
+function getComment(e) {
+
+    if (e.keyCode === 13) {
+        
+        let container = document.createElement('div'); // 다음 댓글 전체 영역
+        let nickname = document.createElement('span'); // 작성자 이름 영역
+        let comment = document.createElement('span'); // 댓글 영역
+        let time = document.createElement('span'); // 시간 영역
 
 
-function getComment() {
-    
+        commentBox.appendChild(container); //commentBox에 div를 자식요소로 넣는다
+        container.appendChild(nickname); // Container에 닉네임을 자식요소로
+        container.appendChild(comment); // comment 닉네임을 자식요소로
+        container.appendChild(time); // Container에 타임을 자식요소로
+
+        nickname.innerHTML = "taeyong";
+        comment.innerHTML = getRecomment.value;
+        time.innerHTML = "1시간 전";
+
+        container.className = "feedCommentLeft";
+        nickname.className = "feedUserName";
+        comment.className = "moreComment";
+        time.className = "feedTime";
+
+        getRecomment.value = '';
+    }
 };
 
+// 게시버튼 눌렀을 때 댓글 달기
+function gettComment() {
+
+    if (getRecomment) {
+        
+        let container = document.createElement('div'); // 다음 댓글 전체 영역
+        let nickname = document.createElement('span'); // 작성자 이름 영역
+        let comment = document.createElement('span'); // 댓글 영역
+        let time = document.createElement('span'); // 시간 영역
 
 
+        commentBox.appendChild(container); //commentBox에 div를 자식요소로 넣는다
+        container.appendChild(nickname); // Container에 닉네임을 자식요소로
+        container.appendChild(comment); // comment 닉네임을 자식요소로
+        container.appendChild(time); // Container에 타임을 자식요소로
+
+        nickname.innerHTML = "taeyong";
+        comment.innerHTML = getRecomment.value;
+        time.innerHTML = "1시간 전";
+
+        container.className = "feedCommentLeft";
+        nickname.className = "feedUserName";
+        comment.className = "moreComment";
+        time.className = "feedTime";
+
+        getRecomment.value = '';
+    }
+};
+
+getRecomment.addEventListener('keyup', getComment);
+recommentButton.addEventListener('click', gettComment);
 
 
 //버튼, 인풋, 댓글 작성칸, 
