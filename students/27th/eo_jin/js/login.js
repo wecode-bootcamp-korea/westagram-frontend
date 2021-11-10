6e3b1693633c02d7fa9c73c5294518a227903c26
eojine94 <eojine294@gmail.com>
2021-11-10 15:18:44 +0900

    Add: [Mission 1 finished and Mission 2 ing]

---
+++ students/27th/eo_jin/js/login.js
@@ -0,0 +1,31 @@
+const inputId = document.querySelector(".inputId")
+const inputPwd = document.querySelector(".inputPwd");
+const loginBtn = document.querySelector(".loginBtn");
+let idAndPwd = [false, false];
+
+// function declaration
+function activeBtn(){
+  if(idAndPwd[0] && idAndPwd[1]){
+    loginBtn.classList.add("active")
+  }else {
+    loginBtn.classList.remove("active")
+  };
+}
+
+function handleValidInandPwd(idx, el){
+  idAndPwd[idx] = el.value;
+  if(!idAndPwd[idx]){
+    idAndPwd[idx] = false;
+  };
+}
+
+// actual event handler
+inputId.addEventListener("input", () => {
+  handleValidInandPwd(0, inputId);
+  activeBtn();
+})
+
+inputPwd.addEventListener("input", () => {
+  handleValidInandPwd(1, inputPwd);
+  activeBtn();
+})
