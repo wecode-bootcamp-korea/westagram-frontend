52b04e20a293741f7ff9cdfd7641eed19d7fa559
성현Maria 김 <globalkmaria@gmail.com>
2021-11-10 21:06:19 +0900

    [add]id, pw validation

--- students/27th/sunghyun/js/login.js
+++ students/27th/sunghyun/js/login.js
@@ -1,16 +1,36 @@
 const id = document.querySelector('#id');
 const password = document.querySelector('#password');
 const loginBtn = document.querySelector('.loginBtn');
+const loginForm = document.querySelector('.inputWapper');
 
-id.addEventListener('keyup', checkloginForm);
-password.addEventListener('keyup', checkloginForm);
-
-function checkloginForm() {
+function checkLoginForm() {
   const allchecked = !!id.value && !!password.value;
 
   allchecked
     ? loginBtn.classList.remove('loginBtn--disable')
     : loginBtn.classList.add('loginBtn--disable');
-
   loginBtn.disabled = !allchecked;
 }
+function checkLoginValidation() {
+  const idvalid = id.value.includes('@');
+  const pwdvalid = password.value.length >= 5;
+  if (idvalid && pwdvalid) {
+    alert(`로그인 되었습니다.`);
+    loginForm.reset();
+    return;
+  }
+
+  let alertText = '';
+  !idvalid && (alertText += `아이디에 '\@'이 포함되어야 합니다.`);
+  !pwdvalid && (alertText += ` 비밀번호는 5자리 이상이어야 합니다.`);
+
+  alert(alertText);
+}
+
+id.addEventListener('keyup', checkLoginForm);
+password.addEventListener('keyup', (e) => {
+  checkLoginForm();
+  e.key === 'Enter' && checkLoginValidation();
+});
+
+loginBtn.addEventListener('click', checkLoginValidation);
