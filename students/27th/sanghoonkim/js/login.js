5ba1b50d923c8a6f49bdd116cfb4e2528725baf0
SangHoon <joa3668@gmail.com>
2021-11-11 17:53:46 +0900

    Add: 로그인 유효성검사, 메인페이지 댓글 추가 기능 구현

--- students/27th/sanghoonkim/js/login.js
+++ students/27th/sanghoonkim/js/login.js
@@ -1,7 +1,21 @@
-'use strict';
+const loginForm = document.querySelector('.loginForm');
 
-const userId = document.querySelector('.userId');
-const password = document.querySelector('.password');
-const loginBtn = document.querySelector('.loginBtn');
+function loginOn() {
+    const userId = document.querySelector('.userId').value;
+    const password = document.querySelector('.password').value;
+    const loginBtn = document.querySelector('.loginBtn');
 
+    if (userId.indexOf('@') > -1 && password.length >= 5) {
+        loginBtn.disabled = false;
+        loginBtn.style.backgroundColor = '#0095f6';
+        loginBtn.style.cursor = 'pointer';
+    } else if (userId.length === 0 || password.length < 5) {
+        loginBtn.disabled = true;
+        loginBtn.style.backgroundColor = '#b2dffc';
+        loginBtn.style.cursor = 'default';
 
+    }
+
+}
+
+loginForm.addEventListener('keyup', loginOn);
--- students/27th/sanghoonkim/js/main.js
+++ students/27th/sanghoonkim/js/main.js
@@ -4,32 +4,29 @@ const commentInput = document.querySelector('.commentInput');
 const submitBtn = document.querySelector('.submitBtn');
 const ulBox = document.querySelector('.commentBox');
 
-// function addComment() {
-//     console.log(commentInput.value);
-//     const li = document.createElement('li');
-//     const id = document.createElement('span');
-//     const comment = document.createElement('span');
-
-//     id.innerHTML = 'Sang_HoOon';
-//     id.setAttribute('class', 'boldBlackName');
-//     comment.innerHTML = commentInput.value;
-//     comment.setAttribute('class', 'comment');
-//     li.appendChild(id);
-//     li.appendChild(comment);
-//     ulBox.appendChild(li);
-// }
-
-submitBtn.addEventListener('click', () => {
-
+function addComment() {
     const li = document.createElement('li');
     const id = document.createElement('span');
     const comment = document.createElement('span');
 
+    if (!commentInput.value) {
+        return;
+    };
+
     id.innerHTML = 'Sang_HoOon';
     id.setAttribute('class', 'boldBlackName');
-    comment.setAttribute('class', 'comment');
     comment.innerHTML = commentInput.value;
+    comment.setAttribute('class', 'comment');
     li.appendChild(id);
     li.appendChild(comment);
     ulBox.appendChild(li);
+    commentInput.value = "";
+}
+
+submitBtn.addEventListener('click', addComment);
+
+commentInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        addComment();
+    }
 });
