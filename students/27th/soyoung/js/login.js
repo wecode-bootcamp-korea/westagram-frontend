80b70b0ea6604dd0f8293b5e1b64b52c613f834c
sozeroJung <sozero.jung@gmail.com>
2021-11-11 21:35:30 +0900

    Add: ID, PW 유효성 검사 기능 구현

--- students/27th/soyoung/js/login.js
+++ students/27th/soyoung/js/login.js
@@ -1,16 +1,28 @@
 const userEmail = document.getElementById("userEmail");
 const userPw = document.getElementById("userPw");
 const loginSubmitBtn = document.getElementById("loginSubmitBtn");
+const emailErrorMs = document.getElementById("emailErrorMs");
+const pwErrorMs = document.getElementById("pwErrorMs");
 
 // make login toggle button
-[userEmail, userPw].map((elem) =>
-  elem.addEventListener("input", makeToggleLoginBtn)
-);
-
 function makeToggleLoginBtn() {
   let emailValueLength = userEmail.value.length;
   let pwValueLength = userPw.value.length;
-  let isInputValid = emailValueLength && pwValueLength;
+  let isEmailValid = userEmail.value.includes("@");
+  let isPwValid = pwValueLength >= 5;
+  let isInputValid = isEmailValid && isPwValid;
+
+  if (emailValueLength)
+    isEmailValid
+      ? (emailErrorMs.innerHTML = "")
+      : (emailErrorMs.innerHTML = "이메일에는 @가 있어야합니다.");
+  else emailErrorMs.innerHTML = "";
+
+  if (pwValueLength)
+    isPwValid
+      ? (pwErrorMs.innerHTML = "")
+      : (pwErrorMs.innerHTML = "비밀번호는 5자리 이상이어야합니다.");
+  else pwErrorMs.innerHTML = "";
 
   isInputValid
     ? (loginSubmitBtn.style.backgroundColor = "var(--color-blue)")
@@ -18,3 +30,8 @@ function makeToggleLoginBtn() {
 
   loginSubmitBtn.disabled = !isInputValid;
 }
+
+// Set input addEventListener
+[userEmail, userPw].map((elem) =>
+  elem.addEventListener("input", makeToggleLoginBtn)
+);
