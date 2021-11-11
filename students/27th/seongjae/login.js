3e4d101ec72cc9afedd69dfaa5743629d0822b26
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-11-11 15:53:41 +0900

    로그인 페이지 Refactoring

--- students/27th/seongjae/login.js
+++ students/27th/seongjae/login.js
@@ -2,7 +2,7 @@ const idInput = document.querySelector(".login-box-child-id-input");
 const passwordInput = document.querySelector(
   ".login-box-child-passsword-input"
 );
-const $button = document.querySelector(".login-box-child-btn button");
+const loginButton = document.querySelector(".login-box-child-btn button");
 
 let handleIdInput;
 let handlepasswordInput;
@@ -15,18 +15,16 @@ passwordInput.addEventListener("keyup", (e) => {
   handlepasswordInput = e.target.value;
 
   if (handleIdInput !== "" && handlepasswordInput !== "") {
-    $button.style.backgroundColor = "#0095F6";
+    loginButton.style.backgroundColor = "#0095F6";
   } else {
-    $button.style.backgroundColor = "#c4e1fb";
+    loginButton.style.backgroundColor = "#c4e1fb";
   }
 });
 
-$button.addEventListener("click", () => {
+loginButton.addEventListener("click", () => {
   if (handleIdInput.includes("@") === true && handlepasswordInput.length >= 5) {
     location.href = "main.html";
   } else {
     alert("아이디에 '@'를 포함하시거나, 비밀번호를 5자리 이상 입력해주세요.");
   }
 });
-
-//color: #0095F6
