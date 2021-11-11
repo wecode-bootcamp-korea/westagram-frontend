80dd2d6833486642ff2b17ed89e357e73113771e
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2021-11-11 18:21:47 +0900

    메인페이지 feed & aside Refactoring

--- students/27th/seongjae/main.js
+++ students/27th/seongjae/main.js
@@ -1,21 +1,6 @@
-//input에 적고 , 게시글 클릭 이벤트 하면
-//새로운 댓글 추가
-
-const commentInput = document.querySelector(".feed-article-add-comment");
-const commentBtn = document.querySelector(".feed-article-add-comment-btn");
-const comment = document.querySelector(".comment-article");
-
-/*
-commentInput.addEventListener("keyup", (e) => {
-  //e.target.value = newComment.innerText;
-  commentBtn.addEventListener("click", () => {
-    newComment.innerHTML = e.target.value;
-    newCommentUser.classList.remove("hidden");
-  });
-});
-*/
-
-// 위에는 class로 display속성 none으로 줬다가 이벤트받으면 보이게 하는거
+const commentInput = document.querySelector(".feed-comment-add-input");
+const commentBtn = document.querySelector(".feed-comment-add-btn");
+const comment = document.querySelector(".newcomment");
 
 commentBtn.addEventListener("click", () => {
   let div = document.createElement("div");
@@ -24,17 +9,16 @@ commentBtn.addEventListener("click", () => {
 
   let span = document.createElement("span");
   span.innerHTML = commentInput.value;
-  div.appendChild(span);
+  comment.appendChild(span);
 
   div.style.fontWeight = 650;
   div.style.marginBottom = "5px";
-  span.style.fontWeight = 400;
-  span.style.marginLeft = "5px";
 
   commentInput.value = "";
 });
 
 //위에는 클릭이벤트 아래는 엔터이벤트
+
 commentInput.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     let div = document.createElement("div");
@@ -55,9 +39,9 @@ commentInput.addEventListener("keydown", (e) => {
 });
 
 commentInput.addEventListener("keyup", (e) => {
-  if (e.value !== false) {
-    commentBtn.style.color = "#0095f6";
+  if (!e.target.value) {
+    commentBtn.style.color = "#b9dcfa";
   } else {
-    commentBtn.style.color = "red";
+    commentBtn.style.color = "#0095f6";
   }
 });
