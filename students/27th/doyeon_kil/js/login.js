07dfb41c5869bb20af4512f6ba09a0b2367e123a
sanha <kgpaper@naver.com>
2021-11-11 21:07:20 +0900

    ADD: ID/PW 유효성검사, 댓글 좋아요/삭제 기능, 반응형 스크린 구현 - 정규식을 통한 ID 유효성검사 및 PW 길이 기반 유효성 검사구현으로 버튼 활성화/비활성화 구현하였습니다. - classList.toggle()을 이용한 피드/댓글 좋아요 기능과 confirm()을 이용한 댓글 삭제기능을 구현하였습니다. - 미디어쿼리를 이용하여 화면 가로길이 960px이하일 때 오른쪽 탭을 보이지 않게하는 반응형 스크린을 구현하였습니다.

--- students/27th/doyeon_kil/js/login.js
+++ students/27th/doyeon_kil/js/login.js
@@ -6,11 +6,13 @@ let idCheck = false;
 let pwCheck = false;
 
 idContainer.addEventListener("keyup", () => {
-  idCheck = idContainer.value.length > 0 ? true : false;
+  const regex = /[0-9a-zA-z-_.]*@[0-9a-zA-z-_.]/g;
+
+  idCheck = idContainer.value.match(regex) != null ? true : false;
   buttonAbler.disabled = !(idCheck && pwCheck);
 });
 
 pwContainer.addEventListener("keyup", () => {
-  pwCheck = pwContainer.value.length > 0 ? true : false;
+  pwCheck = pwContainer.value.length > 4 ? true : false;
   buttonAbler.disabled = !(idCheck && pwCheck);
 });
--- students/27th/doyeon_kil/js/main.js
+++ students/27th/doyeon_kil/js/main.js
@@ -1,22 +1,42 @@
 const theForm = document.getElementById("comment-form");
-let userName = "ralo";
+const userId = "ralo";
+
+let commentId = 1;
 
 const inputClear = () => {
   theForm.getElementsByClassName("comment-input")[0].value = "";
 };
 
-theForm.onsubmit = () => {
+const pushComment = () => {
   const message = theForm.getElementsByClassName("comment-input")[0].value;
+  commentId += 1;
 
   const commentContainer = document.getElementsByClassName("comments")[0];
-  const html = `<div class="comment"><label> <a class="user-name" href="#">${userName}</a>&nbsp;<span>${message}</span> </label><button><i class="far fa-heart"></i></button></div>`;
+  const newComment = `<div id="comment-${commentId}" class="comment">
+  <label> <a class="user-name" href="#">${userId}</a>&nbsp;<span>${message}</span> </label>
+  <label>
+    <button id="comment-like-button-${commentId}" class="comment-like-button" onclick="commentLikeToggle(this)"><i class="far fa-heart"></i></button>
+    <button id="comment-delete-button-${commentId}" class="comment-delete-button" onclick="deleteComment(this)"><i class="fas fa-trash"></i></button>
+  </label>
+</div>`;
 
-  commentContainer.innerHTML += html;
+  commentContainer.innerHTML += newComment;
   inputClear();
 };
 
-theForm.addEventListener("keydown", (e) => {
-  if (e.keyCode === 13) {
-    theForm.onsubmit();
+const feedLikeToggle = (e) => {
+  e.classList.toggle("liked");
+};
+
+const commentLikeToggle = (e) => {
+  e.classList.toggle("liked");
+};
+
+const deleteComment = (e) => {
+  const deleteTarget = e.parentElement.parentElement;
+
+  const deleteConfirm = confirm("댓글을 삭제하시겠습니까?");
+  if (deleteConfirm === true) {
+    deleteTarget.innerHTML = "";
   }
-});
+};
