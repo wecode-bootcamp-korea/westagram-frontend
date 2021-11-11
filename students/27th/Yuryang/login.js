7c3709cbf44581f8c3d3452291f4da2df6a2beff
yuryang Kim <flowratekim@gmail.com>
2021-11-11 14:04:26 +0900

    [27기 김유량] [Mission2]  id, pw 입력 시 로그인 버튼 활성화 기능 구현 비활성화 로그인 상태와 id와 password input시 로그인창 색상 변경 기능을구현하였습니다. 함수 수정 버전

--- students/27th/Yuryang/login.js
+++ students/27th/Yuryang/login.js
@@ -7,7 +7,7 @@ const password = document.querySelector("#password");
 const loginBtn = document.querySelector("#login");
 const input = document.querySelector("#input");
 
-id.addEventListener("keyup", function (event) {
+id.addEventListener("keyup", (event) => {
   if (!!id.value && !!password.value) {
     loginBtn.removeAttribute("disabled", false);
     loginBtn.style.backgroundColor = "#499ee9";
@@ -16,7 +16,7 @@ id.addEventListener("keyup", function (event) {
   button.style.backgroundColor = "#c4e1fb";
 });
 
-password.addEventListener("keyup", function (event) {
+password.addEventListener("keyup", (event) => {
   if (!!id.value && !!password.value) {
     loginBtn.removeAttribute("disabled", false);
     loginBtn.style.backgroundColor = "#499ee9";
