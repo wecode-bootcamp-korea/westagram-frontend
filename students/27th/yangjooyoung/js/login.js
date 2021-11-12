a17929f8b01753633fd76d2e6e4f0514ea0186e7
yangddu <wndun21@naver.com>
2021-11-12 17:39:47 +0900

    리팩토링 추가

--- students/27th/yangjooyoung/js/login.js
+++ students/27th/yangjooyoung/js/login.js
@@ -2,7 +2,7 @@ const inputId = document.getElementsByClassName("inputId")[0];
 const inputPwd = document.getElementsByClassName("inputPwd")[0];
 const inputWrapper = document.getElementsByClassName("inputWrap")[0];
 
-const button = document.querySelector(".button");
+const button = document.querySelector(".loginButton");
 
 const goToMain = () => {
   location.href = "main.html";
@@ -16,35 +16,11 @@ const handleBtn = (e) => {
   const inputIdVal = inputId.value;
   const inputPwdVal = inputPwd.value;
 
-  // if( inputIdVal === '' && inputPwdVal === '' ) {
-  //     button.disabled = true;
-  // }
-
-  // if( inputIdVal !== '' && inputPwdVal === '' ) {
-  //     button.disabled = true;
-  // }
-
-  // if( inputIdVal === '' && inputPwdVal !== '' ) {
-  //     button.disabled = true;
-  // }
-
-  // if( inputIdVal.length !== 0 && inputPwdVal.length !== 0 ) {
-  //     button.disabled = false;
-  // }else {
-  //     button.disabled = true;
-  // }
-
   if (inputIdVal.length !== 0 && inputPwdVal.length !== 0) {
     button.disabled = false;
   } else {
     button.disabled = true;
   }
-
-  // if( inputIdVal.includes('@') && inputPwdVal.length !== 0 ) {
-  //     button.disabled = false;
-  // }else {
-  //     button.disabled = true;
-  // }
 };
 
 inputWrapper.addEventListener("keydown", handleBtn);
