5e6d3dd37a627a32c030e5e87036b388985c2dcf
성현Maria 김 <globalkmaria@gmail.com>
2021-11-12 17:11:33 +0900

    [Modify] small changes

--- students/27th/sunghyun/js/login.js
+++ students/27th/sunghyun/js/login.js
@@ -1,40 +1,44 @@
-const id = document.querySelector('#id');
-const password = document.querySelector('#password');
-const loginBtn = document.querySelector('.loginBtn');
-const loginForm = document.querySelector('.inputWapper');
+(function () {
+  const id = document.querySelector('#id');
+  const password = document.querySelector('#password');
+  const loginBtn = document.querySelector('.loginBtn');
+  const loginForm = document.querySelector('.inputWapper');
 
-const checkLoginForm = () => {
-  const allchecked = !!id.value && !!password.value;
+  const checkLoginForm = () => {
+    const allchecked = !!id.value && !!password.value;
 
-  allchecked
-    ? loginBtn.classList.remove('loginBtn--disable')
-    : loginBtn.classList.add('loginBtn--disable');
+    allchecked
+      ? loginBtn.classList.remove('loginBtn--disable')
+      : loginBtn.classList.add('loginBtn--disable');
 
-  loginBtn.disabled = !allchecked;
-};
+    loginBtn.disabled = !allchecked;
+  };
 
-const checkLoginValidation = () => {
-  const items = loginForm.elements;
-  console.log(items.id.value);
-  for (const item of items) {
-    if (item.name === 'id' && !item.value.includes('@')) {
-      alert(`아이디에 '\@'이 포함되어야 합니다.`);
-      return;
+  const checkLoginValidation = () => {
+    const items = loginForm.elements;
+    console.log(items.id.value);
+    for (const item of items) {
+      if (item.name === 'id' && !item.value.includes('@')) {
+        alert(`아이디에 '\@'이 포함되어야 합니다.`);
+        return;
+      }
+      if (item.name === 'password' && !(password.value.length >= 5)) {
+        alert(`비밀번호는 5자리 이상이어야 합니다.`);
+        return;
+      }
     }
-    if (item.name === 'password' && !(password.value.length >= 5)) {
-      alert(`비밀번호는 5자리 이상이어야 합니다.`);
-      return;
-    }
-  }
-  location.href = 'main.html';
-  alert(`로그인 되었습니다.\n${items.id.value}님 좋은 하루되세요 :)`);
-  loginForm.reset();
-};
+    location.href = 'main.html';
+    alert(`로그인 되었습니다.\n${items.id.value}님 좋은 하루되세요 :)`);
+    loginForm.reset();
+  };
 
-id.addEventListener('keyup', checkLoginForm);
-password.addEventListener('keyup', (e) => {
-  checkLoginForm();
-  e.key === 'Enter' && checkLoginValidation();
-});
-
-loginBtn.addEventListener('click', checkLoginValidation);
+  const init = () => {
+    id.addEventListener('keyup', checkLoginForm);
+    password.addEventListener('keyup', (e) => {
+      checkLoginForm();
+      e.key === 'Enter' && checkLoginValidation();
+    });
+    loginBtn.addEventListener('click', checkLoginValidation);
+  };
+  init();
+})();
--- students/27th/sunghyun/js/main.js
+++ students/27th/sunghyun/js/main.js
@@ -1,164 +1,181 @@
-const userNickName = 'ria';
-const comments = document.querySelector('.feedComments');
-const commentInput = document.querySelector('#commentInput');
-const addCommentBtn = document.querySelector('#addCommentBtn');
-const searchWrapper = document.querySelector('#search');
-const searchInput = document.querySelector('#searchInput');
-const searchTitle = document.querySelector('#searchText');
-const searchUsersWrapper = document.querySelector('.searchUsersWrapper');
-const feedComments = document.querySelector('.feedComments');
-const usersInfo = [
-  {
-    id: 1,
-    username: '짹짹',
-    userInfo: '나의 잭',
-    img: 'p4.jpeg',
-  },
-  {
-    id: 2,
-    username: 'hellolove',
-    userInfo: '',
-    img: 'search2.jpg',
-  },
-  {
-    id: 3,
-    username: 'nypl',
-    userInfo: '',
-    img: 'p1.jpeg',
-  },
-  {
-    id: 4,
-    username: 'hwnhwan12',
-    userInfo: '겸손',
-    img: 'p7.jpeg',
-  },
-  {
-    id: 5,
-    username: 'helloworld',
-    userInfo: 'love love love',
-    img: 'search1.jpg',
-  },
-  {
-    id: 6,
-    username: 'hellowecode',
-    userInfo: '에러로부터 코드를 지키자!',
-    img: 'post3.png',
-  },
-];
+(function () {
+  const comments = document.querySelector('.feedComments');
+  const commentInput = document.querySelector('#commentInput');
+  const addCommentBtn = document.querySelector('#addCommentBtn');
+  const searchWrapper = document.querySelector('#search');
+  const searchInput = document.querySelector('#searchInput');
+  const searchTitle = document.querySelector('#searchText');
+  const searchUsersWrapper = document.querySelector('.searchUsersWrapper');
+  const feedComments = document.querySelector('.feedComments');
 
-let commentId = 2;
+  const usersInfo = [
+    {
+      id: 1,
+      username: '짹짹',
+      userInfo: '나의 잭',
+      img: 'p4.jpeg',
+    },
+    {
+      id: 2,
+      username: 'hellolove',
+      userInfo: '',
+      img: 'search2.jpg',
+    },
+    {
+      id: 3,
+      username: 'nypl',
+      userInfo: '',
+      img: 'p1.jpeg',
+    },
+    {
+      id: 4,
+      username: 'hwnhwan12',
+      userInfo: '겸손',
+      img: 'p7.jpeg',
+    },
+    {
+      id: 5,
+      username: 'helloworld',
+      userInfo: 'love love love',
+      img: 'search1.jpg',
+    },
+    {
+      id: 6,
+      username: 'hellowecode',
+      userInfo: '에러로부터 코드를 지키자!',
+      img: 'post3.png',
+    },
+  ];
 
-const makeCommentInner = (nickName, comment) => {
-  return `
-  <p class="feedCommentText">
-    <b class="commentProfile">${nickName}</b>
-    <span>${comment}</span>
-  </p>
-  <div class="commentBtn">
-    <i class="far fa-trash-alt" id="t_${commentId}"></i>
-    <div class="hearts">
-    <i class="far fa-heart"></i>
-    <i class="fas fa-heart"></i>
-    </div>
-  </div>`;
-};
+  // SEARCH
+  const searchActions = () => {
+    const showSearchedUsers = () => {
+      const searchResults = document.querySelector('.searchResults');
+      if (!searchInput.value) {
+        searchResults.innerHTML =
+          '<li class="searchResultEmpty">검색결과 없음</li>';
+        return;
+      }
+      const filteredUsers = getUsers(searchInput.value, usersInfo);
+      if (!filteredUsers.length) {
+        searchResults.innerHTML =
+          '<li class="searchResultEmpty">검색결과 없음</li>';
+        return;
+      }
+      const dropdownInner = makeUsersDropdownInner(filteredUsers);
+      searchResults.innerHTML = dropdownInner;
+    };
 
-const makeComment = () => {
-  if (!commentInput.value.trim().length) {
-    alert('댓글을 입력해주세요!');
-    return;
-  }
-  const newComment = document.createElement('li');
-  newComment.className = 'feedComment';
-  newComment.innerHTML = makeCommentInner(userNickName, commentInput.value);
-  newComment.id = `comment_${commentId++}`;
-  comments.append(newComment);
-  commentInput.value = '';
-};
+    const makeUsersDropdownInner = (users) => {
+      let dropdown = '';
+      users.forEach((user, i) => {
+        const { username, userInfo, img } = user;
 
-// SEARCH
-const showSearchedUsers = () => {
-  const searchResults = document.querySelector('.searchResults');
-  if (!searchInput.value) {
-    searchResults.innerHTML =
-      '<li class="searchResultEmpty">검색결과 없음</li>';
-    return;
-  }
-  const filteredUsers = getUsers(searchInput.value, usersInfo);
-  if (!filteredUsers.length) {
-    searchResults.innerHTML =
-      '<li class="searchResultEmpty">검색결과 없음</li>';
-    return;
-  }
-  const dropdownInner = makeUsersDropdownInner(filteredUsers);
-  searchResults.innerHTML = dropdownInner;
-};
+        dropdown += `<li class="searchResult" id="search_${i + 1}">
+      <img src="./src/${img}" alt="userprofile">
+      <div class="searchResultInfo">
+        <b>${username}</b>
+        <span>${userInfo}</span>
+      </div>
+    </li>`;
+      });
+      return dropdown;
+    };
 
-const makeUsersDropdownInner = (users) => {
-  let dropdown = '';
-  users.forEach((user, i) => {
-    const { username, userInfo, img } = user;
+    const getUsers = (username, usersInfo) => {
+      return usersInfo.filter((user) => user.username.includes(username));
+    };
 
-    dropdown += `<li class="searchResult" id="search_${i + 1}">
-    <img src="./src/${img}" alt="userprofile">
-    <div class="searchResultInfo">
-      <b>${username}</b>
-      <span>${userInfo}</span>
-    </div>
-  </li>`;
-  });
-  return dropdown;
-};
+    searchWrapper.addEventListener('click', () => {
+      searchInput.focus();
+    });
 
-const getUsers = (username, usersInfo) => {
-  return usersInfo.filter((user) => user.username.includes(username));
-};
+    searchInput.addEventListener('focus', () => {
+      searchUsersWrapper.classList.add('searchUsersWrapper--visible');
+      searchTitle.classList.add('searchText--none');
+      showSearchedUsers();
+    });
 
-searchWrapper.addEventListener('click', () => {
-  searchInput.focus();
-});
+    searchInput.addEventListener('blur', () => {
+      searchUsersWrapper.classList.remove('searchUsersWrapper--visible');
+      if (!!searchInput.value) return;
+      searchTitle.classList.remove('searchText--none');
+    });
+    searchInput.addEventListener('input', showSearchedUsers);
+  };
 
-searchInput.addEventListener('focus', () => {
-  searchUsersWrapper.classList.add('searchUsersWrapper--visible');
-  searchTitle.classList.add('searchText--none');
-  showSearchedUsers();
-});
+  // COMMENT Actions
+  const commentActions = () => {
+    const userNickName = 'ria';
+    let commentId = 2;
+    const makeCommentInner = (nickName, comment) => {
+      return `
+    <p class="feedCommentText">
+      <b class="commentProfile">${nickName}</b>
+      <span>${comment}</span>
+    </p>
+    <div class="commentBtn">
+      <i class="far fa-trash-alt" id="t_${commentId}"></i>
+      <div class="hearts">
+      <i class="far fa-heart"></i>
+      <i class="fas fa-heart"></i>
+      </div>
+    </div>`;
+    };
 
-searchInput.addEventListener('blur', () => {
-  searchUsersWrapper.classList.remove('searchUsersWrapper--visible');
-  if (!!searchInput.value) return;
-  searchTitle.classList.remove('searchText--none');
-});
+    const makeComment = () => {
+      if (!commentInput.value.trim().length) {
+        alert('댓글을 입력해주세요!');
+        return;
+      }
+      const newComment = document.createElement('li');
+      newComment.className = 'feedComment';
+      newComment.innerHTML = makeCommentInner(userNickName, commentInput.value);
+      newComment.id = `comment_${commentId++}`;
+      comments.append(newComment);
+      commentInput.value = '';
+    };
 
-searchInput.addEventListener('keyup', showSearchedUsers);
+    commentInput.addEventListener('input', (e) => {
+      if (!commentInput.value) {
+        addCommentBtn.classList.add('mainBtn--disable');
+        addCommentBtn.disabled = true;
+        return;
+      }
+      addCommentBtn.classList.remove('mainBtn--disable');
+      addCommentBtn.disabled = false;
+    });
+    commentInput.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
+        makeComment();
+        return;
+      }
+    });
+    addCommentBtn.addEventListener('click', makeComment);
+  };
 
-// COMMENT INPUT
-commentInput.addEventListener('keyup', (e) => {
-  if (!commentInput.value) {
-    addCommentBtn.classList.add('mainBtn--disable');
-    addCommentBtn.disabled = true;
-    return;
-  }
-  addCommentBtn.classList.remove('mainBtn--disable');
-  addCommentBtn.disabled = false;
-  if (e.key === 'Enter') {
-    makeComment();
-    return;
-  }
-});
-addCommentBtn.addEventListener('click', makeComment);
+  // COMMENTS Actions
+  const commentsActions = () => {
+    feedComments.addEventListener('click', (e) => {
+      const target = e.target;
+      const targetClass = target.className;
+      const targetId = target.id;
+      if (targetClass.includes('fa-trash-alt')) {
+        const removeTarget = document.querySelector(
+          `#comment_${targetId.split('_')[1]}`
+        );
+        removeTarget.remove();
+        return;
+      }
+      targetClass.includes('fa-heart') && target.classList.toggle('showHeart');
+    });
+  };
 
-// COMMENTS REACT
-feedComments.addEventListener('click', (e) => {
-  const target = e.target;
-  const targetClass = target.className;
-  const targetId = target.id;
-  if (targetClass.includes('fa-trash-alt')) {
-    const removeTarget = document.querySelector(
-      `#comment_${targetId.split('_')[1]}`
-    );
-    removeTarget.remove();
-    return;
-  }
-  targetClass.includes('fa-heart') && target.classList.toggle('showHeart');
-});
+  // init
+  const init = () => {
+    searchActions();
+    commentActions();
+    commentsActions();
+  };
+  init();
+})();
