027a15037fac51ddc56a27640acbbf59c6e076f4
성현Maria 김 <globalkmaria@gmail.com>
2021-11-12 13:44:08 +0900

    [ADD] username 검색기능 추가

--- students/27th/sunghyun/js/main.js
+++ students/27th/sunghyun/js/main.js
@@ -3,9 +3,49 @@ const comments = document.querySelector('.feedComments');
 const commentInput = document.querySelector('#commentInput');
 const addCommentBtn = document.querySelector('#addCommentBtn');
 const searchWrapper = document.querySelector('#search');
-const searchInput = searchWrapper.firstElementChild;
-const searchTitle = searchWrapper.lastElementChild;
+const searchInput = document.querySelector('#searchInput');
+const searchTitle = document.querySelector('#searchText');
+const searchUsersWrapper = document.querySelector('.searchUsersWrapper');
 const feedComments = document.querySelector('.feedComments');
+const usersInfo = [
+  {
+    id: 1,
+    username: '짹짹',
+    userInfo: '나의 잭',
+    img: 'p4.jpeg',
+  },
+  {
+    id: 2,
+    username: 'hellolove',
+    userInfo: '',
+    img: 'search2.jpg',
+  },
+  {
+    id: 3,
+    username: 'nypl',
+    userInfo: '',
+    img: 'p1.jpeg',
+  },
+  {
+    id: 4,
+    username: 'hwnhwan12',
+    userInfo: '겸손',
+    img: 'p7.jpeg',
+  },
+  {
+    id: 5,
+    username: 'helloworld',
+    userInfo: 'love love love',
+    img: 'search1.jpg',
+  },
+  {
+    id: 6,
+    username: 'hellowecode',
+    userInfo: '에러로부터 코드를 지키자!',
+    img: 'post3.png',
+  },
+];
+
 let commentId = 2;
 
 const makeCommentInner = (nickName, comment) => {
@@ -33,16 +73,55 @@ const makeComment = () => {
 };
 
 // SEARCH
+const showSearchedUsers = () => {
+  const searchResults = document.querySelector('.searchResults');
+  if (!searchInput.value) {
+    searchResults.innerHTML =
+      '<li class="searchResultEmpty">검색결과 없음</li>';
+    return;
+  }
+  const filteredUsers = getUsers(searchInput.value, usersInfo);
+  const dropdownInner = makeUsersDropdownInner(filteredUsers);
+  searchResults.innerHTML = dropdownInner;
+};
+
+const makeUsersDropdownInner = (users) => {
+  let dropdown = '';
+  users.forEach((user, i) => {
+    const { username, userInfo, img } = user;
+
+    dropdown += `<li class="searchResult" id="search_${i + 1}">
+    <img src="./src/${img}" alt="userprofile">
+    <div class="searchResultInfo">
+      <b>${username}</b>
+      <span>${userInfo}</span>
+    </div>
+  </li>`;
+  });
+  return dropdown;
+};
+
+const getUsers = (username, usersInfo) => {
+  return usersInfo.filter((user) => user.username.includes(username));
+};
+
 searchWrapper.addEventListener('click', () => {
   searchInput.focus();
 });
+
+searchInput.addEventListener('focus', () => {
+  searchUsersWrapper.classList.add('searchUsersWrapper--visible');
+  searchTitle.classList.add('searchText--none');
+  showSearchedUsers();
+});
+
 searchInput.addEventListener('blur', () => {
+  searchUsersWrapper.classList.remove('searchUsersWrapper--visible');
   if (!!searchInput.value) return;
   searchTitle.classList.remove('searchText--none');
 });
-searchInput.addEventListener('focus', () => {
-  searchTitle.classList.add('searchText--none');
-});
+
+searchInput.addEventListener('keyup', showSearchedUsers);
 
 // COMMENT INPUT
 commentInput.addEventListener('keyup', (e) => {
