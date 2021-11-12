d20e8b00946f43748b0615dbd1b9faab6e3dfdea
Moro-yong <leta@Taeyoungui-MacBookPro.local>
2021-11-12 14:08:39 +0900

    modify : 좋아요 버튼 진행 중

--- students/27th/taeyoung/westargram/login/js/login.js
+++ students/27th/taeyoung/westargram/login/js/login.js
@@ -1,6 +1,10 @@
 const getId = document.querySelector('.inputId');
 const getPassword = document.querySelector('.inputPassword');
 const loginButton = document.querySelector('.loginButton');
+const idBox = document.querySelector('.inputIdBox');
+const pwBox = document.querySelector('.inputPasswordBox');
+
+
 
 function logout() {
     if (!getId.value || !getPassword.value) {
@@ -14,6 +18,31 @@ function logout() {
     };
 }
 
+function idValidation () {
+
+    let idInclude = getId.value.includes('@')
+    if (idInclude === false) {
+        
+        let idMessage = document.createElement('div')
+        idBox.appendChild(idMessage)
+        idMessage.innerHTML = '이메일을 확인해주세요.'
+    } 
+};
+
+function pwAlidation () {
+
+    let pwInclude = getPassword.value.length
+    if (pwInclude < 8) {
+        
+        let pwMessage = document.createElement('div')
+        pwBox.appendChild(pwMessage)
+        pwMessage.innerHTML = '비밀번호를 확인해주세요'
+    } 
+};
+
 
 getId.addEventListener('keyup', logout);
 getPassword.addEventListener('keyup', logout);
+
+loginButton.addEventListener('click', idValidation);
+loginButton.addEventListener('click', pwAlidation);
--- students/27th/taeyoung/westargram/main/js/main.js
+++ students/27th/taeyoung/westargram/main/js/main.js
@@ -2,73 +2,108 @@ const getRecomment = document.querySelector('.recommnetInput');
 const recommentButton = document.querySelector('.recommnetBt');
 const commentBox = document.querySelector('.feedComment');
 
-// 엔터 눌렀을 때 댓글 달기
+// // 엔터 keyup 댓글
 function getComment(e) {
 
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && getRecomment.value) {
         
-        let container = document.createElement('div'); // 다음 댓글 전체 영역
-        let nickname = document.createElement('span'); // 작성자 이름 영역
-        let comment = document.createElement('span'); // 댓글 영역
-        let time = document.createElement('span'); // 시간 영역
+        let feedCommentLeft = document.createElement('div'); // 다음 댓글 전체 영역
+        let feedUserBox = document.createElement('div'); // 댓글 묶음
 
+        let feedUserName = document.createElement('span'); // 작성자 이름 영역
+        let feedLike = document.createElement('span'); // 좋아요 영역
+        let likeIcon = document.createElement('i');// 좋아요 아이콘
+        
+        let moreComment = document.createElement('span'); // 댓글 영역
+        let feedTime = document.createElement('span'); // 시간 영역
 
-        commentBox.appendChild(container); //commentBox에 div를 자식요소로 넣는다
-        container.appendChild(nickname); // Container에 닉네임을 자식요소로
-        container.appendChild(comment); // comment 닉네임을 자식요소로
-        container.appendChild(time); // Container에 타임을 자식요소로
+        commentBox.appendChild(feedCommentLeft); //
+        feedCommentLeft.appendChild(feedUserBox); //
+        feedUserBox.appendChild(feedUserName); // 
+        feedUserBox.appendChild(feedLike); // 
+        feedLike.appendChild(likeIcon); // 
 
-        nickname.innerHTML = "taeyong";
-        comment.innerHTML = getRecomment.value;
-        time.innerHTML = "1시간 전";
+        feedCommentLeft.appendChild(moreComment); // comment 닉네임을 자식요소로
+        feedCommentLeft.appendChild(feedTime); // Container에 타임을 자식요소로
 
-        container.className = "feedCommentLeft";
-        nickname.className = "feedUserName";
-        comment.className = "moreComment";
-        time.className = "feedTime";
+        feedCommentLeft.className = "feedCommentLeft";
+        feedUserBox.className = "feedUserBox";
+        feedUserName.className = "feedUserName";
+        feedLike.className = "feedLike";
+        likeIcon.className = "far fa-heart";
+        moreComment.className = "moreComment";
+        feedTime.className = "feedTime";
+
+        feedUserName.innerHTML = "taeyong";
+        moreComment.innerHTML = getRecomment.value;
+        feedTime.innerHTML = "1시간 전";
 
         getRecomment.value = '';
     }
 };
 
-// 게시버튼 눌렀을 때 댓글 달기
+// 게시 버튼 click 댓글 
 function gettComment() {
 
-    if (getRecomment) {
+    if (getRecomment && getRecomment.value) {
         
-        let container = document.createElement('div'); // 다음 댓글 전체 영역
-        let nickname = document.createElement('span'); // 작성자 이름 영역
-        let comment = document.createElement('span'); // 댓글 영역
-        let time = document.createElement('span'); // 시간 영역
+        let feedCommentLeft = document.createElement('div'); // 다음 댓글 전체 영역
+        let feedUserBox = document.createElement('div'); // 댓글 묶음
 
+        let feedUserName = document.createElement('span'); // 작성자 이름 영역
+        let feedLike = document.createElement('span'); // 좋아요 영역
+        let likeIcon = document.createElement('i');// 좋아요 아이콘
+        
+        let moreComment = document.createElement('span'); // 댓글 영역
+        let feedTime = document.createElement('span'); // 시간 영역
 
-        commentBox.appendChild(container); //commentBox에 div를 자식요소로 넣는다
-        container.appendChild(nickname); // Container에 닉네임을 자식요소로
-        container.appendChild(comment); // comment 닉네임을 자식요소로
-        container.appendChild(time); // Container에 타임을 자식요소로
+        commentBox.appendChild(feedCommentLeft); //
+        feedCommentLeft.appendChild(feedUserBox); //
+        feedUserBox.appendChild(feedUserName); // 
+        feedUserBox.appendChild(feedLike); // 
+        feedLike.appendChild(likeIcon); // 
 
-        nickname.innerHTML = "taeyong";
-        comment.innerHTML = getRecomment.value;
-        time.innerHTML = "1시간 전";
+        feedCommentLeft.appendChild(moreComment); // comment 닉네임을 자식요소로
+        feedCommentLeft.appendChild(feedTime); // Container에 타임을 자식요소로
 
-        container.className = "feedCommentLeft";
-        nickname.className = "feedUserName";
-        comment.className = "moreComment";
-        time.className = "feedTime";
+        feedCommentLeft.className = "feedCommentLeft";
+        feedUserBox.className = "feedUserBox";
+        feedUserName.className = "feedUserName";
+        feedLike.className = "feedLike";
+        likeIcon.className = "far fa-heart";
+        moreComment.className = "moreComment";
+        feedTime.className = "feedTime";
+
+        feedUserName.innerHTML = "taeyong";
+        moreComment.innerHTML = getRecomment.value;
+        feedTime.innerHTML = "1시간 전";
 
         getRecomment.value = '';
+        
     }
+
 };
 
-getRecomment.addEventListener('keyup', getComment);
+getRecomment.addEventListener('keydown', getComment);
 recommentButton.addEventListener('click', gettComment);
 
 
-//버튼, 인풋, 댓글 작성칸, 
 
-//키보드 input의 value를 받아오기
-// 게시버튼 또는 엔터를 눌렀을 때 
-//value의 값을 특정 위치에 추가
 
-// 댓글의 value가 없다면
-// 아무런 기능을 하지 않는다.
+
+
+
+//좋아요 버튼색 변경
+const color = document.querySelectorAll('.fa-heart');
+
+// color.forEach(element => {
+//     if (element.style.color === 'balck') {
+//         element.style.color = 'red';
+//     } else if (element.style.color === 'red') {
+//         element.style.color = 'balck';
+//     };
+// });
+
+
+color.addEventListener('click', );
+
