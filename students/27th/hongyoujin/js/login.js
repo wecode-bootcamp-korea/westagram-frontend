b613ad84bfc99d86c5cdbdfbdf0dcd81d9b0e9fa
howyoujini <byj04193@naver.com>
2021-11-12 13:28:54 +0900

    Fix: cherry pick 후 충돌 해결

--- students/27th/hongyoujin/js/login.js
+++ students/27th/hongyoujin/js/login.js
@@ -1,1 +1,28 @@
-document.querySelector;
+const userName = document.getElementById("name");
+const password = document.getElementById("password");
+
+const form = document.getElementById("form");
+
+const button = document.getElementById("button");
+
+const btnOn = () => {
+  if (
+    userName.value === !"undifined" &&
+    userName.value === !null &&
+    password.value === !"undifined" &&
+    password.value === !null
+  ) {
+    return console.log(userName.value);
+  }
+};
+
+if (userName === "INPUT" && userName.type === "text") {
+  button.disabled = false;
+  document.getElementsByClassName("login_button").style.cursor = "pointer";
+}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(userName.value);
+  //   console.log(event);
+  // userName.
+});
