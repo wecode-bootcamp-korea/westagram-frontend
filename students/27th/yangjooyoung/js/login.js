758e1f7ed7c2cf86976a22fb489316767518c957
yangddu <wndun21@naver.com>
2021-11-12 16:21:08 +0900

    add: 메인 페이지 이동 기능 추가

--- students/27th/yangjooyoung/js/login.js
+++ students/27th/yangjooyoung/js/login.js
@@ -2,32 +2,13 @@ const inputId = document.getElementsByClassName("inputId")[0];
 const inputPwd = document.getElementsByClassName("inputPwd")[0];
 const inputWrapper = document.getElementsByClassName("inputWrap")[0];
 
-// console.log(inputId);
-// console.log(inputPwd);
-// console.log(inputWrapper);
+const button = document.querySelector(".button");
 
-const button = document.getElementsByClassName("button")[0];
+const goToMain = () => {
+  location.href = "main.html";
+};
+button.addEventListener("click", goToMain);
 
-// inputWrapper.addEventListener('input', function() {
-//         const inputIdVal = inputId.value;
-//         const inputPwdVal = inputPwd.value;
-
-//         if( inputIdVal === '' && inputPwdVal === '' ) {
-//             button.disabled = true;
-//         }
-
-//         if( inputIdVal !== '' && inputPwdVal === '' ) {
-//             button.disabled = true;
-//         }
-
-//         if( inputIdVal === '' && inputPwdVal !== '' ) {
-//             button.disabled = true;
-//         }
-
-//         if( inputIdVal !== '' && inputPwdVal !== '' ) {
-//             button.disabled = false;
-//         }
-//     })
 const handleBtn = (e) => {
   if (e.code === "Space") {
     e.preventDefault();
--- students/27th/yangjooyoung/js/main.js
+++ students/27th/yangjooyoung/js/main.js
@@ -1,18 +1,16 @@
 //댓글 입력 후 엔터키를 누르거나 게시 버튼 클릭 시 댓글이 추가될 수 있도록 구현해주세요.
 //함수 로직이 같은 경우 함수를 분리해서 코드를 재활용해주세요.
 
-const commentValue = document.getElementsByClassName('comment')[0];
-const commentSpace = document.querySelector('.commentSpace');
+const commentValue = document.getElementsByClassName("comment")[0];
+const commentSpace = document.querySelector(".commentSpace");
 
-
-const commentBtn = document.getElementsByClassName('commentBtn')[0];
-const feedForm = document.getElementsByClassName('feedForm')[0];
+const commentBtn = document.getElementsByClassName("commentBtn")[0];
+const feedForm = document.getElementsByClassName("feedForm")[0];
 console.log(commentValue);
 console.log(commentSpace);
 console.log(commentBtn);
 
 // if(commentValue.value) {
-
 
 //     commentSpace.appendChild(commentValue);
 // }
@@ -26,45 +24,41 @@ console.log(commentBtn);
 //     }
 // })
 
-
-
 const onAdd = () => {
-    const value = commentValue.value;
-    const li = document.createElement('li');
-    commentSpace.appendChild(li);
-    li.className = "commentLi";
-    
-    const commentText = document.createElement('div');
-    li.appendChild(commentText);
-    commentText.className = "commentText";
+  const value = commentValue.value;
+  const li = document.createElement("li");
+  commentSpace.appendChild(li);
+  li.className = "commentLi";
 
-    const commentLikes = document.createElement('div');
-    li.appendChild(commentLikes);
-    commentLikes.className = 'commentLikes';
-    commentLikes.innerHTML = '<img src="img/likes.png" alt="" width="13px" height="13px" />';
+  const commentText = document.createElement("div");
+  li.appendChild(commentText);
+  commentText.className = "commentText";
 
-    const user = document.createElement('div');
-    commentText.appendChild(user);
-    user.className = 'user';
-    user.innerHTML = "j00_00";
+  const commentLikes = document.createElement("div");
+  li.appendChild(commentLikes);
+  commentLikes.className = "commentLikes";
+  commentLikes.innerHTML =
+    '<img src="img/likes.png" alt="" width="13px" height="13px" />';
 
-    const text = document.createElement('div');
-    text.className = 'text';
-    text.innerHTML = value;
-    commentText.appendChild(text);
+  const user = document.createElement("div");
+  commentText.appendChild(user);
+  user.className = "user";
+  user.innerHTML = "j00_00";
 
+  const text = document.createElement("div");
+  text.className = "text";
+  text.innerHTML = value;
+  commentText.appendChild(text);
 
-    //const commentVal = document.createTextNode(value);
-    // const commentLi = li.appendChild(value);
-    // console.log(li.appendChild(commentVal));
+  //const commentVal = document.createTextNode(value);
+  // const commentLi = li.appendChild(value);
+  // console.log(li.appendChild(commentVal));
 
-    
-    // commentSpace.appendChild(commentLi);
-}
+  // commentSpace.appendChild(commentLi);
+};
 
-
-commentBtn.addEventListener('click', (e) => {
-    onAdd();
-    e.preventDefault();
-    commentValue.value = '';
-})
+commentBtn.addEventListener("click", (e) => {
+  onAdd();
+  e.preventDefault();
+  commentValue.value = "";
+});
