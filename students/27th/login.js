9c70b4e977561ff43881cc7e4362842402b97997
tohjbin <tohjbin@gmail.com>
2021-11-14 19:52:07 +0900

    임시 저장

--- students/27th/login.js
+++ students/27th/login.js
@@ -1,5 +1,57 @@
-let inputData = document.querySelector('login');
+const inputId = document.querySelector('.id');
 
-inputData.addEventListner('keypress', function() {
-  if () {}
+function activeLogin() {
+  inputId.style.backgroundColor = "blue";
+}
+
+inputId.addEventListener('keyup', activeLogin);
+
+/*
+inputId.addEventListener('keyup', activeLogin)
+inputPassword.addEventListener('keyup', activeLogin)
+
+function activeLogin () {
+  let id = inputId.value;
+  let password = inputPassword.value;
+
+  if ((id.length >= 1) && (password.length >= 1)) {
+    btn.style.backgroundColor = "rgb(250, 250, 250"
+  } else {
+    btn.style.backgroundColor = "#FAFAFA"
+  }
+}
+*/
+
+/*
+let inputId = document.querySelector('.id');
+let inputPassword = document.querySelector('.password');
+let activeBtn = document.querySelector('.btn');
+
+
+inputId.addEventListener('keyup', function () {
+  activeBtn.style.backgroundColor = "rgb(250, 250, 250)"
 });
+*/
+
+/*
+document.addEventListener('keyup', function () {
+  activeBtn.style.backgroundColor = "rgb(250, 250, 250)"
+});
+*/
+
+/*
+inputId.addEventListner('keyup', activeLogin);
+inputPassword.addEventListener('keyup', activeLogin);
+
+function activeLogin() {
+
+  const loginId = inputId.value;
+  const loginPassword = inputPassword.value;
+
+  if ((loginId.length >= 1) && (loginPassword.length >= 1)) {
+    activeBtn.style.backgroundColor = "rgb(250, 250, 250)"
+  } else {
+    activeBtn.style.backgroundColor = "#FAFAFA"
+  }
+};
+*/
---
+++ students/27th/main.js
@@ -0,0 +1,7 @@
+let heart = document.querySelector('.commentHeart');
+
+heart.addEventListener('click', function () {
+  console.log(1);
+});
+////
+
