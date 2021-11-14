0dfb0184a6d4c14b540450157ac7ab16e72cf061
leejihyun <ix219@naver.com>
2021-11-14 15:49:05 +0900

    code refactoring

--- students/27th/jihyun/main.js
+++ students/27th/jihyun/main.js
@@ -2,29 +2,9 @@ const feedComment = document.querySelector('.feed-comment input');
 const $btn = document.querySelector('.post-comment');
 const commentList = document.querySelector('.feed-comment-list');
 
-/* < 댓글 달기 >
- * 1. 글자를 입력받을 변수를 생성한다.
- * 2. 글자를 입력한다.
- * 2. 게시버튼을 누른다.
- * 3. 누르면 이벤트 발생
- * 4. 그 변수가 삽입된다. <br>
- **/
 
 
 
-/* <댓글 달기>
- * <li><span class="font-bold"></span></li>
- **/
-
-// 댓글 좋아요 기능
-/* 1. 댓글 추가할 때 같이 아이콘을 생성해야 할듯
- * 2. <li><span class="font-bold">id</span>내용</li>
- * 3. <i class="fas fa-heart"></i> 이게 하트 아이콘
- * 4. <li><span class="font-bold">ID</span>내용<i class="fas fa-heart"></i></li>
- * */
-
-
-// 댓글 추가 + 좋아요 기능
 function onBtnClick() {
     event.preventDefault();
     const commentContent = feedComment.value;
@@ -34,8 +14,6 @@ function onBtnClick() {
         const fontBoldClass = 'font-bold';
         const heart1 = "fas";
         const heart2 = "fa-heart";
-        const trash1 = "far";
-        const trash2 = "fa-trash-alt";
 
         let user = myId;
 
@@ -43,42 +21,35 @@ function onBtnClick() {
         let li = document.createElement('li');
         let span = document.createElement('span');
         let i = document.createElement('i');
-        let i2 = document.createElement('i');
 
 
-        commentList.append(li);   //    <ul><li> </li></ul>
-        li.append(commentContent);     //    <ul><li><댓글 내용<li><ul>
+        commentList.append(li);   
+        li.append(commentContent);     
         li.append(i);
         i.classList.add(heart1, heart2);
-        li.append(i2);
-        i2.classList.add(trash1, trash2);
+        
 
 
         feedComment.value = "";
 
 
-        span.append(myId);   //  <span>atom </span>
+        span.append(myId);   
 
-        li.prepend(span);     // 2. <li><span>atom </span></li>
-
-
-        // <li><span class> atom </span>dsfsdfsfsa 내용 삽입하기 </li>
+        li.prepend(span);     
 
 
-        span.classList.add(fontBoldClass);    // <span class="font-bold"></span>
+        span.classList.add(fontBoldClass);    
 
 
 
-        // 버튼 색깔, 커서 초기화
         $btn.style.color = "#4693a4";
         $btn.style.cursor = "default";
     }
 }
 
 
-// 댓글 좋아요 버튼 누르기 기능
+
 function onLikeClick(e) {
-    // e.target.style.color = "red";    // classList 가 왜 안될까  ---> 우선순위가 밀린 것
     
     if (e.target.classList.contains('fa-heart')) {
         e.target.classList.toggle("red");
@@ -93,7 +64,6 @@ commentList.addEventListener('click', onLikeClick);
 
 
 
-// 글자가 1자 이상이면 버튼 색깔 바꾸기
 function onBtnColorChange() {
     const inputValue = feedComment.value;
     let color = "";
@@ -112,40 +82,6 @@ function onBtnColorChange() {
 }
 
 
-
-
-
-// 댓글 삭제 기능
-/* 
- * 1. */
-
-
-
-
-
-
-function onDeleteComment(e) {
-    if (e.target.tagName === 'LI') {
-        console.log(e.target.children);
-
-        for (let i = 0; i < e.target.children.length; i++) {
-            console.log(e.target.children[i].className);
-            if (e.target.children[i].className.contains('fa-trash-alt')){
-                console.log('ddd');
-            }
-        }
-
-        //if () {    // 휴지통 아이콘이 이미 있다면
-            
-            
-
-
-        //    trashIcon.addEventListener('click', onClickTrash);
-        //}
-    }
-}
-
-commentList.addEventListener('mouseover', onDeleteComment);
 
 
 
--- students/27th/jihyun/script.js
+++ students/27th/jihyun/script.js
@@ -4,15 +4,6 @@ const $loginForm = document.querySelector('.login-form');
 const $loginBtn = document.querySelector('.login-btn');
 
 
-/*
- * 1. 아이디를 입력한다.
- * 2. 비밀번호를 입력한다.
- * 3. 입력한 값을 아이디라는 변수에 저장한다. * 
- * 4. 입력한 값을 비밀번호라는 변수에 저장한다. 
- * 5. 아이디가 1자 이상임을 체크한다.
- * 6. 그리고 비밀번호가 1자 이상이다. 
- * */
-
 
 $loginForm.addEventListener('keyup', function () {
     if ($id.value.length === 0 || $password.value.length === 0) {
@@ -21,8 +12,21 @@ $loginForm.addEventListener('keyup', function () {
     }
     else if ($id.value.length >= 1 && $password.value.length >= 1) {
         $loginBtn.style.backgroundColor = "blue";
-        return;
+
+
+
+        function OnClickLoginBtn() {
+            event.preventDefault();
+            location.href = "main.html";
+        }
+
+        $loginBtn.addEventListener('click', OnClickLoginBtn);
+
+        
     }
     
 })
+
+
+
 
