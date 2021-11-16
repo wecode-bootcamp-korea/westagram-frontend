41760e9bfa6f8279dbdfb34e9d81df06b0e2a329
99cone <qnfrksmd0@gmail.com>
2021-11-16 15:07:30 +0900

    commit

--- students/27th/yeaseul/westa.js
+++ students/27th/yeaseul/westa.js
@@ -1,67 +1,39 @@
 "use strict";
 
-let id = document.querySelector("#id"); // id@
+let id = document.querySelector("#id");
 let pw = document.querySelector("#pw");
 let button = document.querySelector("#button");
-//    let login=document.querySelector("#loginButton");
 
-//    function onAndoff(){
-//        if(document.querySelector("#id").value !== "" && document.querySelector('#pw').value!== ""){
-//            button.disabled= false;
-//            button.style.backgroundColor = "#0095f6"
-//        } else {
-//            button.disabled = true;
-//            button.style.backgroundColor = "#b2dffc"
-//        }
-//    }
+// button disabled or able
+// go to main
+// button click, 
 
-//   id.addEventListener('keyup', onAndoff);
-//   pw.addEventListener('keyup', onAndoff);
 
-function onAndoff() {
+function loginButtonWork() {
   if (
     document.querySelector("#id").value !== "" &&
     document.querySelector("#pw").value !== ""
   ) {
-    button.disabled;
+    button.disabled = true;  
     button.style.backgroundColor = "#0095f6";
+
+    if (window.event.keyCode === 13) {
+      document.querySelector("#button").click();
+    };
+
+
   } else {
     button.style.backgroundColor = "#b2dffc";
   }
 }
 
-id.addEventListener("keyup", onAndoff);
-pw.addEventListener("keyup", onAndoff);
+id.addEventListener("keyup", loginButtonWork);
+pw.addEventListener("keyup", loginButtonWork);
 
-// if(document.querySelector('#id').value !== "" && document.querySelector('#pw').value!==""를 value.length > 0 값도 가능함.
 
-//    function login(){
+function click() {
+  location.href("westagramMain.html");
+  // window.open("http://127.0.0.1:5500/students/27th/yeaseul/westagramMain.html")
+}
 
-//     if(id.value.indexOf("@") != 0 && pw.value.length >= 8) {
-//         document.querySelector('loginButton').disable =false;
-//     } else {
-//         login.disabled = ture;
-
-//       }
-//     }
-
-//    function login(){
-
-//     if(id.indexOf("@") != 0 && pw.length >= 8) {
-//         login.disabled
-//     } else {
-//         login.disabled
-//     }
-//    }
-
-// function login(){
-
-//     let id=document.querySelector('#id');
-//     let pw=document.querySelector('#pw');
-
-//     if(pw.length>=8)&&(id) {
-//     }
-// }
-
-// if(id.indexOf("@") != 0 && pw.length>=8) {
-// }
+// init();
--- students/27th/yeaseul/westaMain.js
+++ students/27th/yeaseul/westaMain.js
@@ -1,10 +1,48 @@
 "use strict";
 
-let name = document.querySelector("#name").value;
-let result = document.querySelector("#result");
+const commentButton = document.querySelector("#submit");
+const commentInput = document.querySelector("#commentInput");
+const commentLists = document.querySelector("#commentLists");
 
-function newComment() {
-  document.getElementById("result").innerText = name;
+function validateComment() {
+  const commentValue = commentInput.value;
+
+  return commentValue.length > 0;
 }
 
-reactComment.addEventListener("keydown", newComment);
+function addComment() {
+  const isCommentValid = validateComment();
+
+  if (isCommentValid === false) {
+    alert("댓글을 입력 해주세요");
+  } else {
+    const newComment = document.createElement("li");
+
+    const newCommentNickName = document.createElement("span");
+    newCommentNickName.classList.add("name");
+    newCommentNickName.innerText = "yes_eulk3";
+
+    const newCommentContents = document.createElement("span");
+    newCommentContents.innerText = commentInput.value;
+
+    newComment.appendChild(newCommentNickName);
+    newComment.appendChild(newCommentContents);
+
+    commentLists.appendChild(newComment);
+
+    commentInput.value = "";
+  }
+}
+
+function init() {
+  commentButton.addEventListener("click", addComment);
+}
+
+init();
+
+// commentButton 버튼을 눌렀을 때 addEventListerner가 실행되고 ('이벤트종류'.'일어날 일(즉 함수명)');
+
+//댓글창에 문자를 입력하게 되면 닉네임 정보/댓글 내용이 칸 안에 어떤 문자를 입력하고 게시 버튼을 누르면 게시됨.
+//즉 1.input창에 댓글 문자열이 입력됨 2.
+
+
