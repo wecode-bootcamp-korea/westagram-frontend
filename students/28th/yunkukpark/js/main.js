87cb297fd504793a6f3a6f5de655a4620ba53668
YunKukPark <a01043340999@gmail.com>
2021-12-08 16:37:07 +0900

    [ADD] SetLayout 함수 구현 - page가 Load 되었을 때 mainRight 부분이 자기 자리를 잡습니다. - page가 resize 되었을 때 mainRight 부분이 자기 자리를 잡습니다.

--- students/28th/yunkukpark/js/main.js
+++ students/28th/yunkukpark/js/main.js
@@ -1,8 +1,15 @@
+const main = document.querySelector('main');
 const mainLeft = document.querySelector('.main-left');
 const mainRight = document.querySelector('.main-right');
 
 const setLayout = () => {
-  console.log('hihi');
+  const mainLeftXoffset = mainLeft.offsetLeft;
+  const mainLeftWidth = mainLeft.offsetWidth;
+  const mainLeftMarginRight = 28;
+
+  const mainRightXoffset = `${mainLeftXoffset + mainLeftWidth + mainLeftMarginRight}px`;
+  mainRight.style.left = mainRightXoffset;
 };
 
+window.addEventListener('load', setLayout);
 window.addEventListener('resize', setLayout);
