bd59f4a8bbe16a359b0ca4c3a3dc9ae3be204e54
YOONKY <lov012726@gmail.com>
2021-12-08 16:37:24 +0900

    Add: login & main 추가

---
+++ students/28th/yoonkyeong/js/main.js
@@ -0,0 +1,20 @@
+// const commentButton = document.querySelector('.comment-input-button');
+// const commentBox = document.querySelector('.comment-wrapper');
+// commentButton.addEventListener('click', submitComment);
+// // const wrapper = document.getElementByClassName('comment-wrapper')
+// function submitComment(e) {
+//     e.preventDefault();
+// const wrapper = document.querySelector('.comment-wrapper')
+
+//     // const comment = document.getElementByClassName('comment-input-text')[0].value;
+//     const comment = document.querySelector('.comment-input-text').value;
+//     console.log(comment)
+    
+//     // const wrapper = document.getElementsByClassName('comment-wrapper')
+//     wrapper.textContent = comment;
+
+// }
+
+
+const 
+
