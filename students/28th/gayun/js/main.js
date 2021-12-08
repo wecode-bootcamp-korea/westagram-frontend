17965ce4de29ce49a37421e11521aef20bdefa4c
gy <gayunlee11@gmail.com>
2021-12-08 21:06:14 +0900

    [Add] 스크롤 끝 도달시 로딩/스켈레톤 구현 진행중 - 스크롤 끝 도달 시 스켈레톤 생성 완료 - 스켈레톤 표시 후 피드 표시 완료 - 단순 html로만 작업, json파일로 동적으로 생성 예정

--- students/28th/gayun/js/main.js
+++ students/28th/gayun/js/main.js
@@ -103,7 +103,26 @@ function handleMyMenu(e) {
     }
 }
 
-
+const skeletonHtml = `
+    <article class="skeleton">
+        <div class="skeleton__header">
+            <span class="skeleton__header--profileImg"></span>
+            <span class="skeleton__header--id"></span>
+        </div>
+        <div class="skeleton__img">
+            <img class="loading" src="images/loading.gif" alt="">
+        </div>
+        <div class="skeleton__comment">
+            <span class="skeleton__comment--id"></span>
+            <span class="skeleton__comment--text"></span>
+        </div>
+        <div class="skeleton__comment">
+            <span class="skeleton__comment--id"></span>
+            <span class="skeleton__comment--text" id="comment--text2"></span>
+        </div>
+        <div class="skeleton__comment--input"></div>
+    </article>
+`
 const $feedEnd = document.querySelector('.feed__end');
 const callback = (entry, observer) => {
     if(entry[0].isIntersecting && entry[0].intersectionRatio === 1) {
@@ -124,14 +143,68 @@ observer.observe($feedEnd);
 
 
 function loading() {
-    console.log('loading...')
-    // const target = e.target;
-    // console.log(target)
-    // const y = e.pageY
-    // console.log(window.scrollY, document.scrollHeight)
+    $feedEnd.innerHTML = skeletonHtml;
+    setTimeout(() => {
+        displayFeed();
+    }, 2000)
+
 }
 
 function displayFeed() {
-
+        $feedEnd.innerHTML = `
+        <article class="feed">
+                <div class="feed__header">
+                    <span class="feed__header--user">
+                        <span class="feed__header--img-container">
+                            <img class="feed__header--img" src="images/profile-img.jpg" alt="">
+                        </span>
+                        <span class="feed__header--id">canon_mj</span>
+                    </span>
+                    <button class="feed_header--button">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </button>
+                </div>
+                <div class="feed__img-container">
+                    <img src="images/profile-img.jpg" alt="" class="feed-img">
+                </div>
+                <div class="feed__buttons">
+                    <span class="feed__buttons1">
+                        <button class="feed__button">
+                            <i class="far fa-heart"></i>
+                        </button>
+                        <button class="feed__button">
+                            <i class="far fa-comment"></i>
+                        </button>
+                        <button class="feed__button">
+                            <i class="far fa-share-square"></i>
+                        </button>
+                    </span>
+                    <span class="feed__buttons2">
+                        <button class="feed__button">
+                            <i class="far fa-bookmark"></i>
+                        </button>
+                    </span>
+                </div>
+                <div class="people-who-like">
+                    <img src="images/profile-img2.jpg" alt="" class="people-who-like__img">
+                    <span class="people-who-like__comment"></span>
+                </div>
+                <li class="comments">
+                    <ul class="comment">
+                        <span class="comment_item">
+                            <span class="comment__id">canon_mj</span>
+                            <span class="comment__text">위워크에서 진행한 베이킹 클래스...</span>
+                        </span>
+                        <button class="comment__delete-button">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </ul>
+                </li>
+                <form class="comment__input">
+                    <input type="text" placeholder=" 댓글 달기..." name="" id="" class="comment__input--text">
+                    <button class="comment__input--button">게시</button>
+                </form>
+            </article>
+        `;
 }
 
