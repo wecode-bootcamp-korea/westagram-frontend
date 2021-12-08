ca18d03d062fdc765e5a0236f38778b7ff71e8ee
hyunjea Ann <hyunjeaann@hyunjeaui-MacBookAir.local>
2021-12-08 17:06:21 +0900

    Refactor: 스스로 리팩토링 - login.js : body태그 전체에 addEventListener메서드 사용했었는데 input태그에서만 addEventListener메서드 사용했을 때로 변경 - main.js : login.js와 마찬가지로 keyup이벤트의 변수 수정

--- students/28th/hyeonze/js/login.js
+++ students/28th/hyeonze/js/login.js
@@ -1,13 +1,37 @@
+// 1차 리팩토링후
 window.onload = function(){
-    let body = document.querySelector('body');
+    let userName = document.querySelector('.username');
+    let userPassword = document.querySelector('.userpassword');
     let button = document.querySelector('button')
 
-    body.addEventListener('keyup', function(){
+    let activateBtn = () => {
         let userName = document.querySelector('.username').value;
         let userPw = document.querySelector('.userpassword').value;
+
         if (userName && userPw) {
             button.style.backgroundColor = '#0095F6';
             button.setAttribute('onclick', "location.href='main.html'");
+        } else {
+            button.style.backgroundColor = '#c5e1fa';
+            button.setAttribute('onclick', "");
         }
-    });
+    }
+
+    userName.addEventListener('keyup', activateBtn);
+    userPassword.addEventListener('keyup', activateBtn);
 }
+
+// 리팩토링전
+// window.onload = function(){
+//     let body = document.querySelector('body');
+//     let button = document.querySelector('button')
+
+//     body.addEventListener('keyup', function(){
+//         let userName = document.querySelector('.username').value;
+//         let userPw = document.querySelector('.userpassword').value;
+//         if (userName && userPw) {
+//             button.style.backgroundColor = '#0095F6';
+//             button.setAttribute('onclick', "location.href='main.html'");
+//         }
+//     });
+// }
--- students/28th/hyeonze/js/main.js
+++ students/28th/hyeonze/js/main.js
@@ -1,18 +1,11 @@
+// 1차 리팩토링후
 window.onload = function() {
-    let body = document.querySelector('body');
     let inputUpload = document.querySelector('.input_upload');
     let btnUpload = document.querySelector('.btn_upload');
     let contentFlag = false;
     let comments = document.querySelector('.comments');
 
-    const addComment = (el) => {
-        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small>`;
-        if (contentFlag) {
-            comments.appendChild(el);
-        }
-    }
-
-    body.addEventListener("keyup", function() {//엘렘바꿔주기
+    inputUpload.addEventListener("keyup", function() {
         if (inputUpload.value) {
             btnUpload.style.color = '#3e99ed';
             contentFlag = true;
@@ -22,26 +15,57 @@ window.onload = function() {
         }
     });
 
-    // btnUpload.addEventListener("click", function() {
-    //     addComment(document.createElement('span'));
-    // });
-
-    // inputUpload.addEventListener("keyup", function(e) {
-    //     if (e.keyCode === 13) addComment(document.createElement('span'));
-    // });
+    const addComment = (el) => {
+        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small>`;
+        if (contentFlag) {
+            comments.appendChild(el);
+        }
+    }
 
     const handleComment = (event) => {
         console.dir(event.target);
         const eventClassName = event.target.className;
 
-        if(eventClassName==='btn_upload') addComment(document.createElement('span'));
-        if(eventClassName==='input_upload') {
+        if(eventClassName === 'btn_upload') addComment(document.createElement('span'));
+        if(eventClassName === 'input_upload') {
             if(event.keyCode === 13) addComment(document.createElement('span'));
         }
     }
 
     btnUpload.addEventListener("click", handleComment);
-
     inputUpload.addEventListener("keyup", handleComment);
-
 }
+
+// 리팩토링전
+// window.onload = function() {
+//     let body = document.querySelector('body');
+//     let inputUpload = document.querySelector('.input_upload');
+//     let btnUpload = document.querySelector('.btn_upload');
+//     let contentFlag = false;
+//     let comments = document.querySelector('.comments');
+
+//     const addComment = (el) => {
+//         el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small>`;
+//         if (contentFlag) {
+//             comments.appendChild(el);
+//         }
+//     }
+
+//     inputUpload.addEventListener("keyup", function() {
+//         if (inputUpload.value) {
+//             btnUpload.style.color = '#3e99ed';
+//             contentFlag = true;
+//         } else {
+//             btnUpload.style.color = '#c5e2fa';
+//             contentFlag = false;
+//         }
+//     });
+
+//     btnUpload.addEventListener("click", function() {
+//         addComment(document.createElement('span'));
+//     });
+
+//     inputUpload.addEventListener("keyup", function(e) {
+//         if (e.keyCode === 13) addComment(document.createElement('span'));
+//     });
+// }
