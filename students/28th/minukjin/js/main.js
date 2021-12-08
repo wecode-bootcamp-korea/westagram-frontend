8f46231e859e4f5d74dbfcac04a31b9f2f5e78cb
minuk <alsdnr4320@gmail.com>
2021-12-08 20:02:03 +0900

    [Add]메인 페이지 댓글 기능 추가 & 레이아웃 구조 변경 -메인 페이지의 댓글 기능 추가 -메인 페이지의 레이아웃 구조 변경

--- students/28th/minukjin/js/main.js
+++ students/28th/minukjin/js/main.js
@@ -1,14 +1,50 @@
 const replyInput = document.querySelector('#reply-input');
 const replyBtn = document.querySelector('#reply-button');
-const replyList = document.querySelector('.feed-reply');
+const replyList = document.querySelector('.feed-replys');
+const replyLike = document.querySelector('.fa-heart');
+const replyForm = document.querySelector('.feed-reply-form');
 
 function replyBtnActive() {
-  console.log(replyInput.value);
+  // console.log(replyInput.value);
   return replyInput.value ? replyBtn.style.color = 'blue' : replyBtn.style.color = 'rgb(197, 225, 251)';
 }
-function replySubmit() {
-  replyList.innerHTML =`<p><span></span>${replyInput.value}</p>`;
+// function replySubmit() {
+//   const inputValue = replyInput.value;
+//   const replyAddTag = document.createElement('p')
+//   const replyAddValue = document.createTextNode(inputValue);
+//   replyAddTag.appendChild(replyAddValue);
+//   replyList.appendChild(replyAddTag);
+// }
+
+const replySubmit = () => {
+  const replyAddTag = document.createElement('li');
+  replyAddTag.className = 'feed-reply';
+  replyAddTag.innerHTML = `
+    <div>
+      <span>user</span>
+      <p>${replyInput.value}</p>
+      <i class="far fa-heart"></i>
+      </div>
+    <span class="feed-reply-time">1분전</span>
+  `;
+  replyList.appendChild(replyAddTag);
+  replyInput.value = null;
 }
 
-replyInput.addEventListener('keyup', replyBtnActive);
-replyBtn.addEventListener('submit', replySubmit);
+//  const likeColorToggle = () => {
+  
+//  }
+
+// replyInput.addEventListener('keyup', (e) => {
+//   e.preventDefault();
+//   if (e.keyCode === 13) replySubmit();
+//   else replyBtnActive();
+// });
+replyForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (e.keyCode === 13) replySubmit();
+  else replyBtnActive();
+})
+replyBtn.addEventListener('click', replySubmit);
+
+// replyLike.addEventListener('click', likeColorToggle);
