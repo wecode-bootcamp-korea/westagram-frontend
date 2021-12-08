9423e5aa87d481b4f06eb06b5c9aec9ca101092b
KangHeeWon <kkhw1202@gmail.com>
2021-12-08 22:11:25 +0900

    [Add] main CSS aside, users story section - main 큰 레이아웃 완성 - 회원 추천 aside section CSS 구현 - 회원 스토리 리스트 박스 CSS 구현 - 회원 피드 article 구현중

--- students/28th/heewon/js/login.js
+++ students/28th/heewon/js/login.js
@@ -8,16 +8,17 @@ const loginBtn = document.getElementById('login_btn');
 const isActiveLogin = () => {
     let idValue = idInput.value;
     let pwValue = pwInput.value;
-
-    if(
-        (idValue && pwValue) && (pwValue.length > 4)
-    ) {
-        loginBtn.classList.add('login_active');
-    }
-    else {
-        loginBtn.classList.remove('login_active');
-    }
     
+    (idValue && pwValue) && (pwValue.length > 4) ? loginBtn.classList.add('login_active') : loginBtn.classList.remove('login_active');
+    
+    // if(
+    //     (idValue && pwValue) && (pwValue.length > 4)
+    // ) {
+    //     loginBtn.classList.add('login_active');
+    // }
+    // else {
+    //     loginBtn.classList.remove('login_active');
+    // }
 }
 
 idInput.addEventListener('keyup', isActiveLogin);
---
+++ students/28th/heewon/js/main.js
@@ -0,0 +1,7 @@
+'user static';
+
+const userProfileImg = document.getElementsByClassName('circle_profile_img');
+
+for(let i=0; i<userProfileImg.length; i++) {
+    userProfileImg[i].src = 'https://swisscognitive.ch/wp-content/uploads/2017/07/Walt-Disney-Studios.jpg';
+}
