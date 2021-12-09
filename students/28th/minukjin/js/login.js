3b4caddd8b8a6737f3499d00d3d5493ea3319207
minuk <alsdnr4320@gmail.com>
2021-12-09 09:32:55 +0900

    [Modify] Login 버튼 활성화 로직 수정 -Login 버튼 활성화를 위해 button의 disabled attribute를 사용

--- students/28th/minukjin/js/login.js
+++ students/28th/minukjin/js/login.js
@@ -3,23 +3,14 @@ const $pw = document.querySelector('#pw');
 const loginBtn = document.querySelector('button');
 
 function loginActive() {
-  // const idValue = $id.value;
-  // const pwValue = $pw.value;
   const notEmpty = $id.value && $pw.value;
   
-  return notEmpty 
-    ? loginBtn.style.backgroundColor = 'rgb(47, 122, 192)'
-    : loginBtn.style.backgroundColor = 'rgb(197, 225, 251)';
+  if (notEmpty) {
+    loginBtn.disabled = false;
+    loginBtn.style.backgroundColor = 'rgb(47, 122, 251)'
+  } else {
+    loginBtn.disabled = true;
+    loginBtn.style.backgroundColor = 'rgb(197, 225, 251)'
+  }
 }
 document.body.addEventListener('keyup', loginActive);
-
-//   if (notEmpty) {
-//     loginBtn.style.backgroundColor = 'rgb(47, 122, 192)';
-//   }
-//   if (!notEmpty) {
-//     loginBtn.style.backgroundColor = 'rgb(197, 225, 251)';
-//   }
-
-// document.querySelector('button').addEventListener('click', () => {
-
-// });
