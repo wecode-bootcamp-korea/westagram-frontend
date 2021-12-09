c4160056876defa14fbf1cfad3da5cefc209b2bc
haesoo <wjdgotn77@gmail.com>
2021-12-09 16:26:21 +0900

    [Refactor: makeComment 함수 수정 및 이벤트핸들링 함수 조건 추가]
    makeComment 함수 수정
    - createElement와 appendChild 사용 대신 innerHTML을 통해 더 간결하게 코드를 작성.
    
    handleInput, handleClick 함수 조건 추가
    - 댓글에 value가 입력되어야만 이벤트 실행되도록 조건 추가.

--- students/28th/haesoo/js/main.js
+++ students/28th/haesoo/js/main.js
@@ -6,31 +6,26 @@ commentInput.addEventListener("keyup", handleInput);
 commentButton.addEventListener("click", handleClick);
 
 function handleInput(event) {
-  if (event.keyCode === 13) {
-    const comment = event.target.value;
+  const comment = commentInput.value;
+  if (comment && event.keyCode === 13) {
     makeComment(comment);
-    comment = "";
+    commentInput.value = "";
   }
 }
 
 function handleClick() {
   const comment = commentInput.value;
-  makeComment(comment);
+  if (comment) {
+    makeComment(comment);
+    commentInput.value = "";
+  }
 }
 
 function makeComment(text) {
-  const commentWrapper = document.createElement("div");
-  const id = document.createElement("span");
-  const comment = document.createElement("span");
-
-  id.textContent = "__jaehyunjeong";
-  comment.textContent = text;
-  commentWrapper.className = "comment-wrapper";
-  id.className = "comment-id";
-  comment.className = "comment-text";
-  commentWrapper.appendChild(id);
-  commentWrapper.appendChild(comment);
-
-  commentContainer.appendChild(commentWrapper);
-  commentInput.value = "";
+  return (commentContainer.innerHTML = `
+      <div class="comment-wrapper">
+        <span class="comment-id">__jaehyunjeong</span>
+        <span class="comment-text">${text}</span>
+      </div>
+    `);
 }
