f4ff6fcf794eb4d45a67046bd13adc0bd74f5dab
YunKukPark <a01043340999@gmail.com>
2021-12-09 14:02:31 +0900

    [Add] Login Validate 기능 구현 - id에 @ 가 들어가있지 않으면 통과하지 않습니다 - pw가 5글자 초과가 아니라면 통과하지 않습니다. - validateLogin 함수는 userId와 userPw를 받아 boolean으로 return 해줍니다 - handleLogin 함수는 validateLogin에서 받은 값으로 판별 후 다음페이지로 들어가게 합니다.

--- students/28th/yunkukpark/js/login.js
+++ students/28th/yunkukpark/js/login.js
@@ -4,17 +4,11 @@ const loginButton = document.querySelector('.login-button');
 
 const handleButton = (event) => {
   const isFilled = checkFilled();
-
   if (isFilled) {
     loginButton.disabled = false;
   } else {
     loginButton.disabled = true;
     return;
-  }
-
-  if (event.code === 'Enter') {
-    // TODO: 유효성 검사
-    window.location = './main.html';
   }
 };
 
@@ -26,9 +20,44 @@ const checkFilled = () => {
   return true;
 };
 
+const validateLogin = (id, password) => {
+  const validateId = (id) => {
+    // TODO: 스트링 메소드 특정 문자 찾기 확인할 것
+    if (id.indexOf('@') === -1) {
+      alert('님 아이디에 @ 안씀');
+      return false;
+    }
+    return true;
+  };
+  const validatePassword = (password) => {
+    if (password.length > 5) {
+      return true;
+    }
+    if (password.length <= 5) {
+      alert('님 비밀번호 5글자 이상 아님?');
+      return false;
+    }
+  };
+
+  const isValidId = validateId(id);
+  const isValidPassword = validatePassword(password);
+
+  const isValidated = isValidId && isValidPassword;
+  return isValidated;
+};
+
+const handleLogin = () => {
+  const isValidated = validateLogin(userId.value, userPassword.value);
+  if (isValidated) {
+    window.location = './main.html';
+  }
+};
+
 userId.addEventListener('keyup', handleButton);
 userPassword.addEventListener('keyup', handleButton);
-
-loginButton.addEventListener('click', () => {
-  window.location = './main.html';
+userPassword.addEventListener('keyup', (event) => {
+  if (event.code === 'Enter') {
+    handleLogin();
+  }
 });
+loginButton.addEventListener('click', handleLogin);
