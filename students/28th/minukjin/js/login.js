5ae67827febd5585594a651fd580e93c330b21a9
minuk <alsdnr4320@gmail.com>
2021-12-09 15:57:28 +0900

    [Add] 로그인 페이지id, password validation 추가 -id validation을 정규표현식을 사용하여 검사 -password validation을 정규표현식을 사용하여 검사

--- students/28th/minukjin/js/login.js
+++ students/28th/minukjin/js/login.js
@@ -1,18 +1,35 @@
 const $id = document.querySelector('#id');
 const $pw = document.querySelector('#pw');
 const loginBtn = document.querySelector('button');
-
-function loginActive() {
-  const notEmpty = $id.value && $pw.value;
+const regExpId = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const regExpPassword = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+// function loginActive() {
+//   const notEmpty = $id.value && $pw.value;
   
-  if (notEmpty) {
+//   if (notEmpty) {
+//     loginBtn.disabled = false;
+//     loginBtn.style.backgroundColor = 'rgb(47, 122, 251)'
+//     loginBtn.className = 'btn-active'
+//   } else {
+//     loginBtn.disabled = true;
+//     loginBtn.style.backgroundColor = 'rgb(197, 225, 251)'
+//     loginBtn.className = '';
+//   }
+// }
+
+const loginAvailable = () => {
+  const idValidation = regExpId.test($id.value);
+  const passwordValidation = regExpPassword.test($pw.value);
+  const fullCheckValidation = idValidation && passwordValidation;
+
+  if (fullCheckValidation) {
     loginBtn.disabled = false;
     loginBtn.style.backgroundColor = 'rgb(47, 122, 251)'
     loginBtn.className = 'btn-active'
   } else {
-    loginBtn.disabled = true;
+    loginBtn.disabled = false;
     loginBtn.style.backgroundColor = 'rgb(197, 225, 251)'
-    loginBtn.className = '';
+    loginBtn.className = 'btn-active'
   }
 }
-document.body.addEventListener('keyup', loginActive);
+document.body.addEventListener('keyup', loginAvailable);
