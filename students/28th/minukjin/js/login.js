82cb2d66e91c9cc4c31a2acf8290ab10f27819e0
minuk <alsdnr4320@gmail.com>
2021-12-09 09:46:38 +0900

    [Add] Login 페이지 버튼 hover 시 그림자 효과 추가 -Login 페이지 로그인 버튼 hover 시 box-shadow 효과 추가

--- students/28th/minukjin/js/login.js
+++ students/28th/minukjin/js/login.js
@@ -8,9 +8,11 @@ function loginActive() {
   if (notEmpty) {
     loginBtn.disabled = false;
     loginBtn.style.backgroundColor = 'rgb(47, 122, 251)'
+    loginBtn.className = 'btn-active'
   } else {
     loginBtn.disabled = true;
     loginBtn.style.backgroundColor = 'rgb(197, 225, 251)'
+    loginBtn.className = '';
   }
 }
 document.body.addEventListener('keyup', loginActive);
