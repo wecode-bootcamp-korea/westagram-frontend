1e7ec96982af609bd87f7c64d608b9c6cb9c0d96
minami-cs <mina9301@gmail.com>
2021-12-09 14:10:41 +0900

    Add: 인스타그램 스토리 버튼 클릭시 슬라이드 기능 구현 추가 및 메인 페이지 세부 레이아웃 추가 - 인스타그램 스토리가 길 때 옆으로 가기 버튼 클릭시 옆으로 움직이는 기능 구현 - 사이드메뉴에 로그인한 사용자 계정 및 추천해주는 세부 레이아웃 추가

--- students/28th/minahjung/static/js/main.js
+++ students/28th/minahjung/static/js/main.js
@@ -1,3 +1,32 @@
+// story
+const forwardBtn = document.getElementsByClassName('story_prev')[0];
+const backwardBtn = document.getElementsByClassName('story_next')[0];
+const storyBox = document.querySelector('ul');
+const maxStoryNum = document.querySelectorAll('li').length;
+const storyWidth = document.querySelectorAll('li')[0].clientWidth;
+const storyBoxWidth = storyBox.clientWidth;
+const moveDistance = ((maxStoryNum * storyWidth) - storyBoxWidth);
+
+(function () {
+    forwardBtn.style.display = 'none';
+
+    function slideNextStoryPhoto() {
+        storyBox.style.transform = `translateX(-${moveDistance + 20}px)`;
+        storyBox.style.transition = 'transform 0.3s ease-in-out';
+        forwardBtn.style.display = 'block';
+    }
+
+    function slidePrevStoryPhoto() {
+        storyBox.style.transform = `translateX(${0}px)`;
+        storyBox.style.transition = 'transform 0.3s ease-in-out';
+        forwardBtn.style.display = 'none';
+    }
+
+    backwardBtn.addEventListener('click', slideNextStoryPhoto);
+    forwardBtn.addEventListener('click', slidePrevStoryPhoto);
+})();
+
+// post
 const prevBtn = document.getElementsByClassName('post_prev')[0];
 const nextBtn = document.getElementsByClassName('post_next')[0];
 const container = document.getElementsByClassName('post_photo_container')[0];
