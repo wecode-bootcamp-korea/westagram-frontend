7b7244b25ee6c5fc02a16f2735078e96ded812db
KangHeeWon <kkhw1202@gmail.com>
2021-12-09 21:57:00 +0900

    Add: main 댓글 기능 구현 - Enter 키, 게시 버튼 클릭하면 댓글 생성 - 댓글 입력값 유효성 입력값이 들어와 있는 상태에서 Enter or 버튼 클릭했을 시에만 댓글 게시, 입력 없는 상태일 시 alert 경고창

--- students/28th/heewon/js/main.js
+++ students/28th/heewon/js/main.js
@@ -2,8 +2,12 @@
 
 const userProfileImg = document.getElementsByClassName('circle_profile_img');
 
-for(let i=0; i<userProfileImg.length; i++) {
-    if(i % 2 === 0) {
+const commentInput = document.getElementById('post_comment_input');
+const commentPostBtn = document.getElementsByClassName('post_comment_btn')[0];
+
+// images random
+for (let i = 0; i < userProfileImg.length; i++) {
+    if (i % 2 === 0) {
         userProfileImg[i].src = 'https://images.unsplash.com/photo-1638893427709-28865ba8f183?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80';
 
     } else {
@@ -11,14 +15,53 @@ for(let i=0; i<userProfileImg.length; i++) {
     }
 }
 
-// 댓글 textarea 영역에 코멘트 많이 입력하면 자동으로 사이즈 늘어나기
-// const comment = document.querySelector('.post_comment_wrap textarea');
-// const commentFixHeight = comment.clientHeight;
+// 댓글 기능
+const postCommentInFeed = () => {
+        // 댓글 입력시 요소 생성
+        const addNewComment = () => {
+            const newCommentLocation = document.getElementsByClassName('comment_list')[0];
+            
+            let newComment = document.createElement('li');
+            let userInfoWrap = document.createElement('div');
+            let userName = document.createElement('em'),
+                userComment = document.createElement('span');
+            let commentLikeBtn = document.createElement('button'),
+                commentLikeIcon = document.createElement('i');
+            
+            userInfoWrap.classList.add('user_desc');
+            userName.innerText = 'user01';
+            userComment.innerText = `${commentInput.value}`;
 
-// comment.addEventListener('keydown', () => {
-//     if(commentFixHeight < comment.scrollHeight) {
-//         comment.style.height = `${comment.scrollHeight}px`;
-//     } else {
-//         comment.style.height = 'auto';
-//     }
-// })
+            commentLikeBtn.appendChild(commentLikeIcon);
+            commentLikeIcon.classList.add('far', 'fa-heart', 'fa-xs');
+
+            userInfoWrap.appendChild(userName);
+            userInfoWrap.appendChild(userComment);
+
+            newComment.appendChild(userInfoWrap);
+            newComment.appendChild(commentLikeBtn);
+
+            newCommentLocation.appendChild(newComment);
+            commentInput.value = '';
+        }
+
+        // 사용자 입력 들어올 시, 게시 버튼 활성화
+        commentInput.addEventListener('keyup', (e) => {
+            commentInput.value ? commentPostBtn.style.opacity = '1' : commentPostBtn.style.opacity = '.3';
+            if (e.code === 'Enter' && commentInput.value) {
+                addNewComment();
+            }
+        })
+
+        // 댓글 게시
+        commentPostBtn.addEventListener('click', (e) => {
+            console.log(e);
+            if (commentInput.value) {
+                addNewComment();
+            } else {
+                alert('댓글이 입력되지 않았습니다.');
+            }
+        })
+        }
+
+        postCommentInFeed();
