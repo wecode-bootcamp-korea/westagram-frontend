97a27a1ec708a96dc5e2e425b6d754cb45808545
hyunjea Ann <hyunjeaann@hyunjeaui-MacBookAir.local>
2021-12-09 13:41:02 +0900

    Add: 댓글 좋아요 기능 및 삭제기능 추가 - 댓글란 끝쪽에 하트를 누르면 눌러집니다. - 댓글을 추가한 후 x버튼을 누르면 삭제됩니다.

--- students/28th/hyeonze/js/main.js
+++ students/28th/hyeonze/js/main.js
@@ -4,10 +4,22 @@ window.onload = function() {
     let btnUpload = document.querySelector('.btn_upload');
     let contentFlag = false;
     let comments = document.querySelector('.comments');
-    let article = document.querySelector('article');
-    let feeds = document.querySelector('.feeds');
+    let hearts = document.querySelectorAll('.hearts');
 
-    inputUpload.addEventListener("keyup", function() {
+    // 댓글좋아요기능 / 삭제기능
+    comments.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target.classList.contains('hearts')) { // 좋아요기능
+            e.target.classList.toggle("red");
+            if (e.target.className.startsWith("far")) e.target.className = e.target.className.replace("far", "fas");
+            else if (e.target.className.startsWith("fas")) e.target.className = e.target.className.replace("fas", "far");
+        } else if (e.target.classList.contains('fa-times-circle')) e.target.parentNode.remove();
+    });
+
+    // 댓글 제거기능
+
+    // 게시버튼 활성화 && 내용포함여부확인 기능
+    inputUpload.addEventListener("keydown", function() {
         if (inputUpload.value) {
             btnUpload.style.color = '#3e99ed';
             contentFlag = true;
@@ -18,11 +30,13 @@ window.onload = function() {
     });
 
     const addComment = (el) => {
-        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small><i class="far fa-heart"></i>`;
-        if (contentFlag) {
-            comments.appendChild(el);
-            inputUpload.value = "";
-        }
+        el.innerHTML = `<strong>username </strong>${inputUpload.value}<small> 방금전</small><i class="fas fa-times-circle"></i><i class="far fa-heart hearts"></i>`;
+        if (!contentFlag) return;
+
+        comments.appendChild(el);
+        inputUpload.value = "";
+
+        hearts = document.querySelectorAll('.hearts');
     }
 
     const handleComment = (event) => {
