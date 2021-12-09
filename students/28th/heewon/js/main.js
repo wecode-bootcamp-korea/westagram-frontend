e6c386cb9c6e02b05fcb933189a89a1637d23bb7
KangHeeWon <kkhw1202@gmail.com>
2021-12-09 20:24:59 +0900

    Add: 메인 레이아웃 CSS 구현 - 이미지 랜덤하게 임의 적용 - input, textarea CSS  초기화 - CSS 속성 순서 리팩토링 - 메인 레이아웃 CSS 전체 구현 완료

--- students/28th/heewon/js/main.js
+++ students/28th/heewon/js/main.js
@@ -3,5 +3,22 @@
 const userProfileImg = document.getElementsByClassName('circle_profile_img');
 
 for(let i=0; i<userProfileImg.length; i++) {
-    userProfileImg[i].src = 'https://swisscognitive.ch/wp-content/uploads/2017/07/Walt-Disney-Studios.jpg';
+    if(i % 2 === 0) {
+        userProfileImg[i].src = 'https://images.unsplash.com/photo-1638893427709-28865ba8f183?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80';
+
+    } else {
+        userProfileImg[i].src = 'https://images.unsplash.com/photo-1638986404993-ab1a1be1f8e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80';
+    }
 }
+
+// 댓글 textarea 영역에 코멘트 많이 입력하면 자동으로 사이즈 늘어나기
+// const comment = document.querySelector('.post_comment_wrap textarea');
+// const commentFixHeight = comment.clientHeight;
+
+// comment.addEventListener('keydown', () => {
+//     if(commentFixHeight < comment.scrollHeight) {
+//         comment.style.height = `${comment.scrollHeight}px`;
+//     } else {
+//         comment.style.height = 'auto';
+//     }
+// })
