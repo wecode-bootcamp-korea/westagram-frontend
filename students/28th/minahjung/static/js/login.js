d104d82178a67e482696997a6356956ce7b25a2c
minami-cs <mina9301@gmail.com>
2021-12-10 18:09:05 +0900

    Add: 로그인 입력값 유효성 검사 기능 추가 구현 완료 - 로그인폼에서 id에 @이 포함되어 있고 비밀번호가 5글자 이상일 때만 로그인 버튼이 활성화되도록 하는 유효성 검사 추가 완료

--- students/28th/minahjung/static/js/login.js
+++ students/28th/minahjung/static/js/login.js
@@ -1,32 +1,43 @@
-const user_id = document.getElementById('user_id');
-const user_pw = document.getElementById('user_pw');
-const login_btn = document.getElementsByClassName('login_btn')[0];
-const login_form = document.querySelector('form');
+const userId = document.getElementById('user_id');
+const userPw = document.getElementById('user_pw');
+const loginBtn = document.getElementsByClassName('login_btn')[0];
+const loginForm = document.querySelector('form');
 
 function changeInputAndButton() {
     // padding 변경
     this.value ? this.className = 'change_padding' : this.className = 'default_padding';
 
     // 로그인 버튼 활성화 변경
-    if (user_id.value && user_pw.value) {
-        login_btn.removeAttribute('disabled');
-        login_btn.style.backgroundColor = 'rgba(0, 149, 246, 1)';
+    if (userId.value && userPw.value) {
+        if (IsInputValid() === true) {
+            loginBtn.removeAttribute('disabled');
+            loginBtn.style.backgroundColor = 'rgba(0, 149, 246, 1)';
+        }
     } else {
-        login_btn.setAttribute('disabled', '');
-        login_btn.style.backgroundColor = 'rgba(0, 149, 246, 0.3)';
+        loginBtn.setAttribute('disabled', '');
+        loginBtn.style.backgroundColor = 'rgba(0, 149, 246, 0.3)';
     }
 }
 
-user_id.addEventListener('input', changeInputAndButton);
-user_pw.addEventListener('input', changeInputAndButton);
+// 로그인 입력값 유효성 검사
+function IsInputValid() {
+    if ((userId.value.includes('@')) && (userPw.value.length > 4)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-login_btn.addEventListener('click', function () {
+userId.addEventListener('input', changeInputAndButton);
+userPw.addEventListener('input', changeInputAndButton);
+
+loginBtn.addEventListener('click', function () {
     if (!this.hasAttribute('disabled')) {
         location.href = './main.html';
     }
 });
-login_form.addEventListener('keydown', function (e) {
-    if ((!login_btn.hasAttribute('disabled')) && (e.key === 'Enter')) {
+loginForm.addEventListener('keydown', function (e) {
+    if ((!loginBtn.hasAttribute('disabled')) && (e.key === 'Enter')) {
         location.href = './main.html';
     }
 })
