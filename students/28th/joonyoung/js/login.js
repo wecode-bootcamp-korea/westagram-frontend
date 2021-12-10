6141e79bb311c7a0e4279ab3d3520a4a9de9eb1a
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-12-11 01:41:17 +0900

    댓글 왼쪽으로 드래그하면 삭제버튼 띄우는 로직 구현 위의 로직 수정 중 각 요소마다 eventListener추가에서 요소를 감싸고 있는 컨테이너에 eventListener를 주어 이벤트 전파하는 형식으로 코드 변경중

--- students/28th/joonyoung/js/login.js
+++ students/28th/joonyoung/js/login.js
@@ -7,16 +7,15 @@ let validation = { email: "", password: "" };
 
 loginForm.addEventListener("input", (e) => {
   validation[e.target?.dataset?.type] = e.target.value;
-  if (validation[EMAIL] && validation[PASSWORD].length >= 6) {
+  if (validation[EMAIL].includes("@") && validation[PASSWORD].length >= 6) {
     loginBtn.classList.add(ACTIVE);
   } else loginBtn.classList.remove(ACTIVE);
 });
 loginForm.addEventListener("keydown", (e) => {
-  e.key && loginToMain();
+  e.key === "Enter" && loginToMain();
 });
 
 loginBtn.addEventListener("click", (e) => {
-  // if (loginBtn.classList.contains(ACTIVE)) window.location.href = "./main.html";
   loginToMain();
 });
 
--- students/28th/joonyoung/js/main.js
+++ students/28th/joonyoung/js/main.js
@@ -1,20 +1,29 @@
 // Const varaibles
+let search = [
+  { id: 1, username: "Enna", img: "./public/stories/story1.jpg" },
+  { id: 2, username: "Enna", img: "./public/stories/story2.jpg" },
+];
 const HIDDEN = "hidden";
+const ACTIVE = "active";
+const LIKE = "liked";
+const HEART = "heart-path";
+const FRONT_COMMENT = "comment-front";
 
 // utilities
 const toggleArrow = (arrow) => arrow.classList.toggle(HIDDEN);
+const addClass = (target, classVar) => target.classList.add(classVar);
+const toggleClass = (target, classVar) => target.classList.toggle(classVar);
+const checkElementNotHaveClass = (target, classVar) =>
+  !target.classList.contains(classVar);
+const getElementRect = (ele) => ele.getBoundingClientRect();
 
 // **************************************************
-// [] Nav profile image Dropdown
+// [X] Nav profile image Dropdown
 const mainProfileImage = document.querySelector(".profile-img");
-const dropdown = mainProfileImage.nextElementSibling;
+mainProfileImage.addEventListener("click", () =>
+  toggleClass(mainProfileImage.nextElementSibling, "open")
+);
 
-mainProfileImage.addEventListener("click", (e) => {
-  console.log("img");
-  dropdown.classList.toggle("open");
-});
-
-// **************************************************
 // SECTION: Carousel for story
 // [X] Carousel for stories
 const storyWrapper = document.querySelector(".stories__wrapper");
@@ -26,7 +35,7 @@ const [leftEnd, rightEnd] = [slideStart.left, slideStart.right];
 const offset = storyWrapper.offsetWidth / 2;
 let prevMoved = 0;
 
-// FIXME: 맨 양쪽에 도달 헀을 때, <- , -> 가 없어지고 다른 버튼 클릭시 생기도록
+// FIXME: 맨 양쪽에 도달 헀을 때, <- , -> 가 없어지고 다른 버튼 클릭시 생기도록 안되는 경우가 있는듯?
 storyLeftBtn.addEventListener("click", (e) => {
   const leftOffset = leftEnd - stories.getBoundingClientRect().left;
   if (storyRightBtn.classList.contains(HIDDEN)) toggleArrow(storyRightBtn);
@@ -47,8 +56,7 @@ storyRightBtn.addEventListener("click", (e) => {
   stories.style.transform = `translateX(-${prevMoved}px)`;
 });
 
-// **************************************************
-// [] Carousel for Feed image -> 모든 feed에 대해 시행해야 한다!!
+// SECTION: Carousel for Feed image -> 모든 feed에 대해 시행해야 한다!!
 const feeds = [...document.querySelectorAll(".feed")];
 feeds.map((feed) => {
   const feedCarousel = feed.querySelector(".feed__carousel");
@@ -76,110 +84,196 @@ feeds.map((feed) => {
     }px)`;
   });
 
-  feedIcons.addEventListener("click", (e) => {
-    if (e.target.nodeName !== "path") return;
-    e.target.classList.toggle("like");
-    const likeCounter = e.target.closest("svg").closest("div")
-      .nextElementSibling.firstElementChild;
+  // FIXME: 3) like버튼 클릭시 좋아요 갯수를 동적으로 update
+  const updateLikes = (e) => {
+    if (!checkElementNotHaveClass(e.target, HEART)) return;
+    toggleClass(e.target, LIKE);
+    const likeCounter = e.target.closest(".feed__icons").lastChildElement;
     let currLikes = parseInt(likeCounter.innerHTML.replace(",", "")) + 1;
     if (!e.target.classList.contains("like")) currLikes -= 2;
     likeCounter.innerHTML = currLikes
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  });
+  };
+
+  feedIcons.addEventListener("click", updateLikes);
 
   // **********************************************************
   // [] Comment 작성 -> commnet upload
   const commentsWrapper = feed.querySelector(".comments__wrapper");
-  const commentsList = commentsWrapper.querySelector(".comments");
-  const likes = commentsWrapper.querySelector("p .like-counter");
+  const commentList = commentsWrapper.querySelector(".comments");
   const commentInput = commentsWrapper.querySelector(
     ".comment__input textarea"
   );
   const addCommentBtn = commentsWrapper.querySelector(".add__comment");
-  let commentIdx = 0;
 
-  // SECTION: 1) delete
-  commentsList.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("fa-trash-alt")) return;
-    commentsList.removeChild(e.target.closest("li"));
-  });
+  // SECTION: btn active
+  const activeBtn = (e) => {
+    if (e.target.value) addClass(addCommentBtn, ACTIVE);
+  };
+  commentInput.addEventListener("input", activeBtn);
+
+  // SECTION:  delete comment
+  const deleteComment = (e) => {
+    if (checkElementNotHaveClass(e.target, "fa-trash-alt")) return;
+    commentList.removeChild(e.target.closest("li"));
+  };
+  commentList.addEventListener("click", deleteComment);
+
+  // SECTION: X 버튼 click시 translate원상복구
+  const resetDelete = (e) => {
+    if (checkElementNotHaveClass(e.target, "fa-times")) return;
+    e.target.closest(
+      "section"
+    ).nextElementSibling.style.transform = `translateX(0px)`;
+  };
+  commentList.addEventListener("click", resetDelete);
 
   // SECTION: 2) comment의 길이가 길어진 경우 see-more클릭시 내용볼 수 있게하는 로직
-  commentsList.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("see-more")) return;
-    e.target.classList.add(HIDDEN);
-    e.target.nextElementSibling.classList.remove(HIDDEN);
-  });
+  const seeMoreComment = (e) => {
+    if (checkElementNotHaveClass(e.target, "see-more")) return;
+    toggleClass(e.target, HIDDEN);
+    toggleClass(e.target.nextElementSibling, HIDDEN);
+  };
+  commentList.addEventListener("click", seeMoreComment);
 
   // SECTION 3) heart button click시 liked class 추가
-  commentsList.addEventListener("click", (e) => {
+  const addHeartClass = (e) => {
     if (e.target.nodeName !== "path") return;
-    e.target.classList.toggle("like");
-  });
+    toggleClass(e.target, LIKE);
+  };
+  commentList.addEventListener("click", addHeartClass);
 
   // SECTION 4) enter / button click시 댓글 달리도록 하는 로직
-  commentInput.addEventListener("keydown", (e) => {
-    if (!(e.key === "Enter")) return;
+  const addCommentWithEnter = (e) => {
+    if (e.key !== "Enter") return;
     e.preventDefault();
-    uploadComment("joonyg10", commentInput.value, commentsList);
-    commentInput.value = null;
-    commentsList.style.height = commentsList.scrollHeight;
-  });
-  addCommentBtn.addEventListener("click", (e) => {
-    uploadComment("joonyg10", commentInput.value, commentsList);
-    commentInput.value = null;
-    commentsList.style.height = commentsList.scrollHeight;
-  });
-});
+    uploadComment("joonyg10", commentInput.value);
+    toggleClass(addCommentBtn, ACTIVE);
+  };
+  const addCommentWithBtnClick = (e) => {
+    uploadComment("joonyg10", commentInput.value);
+    toggleClass(e.target, ACTIVE);
+  };
 
-const uploadComment = (user, comment, commentUl) => {
-  if (!comment) return;
-  comment = checkCommentsLen(comment);
-  const newCommentLi = document.createElement("li");
-  newCommentLi.className = "flex mb-1 items-center";
-  newCommentLi.innerHTML = `
-    <strong>${user}</strong>
-    <span>${comment}</span>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6 heart"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="black"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-      />
-    </svg>
-    <i class="fas fa-trash-alt"></i>
-  `;
-  commentUl.appendChild(newCommentLi);
-};
-// // **************************************************
-// TODO:
-// [] 댓글 추가시 높이는 고정! -> 4개 이상일 때, 더보기 눌러 늘리기
-//
-// [] 더보기 클릭시 긴 댓글 그대로 보이게
-// [] textarea.value = ''에서 line break발생...
+  // FIXME:
+  let isMoving = false,
+    dragAnimationID,
+    startPos,
+    currPos,
+    movedBy;
+  const commentBox = getElementRect(commentList.querySelector(".comment"));
+  const minimumRequiredMove = commentBox.width * 0.05;
+  const maxMoved = 132;
+  console.log(minimumRequiredMove, maxMoved, commentBox.width);
+
+  // FIXME:
+  const commentFront = commentList.querySelector("li.comment .comment-front");
+  const commentDelete = commentList.querySelector("li.comment .comment-delete");
+
+  // SECTION: 7-1) relative logic
+  const startCommentDrag = (e) => {
+    if (checkElementNotHaveClass(e.target, FRONT_COMMENT)) return;
+    isMoving = true;
+    startPos = e.pageX;
+    dragAnimationID = requestAnimationFrame(animation);
+  };
+
+  const movingComment = (e) => {
+    if (!isMoving) return;
+    currPos = e.pageX;
+    movedBy = currPos - startPos;
+    if (Math.abs(movedBy) >= minimumRequiredMove) movedBy = -maxMoved;
+    animation();
+  };
+  const endCommentDrag = (e) => {
+    isMoving = false;
+    cancelAnimationFrame(dragAnimationID);
+    moveCommentByCalc();
+  };
+  const animation = () => {
+    moveCommentByCalc();
+    if (isMoving) requestAnimationFrame(animation);
+  };
+  const moveCommentByCalc = () => {
+    commentFront.style.transform = `translateX(${movedBy}px)`;
+  };
+  commentList.addEventListener("mousedown", startCommentDrag);
+  commentList.addEventListener("mousemove", movingComment);
+  commentList.addEventListener("mouseup", endCommentDrag);
+
+  commentInput.addEventListener("keydown", addCommentWithEnter);
+  addCommentBtn.addEventListener("click", addCommentWithBtnClick);
+
+  // FIXME: comments에 drag하는 eventlistener 추가!
+  const uploadComment = (user, comment) => {
+    if (!comment) return;
+    comment = checkCommentsLen(comment);
+    const newCommentLi = document.createElement("li");
+    newCommentLi.className = "comment";
+    newCommentLi.innerHTML = `
+    <section class="comment-delete">
+      <div class="comment-delete comment flex items-center">
+        <i class="fas fa-times"></i>
+        <i class="fas fa-trash-alt"></i>
+      </div>
+    </section>
+    <section class="comment-front flex items-center">
+      <strong>${user}</strong>&nbsp;&nbsp;
+      <span>${comment}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 heart"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="black"
+      >
+        <path
+          class="heart-path"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </svg>
+    </section>
+    `;
+    commentList.appendChild(newCommentLi);
+    commentInput.value = null;
+  };
+
+  // TODO: 5) textarea auto line change
+  const adjustInputHeight = () => {
+    // console.log(e.target.style);
+    // e.target.style.height = `100px`;
+    // e.target.style.height = resizeInput(e.target.style.height) + "px";
+  };
+  commentInput.addEventListener("keyup", adjustInputHeight);
+
+  // TODO: 6) resize textarea input
+  // const resizeInput = (height) => {
+  //   console.log(height);
+  //   let numberOfLineBreak = height.match(/\n/g || []).length;
+  //   console.log(numberOfLineBreak);
+
+  //   // resized Height = line-height * \n count + padding(top, bottom) + min-height
+  //   let resizedHeight = (numberOfLineBreak + 1) * 16 + 32;
+  // };
+});
 
 // // **************************************************
 // // Utility fuctions
 const checkCommentsLen = (comment) => {
   return comment.length > 40
-    ? `<a class='see-more'>더보기</a> <span class='hidden'>${comment}</span>`
+    ? `<a class='see-more'>...더보기</a> <span class='hidden'>${comment}</span>`
     : comment;
 };
 
-// Heart Svg animation function
-window.addEventListener("DOMContentLoaded", () => {
-  const heartBtns = [...document.querySelectorAll(".heart path")];
-  heartBtns.map((heartBtn) => {
-    heartBtn.addEventListener("click", (e) => {
-      e.target.classList.toggle("liked");
-    });
-  });
-});
+// FIXME: Heart Svg animation function
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   const heartBtns = [...document.querySelectorAll(".heart path")];
+//   heartBtns.map((heartBtn) => {
+//     heartBtn.addEventListener("click", addHeartClass);
+//   });
+// });
