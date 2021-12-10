cb76c43ec453d2931947de17fff46ace28d75b39
haesoo <wjdgotn77@gmail.com>
2021-12-10 14:04:24 +0900

    [Add: 로그인 페이지 아이디 및 비밀번호 유효성 검사]
    - id : @가 포함되어야하는 아이디 조건 추가
    - password : 12자리 이상 입력해야하는 패스워드 조건 추가
    
    구현 사항
    - id와 password 인풋에 각각 유효성 검사하는 함수추가
    - 위의 조건이 충족 되었을 때 form의 submit가능한 함수 추가.

--- students/28th/haesoo/js/login.js
+++ students/28th/haesoo/js/login.js
@@ -1,32 +1,73 @@
-const logInForm = document.querySelector(".login-form");
-const loginId = document.querySelector(".login-form__id");
-const loginPassword = document.querySelector(".login-form__password");
+// 로그인 아이디 인풋에서는 아이디 관련 validation 검사
+// 패스워드 인풋에서는 비밀번호 관련 validation 검사
+// 두 개 다 검사 통과하면 disalbed 해제
+
+const loginForm = document.querySelector(".login-form");
+const loginId = document.querySelector(".js-id");
+const loginPassword = document.querySelector(".js-password");
 const loginButton = document.querySelector(".login-button");
+const validIdMessage = document.querySelector(".valid-id-message");
+const validPwMessage = document.querySelector(".valid-password-message");
 
-logInForm.addEventListener("keyup", handleKeyup);
+let isIdValid = false;
+let isPasswordValid = false;
+let isValid = false;
 
-function handleKeyup(event) {
-  const target = event.target;
-  const inputClass = target.className;
-  const isLoginId = "login-form__id";
-  const isLoginPassword = "login-form__password";
+loginForm.addEventListener("submit", handleSubmit);
+loginId.addEventListener("keyup", handleCheckIdValid);
+loginPassword.addEventListener("keyup", handleCheckPwValid);
 
-  if (inputClass === isLoginId || inputClass === isLoginPassword) {
-    if (loginId.value && loginPassword.value) {
-      loginButton.disabled = false;
-    }
+function handleCheckIdValid(event) {
+  const id = event.target.value;
+
+  if (!id.includes("@")) {
+    loginId.style.color = "red";
+    validIdMessage.classList.remove("hide");
+    validIdMessage.classList.add("show");
+    isIdValid = false;
+  } else {
+    loginId.style.color = "blue";
+    validIdMessage.classList.add("hide");
+    validIdMessage.classList.remove("show");
+    isIdValid = true;
   }
 
-  if (!loginButton.disabled) {
-    loginButton.addEventListener("click", handleButtonClick);
-    if (event.keyCode === 13) goMainPage();
+  onLoginButton();
+}
+
+function handleCheckPwValid(event) {
+  const password = event.target.value;
+
+  if (password.length < 12) {
+    validPwMessage.classList.remove("hide");
+    validPwMessage.classList.add("show");
+    isPasswordValid = false;
+  } else {
+    validPwMessage.classList.add("hide");
+    validPwMessage.classList.remove("show");
+    isPasswordValid = true;
+  }
+
+  onLoginButton();
+}
+
+function onLoginButton() {
+  if (isIdValid && isPasswordValid) {
+    loginButton.disabled = false;
+    isValid = true;
+    return;
+  } else {
+    loginButton.disabled = true;
+    isValid = false;
   }
 }
 
-function handleButtonClick() {
-  goMainPage();
-}
+function handleSubmit(event) {
+  event.preventDefault();
 
-function goMainPage() {
-  location.href = "./main.html";
+  if (isValid) {
+    location.href = "/students/28th/haesoo/main.html";
+  } else {
+    alert("아이디와 비밀번호를 확인해주세요.");
+  }
 }
