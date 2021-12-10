c97e77c48395b6bff539e6c70e15ffcea6d812e9
minuk <alsdnr4320@gmail.com>
2021-12-10 10:17:30 +0900

    [Add] Main 페이지 댓글 삭제 기능 추가 & 댓글 좋아요 기능 수정 -댓글 좋아요 기능 수정 -댓글 삭제 기능 추가

--- students/28th/minukjin/js/main.js
+++ students/28th/minukjin/js/main.js
@@ -3,6 +3,7 @@ const replyBtn = document.querySelector('#reply-button');
 const replyList = document.querySelector('.feed-replyList');
 const replyForm = document.querySelector('.feed-reply-form');
 const feedHeart = document.querySelector('.feed-heart');
+const replys = document.querySelectorAll('.fa-times');
 
 const replyBtnActive = () => {
   return replyInput.value ? replyBtn.style.color = 'blue' : replyBtn.style.color = 'rgb(197, 225, 251)';
@@ -15,9 +16,10 @@ const replySubmit = () => {
     <div>
       <span>user</span>
       <p>${replyInput.value}</p>
-      <i class="far fa-heart reply-heart"></i>
+      <i id="heart" class="far fa-heart reply-heart"></i>
     </div>
     <span class="feed-reply-time">1분전</span>
+    <i id="remove" class="fas fa-times"></i>
   `;
   replyList.appendChild(replyAddTag);
   replyInput.value = null;
@@ -28,14 +30,18 @@ const feedHeartToggle = () => {
 };
 
 const replyHeartToggle = (e) => {
-  e.target.classList.toggle('icon-heart');
+  if (e.target.id === 'heart') e.target.classList.toggle('icon-heart');
+};
+
+const replyRemove = (e) => {
+  if (e.target.id === 'remove') e.target.parentNode.remove();
 };
 
 replyForm.addEventListener('submit', (e) => {
   e.preventDefault();
   replySubmit();
 });
-
 replyInput.addEventListener('keyup', replyBtnActive);
 feedHeart.addEventListener('click', feedHeartToggle);
 replyList.addEventListener('click', replyHeartToggle);
+replyList.addEventListener('dblclick', replyRemove);
