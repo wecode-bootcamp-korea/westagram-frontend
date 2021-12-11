4fddb6153d19de144d4d34b186e5e4a1c39d305b
haesoo <wjdgotn77@gmail.com>
2021-12-11 16:24:19 +0900

    [Add: 아이디 검색 기능 구현]
    - 아이디를 담고 있는 배열 선언.
    - 검색창에 텍스트 입력시 해당 텍스트와 일치하는 아이디가 리스트로 보임.

--- students/28th/haesoo/js/main.js
+++ students/28th/haesoo/js/main.js
@@ -2,12 +2,32 @@ const commentContainer = document.querySelector(".js-feed-comment");
 const commentInput = document.querySelector(".js-comment-input");
 const commentButton = document.querySelector(".js-comment-button");
 
-commentInput.addEventListener("keyup", handleInput);
+const searchInput = document.querySelector(".js-search-input");
+const idList = [
+  {
+    id: "kyung___lee",
+    name: "이경진",
+    src: "/students/28th/haesoo/src/story_images/1.JPG",
+  },
+  {
+    id: "sungnam_byun",
+    name: "변성남",
+    src: "/students/28th/haesoo/src/story_images/2.JPG",
+  },
+  {
+    id: "bumikeem",
+    name: "김부미",
+    src: "/students/28th/haesoo/src/story_images/2.JPG",
+  },
+];
+
+commentInput.addEventListener("keydown", handleInput);
 commentButton.addEventListener("click", handleClick);
+searchInput.addEventListener("keyup", handleSearch);
 
 function handleInput(event) {
   const comment = commentInput.value;
-  if (comment && event.keyCode === 13) {
+  if (comment && event.key === "Enter") {
     makeComment(comment);
   }
 }
@@ -40,4 +60,39 @@ function deleteComment(comment) {
   deleteButton.addEventListener("click", () => {
     comment.remove();
   });
+}
+
+function checkedSearchId(word) {
+  // 이벤트가 발생할 때마다 innerHTML을 비워주고 다시 그려준다.
+  const searchList = document.querySelector(".search-list");
+  searchList.innerHTML = "";
+  idList.forEach((info) => {
+    if (info.id.includes(word)) {
+      const li = document.createElement("li");
+      const liContents = `
+        <img alt="profile image"
+          src=${info.src}
+          class="search-image"
+        />
+        <div class="search-profile">
+          <p class="search-id">${info.id}</p>
+          <p class="search-name">${info.name}</p>
+        </div>
+      `;
+      li.innerHTML = liContents;
+      searchList.appendChild(li);
+    }
+  });
+}
+
+function handleSearch() {
+  const searchContainer = document.querySelector(".search-container");
+  const searchingId = searchInput.value;
+
+  if (searchInput.value) {
+    searchContainer.classList.remove("hide");
+    checkedSearchId(searchingId);
+  } else {
+    searchContainer.classList.add("hide");
+  }
 }
