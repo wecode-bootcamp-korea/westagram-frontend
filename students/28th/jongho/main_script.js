8add38d0320644f43b020a8ffc31e3a9c53b3657
myway8907 <myway8907@gmail.com>
2021-12-11 15:30:56 +0900

    [28기 윤종호] navbar 레이아웃 코드 변경 - 웹페이지 스크롤 시에도 상단에 navbar 위치하도록 position: fixed   속성으로 변경 - nav bar 관련 CSS 전체 삭제 및 새로 작성 - 디자인 관련 세부 작업은 미완료, 현재 진행중

--- students/28th/jongho/main_script.js
+++ students/28th/jongho/main_script.js
@@ -3,9 +3,6 @@
 const postBtn = document.getElementsByClassName('postComment')[0];
 const currentComments = document.getElementsByClassName('commentList')[0];
 const addCommentBtn = document.getElementsByClassName('inputComment')[0];
-//댓글목록 요소 가져온 변수
- //입력한 댓글 텍스트 가져옴
-// console.log(postBtn);
 
 postBtn.addEventListener('click', (ev) => {
   const myComment = document.getElementsByClassName('inputComment')[0].value;
