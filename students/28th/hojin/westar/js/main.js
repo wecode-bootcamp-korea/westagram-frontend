2f175ff443b4aa5638d5c0a4ae05b77d9b9662be
cil04105 <hojin6417@gmail.com>
2021-12-11 19:15:38 +0900

    [ADD] 로그인 화면 파일 경로 수정

--- students/28th/hojin/westar/js/main.js
+++ students/28th/hojin/westar/js/main.js
@@ -1,39 +1,18 @@
-// const instaForm = document.getElementsByClassName("comment")[0];
-// const commentsContainer = document.querySelector("#comments");
-
-// comment.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   const commentInput = instaForm.elements.comment; 
-//   addComment(commentInput.value); 
-//   commentInput.value = "";
-// });
-
-
-//   const addComment = (comment) => {
-//   const newComment = document.createElement("li");
-//   const bTag = document.createElement("b");
-//   newComment.append(bTag);
-//   newComment.append(`${comment}`);
-//   commentsContainer.append(newComment);
-// };
-
 const instaForm = document.querySelector("#instaForm");
 const commentsContainer = document.querySelector("#comments");
 
 instaForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const commentInput = instaForm.elements.comment; 
-  addComment(commentInput.value); 
-  commentInput.value = "";
+    const commentInput = instaForm.elements.comment;
+    addComment(commentInput.value);
+    commentInput.value = "";
 });
 
-
-  const addComment = (comment) => {
-  const newComment = document.createElement("li");
-  const bTag = document.createElement("b");
-  newComment.append(bTag);
-  newComment.append(`-${comment}`);
-  commentsContainer.append(newComment);
+const addComment = (comment) => {
+    const newComment = document.createElement("li");
+    const bTag = document.createElement("b");
+    newComment.append(bTag);
+    newComment.append(`abc-123 ${comment}`);
+    commentsContainer.append(newComment);
 }
