c426384a69589e18fb3093c12331bcccf87e2638
seankim1111 <yongsun199@gmail.com>
2021-12-12 18:49:43 +0900

    메인페이지 댓글입력 구현

--- students/28th/yongsunkim/main.js
+++ students/28th/yongsunkim/main.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const commentContainer = document.querySelector(".writeaComment");
+const commentInput = document.querySelector(".commentInput");
+const commentButton = document.querySelector(".commentBtn");
+
+commentInput.addEventListener("keydown", handleInput);
+commentButton.addEventListener("click", handleClick);
+
+function handleInput(event) {
+  const comment = commentInput.value;
+  if (comment && event.key === "Enter") {
+    commentAdd(comment);
+  }
+}
+
+function handleClick() {
+  const comment = commentInput.value;
+  if (comment) {
+    commentAdd(comment);
+  }
+}
+
+function commentAdd(text) {
+  const commentWrapper = document.createElement("ul");
+  commentWrapper.className = "commentWrapper";
+  
+  const comment = `
+    <ul class="newComment">rlaydtjs</ul>
+    <span>${text}<span>
+  `;
+
+  commentWrapper.innerHTML = comment;
+  commentContainer.appendChild(commentWrapper);
+  commentInput.value = "";
+}
