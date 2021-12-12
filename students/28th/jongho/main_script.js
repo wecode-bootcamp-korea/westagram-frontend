4a45e214046713a65397d6ec032964c29488f10e
myway8907 <myway8907@gmail.com>
2021-12-12 12:08:31 +0900

    [필수구현완료] Westagram 구현 완료 - 로그인, 메인페이지 레이아웃 완료 - 댓글 추가 기능 완료 - 필수 요소 완료

--- students/28th/jongho/main_script.js
+++ students/28th/jongho/main_script.js
@@ -1,27 +1,81 @@
 "use strict";
 
-const postBtn = document.getElementsByClassName('postComment')[0];
-const currentComments = document.getElementsByClassName('commentList')[0];
-const addCommentBtn = document.getElementsByClassName('inputComment')[0];
+const input = document.getElementsByClassName('inputComment')[0];
+const pushBtn = document.getElementsByClassName('pushComment-Btn')[0];
+const commentUl = document.getElementsByClassName('commentList')[0];
 
-postBtn.addEventListener('click', (ev) => {
-  const myComment = document.getElementsByClassName('inputComment')[0].value;
-  const newComment = document.createElement('li');
-  if (myComment.length > 0) {
-    newComment.innerHTML = myComment;
-    newComment.setAttribute('class', 'comments');
-    console.log(newComment.value);
-    currentComments.appendChild(newComment);
+pushBtn.addEventListener('click', function(ev) {
+  appendToUl()
+});
+input.addEventListener('keyup', function(ev) {
+  if(ev.key === 'Enter') {
+    appendToUl()
+    resetInput();
   }
-})
+});
 
-addCommentBtn.addEventListener('keypress', (ev) => {
-  const myComment = document.getElementsByClassName('inputComment')[0].value;
-  const newComment = document.createElement('li');
-  if (myComment.length > 0 && ev.key === 'Enter') {
-    newComment.innerHTML = myComment;
-    newComment.setAttribute('class', 'comments');
-    console.log(newComment.value);
-    currentComments.appendChild(newComment);
+function resetInput() {
+  document.getElementsByClassName('inputComment')[0].value = null;
+}
+//댓글 입력 값 가져오기
+function getValue() {
+  return input.value;
+}
+//span 요소 만들어서 입력 값 넣기
+function makeSpanId() {
+  const spanId = document.createElement('span'); //아이디 용 span
+
+  spanId.innerHTML = 'Jongho Yoon'; //아이디 span에 넣기
+  spanId.setAttribute('class', 'commentId accId'); //아이디 용 클래스 입력
+  return spanId;
+}
+
+function makeSpanComment() {
+  const spanComment = document.createElement('span'); //댓글 용 span
+
+  spanComment.innerHTML = getValue(); //댓글멘트 span에 넣기
+  spanComment.setAttribute('class', 'commentSentence contText');//댓글용 클래스 입력
+  return spanComment;
+}
+
+function makeLi() {
+  const liElement = document.createElement('li');
+
+  liElement.setAttribute('class', 'comments');
+
+  return liElement;
+}
+
+function makeLiSet() {
+  const listElement = makeLi();
+  const idElement = makeSpanId();
+  const commentElement = makeSpanComment();
+  const text = getValue();
+
+  commentElement.innerHTML = text;
+
+  listElement.appendChild(idElement);
+  listElement.appendChild(commentElement);
+
+  return listElement;
+}
+
+function appendToUl() {
+  console.log('appendToUl()');
+  if(checkInput()) {
+    commentUl.appendChild(makeLiSet());
+    resetInput();
+  } else {
+    alert('글 입력하라고 피곤하니까!!!!')    
   }
-})
+
+  return;
+}
+// 입력창 유효성 검사 
+function checkInput() {
+  if(!input.value.length) {
+    return false;
+  } else {
+    return true;
+  }
+}
