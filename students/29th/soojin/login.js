1592f7c3aa65aa0f2cc9e3840df799af20745af9
soojjung <89066676+soojjung@users.noreply.github.com>
2022-01-05 16:36:35 +0900

    Add: keyup event

---
+++ students/29th/soojin/login.js
@@ -0,0 +1,7 @@
+
+const thisIsId = document.getElementById('westaId');
+const thisIsPw = document.getElementById('westaPw');
+const thisIsButton = document.getElementById('westaButton');
+
+
+thisIsId.addEventListener('keyup', (e) => console.log(e));
