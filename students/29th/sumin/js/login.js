7a840dee4987f4c5dde0ce544f110dfad94d9338
oh-sumin <osumin740@gmail.com>
2022-01-05 19:21:03 +0900

    세션 전 코드 업로드

---
+++ students/29th/sumin/js/login.js
@@ -0,0 +1,29 @@
+const userId = document.querySelector(".id");
+const userPw = document. querySelector(".pw");
+const loginButton = document.querySelector("button");
+
+loginButton.disabled = true;
+
+function doLogin() {
+    let username = userId.value;
+    let userpw = userPw.value;
+
+    if(username.length>=1 && userpw.length>=1){
+        loginButton.classList.add('btn_change')
+        if (username.indexOf('@')>=1 && userpw.length>=5){
+            loginButton.disabled = false;
+        }
+    } else{
+        loginButton.classList.remove('btn_change')
+    }
+}
+
+function clickLoginButton(event) {
+    event.preventDefault()
+    window.location.href='main.html'
+}
+
+
+userId.addEventListener('input', doLogin);
+userPw.addEventListener('input', doLogin);
+loginButton.addEventListener('click', clickLoginButton);
