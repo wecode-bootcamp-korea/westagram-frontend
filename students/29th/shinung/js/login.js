32376b63c5916ba8ae627df91c994383a517de38
한신웅 <hsuj86@gmail.com>
2022-01-06 17:57:13 +0900

    change main page class name

--- students/29th/shinung/js/login.js
+++ students/29th/shinung/js/login.js
@@ -25,31 +25,32 @@ function handleSubmit() {
   const id = idInput.value;
   const idResult = id.includes('@');
   const password = pwInput.value;
+  const idInputBorder = (pwInput.style.borderColor = 'tomato');
+  const pwInputBorder = (pwInput.style.borderColor = 'tomato');
 
   if (id === '' && password === '') {
     return;
   } else if (idResult && password.length <= 5) {
     notice.innerHTML = '비밀번호가 짧습니다.';
-    pwInput.style.borderColor = 'tomato';
+    idInputBorder;
   } else if (idResult && password !== PASSWORD) {
-    notice.innerHTML = '비밀번호를 확인해 주세요.';
-    pwInput.style.borderColor = 'tomato';
+    notice.innerHTML = '비밀번호를 올바르지 않습니다.';
   } else if (!idResult && password === PASSWORD) {
     notice.innerHTML = '올바른 이메일 형식이 아닙니다.';
-    idInput.style.borderColor = 'tomato';
+    idInputBorder;
   } else if (id !== ID && password === PASSWORD) {
-    notice.innerHTML = '이메일을 확인해 주세요.';
-    idInput.style.borderColor = 'tomato';
+    notice.innerHTML = '이메일을 올바르지 않습니다.';
+    idInputBorder;
   } else if (id === ID && password === PASSWORD) {
     alert('환영합니다.');
     notice.innerHTML = '';
     idInput.value = '';
     pwInput.value = '';
     idInput.style.borderColor = '#d6d6d6';
-    pwInput.style.borderColor = '#d6d6d6';
+    pwInputBorder;
   } else {
-    idInput.style.borderColor = 'tomato';
-    pwInput.style.borderColor = 'tomato';
+    idInputBorder;
+    pwInputBorder;
   }
 }
 
