a32cf7a502d0d52f3540e26df84f8075b7542a6b
hyunkark <hyunkark@gmail.com>
2022-01-06 16:18:36 +0900

    Fixed: 코멘트 박스가 추가되면 코멘트 박스 크기만큼의 div가 그 다음 element를 밀어내지 않는 문제를 수정했습니다.

--- students/29th/hyunkark/js/main.js
+++ students/29th/hyunkark/js/main.js
@@ -1,19 +1,25 @@
-let addCommentButton = document.querySelector('.add-comment');
-let feedContent = document.querySelector('.feed-content');
-let commentInput = document.querySelector('.comment');
-let timeStamp = document.querySelector('.feeds-minute');
+let section = document.querySelector(".feed-section");
+let addCommentButton = document.querySelector(".add-comment");
+let feedContent = document.querySelector(".feed-content");
+let commentInput = document.querySelector(".comment");
+let commentInputContainer = document.querySelector('.comment-container');
+let userName = 'vittoria.kark';
 
-addCommentButton.addEventListener('click', ()=>{
-    if(commentInput.value.length > 0){
-        let newComment = document.createElement('DIV');
-        newComment.textContent = `vittoria.kark  ${commentInput.value}`;
-        newComment.classList.add('comment-hidden');
-        
-        feedContent.appendChild(newComment);
-        
-        commentInput.value = "";
-        return timeStamp.style.marginTop;
-    }
-    }
-)
+addCommentButton.addEventListener("click", () => {
+    let newComment = document.createElement("DIV");
+    newComment.textContent = `${userName} ${commentInput.value}`;
+    newComment.classList.add("comment-hidden");
+    userName.classList.add("bold");
+    section.insertBefore(newComment,commentInputContainer);
+    commentInput.value = "";
+})
 
+commentInput.addEventListener("keydown", (event)=>{
+  if(event.keyCode===13){
+    let newComment = document.createElement("DIV");
+    newComment.textContent = `vittoria.kark  ${commentInput.value}`;
+    newComment.classList.add("comment-hidden");
+    section.insertBefore(newComment,commentInputContainer);
+    commentInput.value = "";
+  }
+})
