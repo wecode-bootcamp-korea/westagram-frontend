56b4e1eb074c03b6dc34d9bf4bb48f6e549cdf10
cozynye <cozynye@gmail.com>
2022-01-06 17:15:43 +0900

    refactor : 셀프 리팩토링 메인 크기 수정해야 함

--- students/29th/sunyeong/js/login.js
+++ students/29th/sunyeong/js/login.js
@@ -1,36 +1,29 @@
-const loginId = document.getElementById('login_id');
-const loginPassword = document.getElementById('login_password');
-const buttonLogin = document.getElementById('button_login');
+const loginId = document.getElementById('login-id');
+const loginPassword = document.getElementById('login-password');
+const buttonLogin = document.getElementById('login-button');
 
-function loginEvent(){
+loginId.addEventListener('keyup', loginEvent);
+loginPassword.addEventListener('keyup', loginEvent);
 
-    if(loginId.value.length >= 1 && loginPassword.value.length >= 1){
-        console.log('조건 맞음')
-        buttonLogin.disabled=false;
-        buttonLogin.style.backgroundColor='#0095F6';
-        
+function loginEvent() {
 
+    if (loginId.value.length >= 1 && loginPassword.value.length >= 1) {
+        buttonLogin.disabled = false;
+        buttonLogin.style.backgroundColor = '#0095F6';
     }
-    
 }
 
-loginId.addEventListener('keyup',loginEvent);
-loginPassword.addEventListener('keyup',loginEvent);
 
-function login(){
-    console.log(loginId.value);
-    console.log(loginId.value.includes('@'));
-    console.log(loginPassword.value);
-
-    if(!loginId.value.includes('@')){
-        alert('@를 포함하세요');
+function login() {
+    if (!loginId.value.includes('@')) {
+        alert('아이디에 @를 포함하세요');
         return;
     }
-    if(loginPassword.value.length<6) {
+    if (loginPassword.value.length < 6) {
         alert('비밀번호를 5자 이상 입력하세요');
         return;
     }
-    location.href="main.html";
+    location.href = "main.html";
 }
 
 
--- students/29th/sunyeong/js/main.js
+++ students/29th/sunyeong/js/main.js
@@ -1,11 +1,10 @@
 window.onload = function () {
-    // 댓글달기 엔터 이벤트
     let writeBox = document.getElementsByClassName("feed-comment-write");
     for (let i = 0; i < writeBox.length; i++) {
         writeBox[i].addEventListener('keydown', function (e) {
             if (e.code === 'Enter') {
                 let addTag = document.createElement('span'); addTag.innerHTML =
-                    `<span><strong>wecode</strong> ${writeBox[i].value}</span><div><i class='fas fa-heart' onclick='likeFn(this)'></i><button onclick=deleteComment(this) 
+                    `<span><strong>wecode</strong> ${writeBox[i].value}</span><div><i class='fas fa-heart' onclick='heartColorChange(this)'></i><button onclick=deleteComment(this) 
                     class = 'border-none background-none'>삭제</button></div>`
                 writeBox[i].parentNode.parentNode.children[5].appendChild(addTag)
                 writeBox[i].parentNode.children[0].value = '';
@@ -13,42 +12,10 @@ window.onload = function () {
         })
     }
 }
+
+const searchButton = document.getElementById('nav-bar-search');
 const navDetail = document.getElementsByClassName('nav-detail')[0];
-navDetail.style.opacity = 0
-// 댓글 게시 버튼 이벤트
-function fnComment(buttonInfo) {
-    let addTag = document.createElement('span');
-    let text = buttonInfo.parentNode.children[0].value;
-    addTag.innerHTML = `<span><strong>wecode</strong> ${text}</span><div><i class='fas fa-heart' onclick='likeFn(this)'></i><button onclick=deleteComment(this) 
-                    class = 'border-none background-none'>삭제</button></div>`
-    buttonInfo.parentNode.parentNode.children[5].appendChild(addTag);
-    buttonInfo.parentNode.children[0].value = '';
 
-}
-
-
-// 하트 색상 변경 이벤트
-function likeFn(heart) {
-    // console.log(heart);
-    // console.dir(heart);
-
-    if (heart.style.color === '' || heart.style.color === 'gray') heart.style.color = 'red';
-    else {
-        heart.style.color = 'gray';
-
-    }
-
-}
-
-// 댓글 삭제 이벤트 
-function deleteComment(comment) {
-    // console.dir(comment)
-    comment.parentNode.parentNode.remove()
-}
-
-
-//search 이벤트
-const searchButton = document.getElementById('input-search');
 const instaId = [
     { id: 'wecode1', introduction: '반갑습니다', img: "./img/example1.jpg" },
     { id: 'wde12', introduction: '위코드', img: "./img/example2.jpg" },
@@ -63,28 +30,44 @@ const instaId = [
     { id: 'banana', introduction: 'bbbbbb', img: "./img/example1.jpg" },
     { id: 'apple', introduction: '@@@~~~~', img: "./img/example2.jpg" }
 ];
-searchButton.addEventListener('keyup', fnSearch);
 
-function fnSearch() {
-    console.dir(searchButton.parentNode)
-    if (searchButton.parentNode.children.length > 1) {
-        console.log('지우기')
-        searchButton.parentNode.children[1].remove();
+navDetail.style.opacity = 0
 
-    }
-    // console.log('search')
-    // console.log(searchButton.value)
+searchButton.addEventListener('keyup', searchId);
+
+function fnComment(buttonInfo) {
+    let addTag = document.createElement('span');
+    let text = buttonInfo.parentNode.children[0].value;
+    addTag.innerHTML = `<span><strong>wecode</strong> ${text}</span><div><i class='fas fa-heart' onclick='heartColorChange(this)'></i><button onclick=deleteComment(this) 
+                    class = 'border-none background-none'>삭제</button></div>`
+    buttonInfo.parentNode.parentNode.children[5].appendChild(addTag);
+    buttonInfo.parentNode.children[0].value = '';
+
+}
+
+function heartColorChange(heart) {
+    if (heart.style.color === '' || heart.style.color === 'gray') heart.style.color = 'red';
+    else heart.style.color = 'gray';
+}
+
+function deleteComment(comment) {
+    comment.parentNode.parentNode.remove()
+}
+
+function searchId() {
+    if (searchButton.parentNode.children.length > 1) searchButton.parentNode.children[1].remove();
+
     let data = instaId.filter(function (v) {
         return v.id.includes(searchButton.value);
     });;
-    // console.log(data);
-    // console.log(searchButton.parentNode)
+
     let divTag = document.createElement('div');
     divTag.className = 'search-list'
     divTag.innerHTML = ''
+
     for (let i = 0; i < data.length; i++) {
         divTag.innerHTML += `<div class="search-list-model">
-                            <img src="${data[i].img}" class="aside-img img-border-circle">
+                            <img alt = 'search-list-img' src="${data[i].img}" class="aside-img img-border-circle">
                             <div class="aside-img-owner">
                                 <p>${data[i].id}</p>
                                 <span>${data[i].introduction}</span>
@@ -95,44 +78,18 @@ function fnSearch() {
 
 }
 
-
-//
-searchButton.addEventListener('click', function (e) {
-    // console.log('서치버튼')
-    // console.log(e)
-    // console.log(e.target)
-    // console.log(e.currentTarget)
-
-})
-
-
 document.addEventListener('click', function (e) {
-    console.dir(e.target.className)
-    console.dir(e.currentTarget)
-    console.log(e.currentTarget)
-    if (e.target.className != 'input-search') {
+    if (e.target.className != '-inputsearch') {
         if (searchButton.parentNode.children.length > 1) {
             searchButton.parentNode.children[1].remove();
-            searchButton.value = ''
+            searchButton.value = '';
         }
 
     }
-    if (e.target.className != 'profile-img') {
-        console.log('여기')
-        console.log(navDetail.style.opacity)
-
-        navDetail.style.opacity = 0
-
-    }
+    if (e.target.className != 'profile-img') navDetail.style.opacity = 0;
 
 })
 
-
-// 네비바 디테일 열기
 function openNavDetail() {
-    console.log('여기')
-    console.log(navDetail.style.opacity)
-    if (navDetail.style.opacity === 0 || navDetail.style.opacity === '')
-        console.log('whrjsans')
     navDetail.style.opacity = 1
 }
