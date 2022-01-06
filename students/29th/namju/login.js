e89df09e7640b6b9c33077cbfb2b94257f05555f
Namju Yun <southpoley@gmail.com>
2022-01-06 16:08:24 +0900

    Refactoring : login 페이지 (html, css, js)

--- students/29th/namju/login.js
+++ students/29th/namju/login.js
@@ -2,12 +2,6 @@ const inputAccount = document.getElementById("inputAccount");
 const inputPassword = document.getElementById("inputPassword");
 const btnLogin = document.getElementById("btnLogin");
 
-// const activateBtnLogin = () => {
-//   inputAccount.value && inputPassword.value
-//     ? (btnLogin.disabled = false)
-//     : (btnLogin.disabled = true);
-// };
-
 const activateBtnLogin = () => {
   inputAccount.value.includes("@") && inputPassword.value.length >= 5
     ? (btnLogin.disabled = false)
