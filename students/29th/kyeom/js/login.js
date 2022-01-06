1b544969f9cf82dc02989a6c1ded2cb1e24809f5
seoltang <yong_drg@naver.com>
2022-01-06 18:14:54 +0900

    modified: refactoring - html img alt 속성 추가 - html img 속성에서 alt가 src보다 먼저 오도록 순서 변경 - css 속성 순서 변경 - css 줄바꿈 - 불필요한 id를 class로 변경 - javascript 주석 삭제 - javascript 함수로 블록 설정

--- students/29th/kyeom/js/login.js
+++ students/29th/kyeom/js/login.js
@@ -1,21 +1,22 @@
-// id, pw 입력하면 로그인 버튼 활성화
-const id = document.querySelector('#id');
-const password = document.querySelector('#password');
-const submit = document.querySelector('#submit');
-submit.disabled = true;
-
-id.addEventListener('input', disableSubmit);
-password.addEventListener('input', disableSubmit);
-function disableSubmit() {
-    (!id.value || !password.value)?
-    submit.disabled = true : submit.disabled = false;
-}
-
-
-// 로그인 폼 submit
+const id = document.querySelector('.id');
+const password = document.querySelector('.password');
+const submit = document.querySelector('.submit');
 const form = document.querySelector('form');
+
+function disableSubmitBtn () {
+    submit.disabled = true;
+    id.addEventListener('input', disableSubmit);
+    password.addEventListener('input', disableSubmit);
+    function disableSubmit() {
+        (!id.value || !password.value)?
+        submit.disabled = true : submit.disabled = false;
+    }
+}
+disableSubmitBtn();
+
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    sessionStorage.setItem('id', id.value); // id 저장
-    location.href = 'main.html'; // 메인페이지로 이동
+    sessionStorage.setItem('id', id.value);
+    location.href = 'main.html';
 });
--- students/29th/kyeom/js/main.js
+++ students/29th/kyeom/js/main.js
@@ -1,5 +1,5 @@
-// 검색창 활성화했을 때 검색 아이콘 숨기기
-function hideSearchIcon () {
+hideSearchIcon();
+function hideSearchIcon() {
     const searchInput = document.querySelector('input.search');
     const searchIcon = document.querySelector('i.fa-search');
     searchInput.addEventListener('focus', function () {
@@ -9,10 +9,9 @@ function hideSearchIcon () {
         searchIcon.classList.remove('hidden');
     });
 }
-hideSearchIcon();
 
 
-// 좋아요 누르기
+like();
 function like() {
     const likeBtn = document.querySelector('.fa-heart');
     likeBtn.addEventListener('click', function (event) {
@@ -20,59 +19,60 @@ function like() {
         event.target.classList.toggle('far');
     });
 }
-like();
 
 
-// 댓글 폼
-const comment = document.querySelector('input.comment');
-const submit = document.querySelector('button.submit');
-const form = document.querySelector('form.comment');
-const id = sessionStorage.getItem('id');
+addComment();
+function addComment() {
+    const comment = document.querySelector('input.comment');
+    const submit = document.querySelector('button.submit');
+    const form = document.querySelector('form.comment');
+    const id = sessionStorage.getItem('id');
 
-// 댓글 게시 버튼 활성화/비활성화
-submit.disabled = true;
-comment.addEventListener('input', function () {
-    !comment.value ?
-    submit.disabled = true : submit.disabled = false;
-});
-
-// 댓글 게시
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const commentSection = document.querySelector('section.uploaded-comment');
-    const commentWrapper = document.createElement('div');
-    const idElement = document.createElement('span');
-    const commentElement = document.createElement('span');
-
-    idElement.innerHTML = id;
-    commentElement.innerHTML = comment.value;
-    comment.value = '';
     submit.disabled = true;
+    comment.addEventListener('input', function () {
+        !comment.value ?
+        submit.disabled = true : submit.disabled = false;
+    });
 
-    commentWrapper.classList.add('uploaded-comment');
-    idElement.classList.add('name');
-    commentElement.classList.add('uploaded-comment');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    commentWrapper.appendChild(idElement);
-    commentWrapper.appendChild(commentElement);
-    commentSection.appendChild(commentWrapper);
-});
+        const commentSection = document.querySelector('section.uploaded-comment');
+        const commentWrapper = document.createElement('div');
+        const idElement = document.createElement('span');
+        const commentElement = document.createElement('span');
 
+        idElement.innerHTML = id;
+        commentElement.innerHTML = comment.value;
+        comment.value = '';
+        submit.disabled = true;
 
-// main-right left 맞추기
-window.addEventListener('load', adjustMainRight);
-window.addEventListener('resize', adjustMainRight);
-function adjustMainRight () {
-    const feedRight = document.querySelector('.feed-container').getBoundingClientRect().right;
-    const mainRight = document.querySelector('.main-right');
-    mainRight.style.left = `${feedRight + 28}px`;
+        commentWrapper.classList.add('uploaded-comment');
+        idElement.classList.add('name');
+        commentElement.classList.add('uploaded-comment');
+
+        commentWrapper.appendChild(idElement);
+        commentWrapper.appendChild(commentElement);
+        commentSection.appendChild(commentWrapper);
+    });
 }
 
 
-// main-right 프로필에 id 나타내기
+showUserId();
 function showUserId() {
+    const id = sessionStorage.getItem('id');
     const profileId = document.querySelector('.id');
     profileId.innerHTML = id;
 }
-showUserId();
+
+
+putMainRightOnRight();
+function putMainRightOnRight() {
+    window.addEventListener('load', adjustMainRight);
+    window.addEventListener('resize', adjustMainRight);
+    function adjustMainRight () {
+        const feedRight = document.querySelector('.feed-container').getBoundingClientRect().right;
+        const mainRight = document.querySelector('.main-right');
+        mainRight.style.left = `${feedRight + 28}px`;
+    }
+}
