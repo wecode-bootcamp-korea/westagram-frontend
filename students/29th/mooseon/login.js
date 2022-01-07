e71e1b873fde7754c354923ed468ec988518356e
mooseon <whomsp@gmail.com>
2022-01-07 13:59:19 +0900

    modify

--- students/29th/mooseon/login.js
+++ students/29th/mooseon/login.js
@@ -1,23 +1,24 @@
+const btnSubmit = document.getElementById('btn');
+const login1 = document.getElementById('login1');
+const login2 = document.getElementById('login2');
 
-    let btnSubmit = document.getElementById("btn");
-    let login1 = document.getElementById("login1");
-    let login2 = document.getElementById("login2");
+login1.addEventListener('keyup', function () {
+  if (login1.value && login2.value) {
+    console.log();
+    btnSubmit.style.backgroundColor = 'blue';
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+    btnSubmit.style.backgroundColor = '#c4e1fb';
+  }
+});
 
-console.log(login1) 
-    login1.addEventListener('keyup',function(){
-        if (login1.value && login2.value) {
-            btnSubmit.style.backgroundColor="blue"
-            btnSubmit.disabled = false;
-        } else {
-            btnSubmit.disabled = true;
-        }        
-    })
-
-    login2.addEventListener('keyup',function(){
-        if (login1.value && login2.value) {
-            btnSubmit.style.backgroundColor="blue"
-            btnSubmit.disabled = false;
-        } else {
-            btnSubmit.disabled = true;
-        }        
-    })
+login2.addEventListener('keyup', function () {
+  if (login1.value && login2.value) {
+    btnSubmit.style.backgroundColor = 'blue';
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+    btnSubmit.style.backgroundColor = '#c4e1fb';
+  }
+});
