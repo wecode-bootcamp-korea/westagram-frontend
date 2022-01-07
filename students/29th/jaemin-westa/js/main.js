2637654407036230e3e9c47baa67faf1f05c3a85
유재민 <yujaemin@yujaemin-ui-MacBookPro.local>
2022-01-07 18:28:43 +0900

    Add : 좋아요 / 삭제 기능 추가 - 좋아요 클릭 : 좋아요 카운팅 +1, 아이콘 활성화, 중복 클릭 방지 - 삭제 버튼 : 댓글 모두 보기 카운팅-1, 댓글 삭제

--- students/29th/jaemin-westa/js/main.js
+++ students/29th/jaemin-westa/js/main.js
@@ -28,21 +28,22 @@ const commentEvent = function(){
     const addCommentWrap = document.createElement('li');
     const addUser = document.createElement('a');
     const addDescription = document.createElement('span');
-    const addIconWrap = document.createElement('a');
-    const addIcon = document.createElement('i');
+    const addIconWrap = document.createElement('span');
+    const addDelete = document.createElement('span');
 
     addCommentWrap.setAttribute('class', 'comment-list-add');
     addUser.setAttribute('class', 'comment-user');
     addDescription.setAttribute('class', 'user-comment');
     addIconWrap.setAttribute('class', 'icon-mini-heart');
-    addIcon.setAttribute('class', 'far fa-heart');
+    addDelete.setAttribute('class', 'delete-btn');
 
     addCommentWrap.appendChild(addUser);
     addCommentWrap.appendChild(addDescription);
     addCommentWrap.appendChild(addIconWrap);
-    addIconWrap.appendChild(addIcon);
     addLocation.appendChild(addCommentWrap);
+    addCommentWrap.appendChild(addDelete);
 
+    addDelete.innerHTML = 'X';
     addUser.innerHTML = 'raccoons';
     addDescription.innerHTML = commnetArea.value;
     commnetArea.value = null;
@@ -51,14 +52,14 @@ const commentEvent = function(){
     postingBtn.style.cursor = 'default';
 
     // [자체 추가] 댓글 추가 시 모두 보기 갯수 증가
-    const commnetCounting = function(){
-        const counting = document.getElementsByClassName('comment-list')[0].children.length;
-        const conutTxt = document.getElementsByClassName('comment-counting')[0];
-        conutTxt.innerHTML = counting;
-    };
-
     commnetCounting();
 }
+
+const commnetCounting = function(){
+    const counting = document.getElementsByClassName('comment-list')[0].children.length;
+    const conutTxt = document.getElementsByClassName('comment-counting')[0];
+    conutTxt.innerHTML = counting;
+};
 
 // [자체 추가] 더 보기 버튼 클릭 시 설명 글 노출
 const moreBtn = document.getElementsByClassName('desc-more-btn')[0];
@@ -72,19 +73,55 @@ moreBtn.addEventListener('click', function(){
 
 // [Mission] 추가 구현 사항 - Mission 8 | nav 프로필 사진 클릭 시 메뉴 박스 생성
 // why...?? 1. 버튼변수에담기 2.버튼클릭시 이벤트발생 3.style로 값 못가져와서 getComputedStyle 사용 4. 상단값은 되고 여기는 왜..?
-
 document.addEventListener("click", function(event){
     const profilePopUpWrap = document.getElementsByClassName('pop-wrap')[0];
     const profilePopBtn = document.getElementsByClassName("pop-click-btn")[0];
-    let targetElement = event.target; // 클릭 했을 때 클릭 되는 요소
+    let clickElement = event.target; // 클릭 했을 때 클릭 되는 요소
 
     do{
-        if(targetElement == profilePopBtn){
+        if(clickElement == profilePopBtn){
             profilePopUpWrap.style.display = "block"
             return;
         }
-        targetElement = targetElement.parentNode;
-    }while (targetElement);
+        clickElement = clickElement.parentNode;
+    }while (clickElement);
 
     profilePopUpWrap.style.display = "none"
 });
+
+// [Mission] 추가 구현 사항 - Mission 6 | 댓글 좋아요 / 삭제 기능
+const DeleteEvent = document.addEventListener('click', function(e){
+    if(e.target.className == 'delete-btn') e.target.parentNode.remove();
+    commnetCounting();
+})
+
+let num = Number(document.getElementsByClassName('like-counting')[0].innerText);
+
+const LikeEvent = document.addEventListener('click', function(e){
+    if(e.target.className == 'icon-mini-heart'){
+        e.target.className = 'icon-mini-heart-on';
+        num+=1;
+    }else if(e.target.className == 'icon-mini-heart-on'){
+        e.target.className = 'icon-mini-heart';
+        num-=1;
+    }
+    document.getElementsByClassName('like-counting')[0].innerText = num
+})
+
+{/* <i class="fas fa-heart"></i> */}
+
+// [Mission] 추가 구현 사항 - Mission 7 | 아이디 검색 기능
+/*
+    실제 인스타 nav 검색창에 아이디를 입력 시 검색 기능이 실행됩니다.
+    아이디 데이터를 담고 있는 배열을 선언해주세요.
+    검색 창에 텍스트 입력 시 배열의 요소 중 해당 텍스트에 일치하는 아이디만 보일 수 있도록 구현해주세요.
+    for 문이 아닌 다른 array method를 사용해 구현해주세요.
+*/
+
+// 실행o : a.style.opacity = ''
+// 1번 : const a = document.getElementsByClassName('클래스명')[0];
+// if(a.style.opacity === "1") 함수호출();
+
+// 실행x : b.style.display = ''
+// 2번 : const b = document.getElementsByClassName('클래스명')[0];
+// if(b.style.display === "none") 함수호출();
