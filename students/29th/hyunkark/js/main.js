b3426afaa4725e9da73bcb8874c344b937f34967
hyunkark <hyunkark@gmail.com>
2022-01-07 10:07:39 +0900

    Refactor: Changed unnecessary let into const

--- students/29th/hyunkark/js/main.js
+++ students/29th/hyunkark/js/main.js
@@ -1,12 +1,14 @@
-let section = document.querySelector(".feed-section");
-let addCommentButton = document.querySelector(".add-comment");
-let feedContent = document.querySelector(".feed-content");
-let commentInput = document.querySelector(".comment");
-let commentInputContainer = document.querySelector('.comment-container');
+const section = document.querySelector(".feed-section");
+const addCommentButton = document.querySelector(".add-comment");
+const feedContent = document.querySelector(".feed-content");
+const commentInput = document.querySelector(".comment");
+const commentInputContainer = document.querySelector('.comment-container');
 let userName = 'vittoria.kark';
 
+
+
 addCommentButton.addEventListener("click", () => {
-    let newComment = document.createElement("DIV");
+    const newComment = document.createElement("DIV");
     newComment.textContent = `${userName} ${commentInput.value}`;
     newComment.classList.add("comment-hidden");
     userName.classList.add("bold");
@@ -16,7 +18,7 @@ addCommentButton.addEventListener("click", () => {
 
 commentInput.addEventListener("keydown", (event)=>{
   if(event.keyCode===13){
-    let newComment = document.createElement("DIV");
+    const newComment = document.createElement("DIV");
     newComment.textContent = `vittoria.kark  ${commentInput.value}`;
     newComment.classList.add("comment-hidden");
     section.insertBefore(newComment,commentInputContainer);
