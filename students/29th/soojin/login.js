23b77ecc69d7b8e0fa319ae61dc5f65856c39f3c
soojjung <89066676+soojjung@users.noreply.github.com>
2022-01-07 10:24:11 +0900

    Add: test login js and submit before wrapup

--- students/29th/soojin/login.js
+++ students/29th/soojin/login.js
@@ -1,20 +1,10 @@
+// below is a test
 
-const thisIsId = document.getElementById('westaId');
-const thisIsPw = document.getElementById('westaPw');
-const thisIsButton = document.getElementById('westaButton');
-thisIsButton.disabled = true;
+// const thisIsId = document.getElementById('westaId');
+// const thisIsPw = document.getElementById('westaPw');
+const thisIsButton = document.getElementsByClassName('westaButton')[0];
+function myFunction() {
+    thisIsButton.style.opacity = '1';
+}; 
 
-thisIsId.addEventListener('input', wannalogin)
-thisIsPw.addEventListener('input', wannalogin)
-
-function wannalogin() {
-    if (!thisIsId.value || !thisIsPw.value) {
-        thisIsButton.disabled = true;
-    } else {
-        thisIsButton.disabled = false;
-        thisIsButton.style.backgroundColor = blue;
-    }
-}
-
-
-// (e) => console.log(e));
+thisIsButton.addEventListener("click", myFunction);
