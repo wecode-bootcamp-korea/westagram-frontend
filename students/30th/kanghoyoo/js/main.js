ec4fbcb667ff44d05ce00deb1b66e06236f1d9ec
kangho <kangsick98@gmail.com>
2022-02-10 19:38:43 +0900

    댓글기능

--- students/30th/kanghoyoo/js/main.js
+++ students/30th/kanghoyoo/js/main.js
@@ -1,4 +1,31 @@
-function toggleImg() {
-  bt = document.getElementsByClassName("fa-heart");
-  bt.style.color = "red";
+const textBox = document.getElementsByClassName("textbox")[0];
+const searchImg = document.getElementsByClassName("fa-search")[0];
+const textBoxValue = textBox.value;
+
+textBox.addEventListener("keydown", function () {
+  const callbackSearch = () => {
+    if (textBox.value === "") {
+      searchImg.visibility = "visible";
+    } else if (textBox.value !== "") {
+      searchImg.style.visibility = "hidden";
+    }
+  };
+  callbackSearch();
+});
+
+// function toggleImg() {
+//   bt = document.getElementsByClassName("fa-heart");
+//   bt.style.color = "red";
+// }
+
+const ment = document.getElementsByClassName("mentsWrap")[1];
+function mentSend() {
+  const mentGet = document.getElementsByClassName("commentInput")[0].value;
+  const mentTimes = document.getElementsByClassName("mentTime")[0];
+  ment.innerHTML +=
+    '<p class="mentsMoreWrap"><span class="commentId">Kangho_Yoo</span><span class="comment">' +
+    mentGet +
+    "</span></p>";
+  mentTimes.innerText = "1초 전";
+  mentGet = "";
 }
