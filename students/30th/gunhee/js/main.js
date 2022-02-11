07ae6213751eae91a4f4ac2fb21847076146dd63
gunhee-jeong <kyle4619@naver.com>
2022-02-11 14:11:10 +0900

    add

---
+++ students/30th/gunhee/js/main.js
@@ -0,0 +1,7 @@
+const commentInput = document.querySelector('textInput');
+// const commentInput = document.getElementsByClassName('textInput')[0];
+console.dir(commentInput);
+
+// const createCom = ;
+
+commentInput.addEventListener('keyup', function() {}) 
