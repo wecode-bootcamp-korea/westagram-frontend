e1178d09b0281b27efa03ef11471ccea20d28a82
Bueong <pcj0308@gmail.com>
2022-02-12 01:12:16 +0900

    Add: main.js comment 작업 마무리 & comment의 등록 및 auto scrolling 추가

--- students/30th/cheoljinpark/js/main.js
+++ students/30th/cheoljinpark/js/main.js
@@ -1,25 +1,50 @@
 const commentInput = document.getElementsByClassName('comment-text')[0];
-const commentBtn = document.getElementsByClassName('comment-add');
-
-function submit() {
-  const comment = document.getElementsByClassName('comment-text')[0];
-  const commentList = document.getElementsByClassName('feed-commentlist')[0];
-  
-  const newcomment = commentList.appendChild("li");
-  
-  newcomment.value = comment.value;
-}
+const commentBtn = document.getElementsByClassName('comment-add')[0];
 
 console.log(commentBtn);
 
-// commentBtn.addEventListener('click', (e) => {
-//   submit();
-//   commentInput.value = '';
-// });
+function submit() {
+  const commentList = document.getElementsByClassName('feed-commentlist')[0];
+  const newCommentBox = document.createElement('div');
+  const commentName = document.createElement('span');
+  const newComment = document.createElement('li');
+  
+  newCommentBox.classList.add('new-commentbox');
 
-// commentInput.addEventListener('keydown', (e) => {
-//   if (e.code === 'Enter') {
-//     submit();
-//     commentInput.value = '';
-//   }
-// });
+  commentList.appendChild(newCommentBox);
+  newCommentBox.appendChild(commentName);
+  newCommentBox.appendChild(newComment);
+
+  commentName.innerText = "Bueong "
+  newComment.innerText = document.getElementsByClassName('comment-text')[0].value;
+}
+
+function fadeIn() {
+  const newComments = document.querySelectorAll('.new-commentbox');
+  const lastNewComment = newComments[newComments.length - 1];
+
+  console.log(newComments[newComments.length - 1].classList.add('comment-fadein'));
+}
+
+function scrollingDown() {
+  const commentList = document.getElementsByClassName('feed-commentlist')[0];
+  const newComments = document.getElementsByClassName('new-commentbox')[0];
+
+  commentList.scrollTop = commentList.scrollHeight;
+}
+
+commentBtn.addEventListener('click', () => {
+  submit();
+  setTimeout(fadeIn, 1);
+  scrollingDown()
+  commentInput.value = '';
+});
+
+commentInput.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    submit();
+    setTimeout(fadeIn, 1);
+    scrollingDown()
+    commentInput.value = '';
+  }
+});
