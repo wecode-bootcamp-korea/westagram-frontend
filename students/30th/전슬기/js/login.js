ae16f43d49e411c1d8ca48fb51523ac5e28b387e
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-02-11 10:14:14 +0900

    Add : main page active button color

--- students/30th/전슬기/js/login.js
+++ students/30th/전슬기/js/login.js
@@ -2,16 +2,6 @@ const inputId = document.querySelector("#loginId");
 const inputPw = document.querySelector("#loginPassword");
 const btnLogin = document.querySelector("#loginButton");
 
-// function activeBtn() {
-//   if (!inputId.value || !inputPw.value) {
-//     btnLogin.style.backgroundColor = "#c5e1fb";
-//     btnLogin.disabled = true;
-//   } else {
-//     btnLogin.style.backgroundColor = "#0084ff";
-//     btnLogin.disabled = false;
-//   }
-// }
-
 function inactiveBtn() {
   btnLogin.style.backgroundColor = "#c5e1fb";
   btnLogin.disabled = true;
@@ -27,9 +17,7 @@ function moveToMain(event) {
   location.href = "main.html";
 }
 
-// window.addEventListener("keyup", activeBtn);
 window.addEventListener("keyup", () => {
   inputId.value && inputPw.value ? activeBtn() : inactiveBtn();
 });
-
 btnLogin.addEventListener("click", moveToMain);
--- students/30th/전슬기/js/main.js
+++ students/30th/전슬기/js/main.js
@@ -2,13 +2,24 @@ const commentInput = document.querySelector(".comment");
 const commentBtn = document.querySelector(".postButton");
 const commentsDiv = document.querySelector(".comments");
 
-function postComment(event) {
-  event.preventDefault();
-  const newComment = document.createElement("p");
-  const userName = document.querySelector(".user").innerHTML;
-  newComment.innerHTML = `<strong>${userName}</strong> ${commentInput.value}`;
-  commentsDiv.appendChild(newComment);
-  commentInput.value = "";
+function activeBtn() {
+  if (commentInput.value) {
+    commentBtn.style.color = "#3c99f0";
+  } else {
+    commentBtn.style.color = "#c5e1fb";
+  }
 }
 
+function postComment(event) {
+  event.preventDefault();
+  if (commentInput.value) {
+    const newComment = document.createElement("p");
+    const userName = document.querySelector(".user").innerHTML;
+    newComment.innerHTML = `<strong>${userName}</strong> ${commentInput.value}`;
+    commentsDiv.appendChild(newComment);
+    commentInput.value = "";
+  }
+}
+
+commentInput.addEventListener("keyup", activeBtn);
 commentBtn.addEventListener("click", postComment);
