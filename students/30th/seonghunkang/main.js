143b50fac09af91025307fe959664bd79f35e337
Seonghun Kang <gceohoony@gmail.com>
2022-02-11 20:54:49 +0900

    Add: function of adding a comment

---
+++ students/30th/seonghunkang/main.js
@@ -0,0 +1,54 @@
+const comment_btn = document.querySelector(".comment_input");
+const post_btn = document.querySelector("#post_button");
+console.log(comment_btn);
+// const delete_btn = document.querySelector(".delete");
+
+
+function register_comment() {
+    let comment = $("#comment_input").val();
+    console.log(comment);
+    let delete_btn = $(".delete").val();
+    let temp_html = `<ul id="new_comment">
+                        <li class = "a_comment">
+                        <span>wecode_bootcamp</span>
+                        <span>${comment}</span>
+                        </li>
+                        <li>
+                        <span>${delete_btn}</span>
+                        </li>
+                    </ul>  
+    `;
+    $("#comment_box").append(temp_html);
+
+}
+
+post_btn.addEventListener("click", register_comment);
+
+
+
+// userId.innerText, userID.className
+
+// registercomment(value)
+// checkInput() --> input.length === 0
+// document.createElement("span")
+// deletebtn.classList.add("delete");
+//deletebtn.innerText = 'x';
+//deletebtn.classList.add("delete");
+
+
+//newCommend.appendChild(userId)
+//newCommend.appendChild(commentecontent)
+//newCommend.appendChild(newCommentList)
+
+//input.value = "";
+
+
+//dev tool 사용할것!
+
+
+//makeDeleteEvent(newCOmmentlist){}
+//
+
+// 입력란에 남아있는것도 없애야함 ~
+//input 의 value를 비워주면 됨 
+//input.value = ""
