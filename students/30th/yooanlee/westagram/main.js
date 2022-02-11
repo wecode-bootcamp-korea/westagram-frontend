70d3b3105146c60b4838fd73671709d8007d6a80
yaleeannie <yaleeannie@gmail.com>
2022-02-11 14:17:05 +0900

    update for session

---
+++ students/30th/yooanlee/westagram/main.js
@@ -0,0 +1,6 @@
+let commentInput = document.getElementsByClassName("main-icon-like-text-write_comment"[0];
+let submitBtn = document.getElementsByClassName("main_comment_submit"[0]);
+
+function submit (){
+    const commentIn
+}
