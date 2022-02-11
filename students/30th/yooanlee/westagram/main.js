72f9914e7e68cecfce9a0f1edd5253583e8fcddf
yaleeannie <yaleeannie@gmail.com>
2022-02-11 18:12:26 +0900

    login html added

--- students/30th/yooanlee/westagram/main.js
+++ students/30th/yooanlee/westagram/main.js
@@ -2,5 +2,5 @@ let commentInput = document.getElementsByClassName("comment"[0];
 let submitBtn = document.getElementsByClassName("comment-submit"[0]);
 
 function submit (){
-    const commentIn
+    const document.getElementsByClassName()
 }
