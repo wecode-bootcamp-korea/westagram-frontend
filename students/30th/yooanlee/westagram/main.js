7a10a3fa9d3abeb8e4015cd5b18bcab9f02e45b4
yaleeannie <yaleeannie@gmail.com>
2022-02-14 20:20:06 +0900

    Modify: Login page, main page’

--- students/30th/yooanlee/westagram/main.js
+++ students/30th/yooanlee/westagram/main.js
@@ -1,6 +1,59 @@
-let commentInput = document.getElementsByClassName("comment"[0];
-let submitBtn = document.getElementsByClassName("comment-submit"[0]);
+'use strict';
 
-function submit (){
-    const document.getElementsByClassName()
+const commentInput = document.getElementById("name");
+const submit = document.getElementById("submit");
+
+function checkInput() {
+    if(!commentInput.value.length) {
+        alert("댓글을 입력하세요");
+    }else{
+        registerComment(commentInput.value);
+    }
 }
+function registerComment(value){
+    const commentLists = document.getElementById("commentLists");
+    const newCommentList = document.createElement("li");
+    const newComment = `<span class="name">jiwon</span><span>${value}</span><span class="delete">X</span>`
+    
+    newCommentList.innerHTML = newComment;
+    deleteComment(newCommentList);
+    commentLists.appendChild(newCommentList);
+    commentInput.value = "";
+}
+
+function deleteComment(newCommentList) {
+    const deleteBtn = newCommentList.querySelector(".delete");
+    deleteBtn.addEventListener("click", () => newCommentList.remove());
+}
+
+
+commentInput.addEventListener('keyup', () => {
+    if (window.event.code === 'Enter') {
+        checkInput();
+    }else {
+        submit.addEventListener("click", checkInput);
+    }
+});
+
+// const init = () => {
+//     submit.addEventListener("click", checkInput);
+// };
+// init();
+
+const inputId = document.querySelector("#name");
+const button = document.querySelector('.upload');
+
+function commentBtn() {
+    let idValu = inputId.value;
+
+    if(idValu.length > 0) {
+        button.disabled = false;
+        button.style.cursor = 'pointer';
+        button.style.color = '#1c7ed6';
+    } else {
+        button.disabled = true;
+        button.style.cursor = 'default';
+        button.style.color = 'skyblue  ';
+    }
+}
+inputId.addEventListener('keyup', commentBtn);
