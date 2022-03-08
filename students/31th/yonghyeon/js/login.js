19c8ea24d902eab70a3396368fd1d74269f873d8
sunnyfterrain <sunnyfterrain@gmail.com>
2022-03-08 15:20:21 +0900

    Add: 댓글 기능 구현 및 유효성검사 구현 - [Mission4] 댓글 내용 입력 후 Enter 혹은 버튼 클릭시 댓글 추가 기능 - [추가구현 Mission5] ID,PW 유효성 검사 구현 (id에 @포함 및 pw 5자 이상)

--- students/31th/yonghyeon/js/login.js
+++ students/31th/yonghyeon/js/login.js
@@ -1,11 +1,12 @@
+'use strict';
+
 const inputId = document.querySelector('.loginForm_id');
 const inputPw = document.querySelector('.loginForm_password');
 const loginBtn = document.querySelector('.loginForm_loginBtn');
 
-function doLogin() {
+const doLogin = () => {
   const id = inputId.value;
   const pw = inputPw.value;
-
   if (id.length >= 1 && pw.length >= 1) {
     loginBtn.disabled = false;
     loginBtn.style.curser = 'pointer';
@@ -13,7 +14,26 @@ function doLogin() {
     loginBtn.disabled = true;
     loginBtn.style.curser = 'default';
   }
-}
+};
+
+const doValidation = () => {
+  const id = inputId.value;
+  const pw = inputPw.value;
+  if (!id.includes('@') && pw.length > 5) {
+    alert(`이메일에 '@'가 포함되어야 합니다.`);
+  } else if (pw.length < 5 && id.includes('@')) {
+    alert('비밀번호가 5자리 이상이어야 합니다.');
+  }
+
+  if (!id.includes('@') && pw.length < 5) {
+    alert(`이메일에 '@'가 포함되어야 하고, 비밀번호가 5자리 이상이어야 합니다.`);
+  }
+};
 
 inputId.addEventListener('keyup', doLogin);
 inputPw.addEventListener('keyup', doLogin);
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  doValidation();
+});
--- students/31th/yonghyeon/js/main.js
+++ students/31th/yonghyeon/js/main.js
@@ -1,3 +1,23 @@
+'use strict';
+
+const input = document.querySelector('.commentInput');
+const form = document.querySelector('.commentForm');
+const ul = document.querySelector('.footer__comment');
+
+function addComment(e) {
+  e.preventDefault();
+  const li = document.createElement('li');
+  li.innerHTML = `<span>${input.value}</span>
+  <div class="btnContainer">
+    <div class="likeBtn"></div>
+    <button class="delCommentBtn"></button>
+  </div>`;
+  ul.appendChild(li);
+  input.value = '';
+}
+
+form.addEventListener('submit', addComment);
+
 // function addItemHandler() {
 //   const div = document.createElement('div');
 //   const span = document.createElement('span');
@@ -24,102 +44,102 @@
 
 // btn.addEventListener('click', addItemHandler);
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  onAdd();
-});
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   onAdd();
+// });
 
-items.addEventListener('click', (e) => {
-  const id = e.target.dataset.targetId;
-  //* 클릭하는 곳의 id 값을 저장
+// items.addEventListener('click', (e) => {
+//   const id = e.target.dataset.targetId;
+//   //* 클릭하는 곳의 id 값을 저장
 
-  if (id) {
-    //* id가 존재 하는 곳만 클릭 이벤트를 적용
-    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
-    console.log(toBeDeleted);
-    toBeDeleted.remove();
-    //* 클릭하는 곳의 id값과 같은 item__row를 삭제
-  }
-});
+//   if (id) {
+//     //* id가 존재 하는 곳만 클릭 이벤트를 적용
+//     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+//     console.log(toBeDeleted);
+//     toBeDeleted.remove();
+//     //* 클릭하는 곳의 id값과 같은 item__row를 삭제
+//   }
+// });
 
-function createItem(text) {
-  let id = Date.now();
-  //* id 값을 생성
-  const itemRow = document.createElement('li');
-  itemRow.classList.add('item__row');
-  itemRow.setAttribute('data-id', id);
-  //* id 값을 data-id 속성에 넣어줌
+// function createItem(text) {
+//   let id = Date.now();
+//   //* id 값을 생성
+//   const itemRow = document.createElement('li');
+//   itemRow.classList.add('item__row');
+//   itemRow.setAttribute('data-id', id);
+//   //* id 값을 data-id 속성에 넣어줌
 
-  itemRow.innerHTML = `<div class="item">
-  <span class="item__name">${text}</span>
-  <button class="item__delete" data-target-id=${id}>삭제</button>
-  </div>
-  <div class="item__divider"></div>`;
+//   itemRow.innerHTML = `<div class="item">
+//   <span class="item__name">${text}</span>
+//   <button class="item__delete" data-target-id=${id}>삭제</button>
+//   </div>
+//   <div class="item__divider"></div>`;
 
-  // const item = document.createElement('div');
-  // item.classList.add('item');
+// const item = document.createElement('div');
+// item.classList.add('item');
 
-  // const span = document.createElement('span');
-  // span.classList.add('item__name');
-  // span.textContent = text;
+// const span = document.createElement('span');
+// span.classList.add('item__name');
+// span.textContent = text;
 
-  // const deleteBtn = document.createElement('button');
-  // deleteBtn.classList.add('item__delete');
-  // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+// const deleteBtn = document.createElement('button');
+// deleteBtn.classList.add('item__delete');
+// deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-  // deleteBtn.addEventListener('click', (e) => {
-  //   items.removeChild(itemRow);
-  // });
+// deleteBtn.addEventListener('click', (e) => {
+//   items.removeChild(itemRow);
+// });
 
-  // const itemDivider = document.createElement('div');
-  // itemDivider.classList.add('item__divider');
+// const itemDivider = document.createElement('div');
+// itemDivider.classList.add('item__divider');
 
-  // item.appendChild(span);
-  // item.appendChild(deleteBtn);
+// item.appendChild(span);
+// item.appendChild(deleteBtn);
 
-  // itemRow.appendChild(item);
-  // itemRow.appendChild(itemDivider);
+// itemRow.appendChild(item);
+// itemRow.appendChild(itemDivider);
 
-  return itemRow;
-}
+//   return itemRow;
+// }
 
-function createItem(text) {
-  let id = Date.now();
-  //* id 값을 생성
-  const itemRow = document.createElement('li');
-  itemRow.classList.add('item__row');
-  itemRow.setAttribute('data-id', id);
-  //* id 값을 data-id 속성에 넣어줌
+// function createItem(text) {
+//   let id = Date.now();
+//   //* id 값을 생성
+//   const itemRow = document.createElement('li');
+//   itemRow.classList.add('item__row');
+//   itemRow.setAttribute('data-id', id);
+//   //* id 값을 data-id 속성에 넣어줌
 
-  itemRow.innerHTML = `<div class="item">
-  <span class="item__name">${text}</span>
-  <button class="item__delete" data-target-id=${id}>삭제</button>
-  </div>
-  <div class="item__divider"></div>`;
+//   itemRow.innerHTML = `<div class="item">
+//   <span class="item__name">${text}</span>
+//   <button class="item__delete" data-target-id=${id}>삭제</button>
+//   </div>
+//   <div class="item__divider"></div>`;
 
-  // const item = document.createElement('div');
-  // item.classList.add('item');
+// const item = document.createElement('div');
+// item.classList.add('item');
 
-  // const span = document.createElement('span');
-  // span.classList.add('item__name');
-  // span.textContent = text;
+// const span = document.createElement('span');
+// span.classList.add('item__name');
+// span.textContent = text;
 
-  // const deleteBtn = document.createElement('button');
-  // deleteBtn.classList.add('item__delete');
-  // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+// const deleteBtn = document.createElement('button');
+// deleteBtn.classList.add('item__delete');
+// deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-  // deleteBtn.addEventListener('click', (e) => {
-  //   items.removeChild(itemRow);
-  // });
+// deleteBtn.addEventListener('click', (e) => {
+//   items.removeChild(itemRow);
+// });
 
-  // const itemDivider = document.createElement('div');
-  // itemDivider.classList.add('item__divider');
+// const itemDivider = document.createElement('div');
+// itemDivider.classList.add('item__divider');
 
-  // item.appendChild(span);
-  // item.appendChild(deleteBtn);
+// item.appendChild(span);
+// item.appendChild(deleteBtn);
 
-  // itemRow.appendChild(item);
-  // itemRow.appendChild(itemDivider);
+// itemRow.appendChild(item);
+// itemRow.appendChild(itemDivider);
 
-  return itemRow;
-}
+//   return itemRow;
+// }
