71f34edc047f6a4523c8d677c0916e87f8b08375
sunnyfterrain <sunnyfterrain@gmail.com>
2022-03-08 21:07:30 +0900

    Add: 댓글 좋아요/삭제 기능 추가 - [Mission6] 댓글 좋아요 및 삭제 기능 추가

--- students/31th/yonghyeon/js/login.js
+++ students/31th/yonghyeon/js/login.js
@@ -4,9 +4,18 @@ const inputId = document.querySelector('.loginForm_id');
 const inputPw = document.querySelector('.loginForm_password');
 const loginBtn = document.querySelector('.loginForm_loginBtn');
 
+// id 1자 이상, pw 1자 이상 일시 로그인 버튼 활성화
 const doLogin = () => {
   const id = inputId.value;
   const pw = inputPw.value;
+  if (id.charAt(0) === ' ') {
+    alert('이메일 첫글자가 공백입니다.');
+    inputId.value = '';
+  } else if (pw.charAt(0) === ' ') {
+    alert('비밀번호 첫글자가 공백입니다.');
+    inputPw.value = '';
+  }
+
   if (id.length >= 1 && pw.length >= 1) {
     loginBtn.disabled = false;
     loginBtn.style.curser = 'pointer';
@@ -16,6 +25,7 @@ const doLogin = () => {
   }
 };
 
+// id('@' 유무), pw(5글자 이상) 유효성 검사
 const doValidation = () => {
   const id = inputId.value;
   const pw = inputPw.value;
@@ -30,9 +40,11 @@ const doValidation = () => {
   }
 };
 
+// 로그인버튼 활성화 이벤트
 inputId.addEventListener('keyup', doLogin);
 inputPw.addEventListener('keyup', doLogin);
 
+// Id, Pw 유효성 검사 이벤트
 loginBtn.addEventListener('click', (e) => {
   e.preventDefault();
   doValidation();
--- students/31th/yonghyeon/js/main.js
+++ students/31th/yonghyeon/js/main.js
@@ -3,143 +3,95 @@
 const input = document.querySelector('.commentInput');
 const form = document.querySelector('.commentForm');
 const ul = document.querySelector('.footer__comment');
+const likeBtn = document.querySelector('.likeBtn');
+let count = 0;
 
-function addComment(e) {
-  e.preventDefault();
+// 댓글 추가 기능
+const addComment = (e) => {
+  count += 1;
+  const blank = /^\s+|\s+$/g;
+  if (!input.value) {
+    return;
+  }
+
+  if (input.value.replace(blank, '') === '') {
+    return;
+  }
+
+  const dataId = Date.now();
   const li = document.createElement('li');
+  li.setAttribute('data-id', dataId);
+  li.setAttribute('class', 'commentLi');
   li.innerHTML = `<span>${input.value}</span>
-  <div class="btnContainer">
-    <div class="likeBtn"></div>
-    <button class="delCommentBtn"></button>
-  </div>`;
-  ul.appendChild(li);
+                  <div class="btnContainer">
+                  <div class="likeBtn notLike" data-target-key=${count}></div>
+                  <button class="delCommentBtn" data-target-id=${dataId}></button>
+                  </div>`;
   input.value = '';
-}
+  ul.appendChild(li);
+  li.scrollIntoView({ block: 'center' });
 
-form.addEventListener('submit', addComment);
+  if (ul.offsetHeight >= 140) {
+    ul.classList.add('overflow');
+  }
 
-// function addItemHandler() {
-//   const div = document.createElement('div');
-//   const span = document.createElement('span');
-//   const delBtn = document.createElement('button');
-//   const li = document.querySelector('.item__row');
-//   const divider = document.createElement('div');
+  // appendChild 로 만듦
 
-//   div.setAttribute('class', 'item');
-//   span.textContent = input.value;
-//   span.setAttribute('class', 'item__name');
+  // const span = document.createElement('span');
+  // span.textContent = input.value;
 
-//   delBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-//   delBtn.setAttribute('class', 'item__delete');
+  // const div = document.createElement('div');
+  // div.setAttribute('class', 'btnContainer');
+  // const likeDiv = document.createElement('div');
+  // likeDiv.setAttribute('class', 'likeBtn');
+  // const btn = document.createElement('button');
+  // btn.setAttribute('class', 'delCommentBtn');
+  // btn.setAttribute('data-target-id', dataId);
+  // div.appendChild(likeDiv);
+  // div.appendChild(btn);
 
-//   div.append(span);
-//   div.append(delBtn);
+  // li.appendChild(span);
+  // li.appendChild(div);
+};
 
-//   divider.setAttribute('class', 'item__divider');
-//   li.append(divider);
+// 댓글 삭제 기능
+const delComment = (e) => {
+  const targetId = e.target.dataset.targetId;
+  const liId = document.querySelector(`.commentLi[data-id="${targetId}"]`);
+  if (targetId) {
+    liId.remove();
+  }
+  if (ul.offsetHeight <= 140) {
+    ul.classList.remove('overflow');
+  }
+  if (count > 0) {
+    count -= 1;
+  }
+};
 
-//   items.append(div);
-//   input.value = '';
-// }
+// 댓글 좋아요 기능
+const addLike = (e) => {
+  const targetKey = e.target.dataset.targetKey;
+  const conBtn = document.querySelector(`.likeBtn[data-target-key="${targetKey}"]`);
+  if (targetKey) {
+    if (conBtn.classList.contains('like')) {
+      conBtn.classList.remove('like');
+      conBtn.classList.add('notLike');
+    } else if (conBtn.classList.contains('notLike')) {
+      conBtn.classList.remove('notLike');
+      conBtn.classList.add('like');
+    }
+  }
+};
 
-// btn.addEventListener('click', addItemHandler);
+// 댓글 추가기능 이벤트
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addComment();
+});
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   onAdd();
-// });
-
-// items.addEventListener('click', (e) => {
-//   const id = e.target.dataset.targetId;
-//   //* 클릭하는 곳의 id 값을 저장
-
-//   if (id) {
-//     //* id가 존재 하는 곳만 클릭 이벤트를 적용
-//     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
-//     console.log(toBeDeleted);
-//     toBeDeleted.remove();
-//     //* 클릭하는 곳의 id값과 같은 item__row를 삭제
-//   }
-// });
-
-// function createItem(text) {
-//   let id = Date.now();
-//   //* id 값을 생성
-//   const itemRow = document.createElement('li');
-//   itemRow.classList.add('item__row');
-//   itemRow.setAttribute('data-id', id);
-//   //* id 값을 data-id 속성에 넣어줌
-
-//   itemRow.innerHTML = `<div class="item">
-//   <span class="item__name">${text}</span>
-//   <button class="item__delete" data-target-id=${id}>삭제</button>
-//   </div>
-//   <div class="item__divider"></div>`;
-
-// const item = document.createElement('div');
-// item.classList.add('item');
-
-// const span = document.createElement('span');
-// span.classList.add('item__name');
-// span.textContent = text;
-
-// const deleteBtn = document.createElement('button');
-// deleteBtn.classList.add('item__delete');
-// deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-
-// deleteBtn.addEventListener('click', (e) => {
-//   items.removeChild(itemRow);
-// });
-
-// const itemDivider = document.createElement('div');
-// itemDivider.classList.add('item__divider');
-
-// item.appendChild(span);
-// item.appendChild(deleteBtn);
-
-// itemRow.appendChild(item);
-// itemRow.appendChild(itemDivider);
-
-//   return itemRow;
-// }
-
-// function createItem(text) {
-//   let id = Date.now();
-//   //* id 값을 생성
-//   const itemRow = document.createElement('li');
-//   itemRow.classList.add('item__row');
-//   itemRow.setAttribute('data-id', id);
-//   //* id 값을 data-id 속성에 넣어줌
-
-//   itemRow.innerHTML = `<div class="item">
-//   <span class="item__name">${text}</span>
-//   <button class="item__delete" data-target-id=${id}>삭제</button>
-//   </div>
-//   <div class="item__divider"></div>`;
-
-// const item = document.createElement('div');
-// item.classList.add('item');
-
-// const span = document.createElement('span');
-// span.classList.add('item__name');
-// span.textContent = text;
-
-// const deleteBtn = document.createElement('button');
-// deleteBtn.classList.add('item__delete');
-// deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-
-// deleteBtn.addEventListener('click', (e) => {
-//   items.removeChild(itemRow);
-// });
-
-// const itemDivider = document.createElement('div');
-// itemDivider.classList.add('item__divider');
-
-// item.appendChild(span);
-// item.appendChild(deleteBtn);
-
-// itemRow.appendChild(item);
-// itemRow.appendChild(itemDivider);
-
-//   return itemRow;
-// }
+// 댓글 삭제기능 이벤트
+ul.addEventListener('click', (e) => {
+  delComment(e);
+  addLike(e);
+});
