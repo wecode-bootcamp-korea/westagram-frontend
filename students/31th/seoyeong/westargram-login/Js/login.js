2c81ea111570fb9671c031abff237cebbe0b42b2
seo0yoon <genious921225@gmail.com>
2022-03-10 15:26:04 +0900

    ADD: login 페이지 개발 완료 -login 레이아웃 완성

---
+++ students/31th/seoyeong/westargram-login/Js/login.js
@@ -0,0 +1,19 @@
+const inputId = document.querySelector("#id");
+const inputPassword = document.querySelector("#password");
+const button = document.querySelector(".btn");
+
+function loginBtn() {
+  let idValue = inputId.value;
+  let passwordValue = inputPassword.value;
+
+  if(idValue.length > 0 && passwordValue.length > 0) {
+      button.disabled = false;
+      button.style.backgroundColor = "#1c7ed6";
+  }else {
+      button.disabled = true;
+      button.style.backgroundColor = "#bfdffd";
+  }
+};
+
+inputId.addEventListener('keyup', loginBtn);
+inputPassword.addEventListener('keyup', loginBtn);
