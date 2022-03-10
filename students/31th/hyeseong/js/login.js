ff348e81050cbc41097df7f452a7e3b57bfef6af
Moonhyeseong <moonhs97@gmail.com>
2022-03-10 20:43:14 +0900

    add: login validation, 게시글 좋아요버튼 추가

--- students/31th/hyeseong/js/login.js
+++ students/31th/hyeseong/js/login.js
@@ -7,7 +7,10 @@ loginButton.classList.add('deactive-btn');
 
 document.addEventListener('keydown', preventSpace);
 document.addEventListener('keyup', emptyCheck);
-
+loginButton.addEventListener('click', (e) => {
+  checkEmailValidation(e);
+  checkPwdValidation(e);
+});
 function emptyCheck() {
   let isEmpty = true;
   id.value == '' || pwd.value == '' ? (isEmpty = true) : (isEmpty = false);
@@ -24,6 +27,18 @@ function emptyCheck() {
 function preventSpace(e) {
   if (e.keyCode == 32) {
     alert('공백은 입력이 불가합니다.');
+    e.preventDefault();
+  }
+}
+function checkEmailValidation(e) {
+  if (id.value.indexOf('@') == -1) {
+    alert('올바른 형식의 이메일을 작성해주세요.');
+    e.preventDefault();
+  }
+}
+function checkPwdValidation(e) {
+  if (pwd.value.length < 5) {
+    alert('비밀번호는 5자 이상이어야 합니다.');
     e.preventDefault();
   }
 }
--- students/31th/hyeseong/js/main.js
+++ students/31th/hyeseong/js/main.js
@@ -1,11 +1,16 @@
 let input = document.getElementById('commnetInput');
 let inputBtn = document.getElementById('commnetButton');
 let comments = document.getElementById('articleConnents');
-inputBtn.classList.add('deactivate-btn');
+let articleLikeButton = document.getElementById('articleHeart');
 
+let toggleLikeButton = false;
+
+//addEventListener
 document.addEventListener('keyup', inputCheck);
 document.addEventListener('submit', addComment);
+articleLikeButton.addEventListener('click', likeButtonHandler);
 
+//유효성 검사
 function inputCheck() {
   let emptyCheck = input.value.replace(/(\s*)/g, '');
   let isEmpty = true;
@@ -19,6 +24,7 @@ function inputCheck() {
   }
 }
 
+//댓글 요소 추가
 function addComment(e) {
   let newDiv = document.createElement('div');
   newDiv.classList.add('comments');
@@ -45,12 +51,29 @@ function addComment(e) {
   e.preventDefault();
 }
 
+//버튼 활성화
 function submitBtnActivate() {
   inputBtn.classList.add('active-btn');
   inputBtn.classList.remove('deactive-btn');
 }
 
+//버튼 비활성화
 function submitBtnDeactivate() {
   inputBtn.classList.add('deactive-btn');
   inputBtn.classList.remove('active-btn');
+}
+
+//게시글 좋아요
+function likeButtonHandler() {
+  if (toggleLikeButton == false) {
+    articleLikeButton.style.color = 'red';
+    articleLikeButton.classList.remove('fa-regular');
+    articleLikeButton.classList.add('fa-solid');
+    toggleLikeButton = true;
+  } else {
+    articleLikeButton.style.color = 'black';
+    articleLikeButton.classList.remove('fa-solid');
+    articleLikeButton.classList.add('fa-regular');
+    toggleLikeButton = false;
+  }
 }
