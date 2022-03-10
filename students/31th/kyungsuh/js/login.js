c3fefd5954068b864691965da3fb266ef511c274
Kyungsuh Park <pyo05360@gmail.com>
2022-03-10 18:34:47 +0900

    로그인 버튼 활성화

--- students/31th/kyungsuh/js/login.js
+++ students/31th/kyungsuh/js/login.js
@@ -18,14 +18,14 @@ function checkEmailValid() {
     const inpId = document.querySelector(".inpID")
     const inputAlert = document.querySelector('.inputAlert');
     const isEmail = (value) => {
-      // 골뱅이가 있고 골뱅이 뒤에 점이 있다면 
+      // 이메일 조건 : 골뱅이가 있고 골뱅이 뒤에 점이 있다면 
       return (value.indexOf('@') > 1) &&
         (value.split('@')[1].indexOf('.') > 1)
     }
   
     inpId.addEventListener('keyup', (event) => {
       const value = event.currentTarget.value
-      console.log(value)
+      // console.log(value)
       if (isEmail(value)) {
         inputAlert.style.color = "green"
         inputAlert.textContent = `올바른 이메일 형식 입니다.`
@@ -38,19 +38,24 @@ function checkEmailValid() {
 
 checkEmailValid()
 
+const isPassword = (value) => {
+  return (value.length >= 6)
+}
+
 // 비밀번호 6자리 이상인 함수 
 function checkPassword() {
     const inpPw = document.querySelector(".inpPass")
     const inputPassAlert = document.querySelector(".inputPassAlert")
-    const isPassword = (value) => {
-        return (value.length >= 6)
-    }
+    // const isPassword = (value) => {
+    //     return (value.length >= 6)
+    // }
 
     inpPw.addEventListener('keyup', (event) => {
         const value = event.currentTarget.value
-        console.log(value)
+        // console.log(value)
         if(isPassword(value)) {
             inputPassAlert.textContent = ` `
+            btnLogin.classList.add("on")
         } else {
             inputPassAlert.style.color = "#eb5757"
             inputPassAlert.textContent = `비밀번호는 6자리 이상 입력해주세요.`
@@ -59,14 +64,3 @@ function checkPassword() {
 }
 
 checkPassword()
-
-// function changeColor () {
-//     const id = inpId.value
-//     const pw = inpPw.value
-//     if(checkEmailValid() && checkPassword) {
-//         btnLogin.classList.add("on")
-//     }
-// }
-
-// inpId.addEventListener('keyup',changeColor)
-// inpPw.addEventListener('keyup',changeColor)
