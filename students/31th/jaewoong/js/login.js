84fa58f754999797b7b1a193b6397c33bfcb585b
jay yoon <jaytaildish@gmail.com>
2022-03-10 20:25:40 +0900

    ADD:로그인 기능 고도화/ RENAME:index.html->login.js

--- students/31th/jaewoong/js/login.js
+++ students/31th/jaewoong/js/login.js
@@ -8,12 +8,22 @@ const handleBtnActivate = (e) =>{
    username.value && pwd.value? loginBtn.classList.add(BTN_ACTIVATE) : loginBtn.classList.remove(BTN_ACTIVATE)
 };
 
+const checkId=()=>{
+    if(username.value.indexOf("@")== -1) return false;
+    else return true;
+}
+const checkPwd=()=>{
+    if(pwd.value.length<5) return false;
+    else return true;
+}
+
 const loginSuccess=()=>{
     localStorage.setItem("ID",username.value);
     window.location ="main.html"
 }
-const handleSubmit =()=>{
-    username.value && pwd.value ? loginSuccess() : alert("사용자이름과 비밀번호를 확인하세요")
+const handleSubmit =(e)=>{
+    e.preventDefault();
+    checkId() && checkPwd() ? loginSuccess(): alert("사용자이름과 비밀번호를 확인하세요")
 }
 
 username.addEventListener("keyup",handleBtnActivate);
