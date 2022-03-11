1e364fbd4c9fc55ff458e6ec251bc60f3ae6ea11
Kyungsuh Park <pyo05360@gmail.com>
2022-03-11 21:04:35 +0900

    Add: 로그인 성공 페이지 이동 구현

--- students/31th/kyungsuh/js/login.js
+++ students/31th/kyungsuh/js/login.js
@@ -1,31 +1,21 @@
+"use strict";
+
 const inpId = document.querySelector(".inpID")
 const inpPw = document.querySelector(".inpPass")
 const btnLogin = document.querySelector(".btnLogin")
+const inputAlert = document.querySelector('.inputAlert');
 
-// function changeColor () {
-//     const id = inpId.value
-//     const pw = inpPw.value
-//     if(id && pw) {
-//         btnLogin.classList.add("on")
-//     }
-// }
-
-// inpId.addEventListener('keyup',changeColor)
-// inpPw.addEventListener('keyup',changeColor)
 
 // 이메일 형식 검사
 function checkEmailValid() {
     const inpId = document.querySelector(".inpID")
-    const inputAlert = document.querySelector('.inputAlert');
     const isEmail = (value) => {
-      // 이메일 조건 : 골뱅이가 있고 골뱅이 뒤에 점이 있다면 
       return (value.indexOf('@') > 1) &&
         (value.split('@')[1].indexOf('.') > 1)
     }
   
     inpId.addEventListener('keyup', (event) => {
       const value = event.currentTarget.value
-      // console.log(value)
       if (isEmail(value)) {
         inputAlert.style.color = "green"
         inputAlert.textContent = `올바른 이메일 형식 입니다.`
@@ -38,28 +28,31 @@ function checkEmailValid() {
 
 checkEmailValid()
 
-const isPassword = (value) => {
-  return (value.length >= 6)
-}
-
 // 비밀번호 6자리 이상인 함수 
 function checkPassword() {
     const inpPw = document.querySelector(".inpPass")
     const inputPassAlert = document.querySelector(".inputPassAlert")
-    // const isPassword = (value) => {
-    //     return (value.length >= 6)
-    // }
+    const isPassword = (value) => {
+        return (value.length >= 6)
+    }
     inpPw.addEventListener('keyup', (event) => {
         const value = event.currentTarget.value
-        // console.log(value)
         if(isPassword(value)) {
-            inputPassAlert.textContent = ` `
-            btnLogin.classList.add("on")
+          inputPassAlert.textContent = ` `
+          btnLogin.classList.add("on")
         } else {
-            inputPassAlert.style.color = "#eb5757"
-            inputPassAlert.textContent = `비밀번호는 6자리 이상 입력해주세요.`
+          inputPassAlert.style.color = "#eb5757"
+          inputPassAlert.textContent = `비밀번호는 6자리 이상 입력해주세요.`
         }
     })
 }
 
 checkPassword()
+
+function success() {
+  if(inputAlert.style.color = "green" && btnLogin.classList.contains("on") )
+  location.href="main.html";
+}
+
+btnLogin.addEventListener("click", success)
+
