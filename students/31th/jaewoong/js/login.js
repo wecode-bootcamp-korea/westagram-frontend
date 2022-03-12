d1f62dceae66053f6afaf65649298e02ac4ff195
jay yoon <jaytaildish@gmail.com>
2022-03-12 23:35:52 +0900

    fix:리팩토링  - console.log제거  - 내어쓰기 2줄로 변경  - css 클래스명 변경(asis:케밥 tobe:카멜)  - bottomup방식 css 재배치 및 불필요한 클래스 제거

--- students/31th/jaewoong/js/login.js
+++ students/31th/jaewoong/js/login.js
@@ -1,31 +1,47 @@
+"use strict";
+
 const username = document.querySelector('.username');
 const pwd = document.querySelector('.pwd');
-const loginBtn = document.querySelector('.login-btn');
-const BTN_ACTIVATE ="btn-activate";
+const loginBtn = document.querySelector('.loginBtn');
+const BTN_ACTIVATE ="btnActivate";
 
-const handleBtnActivate = (e) =>{
-   e.value=username.value;
-   username.value && pwd.value? loginBtn.classList.add(BTN_ACTIVATE) : loginBtn.classList.remove(BTN_ACTIVATE)
-};
+const btnColorHandler = (e) =>{
+  e.value=username.value;
+  username.value && pwd.value
+  ? loginBtn.classList.add(BTN_ACTIVATE)
+  : loginBtn.classList.remove(BTN_ACTIVATE)
+}
 
 const checkId=()=>{
-    if(username.value.indexOf("@")== -1) return false;
-    else return true;
+  if(username.value.indexOf("@")== -1)
+    return false;
+  else
+    return true;
 }
+
 const checkPwd=()=>{
-    if(pwd.value.length<5) return false;
-    else return true;
+  if(pwd.value.length<5)
+    return false;
+  else
+    return true;
 }
 
 const loginSuccess=()=>{
-    localStorage.setItem("ID",username.value);
-    window.location ="main.html"
-}
-const handleSubmit =(e)=>{
-    e.preventDefault();
-    checkId() && checkPwd() ? loginSuccess(): alert("사용자이름과 비밀번호를 확인하세요")
+  localStorage.setItem("ID",username.value);
+  window.location ="main.html"
 }
 
-username.addEventListener("keyup",handleBtnActivate);
-pwd.addEventListener("keyup",handleBtnActivate);
-loginBtn.addEventListener("click", handleSubmit);
+const loginInfoHandler =(e)=>{
+  e.preventDefault();
+  checkId() && checkPwd()
+  ? loginSuccess()
+  : alert("사용자이름에는 @이 포함되어야 하며 비밀번호는 5글자 이상 입력해주셔야 합니다.")
+}
+
+const init=()=>{
+  username.addEventListener("input",btnColorHandler);
+  pwd.addEventListener("input",btnColorHandler);
+  loginBtn.addEventListener("click", loginInfoHandler);
+}
+
+init();
--- students/31th/jaewoong/js/main.js
+++ students/31th/jaewoong/js/main.js
@@ -1,144 +1,137 @@
-const delay = 100;
-let timer = null;
+'use strict';
+
+const userid=localStorage.getItem("ID");
 const HIDDEN = "hidden"
 const INVISIBLE = 'invisible'
-//로그인할때 받은 userID 받아오기
+
 const insertUserid = document.querySelector('.userid');
-const userid=localStorage.getItem("ID");
 insertUserid.innerHTML=userid;
 
-/////화면뷰 조정에 따른 레이아웃 변경기능/////
-
-//section 좌측좌표 service-symbol좌표로 맞춰주기
-//personal-container 우측좌표 header-func표로 맞춰주고 화면크기 일정사이즈 미만 축소시 invisible처리
-const findLocationLeft=(object)=>{
-    return object.getBoundingClientRect().left+"px";
-}
-const serviceSymbol = document.querySelector('.service-symbol');
+const serviceSymbolContainer = document.querySelector('.serviceSymbolContainer');
 const section = document.querySelector('section');
-const personalContainer = document.querySelector('.personal-container');
+const personalContainer = document.querySelector('.personalContainer');
 
-//화면 첫 로그인시 최초 적용
-section.style.marginLeft =findLocationLeft(serviceSymbol)
-//실시간 이벤트
-window.addEventListener('resize',function(){
-    clearTimeout(timer);
-    timer=setTimeout(() => {
-        section.style.marginLeft =findLocationLeft(serviceSymbol);
-        personalContainer.getBoundingClientRect().right <1800? personalContainer.classList.add(INVISIBLE) : personalContainer.classList.remove(INVISIBLE)
-    }, delay);
-});
+const blankHeart=document.getElementsByClassName('blankHeart')[0];
+const redHeart=document.getElementsByClassName('redHeart')[0];
 
-/////게시글 좋아요버튼 클릭이벤트/////
-const blankHeart=document.getElementsByClassName('blank-heart')[0];
-const redHeart=document.getElementsByClassName('red-heart')[0];
+const postInteractionParts= document.querySelector(".postInteractionParts");
+const replyContainer = document.getElementsByClassName("replyContainer");
+const replyComponent = document.querySelector('.replyComponent');
+const insertReply=document.querySelector('.insertReply');
 
-
-blankHeart.addEventListener('click',()=>{ 
-    blankHeart.classList.add("hidden")
-    redHeart.classList.remove("hidden")
-})
-redHeart.addEventListener('click',()=>{ 
-    redHeart.classList.add("hidden")
-    blankHeart.classList.remove("hidden")
-})
-
-/////댓글입력 이벤트/////
-const postInteractionParts= document.querySelector(".post-interaction-parts");
-const replyContainer = document.getElementsByClassName("reply-container");
-const replyComponent = document.querySelector('.reply-component');
-
-
-const insertReply=document.querySelector('.insert-reply');
-
-//댓글입력함수
-const appendReply=(e)=>{
-    e.preventDefault();
-
-    const listOrder = replyContainer[0].children.length;
-
-    const replyContents = document.createElement("li"); 
-    replyContents.setAttribute("id", "reply"+listOrder);
-    replyContents.setAttribute("class", "reply-line");
-    
-    const viewReply =document.createElement("div");
-    viewReply.setAttribute("class","view-reply");
-
-    const replyWriterId = document.createElement("span")
-    replyWriterId.setAttribute("class", "small-bold-font");
-    replyWriterId.innerText = userid;
-
-    const replyText = document.createElement("span");
-    replyText.setAttribute("class","reply-text");
-    replyText.innerText=insertReply.value;
-    
-    const replyFunc = document.createElement("div");
-    replyFunc.setAttribute("class", "reply-func");
-
-    const deleteReplyIcon = document.createElement('img');
-    deleteReplyIcon.setAttribute("class",`delete-icon reply-icon`);
-    deleteReplyIcon.setAttribute("src","img/redcross.png");
-
-    const likeRedHeart = document.createElement('img');
-    likeRedHeart.setAttribute("class",'already-like-icon reply-icon hidden');
-    likeRedHeart.setAttribute("src","img/redheart.png");
-
-    const unlikeBlankHeart = document.createElement('img');
-    unlikeBlankHeart.setAttribute("class",'unlike-icon reply-icon');
-    unlikeBlankHeart.setAttribute("src","img/heart_logo.png");
-
-    replyContainer[0].appendChild(replyContents);
-    replyContents.appendChild(viewReply);
-    replyContents.appendChild(replyFunc);
-    viewReply.appendChild(replyWriterId);
-    viewReply.appendChild(replyText);
-    replyFunc.appendChild(deleteReplyIcon);
-    replyFunc.appendChild(likeRedHeart);
-    replyFunc.appendChild(unlikeBlankHeart);
-
-    insertReply.value="";
-}
-
-//댓글삭제 및 좋아요 함수
-const replyInteraction=(e)=>{
-    if(e.target.classList.contains("delete-icon")){
-        const replyContents= e.target.parentElement.parentElement;
-        replyContents.remove();
-    }else if(e.target.classList.contains("unlike-icon")){
-        e.target.previousSibling.classList.remove("hidden")
-        e.target.classList.add("hidden")
-    }else if(e.target.classList.contains("already-like-icon")){
-        e.target.nextSibling.classList.remove("hidden");
-        e.target.classList.add("hidden")
-    }
-}
-replyComponent.addEventListener('submit',appendReply);
-postInteractionParts.addEventListener('click',replyInteraction);
-
-/////프로필 누르면 창 나오게하기/////
 const body = document.querySelector("body");
-const myThumbnail = document.getElementsByClassName("my-thumbnail")[0];
-const profileWindow = document.getElementsByClassName("profile-window")[0];
+const myThumbnail = document.getElementsByClassName("myThumbnail")[0];
+const profileWindow = document.getElementsByClassName("profileWindow")[0];
+
+//화면조정에 따라 section위치 조정
+const sectionLocationHandler=()=>{
+section.style.marginLeft =serviceSymbolContainer.getBoundingClientRect().left+"px";
+}
+
+//화면조정값에 따라 aside의 추천창(persornalContainer) 제거
+const personalContainerHandler=()=>{
+  personalContainer.getBoundingClientRect().right <1800
+  ? personalContainer.classList.add(INVISIBLE)
+  : personalContainer.classList.remove(INVISIBLE)    
+}
+
+//댓글입력  //하드코딩으로 풀어서 썼는데 이거 묶어서 써봅시다...
+const appendReply=(e)=>{
+  e.preventDefault();
+  
+  const listOrder = replyContainer[0].children.length;
+  
+  const replyContents = document.createElement("li"); 
+  replyContents.setAttribute("id", "reply"+listOrder);
+  replyContents.setAttribute("class", "replyLine");
+  
+  const viewReply =document.createElement("div");
+  viewReply.setAttribute("class","viewReply");
+  const replyWriterId = document.createElement("span")
+  replyWriterId.setAttribute("class", "smallBoldFont");
+  replyWriterId.innerText = userid;
+  const replyText = document.createElement("span");
+  replyText.setAttribute("class","replyText");
+  replyText.innerText=insertReply.value;
+  
+  const replyFunc = document.createElement("div");
+  replyFunc.setAttribute("class", "replyFunc");
+  const deleteReplyIcon = document.createElement('img');
+  deleteReplyIcon.setAttribute("class",'deleteIcon replyIcon');
+  deleteReplyIcon.setAttribute("src","img/redcross.png");
+  const likeRedHeart = document.createElement('img');
+  likeRedHeart.setAttribute("class",'alreadyLikeIcon replyIcon hidden');
+  likeRedHeart.setAttribute("src","img/redheart.png");
+  const unlikeBlankHeart = document.createElement('img');
+  unlikeBlankHeart.setAttribute("class",'unlikeIcon replyIcon');
+  unlikeBlankHeart.setAttribute("src","img/heart_logo.png");
+  replyContainer[0].appendChild(replyContents);
+  replyContents.appendChild(viewReply);
+  replyContents.appendChild(replyFunc);
+  viewReply.appendChild(replyWriterId);
+  viewReply.appendChild(replyText);
+  replyFunc.appendChild(deleteReplyIcon);
+  replyFunc.appendChild(likeRedHeart);
+  replyFunc.appendChild(unlikeBlankHeart);
+  insertReply.value="";
+}
+
+//댓글좋아요
+const likeReply=(e)=>{
+  if(e.target.classList.contains("unlikeIcon")){
+    e.target.previousSibling.classList.remove("hidden")
+    e.target.classList.add("hidden")
+  }else if(e.target.classList.contains("alreadyLikeIcon")){
+    e.target.nextSibling.classList.remove("hidden");
+    e.target.classList.add("hidden")
+  }
+}
+
+//댓글삭제
+const deleteReply=(e)=>{
+  if(e.target.classList.contains("deleteIcon")){
+    const repliedContents= e.target.parentElement.parentElement
+    repliedContents.remove();
+  }
+}
+
+//프로필아이콘 누르면 프로필창 표시//
 const showProfileBox=()=>{
     profileWindow.classList.remove("invisible")
 }
 const closeProfileBox=()=>{
     profileWindow.classList.add("invisible")
 }
+
+//클릭시 프로필창 제거
 const profileBoxHandler=(e)=>{
-    if(profileWindow.classList.contains("invisible")){
-        if(e.target==myThumbnail) profileWindow.classList.remove("invisible");
-        else profileWindow.classList.add("invisible");
-    }else profileWindow.classList.add("invisible")
+  if(profileWindow.classList.contains("invisible")){
+    if(e.target==myThumbnail)
+      profileWindow.classList.remove("invisible");
+    else
+      profileWindow.classList.add("invisible")
+  }else 
+    profileWindow.classList.add("invisible")
 }
 
-///프로필 마우스 오버시 음영///
-paintBtnBackground=(e)=>{
-    e.target.classList.add("paint-btn-background")
+//프로필창 마우스 오버시 음영//
+const paintBtnBackground=(e)=>{
+  e.target.classList.add("paintBtnBackground")
 }
-removeBtnBackground=(e)=>{
-    e.target.classList.remove("paint-btn-background")
+const removeBtnBackground=(e)=>{
+  e.target.classList.remove("paintBtnBackground")
 }
-body.addEventListener('click', profileBoxHandler);
-profileWindow.addEventListener('mouseover', paintBtnBackground);
-profileWindow.addEventListener('mouseout',removeBtnBackground);
+
+
+const init=()=>{
+  section.style.marginLeft =serviceSymbolContainer.getBoundingClientRect().left+"px";
+  window.addEventListener('resize',personalContainerHandler);
+  window.addEventListener('resize',sectionLocationHandler);
+  replyComponent.addEventListener('submit',appendReply);
+  postInteractionParts.addEventListener('click',likeReply);
+  postInteractionParts.addEventListener('click',deleteReply);
+  profileWindow.addEventListener('mouseover',paintBtnBackground);
+  profileWindow.addEventListener('mouseout',removeBtnBackground);
+  body.addEventListener('click', profileBoxHandler);
+}
+init();
