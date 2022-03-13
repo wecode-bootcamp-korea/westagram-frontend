15b8fed4c1624d223d54686fbb9fc944d5cdab47
bboyooning <bboyooning@gmail.com>
2022-03-13 14:04:55 +0900

    Add: main.js 댓글 게시 버튼 click 기능 구현 - addEventListener() 메소드를 사용하여 게시 버튼에 이벤트 구현 - submitBtn에 click 이벤트가 발생하면 그 이벤트 자체를 매개변수로 받아서 commentsBox() 함수가 호출됨
    Modify: login.css .wrap 가운데 정렬 코드 수정
    - login.css 에서 .wrap 가운데 정렬하기 위해 flex 속성을 이용하여 수정
    
    Refactor: 중복 코드, 불필요한 코드 제거 등을 스스로 리팩토링
    - [html]
    - main.html 구조상 불필요한 div 들을 제거
    - main.html class 명을 좀 더 직관적으로 수정
    
    - [css]
    - css 속성 권장 순서에 맞게 정렬
    - 중복쓰고 있는 css 속성들을 하나로 묶어서 적용
    - 불필요한 style 속성들 제거

--- students/31th/boyoon/login.js
+++ students/31th/boyoon/login.js
@@ -1,25 +1,31 @@
-const id = document.querySelector("input#id")
-const password = document.querySelector("input#password")
-const btn = document.querySelector(".btn")
+//"use strict"; // 자바스크립트는 유동적인 언어이기 때문에, 이러한 유동성 때문에 JS에서 에러가 발생할 가능성이 높아서 씁니다.
 
-function loginBtn (){
-    westaId = id.value;
-    westaPassword = password.value;
+// [Mission2] id, pw 입력 시 로그인 버튼 활성화 기능
 
-    if (westaId.length > 0 && westaPassword.length > 0){
-        btn.disabled = false;
-        btn.style.backgroundColor = "#0096F6";
-        btn.style.cursor = "pointer";
-    } else {
-        btn.disabled = true;
-        btn.style.backgroundColor = "#C0DFFD"
-        btn.style.cursor = "default"
-    }
+const inputId = document.querySelector("input#id");
+const password = document.querySelector("input#password");
+const btn = document.querySelector(".btn");
+
+// 함수 선언식 
+function loginBtn() {
+  westaId = inputId.value;
+  westaPassword = password.value;
+
+  if (westaId.length > 0 && westaPassword.length > 0) {
+    btn.disabled = false;
+    btn.style.backgroundColor = "#0096F6";
+    btn.style.cursor = "pointer";
+  }else {
+    btn.disabled = true;
+    btn.style.backgroundColor = "#C0DFFD";
+    btn.style.cursor = "default";
+  }
 }
 
-id.addEventListener('keypress', loginBtn);
-password.addEventListener('keypress', loginBtn);
 
-id.addEventListener('keyup', loginBtn);
-password.addEventListener('keyup', loginBtn);
+inputId.addEventListener("keyup", loginBtn);
+password.addEventListener("keyup", loginBtn);
 
+// 웹페이지 만들때 keyup 이외에도 마우스로 클릭 복사 등으로 로그인할 수 있기 때문에, 
+inputId.addEventListener("input", loginBtn);
+password.addEventListener("input", loginBtn);
--- students/31th/boyoon/main.js
+++ students/31th/boyoon/main.js
@@ -1,12 +1,55 @@
-const inputComments = document.querySelector(".comments_input");
-const rootDiv = document.querySelector('.comments_title');
-const btn = document.querySelector('.posting_box');
+// "use strict";
 
+// [Mission4] 댓글 내용 입력 후 Enter press, 혹은 게시 버튼 클릭 시 댓글 추가 기능
 
+let inputComments = document.querySelector(".comments_input"); // 댓글 input
+let submitBtn = document.querySelector(".posting_box"); // 게시 등록 버튼
 
+  function commentsBox() {
+    const box = document.querySelector(".main_left_box_comment"); // 댓글 추가하면 들어갈 공간의 div태그 가져옴
 
-// function posting (){
-//     const box = document.getElementsByClassName(".comments_contents")
+    const comments = document.createElement("div"); // 부모요소 역할을 할 div 태그 할당
 
+    const userId = document.createElement("span"); // 사용자의 id가 찍힐 span 태그 할당
+    const commentsText = document.createElement("span"); // 사용자의 댓글이 찍힐 span 태그 할당
+    const likesBtn = document.createElement("button"); // 댓글 오른쪽에 찍힐 작은 하트에 button 태그 할당
 
-// }
+    // .classList.add() 메소드를 사용해서 class를 추가
+    comments.classList.add("comments_title");
+    userId.classList.add("user_id");
+    commentsText.classList.add("comments_contents");
+    likesBtn.classList.add("comments_like_btn");
+
+    // .innerHTML 와 .innerText 를 사용해서 div 안의 내용 읽어오기 
+    userId.innerHTML = '<span class="user_id">아이디</span>'; // 닉네임 이름
+    commentsText.innerText = inputComments.value; // 댓글 내용
+    likesBtn.innerHTML = '<i class="fa-regular fa-heart"></i>'; // 하트 아이콘
+
+    // 자식요소를 부모요소의 순서대로 넣어주는 .appendChild() 메소드로 구조 잡기 
+    comments.appendChild(userId); 
+    comments.appendChild(commentsText);
+    comments.appendChild(likesBtn);
+
+    box.appendChild(comments);
+
+  }
+
+// 게시 버튼 이벤트 구현
+submitBtn.addEventListener("click", (event) => {
+
+  commentsBox();
+  inputComments.value = "";
+
+});
+
+// Enter 키로 댓글 추가 이벤트 구현
+// keydown, keyup 댓글이 중복으로 달리는 오류가 있었는데 keypress 쓰니까 되는데 왜죠..
+inputComments.addEventListener("keypress", (event) => {
+
+  if (event.code === "Enter") {
+    // event.preventDefault();
+    commentsBox();
+    inputComments.value = "";
+  }
+  
+});
