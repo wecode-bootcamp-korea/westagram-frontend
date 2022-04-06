bd5ba7e6d2ea7f37c13fbb5fd35b485f1bea7319
DIDI-SU <jisoodidi86@gmail.com>
2022-04-06 20:51:07 +0900

    Add:댓글 수정

--- students/32nd/이지수/JS/main.js
+++ students/32nd/이지수/JS/main.js
@@ -2,25 +2,23 @@
 const input = document.querySelector(".commentinput");
 const ul = document.querySelector(".comment>ul");
 const btn = document.querySelector("button");
-const all = document.querySelector(".indiComment");
-const deleteBtn = document.querySelectorAll("#delete");
-let li;
-console.log(deleteBtn);
-
+const comment = document.querySelector(".indiComment");
 const currentUser = JSON.parse(window.localStorage.getItem("user"));
 
-function getValue(e) {
-  let { value } = e.target;
-  const clone = all.cloneNode(true);
+let li;
+
+const getValue = function (e) {
+  const { value } = e.target;
+  const clone = comment.cloneNode(true);
   clone.children[0].innerText = currentUser.name;
   clone.children[1].innerText = value;
   let key = e.key;
   if (key === "Enter") {
-    ul.appendChild(clone);
     clone.id = currentUser.id;
+    ul.appendChild(clone);
     input.value = "";
   }
-}
+};
 
 //event
 btn.addEventListener("click", () => {
@@ -28,11 +26,3 @@ btn.addEventListener("click", () => {
   input.value = "";
 });
 input.addEventListener("keydown", getValue);
-
-const check = () => {
-  const clone = all.cloneNode(true);
-  clone.children[0].innerText = currentUser.name;
-  clone.children[1].innerText = "댓글내용";
-  console.log(clone);
-};
-check();
