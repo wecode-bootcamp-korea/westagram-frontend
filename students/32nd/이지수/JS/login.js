3e98ec62fc606cc8b13a488aec1c7fa90ac55aba
DIDI-SU <jisoodidi86@gmail.com>
2022-04-06 17:16:26 +0900

    Add: 파일명 수정 및 아이디 비밀번호 확인추가

--- students/32nd/이지수/JS/login.js
+++ students/32nd/이지수/JS/login.js
@@ -2,26 +2,29 @@
 const id = document.querySelector("#id");
 const pw = document.querySelector("#pw");
 const btn = document.querySelector("button");
-export let idvalue;
+let idvalue;
 let pwvalue;
-
-function checkBlank(e) {
+pw.setAttribute("minlength", 5);
+const checkBlank = function (e) {
   if (e.target.id === `id`) {
     idvalue = e.target.value;
   }
   if (e.target.id === "pw") {
     pwvalue = e.target.value;
   }
-  if (idvalue !== undefined && pwvalue !== undefined) {
+  if (idvalue.includes("@") && pwvalue.length >= 5) {
     btn.disabled = false;
     btn.style.backgroundColor = "blue";
   }
-}
+};
 
 id.addEventListener("change", checkBlank);
 pw.addEventListener("change", checkBlank);
 
 btn.addEventListener("click", (e) => {
+  const path = window.location.pathname;
+  const num = path.indexOf("login");
+  const resultPath = path.substring(0, num);
   e.preventDefault();
-  location.replace("/main.html");
+  location.href = `${resultPath}main.html`;
 });
