7c1adff883fdb331612bf589487bb0cabda90fe9
SeokYoung <willy4202@naver.com>
2022-04-06 17:23:07 +0900

    Add: login-validation 구현

--- students/32nd/SeokYoung/js/login.js
+++ students/32nd/SeokYoung/js/login.js
@@ -7,12 +7,6 @@ loginBtn.addEventListener("click", btnHandler);
 inputBox.addEventListener("keyup", activeLogin);
 inputId.addEventListener("keyup", checkId);
 
-function btnHandler() {
-  console.log(inputId.value);
-  console.log(inputPass.value.length);
-  alert("비밀번호를 확인해주세요!");
-}
-
 function activeLogin() {
   if (inputId.value && inputPass.value) {
     loginBtn.disabled = false;
@@ -20,6 +14,17 @@ function activeLogin() {
   } else {
     loginBtn.disabled = true;
     loginBtn.style.backgroundColor = "rgb(" + 178 + "," + 223 + "," + 252 + ")";
+  }
+}
+
+function btnHandler() {
+  const idCheck = "@";
+  if (!inputId.value.includes(idCheck)) {
+    alert("이메일 형식이 아닙니다.");
+  } else if (inputPass.value.length < 8) {
+    alert("비밀번호는 8자리 이상이여야 합니다.");
+  } else {
+    alert("로그인은 완료했는데,,,유효성은 모르겠네요. 일단 다음으로 가세요");
   }
 }
 
