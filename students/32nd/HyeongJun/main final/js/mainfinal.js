d30469d9d8e44bf57c30fb90f3c83267305caf53
전형준 <wehj0105@gamil.com>
2022-04-06 20:15:40 +0900

    메인구성완료

---
+++ students/32nd/HyeongJun/main final/js/mainfinal.js
@@ -0,0 +1,28 @@
+    const commentInput = document.getElementsByClassName('inputComment')[0];
+    const commentPostBtn = document.getElementsByClassName('btn')[0];
+    const newCommentLocation = document.getElementsByClassName('comment')[0];
+    
+    const addNewComment = () => {
+        const newComment = document.createElement('div');
+  
+      newComment.innerHTML = 
+          `<strong>gud_wns_</strong>
+          <span>${commentInput.value}</span>`
+        
+  
+      newCommentLocation.appendChild(newComment);
+      commentInput.value = '';
+    }
+    
+    commentPostBtn.addEventListener('click', () => {
+        if (commentInput.value) {
+          addNewComment();
+        }
+      })
+
+      function enterkey() {
+          if (window.event.keyCode == 13 && commentInput.value ) {
+            addNewComment();
+        } 
+    }
+
