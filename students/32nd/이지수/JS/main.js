d0841b7383058ca398182e3153ec832c8f6ddc6d
DIDI-SU <jisoodidi86@gmail.com>
2022-04-06 18:00:05 +0900

    Add:닉네임+내용 방식으로 댓글 변경 및 css 임시수정

--- students/32nd/이지수/JS/main.js
+++ students/32nd/이지수/JS/main.js
@@ -4,16 +4,21 @@ const ul = document.querySelector(".comment>ul");
 const btn = document.querySelector("button");
 let li;
 
-const current = JSON.parse(window.localStorage.getItem("test"));
+const currentUser = JSON.parse(window.localStorage.getItem("test"));
 
 function getValue(e) {
   let { value } = e.target;
   li = document.createElement("li");
-  li.innerText = value;
+  const name = document.createElement("p");
+  const content = document.createElement("p");
+  li.appendChild(name);
+  li.appendChild(content);
+  name.innerText = currentUser.name;
+  content.innerText = value;
   let key = e.key;
   if (key === "Enter") {
     ul.appendChild(li);
-    li.id = current.id;
+    li.id = currentUser.id;
     input.value = "";
   }
 }
