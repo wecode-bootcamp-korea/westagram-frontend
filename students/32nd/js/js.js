492cdcc714161673af0e94ceb3f95c0ccd6e3319
sooboi <dlgbrkd123@gmail.com>
2022-04-06 16:17:06 +0900

    Add : 로그인 페이지 작업 완료

---
+++ students/32nd/js/js.js
@@ -0,0 +1,25 @@
+const id = document.getElementById('idIn');
+const pw = document.getElementById('pwIn');
+const login = document.getElementById('login-bt');
+
+id.addEventListener('keyup', active);
+pw.addEventListener('keyup', active);
+
+function active() {
+    if(id.value != "" && pw.value !="") {
+        login.disabled = false;
+    }else {
+        login.disabled = true;
+    }
+};
+
+login.addEventListener('click', active2);
+
+function active2() {
+    if(login.disabled = true) {
+        alert("환영합니다 😎")
+        window.location.href = './main.html'
+    }else {
+        alert("빈칸을 채우세요.")
+    }
+}
