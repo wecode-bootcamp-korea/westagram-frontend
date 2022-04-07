205cd8653c2d30768002edb5dc31199d0422247e
전형준 <wehj0105@gamil.com>
2022-04-07 18:25:03 +0900

    로그인 페이지,메인페이지 리팩토링 완료

--- students/32nd/HyeongJun/login/js/login.js
+++ students/32nd/HyeongJun/login/js/login.js
@@ -7,17 +7,12 @@ selectButton.disabled = true;
 id.addEventListener("keyup", loginOn);
 pw.addEventListener("keyup", loginOn);
 
-function loginOn(){
-    if (id.value && pw.value) {
-        selectButton.disabled = false;
-        selectButton.style.backgroundColor = '#0095f6';
-    }
-    else {
-        selectButton.disabled = true;
-        selectButton.style.backgroundColor = '#b2dffc';
-    }
+function loginOn() {
+  if (id.value && pw.value) {
+    selectButton.disabled = false;
+    selectButton.style.backgroundColor = "#0095f6";
+  } else {
+    selectButton.disabled = true;
+    selectButton.style.backgroundColor = "#b2dffc";
+  }
 }
-
-// selectButton.addEventListener('click', () => {       //로그인 버튼 활성화 시 버튼눌리는 액션이 없어서, 주석풀고 콘솔켜고 클릭하면 클릭이 되고 있다는 걸 알 수 있다.
-//    console.log('click')                              //버튼태그에 border속성을 건드리면 버튼이 눌리는 액션이 없다!!
-// })
--- students/32nd/HyeongJun/main final/js/mainfinal.js
+++ students/32nd/HyeongJun/main final/js/mainfinal.js
@@ -1,28 +1,25 @@
-    const commentInput = document.getElementsByClassName('inputComment')[0];
-    const commentPostBtn = document.getElementsByClassName('btn')[0];
-    const newCommentLocation = document.getElementsByClassName('comment')[0];
-    
-    const addNewComment = () => {
-        const newComment = document.createElement('div');
-  
-      newComment.innerHTML = 
-          `<strong>gud_wns_</strong>
-          <span>${commentInput.value}</span>`
-        
-  
-      newCommentLocation.appendChild(newComment);
-      commentInput.value = '';
-    }
-    
-    commentPostBtn.addEventListener('click', () => {
-        if (commentInput.value) {
-          addNewComment();
-        }
-      })
+const commentInput = document.getElementsByClassName("inputComment")[0];
+const commentPostBtn = document.getElementsByClassName("commentBtn")[0];
+const newCommentLocation = document.getElementsByClassName("comment")[0];
 
-      function enterkey() {
-          if (window.event.keyCode == 13 && commentInput.value ) {
-            addNewComment();
-        } 
-    }
+const addNewComment = () => {
+  const newComment = document.createElement("div");
 
+  newComment.innerHTML = `<strong>gud_wns_</strong>
+  <span>${commentInput.value}</span>`;
+
+  newCommentLocation.appendChild(newComment);
+  commentInput.value = "";
+};
+
+commentPostBtn.addEventListener("click", () => {
+  if (commentInput.value) {
+    addNewComment();
+  }
+});
+
+function enterkey() {
+  if (window.event.keyCode == 13 && commentInput.value) {
+    addNewComment();
+  }
+}
