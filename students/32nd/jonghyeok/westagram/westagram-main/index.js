e5c7f1d790b4553a45e1ccb56016ada56f4f6d76
Marco <jonghyeok.lim14@gmail.com>
2022-04-07 16:39:18 +0900

    Add: 필수기능구현

--- students/32nd/jonghyeok/westagram/westagram-main/index.js
+++ students/32nd/jonghyeok/westagram/westagram-main/index.js
@@ -1,18 +1,54 @@
 //댓글 input에 글이 입력되고 게시버튼, enter 클릭 시 새로운 글이 입력이 된다.
 //<div class="comment-box"> 에 글을 추가 한다.
-//resultHtml = [] 입력값을 준다.
-//resultHtml = [] 여기에 들어온 값을 <div class="comment-box"> 에 추가
+//commentList = [] 입력값을 준다.
+//commentList = [] 여기에 들어온 값을 <div class="comment-box"> 에 추가
 
-const newComment = document.querySelector('.writing-input');
-const addButton = document.querySelector('.writing-btn');
-const tabs = document.querySelectorAll()
+let newComment = document.querySelector('.writing-input');
+let addButton = document.querySelector('.writing-btn');
 
-const addTask = []
+let commentList = [];
 
-addButton.addEventListener("click", addTask);
+ 
+
+addButton.addEventListener('click', addComment);
 
 
-newComment.addEventListener('click', function(){
-  const commentValue = newComment.value
-  
-})
+// 데이터 세팅(=배열에 input value 추가)
+function addComment() {
+  let userComment = newComment.value;
+  commentList.push(userComment)
+  newComment.value = '' //값 push 이후 input 창 비우기
+  render();  
+}
+
+
+// 데이터를 html 형태로 바꿔서 화면에 그리기
+function render() {
+  let resultHTML = '';
+
+  for(let i=0; i<commentList.length; i++){
+    let commentHTML = `
+    <div class="comment">
+     <div class="comment-box">
+      <div>
+        <span class="userId">afdgdfsd</span>
+        <span>${commentList[i]}</span>
+      </div>
+      <i class="fa-regular fa-heart"></i>
+    </div>
+    <div class="comment-time">42분 전</div>
+    </div>`
+
+    resultHTML = resultHTML + commentHTML
+    // document.querySelector('.comments-section').innerHTML = resultHTML
+  } 
+  document.querySelector('.comments-section').innerHTML = resultHTML
+}
+
+function onKeyUp(e){
+  if(e.key === "Enter"){
+    addComment();
+  }
+}
+
+
