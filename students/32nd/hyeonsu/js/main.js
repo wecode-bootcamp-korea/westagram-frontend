878b51de58b928b7ab12e66551437758225dc621
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-04-07 18:22:59 +0900

    Refactor: 새로운 요소를 만드는 함수 두 가지를 템플릿 리터럴을 사용해 짧게 리팩토링

--- students/32nd/hyeonsu/js/main.js
+++ students/32nd/hyeonsu/js/main.js
@@ -26,53 +26,20 @@ const createCommentLi = (id, comment) => {
   const postCommentItem = document.createElement("li");
   postCommentItem.className = "post-comment-item";
 
-  const postCommentItemLeft = document.createElement("div");
-  postCommentItemLeft.className = "post-comment-item-left";
-
-  const userProfileLink = document.createElement("a");
-  userProfileLink.href = "#";
-  const userId = document.createElement("strong");
-  userId.className = "user-id";
-  userId.textContent = id;
-  userProfileLink.appendChild(userId);
-
-  const commentContent = document.createElement("span");
-  commentContent.className = "post-comment-content";
-  commentContent.textContent = comment;
-
-  postCommentItemLeft.appendChild(userProfileLink);
-  postCommentItemLeft.appendChild(commentContent);
-
-  const postCommentItemRight = document.createElement("div");
-  postCommentItemRight.className = "post-comment-item-right";
-
-  const heartButton = document.createElement("button");
-  heartButton.className = "button-heart";
-  const heartIcon = document.createElement("i");
-  heartIcon.className = "fa-regular fa-heart";
-  heartButton.appendChild(heartIcon);
-
-  heartButton.addEventListener("click", () => {
-    heartButton.classList.toggle("liked");
-    heartButton.querySelector("i").classList.toggle("fa-regular");
-    heartButton.querySelector("i").classList.toggle("fa-solid");
-  });
-
-  const deleteButton = document.createElement("button");
-  deleteButton.className = "button-delete";
-  const deleteStrong = document.createElement("strong");
-  deleteStrong.textContent = "삭제";
-  deleteButton.appendChild(deleteStrong);
-
-  deleteButton.addEventListener("click", (e) => {
-    e.target.closest("li").remove();
-  });
-
-  postCommentItemRight.appendChild(heartButton);
-  postCommentItemRight.appendChild(deleteButton);
-
-  postCommentItem.appendChild(postCommentItemLeft);
-  postCommentItem.appendChild(postCommentItemRight);
+  postCommentItem.innerHTML = `<div class="post-comment-item-left">
+  <a>
+    <strong class="user-id">${id}</strong>
+  </a>
+  <span class="post-comment-content">${comment}</span>
+</div>
+<div class="post-comment-item-right">
+  <button class="button-heart">
+    <i class="fa-regular fa-heart" aria-hidden ></i>
+  </button>
+  <button class="button-delete">
+    <strong class="button-delete-strong">삭제</strong>
+  </button>
+</div>`;
 
   return postCommentItem;
 };
@@ -88,6 +55,16 @@ postCommentForm.addEventListener("submit", (e) => {
   postCommentInput.value = "";
   postCommentButton.disabled = true;
   postCommentList.appendChild(newComment);
+});
+
+postCommentList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-heart")) {
+    e.target.parentNode.classList.toggle("liked");
+    e.target.classList.toggle("fa-regular");
+    e.target.classList.toggle("fa-solid");
+  } else if (e.target.classList.contains("button-delete-strong")) {
+    e.target.closest(".post-comment-item").remove();
+  }
 });
 
 const gnbSearchBar = document.body.querySelector(".gnb-search");
@@ -138,40 +115,18 @@ const paintMatchedArray = () => {
     const matchedLi = document.createElement("li");
     matchedLi.className = "gnb-search-result-item modal-item";
 
-    const matchedLiLink = document.createElement("a");
-    matchedLiLink.href = "#";
-
-    const matchedLiLeft = document.createElement("div");
-    matchedLiLeft.className = "gnb-search-result-item-left";
-    const profileImg = document.createElement("img");
-    profileImg.src = el.profileImgUrl;
-    profileImg.className = "gnb-search-result-item-img";
-    profileImg.alt = `Profile image of ${el.username}`;
-    const idUsernameWrapper = document.createElement("div");
-    idUsernameWrapper.className = "gnb-search-result-item-profile";
-    const id = document.createElement("strong");
-    id.textContent = el.id;
-    id.className = "gnb-search-result-item-id";
-    const username = document.createElement("span");
-    username.textContent = el.username;
-    username.className = "gnb-search-result-item-username";
-
-    idUsernameWrapper.appendChild(id);
-    idUsernameWrapper.appendChild(username);
-
-    matchedLiLeft.appendChild(profileImg);
-    matchedLiLeft.appendChild(idUsernameWrapper);
-
-    const matchedLiRight = document.createElement("div");
-    matchedLiRight.className = "gnb-search-result-item-right";
-    const xButton = document.createElement("i");
-    xButton.className = "fa-solid fa-x";
-    matchedLiRight.appendChild(xButton);
-
-    matchedLiLink.appendChild(matchedLiLeft);
-    matchedLiLink.appendChild(matchedLiRight);
-
-    matchedLi.appendChild(matchedLiLink);
+    matchedLi.innerHTML = `<a href="#">
+        <div class="gnb-search-result-item-left">
+          <img alt="Profile image of ${el.username}" src="${el.profileImgUrl}" class="gnb-search-result-item-img"/>
+          <div>
+            <strong class="gnb-search-result-item-id">${el.id}</strong>
+            <span class="gnb-search-result-item-id">${el.username}</span>
+          </div>
+        </div>
+        <div class="gnb-search-result-item-right">
+          <i class="fa-solid fa-x" aria-hidden></i>
+        </div>
+      </a>`;
 
     gnbSearchResultList.appendChild(matchedLi);
   });
