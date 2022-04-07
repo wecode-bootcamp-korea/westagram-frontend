02f3a5c44f7dd82f82964f4efd1613ded1ff0590
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-04-07 17:49:31 +0900

    Modify: 로그인 페이지 ID, PW 유효성 검사를 따로 분리

--- students/32nd/hyeonsu/js/login.js
+++ students/32nd/hyeonsu/js/login.js
@@ -3,9 +3,11 @@ const idInput = document.querySelector(".input-id");
 const pwInput = document.querySelector(".input-pw");
 const loginBtn = document.querySelector(".login-button");
 
-loginInputs.addEventListener("keyup", () => {
+const validateIdAndPw = () => {
   const isIncludeAt = idInput.value.includes("@");
   const isPwMoreThanFive = pwInput.value.length >= 5;
 
   loginBtn.disabled = isIncludeAt && isPwMoreThanFive ? false : true;
-});
+};
+
+loginInputs.addEventListener("keyup", validateIdAndPw);
