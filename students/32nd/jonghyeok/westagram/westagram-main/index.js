25ff4a013ba0a13d779e94be0701698194cfa6fc
Marco <jonghyeok.lim14@gmail.com>
2022-04-07 18:48:09 +0900

    [32기 임종혁]리펙토링

--- students/32nd/jonghyeok/westagram/westagram-main/index.js
+++ students/32nd/jonghyeok/westagram/westagram-main/index.js
@@ -1,28 +1,17 @@
-//댓글 input에 글이 입력되고 게시버튼, enter 클릭 시 새로운 글이 입력이 된다.
-//<div class="comment-box"> 에 글을 추가 한다.
-//commentList = [] 입력값을 준다.
-//commentList = [] 여기에 들어온 값을 <div class="comment-box"> 에 추가
-
 let newComment = document.querySelector('.writing-input');
 let addButton = document.querySelector('.writing-btn');
 
 let commentList = [];
 
- 
-
 addButton.addEventListener('click', addComment);
 
-
-// 데이터 세팅(=배열에 input value 추가)
 function addComment() {
   let userComment = newComment.value;
   commentList.push(userComment)
-  newComment.value = '' //값 push 이후 input 창 비우기
+  newComment.value = ''
   render();  
 }
 
-
-// 데이터를 html 형태로 바꿔서 화면에 그리기
 function render() {
   let resultHTML = '';
 
@@ -40,7 +29,6 @@ function render() {
     </div>`
 
     resultHTML = resultHTML + commentHTML
-    // document.querySelector('.comments-section').innerHTML = resultHTML
   } 
   document.querySelector('.comments-section').innerHTML = resultHTML
 }
