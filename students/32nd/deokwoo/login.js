469f8a204f13ff41c70ded115e872abeee448cf2
jungdeokwoo <97271725+jungdeokwoo@users.noreply.github.com>
2022-04-07 17:43:30 +0900

    [32기 정덕우]들여쓰기수정

--- students/32nd/deokwoo/login.js
+++ students/32nd/deokwoo/login.js
@@ -1,29 +1,26 @@
-let id = document.querySelector(".id input")
-let pw = document.querySelector(".pw input")
-let btn = document.querySelector(".button .btn")
-let main = document.querySelector(".wrapper")
+let id = document.querySelector(".id input");
+let pw = document.querySelector(".pw input");
+let btn = document.querySelector(".button .btn");
+let main = document.querySelector(".wrapper");
 
 function validate() {
-    if(id.value && pw.value){
-        // btn.removeAttribute("disabled")
-        // btn.classList.add("active")
-        if(id.value.includes("@")&&pw.value.length > 5){
-            btn.removeAttribute("disabled")
-            btn.classList.add("active")
-        }  
-    }else {
-        btn.setAttribute("disabled",true)
-        btn.classList.remove("active")
+  if (id.value && pw.value) {
+    // btn.removeAttribute("disabled")
+    // btn.classList.add("active")
+    if (id.value.includes("@") && pw.value.length > 5) {
+      btn.removeAttribute("disabled");
+      btn.classList.add("active");
     }
+  } else {
+    btn.setAttribute("disabled", true);
+    btn.classList.remove("active");
+  }
 }
 
-main.addEventListener("keyup",validate)
-
-
-
+main.addEventListener("keyup", validate);
 
 function gomain() {
-    window.location.href = 'http://127.0.0.1:5500/main.html'
+  window.location.href = "http://127.0.0.1:5500/main.html";
 }
 
-btn.addEventListener('click', gomain)
+btn.addEventListener("click", gomain);
--- students/32nd/deokwoo/main.js
+++ students/32nd/deokwoo/main.js
@@ -1,106 +1,92 @@
-let src = document.querySelector(".search")
-let point = document.querySelector(".inputsrc")
+let src = document.querySelector(".search");
+let point = document.querySelector(".inputsrc");
 
 function hideSearch() {
-    src.style.visibility = 'hidden'
-    point.setAttribute("placeholder", "검색")
-    point.focus()
+  src.style.visibility = "hidden";
+  point.setAttribute("placeholder", "검색");
+  point.focus();
 }
 
 function showSearch() {
-    src.style.visibility = ""
-    point.removeAttribute("placeholder")
+  src.style.visibility = "";
+  point.removeAttribute("placeholder");
 }
 
-src.addEventListener("click",hideSearch)
-point.addEventListener("focusout",showSearch)
+src.addEventListener("click", hideSearch);
+point.addEventListener("focusout", showSearch);
 
-
-
-let reply = document.querySelector(".reply_list")
-let ent = document.querySelector(".replyent")
-let text = document.querySelector(".replyInput")
+let reply = document.querySelector(".reply_list");
+let ent = document.querySelector(".replyent");
+let text = document.querySelector(".replyInput");
 let btn = document.querySelector(".replyent");
 
-
-
 function activeReply() {
-    if(text.value){
-        btn.removeAttribute("disabled")
-        btn.classList.add("active")
-    } else {
-        btn.setAttribute("disabled",true)
-        btn.classList.remove("active")
-    }
+  if (text.value) {
+    btn.removeAttribute("disabled");
+    btn.classList.add("active");
+  } else {
+    btn.setAttribute("disabled", true);
+    btn.classList.remove("active");
+  }
 }
 
-text.addEventListener('keyup',activeReply)
-
-
+text.addEventListener("keyup", activeReply);
 
 function addReply() {
-    let li = document.createElement("li")
-    let id = document.getElementById("id").textContent
-    li.innerHTML = `<span>${id}</span> ${text.value}<i class="fa-regular fa-heart"></i>`
-    
-    let delbtn = document.createElement("button")
-    delbtn.setAttribute("class","delete")
-    delbtn.innerText = "삭제"
-    delbtn.addEventListener('click', () => li.remove())
-    
-    li.appendChild(delbtn)
-    reply.appendChild(li)
-    
-    text.value = ""
-    btn.setAttribute("disabled",true)
-    btn.classList.remove("active")
+  let li = document.createElement("li");
+  let id = document.getElementById("id").textContent;
+  li.innerHTML = `<span>${id}</span> ${text.value}<i class="fa-regular fa-heart"></i>`;
+
+  let delbtn = document.createElement("button");
+  delbtn.setAttribute("class", "delete");
+  delbtn.innerText = "삭제";
+  delbtn.addEventListener("click", () => li.remove());
+
+  li.appendChild(delbtn);
+  reply.appendChild(li);
+
+  text.value = "";
+  btn.setAttribute("disabled", true);
+  btn.classList.remove("active");
 }
 
-ent.addEventListener("click",addReply)
-
-
+ent.addEventListener("click", addReply);
 
 function enterKey(e) {
-    if(e.code === 'Enter'){
-        addReply()
-    }
+  if (e.code === "Enter") {
+    addReply();
+  }
 }
 
-text.addEventListener('keyup',enterKey)
+text.addEventListener("keyup", enterKey);
 
-
-
-
-let del = document.querySelector(".delete")
+let del = document.querySelector(".delete");
 
 function delHandler(e) {
-    e.target.parentNode.remove()
+  e.target.parentNode.remove();
 }
 
-del.addEventListener("click",delHandler)
+del.addEventListener("click", delHandler);
 
-
-
-
-let info = document.querySelector(".information")
-let pf = document.querySelector(".profile")
+let info = document.querySelector(".information");
+let pf = document.querySelector(".profile");
 
 function showInfo() {
-    info.classList.add("show")
-    let allScreen = document.createElement("div")
+  info.classList.add("show");
+  let allScreen = document.createElement("div");
 
-    allScreen.setAttribute("class","hide")
-    allScreen.addEventListener("click",() => {
-        info.classList.remove("show")
-        let all = document.querySelector(".hide")
-        all.remove()
-        pf.style.border = "none"
-    })
+  allScreen.setAttribute("class", "hide");
+  allScreen.addEventListener("click", () => {
+    info.classList.remove("show");
+    let all = document.querySelector(".hide");
+    all.remove();
+    pf.style.border = "none";
+  });
 
-    document.querySelector(".navigation").append(allScreen)
-    
-    pf.style.border = "2px solid red"
-    pf.style.borderRadius = "50%"
+  document.querySelector(".navigation").append(allScreen);
+
+  pf.style.border = "2px solid red";
+  pf.style.borderRadius = "50%";
 }
 
-pf.addEventListener("click",showinfo)
+pf.addEventListener("click", showInfo);
