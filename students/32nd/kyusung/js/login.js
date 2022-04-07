80200b8ed61e2a91cd1e991fc1b286515ff9c299
cks612 <kyusung612@gmail.com>
2022-04-07 17:38:54 +0900

    Add: 리펙토링 추가

--- students/32nd/kyusung/js/login.js
+++ students/32nd/kyusung/js/login.js
@@ -1,32 +1,26 @@
-const login_id = document.getElementById('login_id');
-const password = document.getElementById('password');
-const submit = document.getElementById('submit');
+const login_id = document.getElementById("login_id");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
 
-// login_id.addEventListener('keyup', loginEnabled);
-// password.addEventListener('keyup', loginEnabled);
-
-
-
-// function loginEnabled(){
-//   console.log(!(login_id.value && password.value));
-//   switch(!(login_id.value && password.value)){
-//     case true:
-//       submit.disabled = true;
-//       break;
-//     case false:
-//       submit.disabled = false;
-//       break;
-//   }
-// };
-
-
-password.addEventListener('keyup', () => {
+password.addEventListener("keyup", () => {
   const password_input = password.value;
-  if(login_id.value && password_input){
+  if (login_id.value && password_input) {
     submit.disabled = false;
   }
-  if(password_input === '' ){
+  if (password_input === "") {
     submit.disabled = true;
   }
   console.log(password_input);
+});
+
+submit.addEventListener("click", () => {
+  if (login_id.value.indexOf("@") === -1) {
+    alert("아이디에 @를 포함시키세요");
+    login_id.focus();
+  } else if (password.value.length < 5) {
+    alert("비밀번호를 다시 입력하세요");
+    password.focus();
+  } else {
+    location.href = "./main.html";
+  }
 });
