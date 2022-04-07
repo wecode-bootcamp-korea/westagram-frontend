4c71cf692543c1de1dc6e8754188359bd6c0a26d
doyu <ydh0905@nate.com>
2022-04-07 18:20:29 +0900

    add function: remove comment when click the delete button

--- students/32nd/donghyuk/js/main.js
+++ students/32nd/donghyuk/js/main.js
@@ -21,27 +21,36 @@ window.addEventListener("resize", () => {
 });
 
 const addComment = (id) => {
-  let commentRow = document.createElement("div");
+  let commentRow = document.createElement("li");
   let commentRowLeft = document.createElement("div");
+  let commentRowRight = document.createElement("div");
   let commentId = document.createElement("span");
   let commentText = document.createElement("span");
-  let commentHeart = document.createElement("span");
+  let commentDelete = document.createElement("button");
+  let commentHeart = document.createElement("button");
 
   commentRow.classList.add("article-comment__row");
   commentRowLeft.classList.add("comment__row-left");
+  commentRowRight.classList.add("comment__row-right");
   commentId.classList.add("comment__id");
   commentText.classList.add("comment__text");
+  commentDelete.classList.add("comment-delete");
   commentHeart.classList.add("comment-heart");
 
   commentId.innerText = id;
   commentText.innerText = commentInput.value;
-  commentHeart.innerHTML = "<i class='fas fa-heart'></i>";
+  commentDelete.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  commentHeart.innerHTML = '<i class="fas fa-heart"></i>';
+
+  // commentRow.setAttribute("id", Date.now());
   commentHeart.setAttribute("data-clicked", "false");
 
   commentRowLeft.appendChild(commentId);
   commentRowLeft.appendChild(commentText);
+  commentRowRight.appendChild(commentDelete);
+  commentRowRight.appendChild(commentHeart);
   commentRow.appendChild(commentRowLeft);
-  commentRow.appendChild(commentHeart);
+  commentRow.appendChild(commentRowRight);
   comments.appendChild(commentRow);
 
   commentInput.value = "";
@@ -58,9 +67,7 @@ commentInput.addEventListener("keyup", (event) => {
     : (commentBtn.style.color = "#c4e1fb");
 });
 
-comments.addEventListener("click", (event) => {
-  const target = event.target;
-  const targetParentNode = target.parentNode;
+const toggleHeartColor = (target, targetParentNode) => {
   if (targetParentNode.dataset.clicked === "false") {
     target.style.color = "red";
     targetParentNode.setAttribute("data-clicked", "true");
@@ -68,4 +75,16 @@ comments.addEventListener("click", (event) => {
     target.style.color = "black";
     targetParentNode.setAttribute("data-clicked", "false");
   }
+};
+
+const removeComment = (target) => {
+  target.closest(".article-comment__row").remove();
+};
+
+comments.addEventListener("click", (event) => {
+  const target = event.target;
+  const targetParentNode = target.parentNode;
+  target.className === "fas fa-trash-alt"
+    ? removeComment(target)
+    : toggleHeartColor(target, targetParentNode);
 });
