556eb056b831bd99b306e997a821ebd0736adf88
Marco <jonghyeok.lim14@gmail.com>
2022-04-09 17:52:05 +0900

    [32기 임종혁]댓글삭제기능추가

--- students/32nd/jonghyeok/westagram/westagram-main/index.js
+++ students/32nd/jonghyeok/westagram/westagram-main/index.js
@@ -12,6 +12,13 @@ function addComment() {
   render();  
 }
 
+function deleteComment(commentText) {
+  //commentList에서 commentText와 일치하는 요소 찾아서 삭제...!
+  // console.log(commentText)
+  let index = commentList.indexOf(commentText)
+  commentList.splice(index, 1)
+}
+
 function render() {
   let resultHTML = '';
 
@@ -21,40 +28,44 @@ function render() {
       <div class="comment-box">
         <div>
           <span class="userId">afdgdfsd</span>
-          <span>${commentList[i]}</span>
+          <span class="commentText">${commentList[i]}</span>
         </div>
         <div class="deletebtn">
           <button class='commentDelete'>삭제</button>
-          <i class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-heart comment-heart"></i>
         </div>
       </div>
       <div class="comment-time">42분 전</div>
     </div>`
 
     resultHTML = resultHTML + commentHTML
-  } 
-  document.querySelector('.comments-section').innerHTML = resultHTML
+    document.querySelector('.comments-section').innerHTML = resultHTML
+    
+    let deleteBtns = document.querySelectorAll('.commentDelete'); //배열
+    //반복문으로 배열을 순회하여 요소마다 addEventListener 추가!
+
+    for (let i = 0; i< deleteBtns.length; i++) {
+      deleteBtns[i].addEventListener('click', function(event) {
+        let commentText = document.querySelector('.commentText').innerHTML
+        event.target.parentNode.parentNode.parentNode.remove();
+        deleteComment(commentText)
+      })
+    }
+  }
 }
 
-function onKeyUp(e){
-  if(e.key === "Enter"){
+function onKeyUp(event){
+  if(event.key === "Enter"){
     addComment();
   }
 }
 
 
 // let borderHeart = document.querySelector('.fa-regular fa-heart');
-
+// console.log(borderHeart)
 // changeHeart.addEventListener('click', function() {
 //  borderHeart.classList.toggle('borderHeart')
 // })
 
 // let redHeart = document.querySelector('.fa-solid fa-heart').style.display = 'none'
 
-let deleteButton = document.querySelector('.commentDelete');
-
-deleteButton.addEventListener('click', deleteComment);
-console.log('clicked')
-function deleteComment() {
-
-}
