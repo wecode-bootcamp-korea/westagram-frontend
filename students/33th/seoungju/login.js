bfa0ee0b071d49d0f4ed06d16813eded9733f7a5
SeoungJu <wkddb1359@naver.com>
2022-05-04 15:25:39 +0900

    Add: 기존 코드 옮김

---
+++ students/33th/seoungju/login.js
@@ -0,0 +1,29 @@
+const id = document.querySelector('#id');
+const password = document.querySelector('#password');
+const btn = document.querySelector('.login_button');
+
+
+const loginActive = function() {
+
+    let idValue = id.value;
+    let passwordValue = password.value;
+
+    if((idValue.length > 1 && passwordValue.length > 1))
+    {
+        btn.classList.add('login_button_active');
+
+    }
+    else
+    {
+        btn.classList.remove('login_button_active')
+    }
+
+}
+
+    
+
+
+
+id.addEventListener('keyup',loginActive);
+password.addEventListener('keyup',loginActive);
+
