fa90c19e32883c47e4102b13f94fca1b324c9ff3
Hengxi <jinhengxi93@gmail.com>
2022-05-04 15:25:40 +0900

    ADD: 기존 코드 옮김

---
+++ students/33th/hyungseok/login.js
@@ -0,0 +1,117 @@
+const thisIsId = document.querySelector('.id');
+const thisIsPsw = document.querySelector('.password');
+const thisIsBtn = document.querySelector('.btn')
+const loginPage = document.querySelector('.westargram');
+const mainPage = document.querySelector('.mainContainer');
+const commentPost = document.querySelector('.commentPost');
+const commentInput = document.querySelector('.commentInput');
+const commentBox = document.querySelector('.commentBox')
+
+
+
+
+
+thisIsPsw.addEventListener('keydown',()=>{
+    {
+        thisIsId.value !== undefined && thisIsPsw.value !== undefined
+        ?thisIsBtn.classList.add('btnOn')
+        :thisIsBtn.classList.remove('btnOn')
+    }
+})
+
+thisIsBtn.addEventListener('click',()=>{
+    let Id = thisIsId.value
+    let psw = thisIsPsw.value
+    let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    {
+        Id.match(regEmail) && psw.length > 5
+        ?(loginPage.style.display = "none") (mainPage.style.display = "block")
+        :alert('아이디, 비밀번호를 확인해주세요.')
+    }
+})
+
+
+
+
+function chatInput(){
+    
+    let inputValue = commentInput.value;
+    
+    const spanNickname = document.createElement("span");
+    spanNickname.classList.add('nickname')
+    spanNickname.innerHTML = 'hengxi '
+    commentBox.appendChild(spanNickname)
+
+    const spanContent = document.createElement('span')
+    spanContent.classList.add('comment')
+    spanContent.innerHTML = inputValue;
+    commentBox.appendChild(spanContent)
+
+    const divGood = document.createElement('div')
+    divGood.classList.add('goodBox')
+    commentBox.appendChild(divGood)
+
+    const pContent = document.createElement('p')
+    pContent.classList.add('time')
+    pContent.innerHTML = '10분전';
+    divGood.appendChild(pContent)
+
+    const iHeart = document.createElement('i')
+    iHeart.classList.add('fa-regular', 'fa-heart', 'heart')
+    divGood.appendChild(iHeart)
+
+    const idelete = document.createElement('i')
+    idelete.classList.add('fa-solid', 'fa-delete-left')
+    divGood.appendChild(idelete)
+
+    iHeart.addEventListener('click',()=>{
+        console.log("d")
+        {
+        iHeart.style.color !== 'red'
+        ?iHeart.style.color = 'red'
+        :iHeart.style.color = 'black'
+        }
+    })
+    
+    idelete.addEventListener('click',()=>{
+        divGood.remove();
+        spanNickname.remove();
+        spanContent.remove();
+    })
+
+    commentInput.value = "";
+    commentInput.focus()
+}
+
+
+
+commentPost.addEventListener('click',()=>{
+    {
+    commentInput.value !== ""
+    ?chatInput()
+    :null
+    }
+})
+
+commentInput.addEventListener('keypress',(e)=>{
+    {
+    e.code === 'Enter' && commentInput.value !== ""
+    ?chatInput()
+    :null
+    }
+})
+
+commentInput.addEventListener('input',()=>{
+    {
+    commentInput.value !== ""
+    ?commentPost.style.color = '#1995f6'
+    :commentPost.style.color = '#b2dffc'
+    }
+})
+
+
+
+
+
+
+
