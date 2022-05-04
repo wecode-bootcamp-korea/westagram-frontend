23a7f8f780b01a083f9982742791428a85d4b794
Yumjong <yumjongeun@gmail.com>
2022-05-04 16:36:06 +0900

    ADD:login 화면 추가 구현

--- students/33th/jongeun/westagram_login/login.js
+++ students/33th/jongeun/westagram_login/login.js
@@ -3,12 +3,21 @@ const pwInput = document.querySelector('.pw_input')
 const loginBtn = document.querySelector('.login_btn')
 
 
+
+
+
 pwInput.addEventListener('input',inputIdPw)
 
 function inputIdPw() {
-    if(idInput.value && pwInput.value){
+    const idValidation = idInput.value
+    const pwValidation = pwInput.value
+
+    if(idValidation.includes('@')  && pwValidation.length >= 5){
         loginBtn.setAttribute('style','cursor:pointer')
         loginBtn.disabled = false;
+    }else{
+        loginBtn.setAttribute('style','cursor:auto')
+        loginBtn.disabled = true;
     }
-    idInput.value && pwInput.value ? loginBtn.style.backgroundColor ='rgb(63, 157, 251)':loginBtn.style.backgroundColor ='rgb(204, 231, 255)'
+    idValidation.includes('@') && pwValidation.length >= 5 ? loginBtn.style.backgroundColor ='rgb(63, 157, 251)':loginBtn.style.backgroundColor ='rgb(204, 231, 255)'
 }
