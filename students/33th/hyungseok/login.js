86b590e66ca18f3ded180e2d66ceac745b818132
Hengxi <jinhengxi93@gmail.com>
2022-05-05 15:57:07 +0900

    js modify

--- students/33th/hyungseok/login.js
+++ students/33th/hyungseok/login.js
@@ -1,8 +1,9 @@
 const thisIsId = document.querySelector(".id");
 const thisIsPsw = document.querySelector(".password");
-const thisIsBtn = document.querySelector(".btn");
-const loginPage = document.querySelector(".westargram");
-const mainPage = document.querySelector(".mainContainer");
+const thisIsBtn = document.querySelector(".loginBtn");
+const loginPage = document.querySelector(".loginPage");
+const mainPage = document.querySelector(".mainPage");
+
 const commentPost = document.querySelector(".articlePost");
 const commentInput = document.querySelector(".articleInput");
 const commentBox = document.querySelector(".articleCommentBox");
@@ -23,42 +24,43 @@ thisIsBtn.addEventListener("click", () => {
   {
     Id.match(regEmail) && psw.length > 5
     ? (loginPage.style.display = "none")((mainPage.style.display = "block"))
-    : alert("아이디, 비밀번호를 확인해주세요.");
+    : alert("ID : '@', '.' 포함 / PW : 5글자 이상");
   }
 });
+
+
 
 function chatInput() {
   let inputValue = commentInput.value;
 
   const spanNickname = document.createElement("span");
-  spanNickname.classList.add("nickname");
+  spanNickname.classList.add("articleNickname");
   spanNickname.innerHTML = "hengxi ";
   commentBox.appendChild(spanNickname);
 
   const spanContent = document.createElement("span");
-  spanContent.classList.add("comment");
+  spanContent.classList.add("articleComment");
   spanContent.innerHTML = inputValue;
   commentBox.appendChild(spanContent);
 
   const divGood = document.createElement("div");
-  divGood.classList.add("goodBox");
+  divGood.classList.add("likeIcon");
   commentBox.appendChild(divGood);
 
   const pContent = document.createElement("p");
-  pContent.classList.add("time");
+  pContent.classList.add("articleTime");
   pContent.innerHTML = "10분전";
   divGood.appendChild(pContent);
 
   const iHeart = document.createElement("i");
-  iHeart.classList.add("fa-regular", "fa-heart", "heart");
+  iHeart.classList.add("fa-regular", "fa-heart", "likeIconsHeart", "likeIcons");
   divGood.appendChild(iHeart);
 
   const idelete = document.createElement("i");
-  idelete.classList.add("fa-solid", "fa-delete-left");
+  idelete.classList.add("fa-solid", "fa-delete-left", "likeIconsDelete", "likeIcons");
   divGood.appendChild(idelete);
 
   iHeart.addEventListener("click", () => {
-    console.log("d");
     {
       iHeart.style.color !== "red"
         ? (iHeart.style.color = "red")
@@ -99,30 +101,30 @@ commentInput.addEventListener("input", () => {
 
 // 검색기능
 
-id = ["1", "2", "3", "4", "5", "22"];
+// id = ["1", "2", "3", "4", "5", "22"];
 
-const searchInput = document.querySelector(".searchInput");
+// const searchInput = document.querySelector(".searchInput");
 
-searchInput.addEventListener("input", () => {
-  const searchBox = document.querySelector(".idFindBox");
+// searchInput.addEventListener("input", () => {
+//   const searchBox = document.querySelector(".idFindBox");
 
-  for (let i = 0; i < id.length; i++) {
-    if (searchInput.value === id[i]) {
-      const searchDiv = document.createElement("div");
-      searchDiv.classList.add("idFindContent");
-      searchBox.appendChild(searchDiv);
+//   for (let i = 0; i < id.length; i++) {
+//     if (searchInput.value === id[i]) {
+//       const searchDiv = document.createElement("div");
+//       searchDiv.classList.add("idFindContent");
+//       searchBox.appendChild(searchDiv);
 
-      const searchImg = document.createElement("img");
-      searchImg.classList.add("searchImg");
-      searchDiv.appendChild(searchImg);
+//       const searchImg = document.createElement("img");
+//       searchImg.classList.add("searchImg");
+//       searchDiv.appendChild(searchImg);
 
-      const searchText = document.createElement("span");
-      searchText.classList.add("searchText");
-      searchText.innerHTML = id[i];
-      searchDiv.appendChild(searchText);
-    }
-    if (searchInput.value !== id[i]) {
-      searchDiv.remove();
-    }
-  }
-});
+//       const searchText = document.createElement("span");
+//       searchText.classList.add("searchText");
+//       searchText.innerHTML = id[i];
+//       searchDiv.appendChild(searchText);
+//     }
+//     if (searchInput.value !== id[i]) {
+//       searchDiv.remove();
+//     }
+//   }
+// });
