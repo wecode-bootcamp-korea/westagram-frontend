372e195d1e7cf7e0f6c9910314a23d2312a94075
hymn9611 <hymn9611@naver.com>
2022-05-05 18:30:00 +0900

    추가구현 5단계 완료

--- students/33th/hyunmin/login.js
+++ students/33th/hyunmin/login.js
@@ -2,12 +2,19 @@ const id = document.getElementById('id');
 const password = document.getElementById('password');
 const loginBtn = document.getElementsByClassName('loginBtn')[0];
 
+const verifyId = () => {
+    const idValue = id.value;
+    return idValue.includes('@') ? true : false;
+}
+
+const verifyPw = () => {
+    const pwValue = password.value;
+    return pwValue.length>=5 ? true : false;
+}
+
 // ID, PW 길이에 따른 로그인버튼
 const activateLogin = () => {
-    let idValue = id.value;
-    let pwValue = password.value;
-
-    if(idValue.length >= 1 && pwValue.length >= 1) {
+    if(verifyId() && verifyPw()) {
         loginBtn.disabled = false;
         loginBtn.style.backgroundColor = '#0095F6';
         loginBtn.style.cursor = 'pointer';
@@ -20,6 +27,7 @@ const activateLogin = () => {
 
 const successLogin = () => {
     alert('로그인 성공!');
+    location.href = './main.html'
 }
 
 id.addEventListener('keyup', activateLogin);
