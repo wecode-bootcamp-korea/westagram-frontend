7c08442669ca6c0714c8e4a6e9df5bb88c10da21
hymn9611 <hymn9611@naver.com>
2022-05-05 19:40:41 +0900

    추가구현 6단계 레이아웃 구현

--- students/33th/hyunmin/main.js
+++ students/33th/hyunmin/main.js
@@ -7,16 +7,24 @@ function submitComment() {
     const commentDiv = document.createElement("div");
     const userName = document.createElement("p");
     const comment = document.createElement("div");
+    const heart = document.createElement("p");
+    const deleteComment = document.createElement("p");
 
     commentDiv.classList.add("userCommentBox");
     userName.classList.add("userName");
     comment.classList.add("userComment");
+    heart.classList.add("userHeart");
+    deleteComment.classList.add("delete");
 
-    userName.innerHTML = "hacker";
+    userName.innerHTML = "happyhappy"; // *** 임시 닉네임 ***
     comment.innerText = inputComment.value;
+    heart.innerHTML = '<i class="fa-solid fa-heart commentIcon"></i>';
+    deleteComment.innerHTML = '<i class="fa-solid fa-trash commentIcon"></i>';
 
     commentDiv.appendChild(userName);
     commentDiv.appendChild(comment);
+    commentDiv.appendChild(heart);
+    commentDiv.appendChild(deleteComment);
 
     newUserComment.appendChild(commentDiv);
 }
