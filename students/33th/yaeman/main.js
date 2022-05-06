6af6a83acf8c167446e9e9d8ae3325ac720687e2
chldpaks <chldpaks@gmail.com>
2022-05-06 10:13:42 +0900

    댓글 기능 구현중

--- students/33th/yaeman/main.js
+++ students/33th/yaeman/main.js
@@ -1,18 +1,88 @@
-// 1.canvas 엘리먼트 취득
-const canvas = document.getElementById("userProfile"); 
+const commentForm = document.querySelector('.upload-form');
+const commentInput = document.querySelector('#comment-write');
+const uploadButton = document.querySelector('.upload');
+const commentValue = commentInput.value;
 
-// 2. 2d모드의 그리기 객체 취득 
-const ctx = canvas.getContext("2d"); 
+const commented = document.querySelector('.commented');
 
-// 3. path 그리기 시작 설정 
-ctx.beginPath(); 
+// function uploadBtn() {
+//   const commentValue = commentInput.value;
 
-// 4. 원 모양 설정 
-ctx.arc(100, 75, 50, 0, 2*Math.PI); 
+//   if(commentValue.length > 0){ 
+//     uploadButton.disabled = false;
+//     uploadButton.style.cursor = "pointer";
+//     uploadButton.style.color = "#0095f6";
+//   } else {
+//     uploadButton.disabled = true;
+//     uploadButton.style.cursor = "default";
+//     uploadButton.style.color = "#99d2f8";
+//   }
+// }
 
-// 5. 그리기 
-ctx.stroke(); 
 
-// 6. 원 내부 색 채우기 
-ctx.fillStyle = 'blue'; 
-ctx.fill();
+// function createItem(text) {
+//   // console.log(text);
+//   const reply = document.createElement("li");
+//   reply.setAttribute("class", reply);
+//   reply.innerHTML =
+//   `<div class="replyDetail">
+//     <a href="" class="user-id"><span>kongji<span></a>
+//     <span>${text}</span>
+//   </div>
+//   <span class="sub-heart">
+//     <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+//               width="12px" height="12px">
+//   </span>
+//   `
+//   commented.appendChild(reply);
+// }
+
+function uploadBtn(e) {
+  let commentValue = commentInput.value; 
+  if(commentValue) {
+    uploadButton.disabled = false;
+    // uploadButton.style.opacity = 1;
+    uploadButton.style.cursor = "pointer";
+    uploadButton.style.color = "#0095f6";
+  } else {
+    uploadButton.disabled = true;
+    uploadButton.style.cursor = "default";
+    uploadButton.style.opacity = .4;
+  }
+  if(uploadButton.disabled=false && e.key === "Enter") {
+    e.preventDefault();
+    enterPost();
+    document.querySelector('.upload').click();
+  }
+}
+
+function enterPost() {
+  if(commentInput.value) {
+    const newUser = document.createElement('span');
+    newUser.className = "user-id";
+    newUser.innerText = "ym";
+
+    const newComment = document.createElement('span');
+    newComment.className = "comment-box";
+    newComment.appendChild(newUser);
+    newComment.innerHTML += commentInput.value;
+
+    const newImg = document.createElement('img');
+    newImg.className = "comment-heart";
+    newImg.setAttribute('src', "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png");
+
+    const newList = document.createElement('li');
+    newList.className = "newcomment-list";
+    newList.appendChild(newComment);
+    newList.appendChild(newImg);
+    commented.appendChild(newList);
+    commentInput.value = "";
+
+    uploadButton.disabled = true;
+    uploadButton.style.cursor = "default";
+    uploadButton.style.opacity = .4;
+  }
+}
+
+commentInput.addEventListener('keyup', uploadBtn);
+uploadButton.addEventListener('click', enterPost);
