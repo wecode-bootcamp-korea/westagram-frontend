3b3057494f123b8e4c40459b3c2808055ab4c472
Yuntroll95 <yuntrollpark@gamail.com>
2022-05-06 16:07:13 +0900

    ADD: keyup 에러 해결

--- students/33th/kyeongyeon/login.js
+++ students/33th/kyeongyeon/login.js
@@ -12,9 +12,10 @@ function loginBtn(){
         thisIsButton.disabled = true;
     }
 }
-inputId.addEventListener('keyup'. loginBtn);
-inputPw.addEventListener('keyup'. loginBtn);
+inputId.addEventListener('keyup', loginBtn());
+inputPw.addEventListener('keyup', loginBtn());
 
+//엔터나 로그인버튼 클릭시 a href 여기 링크에 걸린애로 넘어가는 함수 하나 더 필요(버튼 클릭이벤트) / 키프레스 혹은 키업 엔터를 눌렀을때 이동하는 함수!
 
 /* const thisIsButton = document.getElementsByClassName("btn-login")[0];
 thisIsButton.addEventListener('click', function () {
