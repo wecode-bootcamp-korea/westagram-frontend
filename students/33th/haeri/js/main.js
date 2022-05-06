986cfdea4a055db1ac95de96aa8c942d878c0d37
yesharry <reerohh@gmail.com>
2022-05-06 11:30:20 +0900

    files edit 2

--- students/33th/haeri/js/main.js
+++ students/33th/haeri/js/main.js
@@ -28,7 +28,7 @@ function paintToDo(newTodo) {
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
-    toDoList.appendChild(li);
+    toDoList.appendChild(li); 
 }
 
 function handleToDoSubmit(event) {
