8a75c859fc7439fffda1cbdd5cdfa5a5bd5bbbb8
jjk <jjkm0222@gmail.com>
2022-05-06 14:08:54 +0900

    엔터키로입력, 마우스 커서 기능 추가

--- students/33th/jungjun/JS/main.js
+++ students/33th/jungjun/JS/main.js
@@ -1,29 +1,12 @@
-/*
-// Get the input field
-var input = document.querySelector(".article__commentsInput");
-
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.querySelector(".article__commentsInput").click();
-  }
-});
-*/
-
-
 // <div class = "article__commentsShow">...</div>
 var commentsShow = document.querySelector(".article__commentsShow")
-console.log(commentsShow)
+
 // <input class = "article__commentsInput" type="text" placeholder="댓글 달기">
 var commentsInput = document.querySelector(".article__commentsInput")
-console.log(commentsInput)
+
 
 /*
-실수) 여기다가 var comment = commentsInput.value를 입력하면
+실수1) 여기다가 var comment = commentsInput.value를 입력하면
 comment에는 commentsInput.value의 초깃값인 '' 빈문자열이 들어가 있다. 
 입력한 댓글 내용을 추가하려면 event가 발생했을 때의 value로 --> addPost()로 옮기기
 
@@ -34,12 +17,11 @@ console.log(comment)
 
 // <span class = "post">게시</span>
 var post = document.querySelector(".post")
-console.log(post)
 
 // '게시'에 마우스 올리면 '포인터'로 커서 바꾸기
-// post.addEventListener("mouseover",()=>post.sytle.cursor = "pointer")
-post.addEventListener("click",addPost) // '게시' 누르면 addPost()함수 실행
+post.addEventListener("mouseover",()=>post.style.cursor = "pointer")
 
+post.addEventListener("click",addPost) // '게시' 누르면 addPost()함수 실행
 function addPost(){
     // comment = post를 "click" 했을 때의 commentsInput.value 
     let comment = commentsInput.value 
@@ -59,3 +41,31 @@ function addPost(){
     commentsInput.focus()
 
 }
+
+// Enter키 눌러서 댓글 입력하기
+// Get the input field
+// var commentsInput = document.querySelector(".article__commentsInput")
+
+// Execute a function when the user presses a key on the keyboard
+commentsInput.addEventListener("keypress", function(event) {
+  console.log(event) // event에는 KeyboardEvent가 자동으로 들어가 있다.
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    post.click();
+  }
+});
+// 여기까지가 Enter키 눌러서 댓글 입력하는 코드
+/* 새로 배운 내용
+addEventListener("이벤트명",function(event)) 하면 event 매개변수에는 
+이벤트명이 자동으로 들어간다. 
+function(event,a,b) 처럼 매개변수가 2개 이상이면 오류발생하는듯...
+
+addEventListener("이벤트명",함수이름)
+function 함수이름(event){
+  console.log(event)
+}
+하면 콘솔에 이벤트 이름 나온다.
+  */
