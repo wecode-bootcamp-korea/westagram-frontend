64c15905319758e53075e079f782f854a86a6df8
SeoungJu <wkddb1359@naver.com>
2022-05-06 16:36:59 +0900

    Modify: 코드 리펙토링

--- students/33th/seoungju/login.js
+++ students/33th/seoungju/login.js
@@ -1,19 +1,21 @@
-const id = document.querySelector('#id');
-const password = document.querySelector('#password');
-const btn = document.querySelector('.login_button');
+const id = document.querySelector('.id');
+const password = document.querySelector('.password');
+const loginBtn = document.querySelector('.login_button');
 
 const loginActive = function() {
-
     let idValue = id.value;
     let passwordValue = password.value;
 
-    if((idValue.length > 1 && passwordValue.length > 1)) {
-        btn.classList.add('login_button_active')
+    if(idValue && passwordValue) {
+        loginBtn.classList.add('login_button_active')
+
+        loginBtn.addEventListener('click',function() {
+            location.href="main.html"
+        })
     }
     else {
-        btn.classList.remove('login_button_active')
+        loginBtn.classList.remove('login_button_active')
     }
-
 }
 
 id.addEventListener('keyup',loginActive);
--- students/33th/seoungju/main.js
+++ students/33th/seoungju/main.js
@@ -1,15 +1,11 @@
-const commentFoam = document.querySelector('.summary_comment');
-const comment = document.querySelector('.comment_input');
-const commentBtn = document.querySelector('.comment_button');
-
+const commentFoam = document.querySelector('.feeds_like_wrapper');
+const comment = document.querySelector('.feeds_comment_input');
+const commentBtn = document.querySelector('.feeds_comment_button');
 
 const commentActive = function() {
 
     let commentInput = comment.value;
     const newComment = document.createElement('div');
-
-    if(window.event.keyCode === 13 && comment.value) {
-        console.log("엔터키 발생")
 
         newComment.innerHTML = `
         <b>Wkddb1359</b>
@@ -17,36 +13,19 @@ const commentActive = function() {
 
         commentFoam.appendChild(newComment);
         comment.value='';
-
-    }
-
-    if(comment.value) {
-        commentBtn.style.opacity = '1';
-    }
-    else {
-        commentBtn.style.opacity = '.3';
-    }
-
-}
-const commentBtnActive = function() {
-
-    let commentInput = comment.value;
-    const newComment = document.createElement('div');
-
-    newComment.innerHTML = `
-    <b>Wkddb1359</b>
-    <span>${commentInput}</span>`;
-
-    commentFoam.appendChild(newComment);
-    comment.value='';
 }
 
-comment.addEventListener('keydown',commentActive);
+comment.addEventListener('keydown', () => {
+    comment.value ? commentBtn.style.opacity = '1' : commentBtn.style.opacity = '.3';
+    if(window.event.keyCode === 13 && comment.value) {
+        commentActive();
+    }
+});
+
 commentBtn.addEventListener('click', () => {
     if(comment.value) {
-        commentBtnActive();
-    }
-    else {
+        commentActive();
+    } else {
         alert("내용을 입력해주세요.");
     }
 })
