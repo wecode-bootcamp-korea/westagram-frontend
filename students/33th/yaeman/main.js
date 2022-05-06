701c7eb909910cf1f5f18ac28a078c0593a730c0
chldpaks <chldpaks@gmail.com>
2022-05-06 14:02:51 +0900

    댓글 기능 enter구현중

--- students/33th/yaeman/main.js
+++ students/33th/yaeman/main.js
@@ -1,9 +1,9 @@
 const commentForm = document.querySelector('.upload-form');
-const commentInput = document.querySelector('#comment-write');
-const uploadButton = document.querySelector('.upload');
-const commentValue = commentInput.value;
+const commentInput = document.querySelector('#comment-write'); //input창
+const uploadButton = document.querySelector('.upload'); //게시버튼
+const commentValue = commentInput.value; //input값
 
-const commented = document.querySelector('.commented');
+const commented = document.querySelector('.commented'); //추가될 댓글박스
 
 // function uploadBtn() {
 //   const commentValue = commentInput.value;
@@ -37,52 +37,91 @@ const commented = document.querySelector('.commented');
 //   commented.appendChild(reply);
 // }
 
-function uploadBtn(e) {
-  let commentValue = commentInput.value; 
-  if(commentValue) {
-    uploadButton.disabled = false;
-    // uploadButton.style.opacity = 1;
-    uploadButton.style.cursor = "pointer";
-    uploadButton.style.color = "#0095f6";
-  } else {
-    uploadButton.disabled = true;
-    uploadButton.style.cursor = "default";
-    uploadButton.style.opacity = .4;
+// function uploadBtn(e) {
+//   let commentValue = commentInput.value; 
+//   if(commentValue) {
+//     uploadButton.disabled = false;
+//     uploadButton.style.cursor = "pointer";
+//     uploadButton.style.color = "#0095f6";
+//   } else {
+//     uploadButton.disabled = true;
+//     uploadButton.style.cursor = "default";
+//     uploadButton.style.opacity = .4;
+//   }
+//   // if(uploadButton.disabled=false && e.code === 'Enter') {
+//   //   enterPost();
+//   // }
+// }
+
+// function enterPost() {
+//   if(commentInput.value) {
+//     const newUser = document.createElement('span');
+//     newUser.className = "user-id";
+//     newUser.innerText = "ym";
+
+//     const newComment = document.createElement('span');
+//     newComment.className = "comment-box";
+//     newComment.appendChild(newUser);
+//     newComment.innerHTML += commentInput.value;
+
+//     const newImg = document.createElement('img');
+//     newImg.className = "comment-heart";
+//     newImg.setAttribute('src', "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png");
+
+//     const newList = document.createElement('li');
+//     newList.className = "newcomment-list";
+//     newList.appendChild(newComment);
+//     newList.appendChild(newImg);
+//     commented.appendChild(newList);
+//     commentInput.value = "";
+
+//     uploadButton.disabled = true;
+//     uploadButton.style.cursor = "default";
+//     uploadButton.style.opacity = .4;
+//   }
+// }
+
+const postCommentInFeed = () => {
+  const commentInput = document.querySelector('#comment-write'); 
+  const uploadButton = document.querySelector('.upload');
+
+  //댓글 입력시 요소 생성
+  const addNewComment = () => {
+    const newCommentLocation = document.getElementsByClassName('comment-list')[0];
+    const newComment = document.createElement('li');
+    
+    newComment.innerHTML =`
+      <div class="user-board">
+        <a href="">yaeman</a>
+        <span>${commentInput.value}</span>
+      </div>
+      <button><i class="fa-regular fa-heart"></i></button>
+    `
+    newCommentLocation.appendChild(newComment);
+    commentInput.value="";
   }
-  if(uploadButton.disabled=false && e.key === "Enter") {
-    e.preventDefault();
-    enterPost();
-    document.querySelector('.upload').click();
-  }
+
+  //사용자 입력 들어올 시, 게시 버튼 활성화
+  commentInput.addEventListener('keyup', (e) => {
+    commentInput.value ? uploadButton.style.opacity = '1' : uploadButton = '.3';
+    if(e.keyCode === 13 && commentInput.value) {
+      e.preventDefault();
+      addNewComment();
+    }
+  });
+
+  //댓글 게시
+  uploadButton.addEventListener('click', () => {
+    if(commentInput.value) {
+      addNewComment();
+    } else {
+      alert("댓글이 입력되지 않음");
+    }
+  });
 }
- 
-function enterPost() {
-  if(commentInput.value) {
-    const newUser = document.createElement('span');
-    newUser.className = "user-id";
-    newUser.innerText = "ym";
+postCommentInFeed();
 
-    const newComment = document.createElement('span');
-    newComment.className = "comment-box";
-    newComment.appendChild(newUser);
-    newComment.innerHTML += commentInput.value;
 
-    const newImg = document.createElement('img');
-    newImg.className = "comment-heart";
-    newImg.setAttribute('src', "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png");
 
-    const newList = document.createElement('li');
-    newList.className = "newcomment-list";
-    newList.appendChild(newComment);
-    newList.appendChild(newImg);
-    commented.appendChild(newList);
-    commentInput.value = "";
-
-    uploadButton.disabled = true;
-    uploadButton.style.cursor = "default";
-    uploadButton.style.opacity = .4;
-  }
-}
-
-commentInput.addEventListener('keyup', uploadBtn);
-uploadButton.addEventListener('click', enterPost);
+// commentInput.addEventListener('keyup', uploadBtn);
+// uploadButton.addEventListener('click', enterPost);
