8cfd395f69f2205e0e13dd2df2951d2dbedb1b02
haeun <yoohaaeun@gmail.com>
2022-05-06 10:21:17 +0900

    ADD : Mission 1~4 구현완료, 추가 기능 구현중

--- students/33th/yoohaeun/js/main.js
+++ students/33th/yoohaeun/js/main.js
@@ -1,38 +1,80 @@
-const commentList = document.querySelector('#commentList');
+const commentList = document.querySelector('.commentList');
 const commentForm = document.querySelector('.commentInput');
 const commentInput = commentForm.querySelector('input');
-const commentBtn = document.querySelector('commentBtn');
+const commentBtn = document.querySelector('.commentBtn');
+
+const COMMENT_KEY = 'comment';
+
+let comments = [];
+
+function saveCommetns() {
+  localStorage.setItem(COMMENT_KEY, JSON.stringify(comments));
+}
+
+function deleteComment(e) {
+  const li = e.target.parentElement.parentElement;
+  li.remove();
+  comments = comments.filter((comment) => comment.id !== parseInt(li.id));
+  saveCommetns();
+}
 
 function paintComment(comment) {
   if (comment === '') {
     return;
   }
+
   const li = document.createElement('li');
+  li.id = comment.id;
+
   const strong = document.createElement('strong');
   strong.innerText = 'nueahooy';
   strong.classList.add('commentUserId');
 
-  const span = document.createElement('span');
-  span.innerText = comment;
-  const button = document.createElement('button');
-  button.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  const div = document.createElement('div');
+  div.classList.add('commentText');
 
+  const span = document.createElement('span');
+  span.innerText = comment.text;
+
+  const delBtn = document.createElement('button');
+  delBtn.innerText = 'ⅹ';
+  delBtn.addEventListener('click', deleteComment);
+
+  div.appendChild(span);
+  div.appendChild(delBtn);
   li.appendChild(strong);
-  li.appendChild(span);
-  // li.appendChild(button);
+  li.appendChild(div);
   commentList.appendChild(li);
 }
 
 function handleCommentSubmit(e) {
   e.preventDefault();
-  const comment = commentInput.value;
+  const newComment = commentInput.value;
+  const newCommentObj = {
+    text: newComment,
+    id: Date.now(),
+  };
   commentInput.value = '';
-  paintComment(comment);
+  comments.push(newCommentObj);
+  saveCommetns();
+  paintComment(newCommentObj);
 }
 
 function onBtnActive(e) {
-  console.log(e.target.value);
+  if (e.target.value.length > 0) {
+    commentBtn.disabled = false;
+  } else {
+    commentBtn.disabled = true;
+  }
 }
 
 commentForm.addEventListener('submit', handleCommentSubmit);
 commentInput.addEventListener('keyup', onBtnActive);
+
+const savedComments = localStorage.getItem(COMMENT_KEY);
+
+if (savedComments) {
+  const parseComments = JSON.parse(savedComments);
+  comments = parseComments;
+  parseComments.forEach(paintComment);
+}
