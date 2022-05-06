a00207550c001559b19a59f2c464d3a9c9c7c398
seul <kseul06@gmail.com>
2022-05-06 21:59:18 +0900

    Refactor : login 페이지 리팩토링

--- students/33th/seulbikim/js/login.js
+++ students/33th/seulbikim/js/login.js
@@ -1,15 +1,18 @@
 const loginForm = document.querySelector('.login__form');
-const loginId = document.querySelector('.login__input-id');
-const loginPw = document.querySelector('.login__input-pw');
+
+const checkID = (idValue) => {
+  return idValue.includes('@') && idValue.length > 0;
+};
+
+const checkPw = (pwValue) => {
+  return pwValue.length > 5;
+};
 
 const loginHandler = (e) => {
   const loginBtn = document.querySelector('.login__btn');
-  const idValue = loginId.value;
-  const pwValue = loginPw.value;
-
-  idValue.includes('@') && idValue.length > 0 && pwValue.length > 5
-    ? (loginBtn.disabled = false)
-    : (loginBtn.disabled = true);
+  const idValue = document.querySelector('.login__input-id').value;
+  const pwValue = document.querySelector('.login__input-pw').value;
+  loginBtn.disabled = !(checkID(idValue) && checkPw(pwValue));
 };
 
 loginForm.addEventListener('keyup', loginHandler);
