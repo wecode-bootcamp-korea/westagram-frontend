0dd1784bb96b8a3fe7fc311feed72269b7f3d979
sourmix <75124027+sourmix@users.noreply.github.com>
2022-05-07 23:40:36 +0900

    댓글 삭제
    기능 추가

--- students/33th/yoonji/js/main.js
+++ students/33th/yoonji/js/main.js
@@ -2,13 +2,23 @@ const replyContainer = document.querySelector(".reply");
 const replyInput = document.querySelector(".reply__input__text");
 const replyForm = document.querySelector(".reply__input");
 
+function deleteReply(e) {
+    const li = e.target.parentElement;
+    li.remove();
+}
+
 function paintReply(newRe) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    const strong = document.createElement("strong");
-    li.appendChild(strong);
+    const b = document.createElement("b");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.classList.add("btn__right");
+    li.appendChild(b);
     li.appendChild(span);
-    strong.innerText = "I.like.U ";
+    li.appendChild(button);
+    button.addEventListener("click", deleteReply);
+    b.innerText = "I.like.U ";
     span.innerText = newRe;
     replyContainer.appendChild(li);
     // nameId.classList.remove("hidden");
