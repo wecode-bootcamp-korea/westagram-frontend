dfabace54dc5c811ed7765866a7477a7930fa7b2
chldpaks <chldpaks@gmail.com>
2022-05-07 21:39:17 +0900

    로그인 유효성 검사

--- students/33th/yaeman/login.js
+++ students/33th/yaeman/login.js
@@ -1,22 +1,87 @@
-const idForm = document.querySelector('#login-id')
-const pwForm = document.querySelector('#login-pw');
-const loginButton = document.querySelector('.login-btn');
+"use strict"; //엄격모드
+//로그인 버튼을 비활성화/활성화
+//글자가 하나 이상일 때
+//두 input창의 값이 있을 때
 
-function loginBtn() {
-    const idVaule = idForm.value;
-    const pwVaule = pwForm.value;
+const inputs = document.getElementsByClassName("loginForm")[0];
+const loginBtn = document.getElementById("loginBtn");
 
-    if(idVaule.length > 0 && pwVaule.length > 0) {
-        loginButton.disabled = false; //disabled 속성 설정하기 true면 비활성화 false면 활성화
-        loginButton.style.cursor = "pointer";
-        loginButton.style.backgroundColor = "#1c7ed6";
-    } else {
-        loginButton.disabled = true;
-        loginButton.style.cursor = "default";
-        loginButton.style.backgroundColor = "#C4E1FB";
+const handleInput = () => {
+    const idValue = document.getElementById("id").value;
+    const pwValue = document.getElementById("pw").value;
+
+    //---내 코드--->
+    const isvalidValue = checkLogin(idValue, pwValue);
+    // if(isvalidValue) {
+    //     handleBtn(true);
+    // } else {
+    //     handleBtn(false);
+    // }
+    console.log("~~~~",isvalidValue);
+    //<----------
+    
+    // 원본 코드
+    // const isValidId = checkValue(idValue);
+    // const isValidPw = checkValue(pwValue);
+
+    console.log("idValue:", idValue, "pwValue:", pwValue);
+    // const isvalidValue = isValidId && isValidPw;
+
+    //1.
+    // if (isValidId && isValidPw) {
+    //     handleBtn(true);
+    // } else {
+    //     handleBtn(false);
+    // }
+    //2.
+    //return isvalidValue ? handleBtn(true) : handleBtn(false);
+    //3.
+    return handleBtn(isvalidValue);
+};
+
+// 원본 코드
+// const checkValue = (value) => {
+//     // if(value.length > 0) {
+//     //     return true;
+//     // } else {
+//     //     return false;
+//     // }
+    
+//     //삼항연산자
+//     // 1. return value.length > 0? true : false;
+//     return value.length > 0;
+// };
+
+const handleBtn = (isBtnValid) => {
+    //3항 연산자
+    loginBtn.disabled = !isBtnValid; // loginBtn.disabled = isBtnValid ? false : true;
+    loginBtn.style.opacity = isBtnValid ? 1 : 0.5;
+    loginBtn.style.cursor = isBtnValid ? "pointer" : "default";
+
+    if(window.event.code === "Enter") {
+        success();
+    } 
+};
+
+const success = () => {
+    location.href = "http://127.0.0.1:5500/main.html";
+    // location.href = "file:///C:/Users/User/OneDrive/%EB%B0%94%ED%83%95%20%ED%99%94%EB%A9%B4/wecode/westagram-frontend/students/33th/yaeman/login.html?";
+};
+
+//---내 코드--->
+const checkLogin = (id, pw) => {
+    if (id.indexOf("@") >= 0 && pw.length >= 5) {
+        return true;
+    } else if (!id || pw.length < 5) {
+        return false;
     }
-}
+};
+//<----------
 
-idForm.addEventListener('keyup', loginBtn);
-pwForm.addEventListener('keyup', loginBtn);
+const init = () => {
+    inputs.addEventListener("input", handleInput);
+    inputs.addEventListener("keyup", handleInput);
+    loginBtn.addEventListener("click", success);
+};
 
+init();
--- students/33th/yaeman/main.js
+++ students/33th/yaeman/main.js
@@ -1,127 +1,70 @@
-const commentForm = document.querySelector('.upload-form');
-const commentInput = document.querySelector('#comment-write'); //input창
-const uploadButton = document.querySelector('.upload'); //게시버튼
-const commentValue = commentInput.value; //input값
+"use strict"; //엄격모드
 
-const commented = document.querySelector('.commented'); //추가될 댓글박스
+const input = document.getElementById("commentInput");
+const submit = document.getElementById("submit");
 
-// function uploadBtn() {
-//   const commentValue = commentInput.value;
-
-//   if(commentValue.length > 0){ 
-//     uploadButton.disabled = false;
-//     uploadButton.style.cursor = "pointer";
-//     uploadButton.style.color = "#0095f6";
-//   } else {
-//     uploadButton.disabled = true;
-//     uploadButton.style.cursor = "default";
-//     uploadButton.style.color = "#99d2f8";
-//   }
-// }
-
-
-// function createItem(text) {
-//   // console.log(text);
-//   const reply = document.createElement("li");
-//   reply.setAttribute("class", reply);
-//   reply.innerHTML =
-//   `<div class="replyDetail">
-//     <a href="" class="user-id"><span>kongji<span></a>
-//     <span>${text}</span>
-//   </div>
-//   <span class="sub-heart">
-//     <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-//               width="12px" height="12px">
-//   </span>
-//   `
-//   commented.appendChild(reply);
-// }
-
-// function uploadBtn(e) {
-//   let commentValue = commentInput.value; 
-//   if(commentValue) {
-//     uploadButton.disabled = false;
-//     uploadButton.style.cursor = "pointer";
-//     uploadButton.style.color = "#0095f6";
-//   } else {
-//     uploadButton.disabled = true;
-//     uploadButton.style.cursor = "default";
-//     uploadButton.style.opacity = .4;
-//   }
-//   // if(uploadButton.disabled=false && e.code === 'Enter') {
-//   //   enterPost();
-//   // }
-// }
-
-// function enterPost() {
-//   if(commentInput.value) {
-//     const newUser = document.createElement('span');
-//     newUser.className = "user-id";
-//     newUser.innerText = "ym";
-
-//     const newComment = document.createElement('span');
-//     newComment.className = "comment-box";
-//     newComment.appendChild(newUser);
-//     newComment.innerHTML += commentInput.value;
-
-//     const newImg = document.createElement('img');
-//     newImg.className = "comment-heart";
-//     newImg.setAttribute('src', "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png");
-
-//     const newList = document.createElement('li');
-//     newList.className = "newcomment-list";
-//     newList.appendChild(newComment);
-//     newList.appendChild(newImg);
-//     commented.appendChild(newList);
-//     commentInput.value = "";
-
-//     uploadButton.disabled = true;
-//     uploadButton.style.cursor = "default";
-//     uploadButton.style.opacity = .4;
-//   }
-// }
-
-const postCommentInFeed = () => {
-  const commentInput = document.querySelector('#comment-write'); 
-  const uploadButton = document.querySelector('.upload');
-
-  //댓글 입력시 요소 생성
-  const addNewComment = () => {
-    const newCommentLocation = document.getElementsByClassName('comment-list')[0];
-    const newComment = document.createElement('li');
-    
-    newComment.innerHTML =`
-      <div class="user-board">
-        <a href="">yaeman</a>
-        <span>${commentInput.value}</span>
-      </div>
-      <button><i class="fa-regular fa-heart"></i></button>
-    `
-    newCommentLocation.appendChild(newComment);
-    commentInput.value="";
+const checkInput = (e) => {
+  e.preventDefault();
+  if(!input.value.length) {
+    return;
   }
+  registerComment(input.value);
+};
 
-  //사용자 입력 들어올 시, 게시 버튼 활성화
-  commentInput.addEventListener('keyup', (e) => {
-    commentInput.value ? uploadButton.style.opacity = '1' : uploadButton = '.3';
-    if(e.keyCode === 13 && commentInput.value) {
-      e.preventDefault();
-      addNewComment();
-    }
-  });
+const registerComment = (value) => { //댓글등록
+  const commentLists = document.getElementById("commentLists");
+  const newCommentList = document.createElement("li");
+  // const newComment = `
+  //   <span class="name">yaeman</span>
+  //   <span>${value}</span>
+  //   <span class="delete">x</span>
+  // `;
+  const newComment = `
+    <div class="user-board">
+        <a href="">yaeman</a>
+        <span>${value}</span>
+    </div>
+    <button><i class="fa-regular fa-heart"></i></button>
+    <span class="delete">x</span>
+  `;
+  newCommentList.innerHTML = newComment;
 
-  //댓글 게시
-  uploadButton.addEventListener('click', () => {
-    if(commentInput.value) {
-      addNewComment();
-    } else {
-      alert("댓글이 입력되지 않음");
-    }
-  });
-}
-postCommentInFeed();
+  // const newUserId = document.createElement("span");
+  // newUserId.className = "name";
+  // newUserId.innerText = "yaeman";
 
+  // // console.log(newUserId);
+  // const commentValue = document.createElement("span");
+  // commentValue.innerText = value;
+  // // console.log(commentValue);
 
+  // const deleteBtn = document.createElement("span");
+  // deleteBtn.classList.add("delte");
+  // deleteBtn.innerText = "x";
+  // // console.log(deleteBtn);
 
-// commentInput.addEventListener('keyup', uploadBtn);
-// uploadButton.addEventListener('click', enterPost);
+  // newCommentList.appendChild(newUserId);
+  // newCommentList.appendChild(commentValue);
+  // newCommentList.appendChild(deleteBtn);
+
+  // console.log(newCommentList);
+  makeDeleteEvent(newCommentList);
+  commentLists.appendChild(newCommentList);
+
+  input.value = ""; //clear시켜주는 부분
+};
+
+const makeDeleteEvent = (newCommentList) => {
+  const deleteBtn = newCommentList.querySelector(".delete");
+  deleteBtn.addEventListener("click", () => deleteComment(newCommentList));
+};
+
+const deleteComment = (newCommentList) => {
+  newCommentList.remove();
+};
+
+const init = () => {
+  submit.addEventListener("click", checkInput);
+};
+
+init();
