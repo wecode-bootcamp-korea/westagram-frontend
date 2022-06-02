6ae91f726679db41e839ba4231dfd07a7482329f
conqueror-G <gitconquerorg@gmail.com>
2022-06-02 21:19:58 +0900

    EDIT: CSS sideBar_history and sideBar_recommendation -> bottom-up way

--- students/34th/minseok/js/main.js
+++ students/34th/minseok/js/main.js
@@ -2,20 +2,20 @@
 // HTML addCommentButton : button tag
 // HTML feedPostOthersComment : div tage
 
-const ADD_COMMENT_TEXT = document.querySelector('.posting_addComment_text');
+const ADD_COMMENT_INPUT = document.querySelector('.posting_addComment_text');
 const ADD_COMMENT_BUTTON = document.querySelector('.posting_addComment_button');
 const FEED_POST_OTHERS_COMMENT = document.querySelector('.posting_othersComment');
 
 const onAdd = () => {
-  const INPUT = ADD_COMMENT_TEXT.value;
+  const INPUT = ADD_COMMENT_INPUT.value;
 
   const TEXT = document.createElement('p');
   TEXT.setAttribute('class', 'posting_othersComment_comment');
   TEXT.innerHTML = INPUT;
 
   FEED_POST_OTHERS_COMMENT.appendChild(TEXT);
-  ADD_COMMENT_TEXT.value = '';
-  ADD_COMMENT_TEXT.focus();
+  ADD_COMMENT_INPUT.value = '';
+  ADD_COMMENT_INPUT.focus();
 }
 
 ADD_COMMENT_BUTTON.addEventListener('click', onAdd);
