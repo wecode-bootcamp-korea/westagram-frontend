8f94dd77566df1d471128f2cd78b88a90e9a0796
shs <hs.seo@kisos.net>
2022-06-02 22:51:54 +0900

    ADD : 피드 좋아요 기능 추가, 댓글 좋아요, 삭제 기능 추가

--- students/34th/seohansaem/src/main.js
+++ students/34th/seohansaem/src/main.js
@@ -1,7 +1,21 @@
-const feedCommentInput = document.querySelector('.comment-input');
-const feedCommentList = document.querySelector('.feed-comments');
+const feedCommentInput = document.querySelectorAll('.comment-input');
+const feedCommentList = document.querySelectorAll('.feed-comments');
+const feedLikeButton = document.querySelectorAll('.feed-like');
+const feedCommentLikeButton = document.querySelectorAll('.feed-comment-like');
+const feedCommentDeleteButton = document.querySelectorAll(
+  '.feed-comment-delete'
+);
+const handleLikeButtonClick = (ev) => {
+  ev.target.classList.toggle('check');
+  ev.target.classList.toggle('fa-solid');
+  ev.target.classList.toggle('fa-regular');
+};
 
-const handleCommentInputAdd = (ev) => {
+const handleDeleteButtonClick = (ev) => {
+  ev.path[1].remove();
+};
+
+const handleCommentInputAdd = (ev, feedCommentList) => {
   if (ev.keyCode !== 13 || ev.target.value === '') return;
   const comment = ev.target.value;
   ev.target.value = '';
@@ -18,6 +32,9 @@ const handleCommentInputAdd = (ev) => {
   heartIcon.className = 'feed-comment-like fa-regular fa-heart';
   deleteIcon.className = 'feed-comment-delete fa-regular fa-x';
 
+  heartIcon.addEventListener('click', handleLikeButtonClick);
+  deleteIcon.addEventListener('click', handleDeleteButtonClick);
+
   newCommentUser.innerText = 'kor-sams-dev';
   newCommentInfo.innerText = comment;
   newComment.appendChild(newCommentUser);
@@ -28,4 +45,20 @@ const handleCommentInputAdd = (ev) => {
   feedCommentList.appendChild(newComment);
 };
 
-feedCommentInput.addEventListener('keypress', handleCommentInputAdd);
+for (let i = 0; i < feedCommentInput.length; i++) {
+  feedCommentInput[i].addEventListener('keypress', (ev) => {
+    handleCommentInputAdd(ev, feedCommentList[i]);
+  });
+}
+
+for (let i = 0; i < feedLikeButton.length; i++) {
+  feedLikeButton[i].addEventListener('click', handleLikeButtonClick);
+}
+
+for (let i = 0; i < feedCommentLikeButton.length; i++) {
+  feedCommentLikeButton[i].addEventListener('click', handleLikeButtonClick);
+}
+
+for (let i = 0; i < feedCommentDeleteButton.length; i++) {
+  feedCommentDeleteButton[i].addEventListener('click', handleDeleteButtonClick);
+}
