a9aec0914e0cba8494619db6090e460bca296afb
conqueror-G <gitconquerorg@gmail.com>
2022-06-02 18:45:02 +0900

    EDIT: CSS property position and unnecessary property delete

--- students/34th/minseok/js/login.js
+++ students/34th/minseok/js/login.js
@@ -1,14 +1,10 @@
-const USER_ID = document.querySelector('.userName');
-const USER_PW = document.querySelector('.userPassword');
-const LOGIN_BUTTON = document.querySelector('.loginButton');
+const USER_ID = document.querySelector('.loginArea_userInterface_userName');
+const USER_PW = document.querySelector('.loginArea_userInterface_userPassword');
+const LOGIN_BUTTON = document.querySelector('.loginArea_userInterface_loginButton');
 
-// keyup 이벤트시 enabledButton함수 실행
 USER_ID.addEventListener('keyup', enabledButton);
 USER_PW.addEventListener('keyup', enabledButton);
 
-
-// input에 입력되는 값을 보려면 e.target.value
-// 이벤트 객체의 프로퍼티
 function enabledButton() {
   if (USER_ID.value && USER_PW.value) {
     LOGIN_BUTTON.disabled = false;
