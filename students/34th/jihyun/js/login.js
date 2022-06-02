0d86f258a14aa16284e8a9281ef78563bdda6672
jihyun-jeon <jhplus13@naver.com>
2022-06-02 15:58:56 +0900

    ADD: 위스타그램 클론 초기커밋

---
+++ students/34th/jihyun/js/login.js
@@ -0,0 +1,49 @@
+const formEl = document.querySelector("form");
+const btnEl = document.querySelector("button");
+const userId = document.querySelector(".userId");
+const userPw = document.querySelector(".userPassword");
+
+// 1. < input에 값이 입력되면 버튼 활성화 되기 >
+// 0) [p1]- input 이벤트는 <input>, <select> 및 <textarea> 요소의 value 속성이 바뀔 때마다 발생한다.
+formEl.addEventListener("input", (e) => {
+  // 1) 로그인 버튼 활성화
+  if (userId.value && userPw.value) {
+    btnEl.style.backgroundColor = "#0095f6";
+    // 버튼 누르면 main창 연동
+    btnEl.addEventListener("click", () => {
+      location.href = "main.html";
+    });
+  } else {
+    btnEl.style.backgroundColor = "#c0dffd";
+  }
+
+  // 2) input에 값 하나라도 입력되면 회색으로
+  if (userId.value) {
+    userId.style.borderColor = "#ddd";
+  }
+
+  if (userPw.value) {
+    userPw.style.borderColor = "#ddd";
+  }
+});
+
+// 2. < id pw 유효성 검사 >
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (userId.value.length === 0 || userPw.value.length === 0) {
+    return;
+  }
+
+  if (userId.value.length < 4) {
+    userId.value = "";
+    userId.placeholder = "아이디 4글자 이상 입력하세요!";
+    userId.style.borderColor = "red";
+  }
+
+  if (userPw.value.length < 4) {
+    userPw.value = "";
+    userPw.placeholder = "비밀번호 4글자 이상 입력하세요!";
+    userPw.style.borderColor = "red";
+  }
+});
