368fb5fa70de83f53f27f45a66663d5746bcdbd3
조예지 <jennycho@joyejiui-MacBookPro.local>
2022-06-02 18:08:03 +0900

    add: 좋아요, 댓글 기능 추가

--- students/34th/yejeecho/main.js
+++ students/34th/yejeecho/main.js
@@ -2,22 +2,25 @@ const likeButton = document.querySelector('#likeButton')
 const postedComment = document.querySelector('.postedComment');
 const comment = document.querySelector('.comment');
 const postButton = document.querySelector('.postButton');
-const likeButtonImage = document.querySelector('.button')[0]
+const likeButtonImage = document.querySelector('.button')
+
 
 postButton.addEventListener('click', () => {
     postedComment.innerText = `admin ${comment.value}`;
-    comment.vlaue= "";
+    comment.value= "";
 })
 
 // 게시버튼 엔터키
 postButton.addEventListener('keyup', (e) => {
     if(e.code === 'Enter') {
         postedComment.innerText = `admin ${comment.value}`;
-        comment.vlaue = '';
+        comment.value = '';
     }
 });
 
 // 좋아요 버튼
 likeButton.addEventListener('click', () => {
+    console.log(likeButtonImage)
     likeButtonImage.src = "images/redheart.png"
+    
 }) 
