aa35ddb92ed55132bb785dc84defa72337260df5
hjpark625 <zero950@gmail.com>
2022-06-02 16:39:11 +0900

    MODIFY: 댓글버튼 스타일링중

--- students/34th/heejupark/js/main.js
+++ students/34th/heejupark/js/main.js
@@ -8,11 +8,13 @@ const submit = () => {
   const username = document.createElement("span");
   const mainText = document.createElement("p");
   const mainIcon = document.createElement("img");
+  mainIcon.src =
+    "https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png";
 
   comments.classList.add("comment_box");
   username.classList.add("username");
   mainText.classList.add("comment");
-  mainIcon.classList.add("fa-heart");
+  mainIcon.classList.add("like_heart");
 
   username.innerHTML = " hjpark625 ";
   mainText.innerText = commentInput.value;
