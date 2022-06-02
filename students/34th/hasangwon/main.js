c661a2f45d2bdf2422f4aa4d88f5a3ad36a7db72
Sangwon Ha <305243@naver.com>
2022-06-02 18:42:02 +0900

    추가기능 6

--- students/34th/hasangwon/main.js
+++ students/34th/hasangwon/main.js
@@ -1,10 +1,11 @@
 const submit = document.querySelector(".submit");
-const commentBox = document.querySelector(".comment")
+const commentBox = document.querySelector(".comment");
 const heart = document.querySelector("#heart");
-const commentHeart = document.querySelector(".buttonz");
-// const smallheart =
-let count = 0, i=0;
+const inputbox = document.querySelector(".inputbox");
+const searchIcon = document.querySelector(".search");
+let count = 0;
 
+//버튼 활성화
 window.addEventListener('keyup', function() {
     const commentCheck = commentBox.value;
 
@@ -18,26 +19,57 @@ window.addEventListener('keyup', function() {
     }
 });
 
+//버튼
 submit.addEventListener('click',function(){
     createComment();
 })
 
+//엔터키
 commentBox.addEventListener('keyup',function(event){
     if(event.code =="Enter" || event.code =="NumpadEnter"){
         createComment();
     }
 })
 
+//댓글 좋아용
+heart.addEventListener("click",function(){
+    if( heart.className == "fa-regular fa-heart buttons fa-2x"){    
+        heart.className = "fa-solid fa-heart buttons fa-2x";
+        heart.style.color = "red";
+    }
+    else{
+        heart.className = "fa-regular fa-heart buttons fa-2x"
+        heart.style.color = "black";
+    }
+})
+
+// inputbox.addEventListener("onfocus",function(){
+//     searchIcon.style.display = "none";
+// })
+
+
+inputbox.onfocus = function(){
+    searchIcon.style.display = "none";
+}
+inputbox.onblur = function(){
+    searchIcon.style.display = "block";
+}
+
+//댓글달기
 function createComment(){    
     const comment = commentBox.value;
     const commentDiv = document.createElement("div");
     const commentHeart = document.createElement("i");
+    const commentDelete = document.createElement("i");
     const commentPlace = document.querySelector(".commentSection");
 
     if( comment ){
         commentHeart.className = "fa-regular fa-heart buttonz"
+        commentDelete.className = "fa-regular fa-x buttonz"
         commentDiv.innerHTML = "<b>hasang0.0</b> " + comment;
 
+
+        commentDiv.appendChild(commentDelete); 
         commentDiv.appendChild(commentHeart); 
         commentPlace.appendChild(commentDiv); // 댓글 + 하트
 
@@ -45,34 +77,19 @@ function createComment(){
         submit.disabled = true;
         submit.style.color = "#BFE0FD";
     }
-}
 
-heart.addEventListener("click",function(){
-    clickHeart();
-})
+    commentHeart.addEventListener("click",function(){
+        if(commentHeart.className == "fa-regular fa-heart buttonz"){    
+            commentHeart.className = "fa-solid fa-heart buttonz";
+            commentHeart.style.color = "red";
+        }
+        else{
+            commentHeart.className = "fa-regular fa-heart buttonz"
+            commentHeart.style.color = "black";
+        }
+    })
 
-commentHeart.addEventListener("click",function(){
-    if(count == 0){    
-        commentHeart.className = "fa-solid fa-heart buttonz";
-        commentHeart.style.color = "red";
-        count = count + 1;
-    }
-    else if(count == 1){
-        commentHeart.className = "fa-regular fa-heart buttonz"
-        commentHeart.style.color = "black";
-        count = count - 1;
-    }
-})
-
-function clickHeart(){
-    if(count == 0){    
-        heart.className = "fa-solid fa-heart buttons fa-2x";
-        heart.style.color = "red";
-        count = count + 1;
-    }
-    else if(count == 1){
-        heart.className = "fa-regular fa-heart buttons fa-2x"
-        heart.style.color = "black";
-        count = count - 1;
-    }
+    commentDelete.addEventListener("click",function(){
+        commentPlace.removeChild(commentDiv);
+    })
 }
