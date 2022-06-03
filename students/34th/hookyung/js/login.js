28c57efba9cc3457f91deec1a968743270ec6294
kyungbaa <cocoroto@naver.com>
2022-06-03 16:41:43 +0900

    Modify: prettier 적용

--- students/34th/hookyung/js/login.js
+++ students/34th/hookyung/js/login.js
@@ -1,51 +1,33 @@
-"use stric"; 
+"use stric";
 // 없을 경우는 변수 할당을 let이나 const로 해주지 않아도 되지만
 // 이렇게 "use stric"; 사용할 경우 뜨지 않는다.
 // >> 사용 이유: 제대로 선언하지 않은 변수가 다른 scope에 영향을 끼칠 수 있기 때문에 사용 권장
-
-
 
 const button = document.querySelector(".loginButton");
 const realID = "dddd@.com";
 const realPassword = "1111";
 
+window.addEventListener("keyup", function () {
+  const inputIdValue = document.querySelector(".loginID").value;
+  const inputPwValue = document.querySelector(".loginPW").value;
 
-window.addEventListener('keyup',function(){
-    const inputIdValue = document.querySelector(".loginID").value;
-    const inputPwValue = document.querySelector(".loginPW").value;
-    
-
-      
-    
- 
-    const loginHandle = function (){
-
-        if(inputIdValue&&inputPwValue){
-            button.style.backgroundColor = "#0095F6";
-            button.disable =false;
-            const pageMove = button.addEventListener('click',() => {
-                console.log("ddddd");
-                
-             })
-             pageMove();
-           
-            
-        } else {
-            button.disable =true;
-            button.style.backgroundColor = "#C4E1FB";
-            
-        }
-    }
-    loginHandle();
-
-    const pageMove = button.addEventListener('click',() => {
+  const loginHandle = function () {
+    if (inputIdValue && inputPwValue) {
+      button.style.backgroundColor = "#0095F6";
+      button.disable = false;
+      const pageMove = button.addEventListener("click", () => {
         console.log("ddddd");
-        
-     })
-     pageMove();
-   
-})
+      });
+      pageMove();
+    } else {
+      button.disable = true;
+      button.style.backgroundColor = "#C4E1FB";
+    }
+  };
+  loginHandle();
 
-
-
-
+  const pageMove = button.addEventListener("click", () => {
+    console.log("ddddd");
+  });
+  pageMove();
+});
--- students/34th/hookyung/js/main.js
+++ students/34th/hookyung/js/main.js
@@ -1,68 +1,61 @@
 const commentWriteButton = document.querySelector(".commentWrite__button");
 let userId = 0;
 
+const IscommentContents = commentWriteButton.addEventListener("click", () => {
+  const commentView = document.querySelector(".comment");
 
-const IscommentContents = commentWriteButton.addEventListener('click',()=>{
+  // 새로운 코멘트 div 생성
+  let newCommentDiv = document.createElement("div");
+  newCommentDiv.className = "commentView";
 
-    
-    const commentView = document.querySelector(".comment");    
+  // newCommentDiv
+  commentView.appendChild(newCommentDiv);
+  // newCommentDiv 안에 append
 
-    // 새로운 코멘트 div 생성 
-    let newCommentDiv = document.createElement("div");
-    newCommentDiv.className = "commentView";
-    
-    // newCommentDiv
-    commentView.appendChild(newCommentDiv);
-    // newCommentDiv 안에 append
+  AddNewComment();
+});
 
-    AddNewComment();
-})
+const AddNewComment = () => {
+  // 마지막으로 생성된 comment
+  const NewcommentView = document.querySelector(".comment").lastElementChild;
 
-const AddNewComment = () =>{
-    // 마지막으로 생성된 comment
-    const NewcommentView = document.querySelector(".comment").lastElementChild;
-   
+  //  댓글 input value 저장
+  const commentContentsValue = document.querySelector(".commentContents").value;
 
-    //  댓글 input value 저장
-    const commentContentsValue = document.querySelector(".commentContents").value;
+  // 댓글 좋아요 저장
+  const commentLike = document.querySelector(".commentLike").innerHTML;
 
-    // 댓글 좋아요 저장
-    const commentLike =  document.querySelector(".commentLike").innerHTML;
-    
-    
-    // 클릭할 때마다 유저 아이디 숫자 카운트
-    userId++;
+  // 클릭할 때마다 유저 아이디 숫자 카운트
+  userId++;
 
-    // 새로운 ID생성
-    let newCommentId = document.createElement("span");
+  // 새로운 ID생성
+  let newCommentId = document.createElement("span");
 
-    //*** innerHTML없이 newCommentId = `user ${userId}`로 하게 되면 span까지 사라진채 text만 할당된다. */
-    newCommentId.innerHTML= `appeenndd_${userId}`
-    newCommentId.className = 'comment__userId';
+  //*** innerHTML없이 newCommentId = `user ${userId}`로 하게 되면 span까지 사라진채 text만 할당된다. */
+  newCommentId.innerHTML = `appeenndd_${userId}`;
+  newCommentId.className = "comment__userId";
 
-    // 새로운 코멘트 내용 
-    let newCommentContents = document.createElement("span");
-    newCommentContents.innerHTML = commentContentsValue;
-    newCommentContents.className = 'comment_content';
+  // 새로운 코멘트 내용
+  let newCommentContents = document.createElement("span");
+  newCommentContents.innerHTML = commentContentsValue;
+  newCommentContents.className = "comment_content";
 
-    // 새로운 하트 생성 
-    let newCommentLike = document.createElement('span');
-    newCommentLike.innerHTML = commentLike;
-    newCommentLike.className = "commentLike";
+  // 새로운 하트 생성
+  let newCommentLike = document.createElement("span");
+  newCommentLike.innerHTML = commentLike;
+  newCommentLike.className = "commentLike";
 
-    NewcommentView.append(newCommentId);
-    NewcommentView.append(newCommentContents);
-    NewcommentView.append(newCommentLike);
+  NewcommentView.append(newCommentId);
+  NewcommentView.append(newCommentContents);
+  NewcommentView.append(newCommentLike);
 
-    CommentInputReset(commentContentsValue);
+  CommentInputReset(commentContentsValue);
+};
+// 댓글 input 내용 삭제 **********  인풋 창 value 갯수
+const CommentInputReset = (commentContentsValue) => {
+  let commentContents = document.querySelector(".commentContents");
 
-}
- // 댓글 input 내용 삭제 **********  인풋 창 value 갯수  
-const CommentInputReset = (commentContentsValue) =>{
-    
-    let commentContents = document.querySelector(".commentContents");
-
-    if (commentContentsValue.length>0){
-        return commentContents.value = null;
-    }
-}
+  if (commentContentsValue.length > 0) {
+    return (commentContents.value = null);
+  }
+};
