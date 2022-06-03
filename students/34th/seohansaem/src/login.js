cbb2129d5cb426f8460933c5f293a5749a2b9b1f
shs <hs.seo@kisos.net>
2022-06-03 10:17:14 +0900

    ADD : 로그인시 이메일 형식, 비밀번호 자릿수 검증 추가

--- students/34th/seohansaem/src/login.js
+++ students/34th/seohansaem/src/login.js
@@ -1,20 +1,25 @@
 const loginForm = document.querySelector('.login-form');
 const inputId = document.querySelector('.login-id');
 const inputPw = document.querySelector('.login-pw');
+const loginButton = document.querySelector('.login-button');
+
 const handleLogin = (ev) => {
   ev.preventDefault();
+  if (inputId.value.indexOf('@') === -1) {
+    alert('전화번호 또는 이메일 형식을 확인해주세요.');
+    return;
+  } else if (inputPw.value.length < 6) {
+    alert('비밀번호를 6자 이상 입력해주세요.');
+    return;
+  }
   location.href = 'main.html';
 };
 
 const handleChangeInfo = () => {
-  const _inputId = document.querySelector('.login-id');
-  const _inputPw = document.querySelector('.login-pw');
-
-  const _loginButton = document.querySelector('.login-button');
-  if (_inputId.value.length > 0 && _inputPw.value.length > 0) {
-    _loginButton.disabled = false;
+  if (inputId.value && inputPw.value) {
+    loginButton.disabled = false;
   } else {
-    _loginButton.disabled = true;
+    loginButton.disabled = true;
   }
 };
 
