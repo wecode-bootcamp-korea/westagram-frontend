f82705e7f4694bb7a35982785a96076d090b52c5
Vanessa <bohyunparkv@gmail.com>
2022-06-03 12:07:00 +0900

    ADD : new edit

--- students/34th/Vanessas/main/main.js
+++ students/34th/Vanessas/main/main.js
@@ -1,9 +1,23 @@
+
 const comment = document.getElementById('comments-box');
 const button = document.getElementById('postButton');
 
+const input = document.querySelector('#comments-box');
+const text = input.value.trim();
+
 button.addEventListener('click', () => {
-    const input = document.querySelector('#comments-box');
-    const text = input.value.trim();
+    
+    if (input.value !== "") {
+        postComment();
+    }
+
+});
+
+input.addEventListener('keyup', (e) => {
+
+    if (e.key === "Enter") {
+        postComment();
+    }
 
     if (text !== '') {
         postComment();
@@ -20,26 +34,26 @@ function postComment() {
 
     const newComment = document.createElement('div');
 
-    newComment.innerHTML = texts;
+    newComment.innerHTML = `<strong>arielle</strong> ${texts}`;
 
     place.appendChild(newComment);
+
+    inputs.value = null;
+
 }
 
 
 // post button을 누르면 input text를 받아와서 올린다
 // .onclick 
 
+// bring commentbox, post button, heart = variables
 
+// addeventlistener 2개 => 1) enter keyup 2) click post button
+//  { comment 만드는것 함수 실행 }
 
-bring commentbox, post button, heart = variables
-
-addeventlistener 2개 => 1) enter keyup 2) click post button
- { comment 만드는것 함수 실행 }
-
-
-button.addeventlistener  .'click'
-{
+// button.addeventlistener  .'click'
+// {
     
-}
+// }
 
 
