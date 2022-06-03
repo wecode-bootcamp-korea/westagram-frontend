a25d7d09c6749b0074c2d2a46012221aa96f8d88
정예지 <jeong-yeji@jeong-yejiui-MacBookAir.local>
2022-06-03 13:11:23 +0900

    ADD : 필수기능구현 + 추가기능 구현중 +  리펙토링

--- students/34th/jeongyeji/js/login.js
+++ students/34th/jeongyeji/js/login.js
@@ -1,25 +1,25 @@
 const thisIsId = document.getElementById('userID');
 const thisIsPw = document.getElementById('userPW');
 const loginBtn = document.getElementById('loginBtn');
+const loginForm = document.getElementById('loginForm');
 
-thisIsPw.addEventListener('keyup', function() {
-  const thisIsIdVal = document.getElementById('userID').value;
+loginForm.addEventListener('input', (e)=> {
   const thisIsPwVal = document.getElementById('userPW').value;
-
-  if(thisIsIdVal.includes('@')){
-    loginBtn.style.backgroundColor = '#0095f6'
-    //location.href="main.html"
-  } else {
-    alert('아이디를 확인해주세요.');
-    loginBtn.style.backgroundColor = 'rgba(0,149,246,.3)'
-  }
-
-  if(thisIsPwVal.length >= 5){
-    loginBtn.style.backgroundColor = '#0095f6'
-    //location.href="main.html"
-  } else {
-    alert('비밀번호를 확인해주세요.');
-    loginBtn.style.backgroundColor = 'rgba(0,149,246,.3)'
-
-  }
+  const thisIsIdVal = document.getElementById('userID').value;
+  
+  if(thisIsIdVal.includes('@') && thisIsPwVal.length >= 5){
+    loginBtn.style.backgroundColor = '#0095f6';
+  } 
+});
+loginForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const thisIsPwVal = document.getElementById('userPW').value;
+    const thisIsIdVal = document.getElementById('userID').value;
+    
+    if(thisIsIdVal.includes('@') && thisIsPwVal.length >= 5){
+      location.href="main.html";
+    } else {
+      alert('비밀번호를 확인해주세요.');
+      loginBtn.style.backgroundColor = 'rgba(0,149,246,.3)';
+    }
 });
