75de835bdbf0347412741c53d36444b459492fb0
bbbb <bgh9651@gmail.com>
2022-06-03 12:48:01 +0900

    refactoring

--- students/34th/gwanghyunbaek/js/post.js
+++ students/34th/gwanghyunbaek/js/post.js
@@ -2,32 +2,38 @@
 
 const text = document.querySelector("textarea");
 const btn = document.querySelector(".comment_post");
-const postdiv = document.querySelector(".feeds_ul");
+const postUl = document.querySelector(".feeds_ul");
 const img = document.createElement("img");
-
-
-
+img.src = './image/heart.png';
 
 function addComment() {
     const comment = document.createElement("li");
+   
    if(text.value !== "" ) {
-       comment.innerHTML = '<span><strong>bbbb_0221</strong>'+ text.value+ '</span>';
-        postdiv.appendChild(comment);
-        img.src = './image/heart.png';
-    postdiv.appendChild(img);
-       text.value="";
+        comment.innerHTML = `<div><strong>bbbb_0221</strong> ${text.value}</div>`;
+        postUl.appendChild(comment);
+        comment.appendChild(img);
+        text.value="";
    }
+   function x(e) {
+    let result = e.target;
+     console.log(result);
+    let red = 'http://127.0.0.1:5500/students/34th/gwanghyunbaek/image/heart.png';
+        result.src = (result.src === red) ? './image/heart1.png' : red;
+    }
+        img.addEventListener("click",x);
 }
 btn.addEventListener("click", addComment);
 
 function enterkey() {
-    const comment = document.createElement("span");
+    const comment = document.createElement("li");
     if (window.event.keyCode == 13) {
          if(text.value !== "" ) {
-       comment.innerHTML ='<span><strong>bbbb_0221</strong>'+ text.value+ '</span>';
-        postdiv.appendChild(comment);
-       text.value="";
-    }
+            comment.innerHTML = `<div><strong>bbbb_0221</strong> ${text.value}</div>`;
+            postUl.appendChild(comment);
+            comment.appendChild(img);
+            text.value="";
+          }
 }
 }
 text.addEventListener("keyup",btnStyle);
@@ -41,6 +47,13 @@ function btnStyle() {
         btn.disabled = true;
     }
 }
+
+
+// 
+
+
+
+
 /*if(text.value > 0 ) {
     const comment = document.createElement("span");
     comment.innerHTML = `hi`;
