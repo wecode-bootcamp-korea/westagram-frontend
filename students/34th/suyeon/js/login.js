a2a144c608013c8238c9d4cc66dc52ad537e373e
dduddu92 <suyun107@gmail.com>
2022-06-03 13:06:56 +0900

    ADD : 내용 수정 및 리팩토링 반영 완료

--- students/34th/suyeon/js/login.js
+++ students/34th/suyeon/js/login.js
@@ -1,19 +1,22 @@
-const userId = document.querySelector('#userName');
-const userPw = document.querySelector('#pw');
-const button = document.querySelector('#logInBtn')
+const userId = document.querySelector("#userName");
+const userPw = document.querySelector("#pw");
+const loginButton = document.querySelector("#logInBtn");
 
-function logInActive (){
-    button.style.backgroundColor = '#0195f7';
-    button.disabled = false;
+function logInActive() {
+  loginButton.style.backgroundColor = "#0195f7";
+  loginButton.disabled = false;
 }
 
-function logInInactive (){
-    button.style.backgroundColor = '#b2dffc';
-    button.disabled = true;
+function logInInactive() {
+  loginButton.style.backgroundColor = "#b2dffc";
+  loginButton.disabled = true;
 }
 
-window.addEventListener('keyup', () =>{
-  userId.value && userPw.value ? logInActive() : logInInactive()
-})
+window.addEventListener("keyup", () => {
+  userId.value && userPw.value ? logInActive() : logInInactive();
+});
 
-
+// //로그인 버튼 클릭 시 메인 페이지로 이동 - 아직 구현 못함...
+// loginButton.addEventListener("click", () => {
+//   location.href = "mainpage.html";
+// });
