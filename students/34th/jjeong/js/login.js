ff001ee628ccb2afa1716eb97b8baa5766bff43f
banhera <87149751+banhera@users.noreply.github.com>
2022-06-03 13:18:04 +0900

    2week refactoring

--- students/34th/jjeong/js/login.js
+++ students/34th/jjeong/js/login.js
@@ -1,18 +1,18 @@
-const idd = document.getElementById("id");  //DOM에 접근
-const pwdd = document.getElementById("password");
-const btnBtn = document.getElementsByClassName("btn")[0];
+const idd = document.getElementById("login_id");  //DOM에 접근
+const pwdd = document.getElementById("login_pwd");
+const btnBtn = document.getElementsByClassName("login_btn")[0];
 
-function login (){
-
-    const id = idd.value;
-    const pass = pwdd.value;
-
-    if (id && pass){
-        btnBtn.classList.remove('xxx');
-    } else {
-        btnBtn.classList.add('xxx'); //그렇지 않으면 xxx 태그가 추가된다.
-    } 
+const activeLogin = () => {
+    let id = idd.value;
+    let pwd = pwdd.value;
+    
+    if((id && pwd) && (pwd.length > 1)) {
+        btnBtn.classList.add('login_input');
+    }
+    else {
+        btnBtn.classList.remove('login_input');
+    }
 }
 
-idd.addEventListener("keyup", login);  //keyup 이벤트가 발생하면 함수가 실행된다.
-pwdd.addEventListener("keyup", login);
+idd.addEventListener('keyup', activeLogin);  //keyup 이벤트가 발생하면 함수가 실행된다.
+pwdd.addEventListener('keyup', activeLogin);
