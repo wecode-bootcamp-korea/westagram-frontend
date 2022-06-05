e7539bff0fd7c03cd4df06eb96a24dcc2bd77d79
conqueror-G <gitconquerorg@gmail.com>
2022-06-05 22:36:38 +0900

    EDIT: directory move (pages/Login, pages/Main)

--- students/34th/minseok/westagram-react/src/index.js
+++ students/34th/minseok/westagram-react/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Login from './Login/Login';
-import Main from './Main/Main';
+// import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
