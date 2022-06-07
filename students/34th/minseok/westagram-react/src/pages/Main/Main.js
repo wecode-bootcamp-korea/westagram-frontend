6253faedc7fb747f3d6af9968ab7cf33987eac7e
conqueror-G <gitconquerorg@gmail.com>
2022-06-07 15:31:38 +0900

    EDIT: main.js, main.css naming convention

--- students/34th/minseok/westagram-react/src/pages/Main/Main.js
+++ students/34th/minseok/westagram-react/src/pages/Main/Main.js
@@ -3,18 +3,13 @@ import './Main.css';
 
 function Main() {
   return (
-    // gridArea
     <div className='twoDividedArea'>
-      {/* header */}
       <header className='topArea'>
-        {/* nav */}
         <nav className='logo'>
           <img src='/images/instagram.png' alt='westagramLogo' />
           <span>westagram</span>
         </nav>
-        {/* searchId */}
         <input type='search' placeholder='🔍 검색' />
-        {/* nav */}
         <nav className='icon'>
           <img
             src='https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png'
@@ -31,244 +26,150 @@ function Main() {
         </nav>
       </header>
       <div className='bottomArea'>
-        {/* gridArea */}
         <main className='main'>
           <article className='posting'>
-            <div className='posting_profile'>
-              <img
-                className='posting_profile_image'
-                src='/images/penguin-img.png'
-                alt='프로필이미지'
-              />
+            <div className='writerProfile'>
+              <img className='writerProfileImage' src='/' alt='프로필이미지' />
               <p>cannon_mj</p>
               <img
-                className='posting_profile_details'
+                className='writerProfileDetails'
                 src='/images/addHost.png'
                 alt='더보기'
               />
             </div>
-            <div className='posting_image'>
+            <div className='postingImage'>
               <img src='/images/feed-images.jpg' alt='바다사진' />
             </div>
-            <i className='posting_icon'>
+            <i className='postingIcon'>
               <img
-                className='posting_icon_heart'
+                className='postingIconHeart'
                 src='/images/feedHeart.png'
                 alt='좋아요아이콘'
               />
               <img
-                className='posting_icon_sayBallon'
+                className='postingIconSayballon'
                 src='/images/sayBalloon.png'
                 alt='말풍선아이콘'
               />
               <img
-                className='posting_icon_share'
+                className='postingIconShare'
                 src='/images/share.png'
                 alt='공유하기아이콘'
               />
               <img
-                className='posting_icon_bookMark'
+                className='postingIconBookmark'
                 src='/images/bookMark.png'
                 alt='북마크아이콘'
               />
             </i>
-            {/* inputComment */}
-            <div className='posting_othersComment'>
-              <p className='posting_othersComment_representative'>
-                땡땡이 좋아합니다
-              </p>
-              <p className='posting_othersComment_comment'>
-                sss 위워크에서 진행은 베이킹 클래스
-              </p>
-              <p className='posting_othersComment_comment'>
-                ddd 거봐 좋았잖아~~~~🥰
-              </p>
+            <div className='postingComment'>
+              <p className='RepresentativeUser'>땡땡이 좋아합니다</p>
+              <p className='userComment'>sss 위워크에서 진행은 베이킹 클래스</p>
+              <p className='userComment'>ddd 거봐 좋았잖아~~~~🥰</p>
             </div>
-            {/* active_addComment */}
-            <div className='posting_addComment'>
+            <div className='addComment'>
               <input
-                className='posting_addComment_text'
+                className='addCommentText'
                 type='text'
                 placeholder='댓글 달기...'
               />
-              <button
-                className='posting_addComment_button commonButton'
-                type='submit'
-              >
+              <button className='addCommentButton commonButton' type='submit'>
                 게시
               </button>
             </div>
           </article>
-          {/* sideBar */}
-          <aside className='sideBar'>
-            <div className='sideBar_host'>
+          <aside className='sidebar'>
+            <div className='sidebarHost'>
               <img
-                className='sideBar_host_image'
+                className='sidebarHostImage'
                 src='/images/penguin-img.png'
                 alt='호스트이미지'
               />
-              <div className='sideBar_host_id'>
-                <p className='sideBar_host_id_name'>김펭귄</p>
-                <p className='sideBar_host_id_description'>
-                  프론트 앤드 개발자
-                </p>
+              <div className='sidebarHostId'>
+                <p className='sidebarHostIdName'>김펭귄</p>
+                <p className='sidebarHostIdDescription'>프론트 앤드 개발자</p>
               </div>
             </div>
-            {/* userHistory */}
-            <div className='sideBar_history'>
-              <div className='sideBar_history_title'>
+            <div className='sidebarHistory'>
+              <div className='sidebarHistoryTitle'>
                 <h1>스토리</h1>
                 <button className='commonButton'>모두 보기</button>
               </div>
-              <ul className='sideBar_history_user'>
-                <li className='sideBar_history_user_list'>
+              <ul className='sidebarHistoryUser'>
+                <li className='sidebarHistoryUserList'>
                   <img
-                    className='sideBar_history_user_list_image'
+                    className='sidebarHistoryUserListImage'
                     src='/images/penguin-img.png'
                     alt='유저이미지'
                   />
-                  <div className='sideBar_history_user_list_id'>
-                    <div className='sideBar_history_user_list_id_name'>
-                      김원숭
-                    </div>
-                    <div className='sideBar_history_user_list_id_time'>
-                      16분 전
-                    </div>
+                  <div className='sidebarHistoryUserListId'>
+                    <div className='sidebarHistoryUserListIdName'>김원숭</div>
+                    <div className='sidebarHistoryUserListIdTime'>16분 전</div>
                   </div>
                 </li>
-                <li className='sideBar_history_user_list'>
+                <li className='sidebarHistoryUserList'>
                   <img
-                    className='sideBar_history_user_list_image'
+                    className='sidebarHistoryUserListImage'
                     src='/images/penguin-img.png'
                     alt='유저이미지'
                   />
-                  <div className='sideBar_history_user_list_id'>
-                    <div className='sideBar_history_user_list_id_name'>
-                      김원숭
-                    </div>
-                    <div className='sideBar_history_user_list_id_time'>
-                      16분 전
-                    </div>
+                  <div className='sidebarHistoryUserListId'>
+                    <div className='sidebarHistoryUserListIdName'>김원숭</div>
+                    <div className='sidebarHistoryUserListIdTime'>16분 전</div>
                   </div>
                 </li>
-                <li className='sideBar_history_user_list'>
+                <li className='sidebarHistoryUserList'>
                   <img
-                    className='sideBar_history_user_list_image'
+                    className='sidebarHistoryUserListImage'
                     src='/images/penguin-img.png'
                     alt='유저이미지'
                   />
-                  <div className='sideBar_history_user_list_id'>
-                    <div className='sideBar_history_user_list_id_name'>
-                      김원숭
-                    </div>
-                    <div className='sideBar_history_user_list_id_time'>
-                      16분 전
-                    </div>
+                  <div className='sidebarHistoryUserListId'>
+                    <div className='sidebarHistoryUserListIdName'>김원숭</div>
+                    <div className='sidebarHistoryUserListIdTime'>16분 전</div>
                   </div>
                 </li>
-                <li className='sideBar_history_user_list'>
+                <li className='sidebarHistoryUserList'>
                   <img
-                    className='sideBar_history_user_list_image'
+                    className='sidebarHistoryUserListImage'
                     src='/images/penguin-img.png'
                     alt='유저이미지'
                   />
-                  <div className='sideBar_history_user_list_id'>
-                    <div className='sideBar_history_user_list_id_name'>
-                      김원숭
-                    </div>
-                    <div className='sideBar_history_user_list_id_time'>
-                      16분 전
-                    </div>
+                  <div className='sidebarHistoryUserListId'>
+                    <div className='sidebarHistoryUserListIdName'>김원숭</div>
+                    <div className='sidebarHistoryUserListIdTime'>16분 전</div>
                   </div>
                 </li>
               </ul>
             </div>
-            {/* serRecommendation */}
-            <div className='sideBar_recommendation'>
-              <div className='sideBar_recommendation_title'>
+            <div className='sidebarFallow'>
+              <div className='sidebarFallowTitle'>
                 <h1>회원님을 위한 추천</h1>
                 <button className='commonButton'>모두 보기</button>
               </div>
-              <ul className='sideBar_recommendation_user'>
-                <li className='sideBar_recommendation_user_list'>
+              <ul className='sidebarFallowUser'>
+                <li className='sidebarFallowUserList'>
                   <img
-                    className='sideBar_recommendation_user_list_image'
+                    className='sidebarFallowUserListImage'
                     src='/images/penguin-img.png'
                     alt='유저이미지'
                   />
-                  <div className='sideBar_recommendation_user_list_id'>
-                    <p className='sideBar_recommendation_user_list_id_name'>
-                      김다람
-                    </p>
-                    <p className='sideBar_recommendation_user_list_id_time'>
+                  <div className='sidebarFallowUserListId'>
+                    <p className='sidebarFallowUserListIdName'>김다람</p>
+                    <p className='sidebarFallowUserListIdTime'>
                       ...외 2명이 추천누름
                     </p>
                   </div>
-                  <button className='sideBar_recommendation_user_list_Fallow commonButton'>
-                    팔로우
-                  </button>
-                </li>
-                <li className='sideBar_recommendation_user_list'>
-                  <img
-                    className='sideBar_recommendation_user_list_image'
-                    src='/images/penguin-img.png'
-                    alt='유저이미지'
-                  />
-                  <div className='sideBar_recommendation_user_list_id'>
-                    <p className='sideBar_recommendation_user_list_id_name'>
-                      김다람
-                    </p>
-                    <p className='sideBar_recommendation_user_list_id_time'>
-                      ...외 2명이 추천누름
-                    </p>
-                  </div>
-                  <button className='sideBar_recommendation_user_list_Fallow commonButton'>
-                    팔로우
-                  </button>
-                </li>
-                <li className='sideBar_recommendation_user_list'>
-                  <img
-                    className='sideBar_recommendation_user_list_image'
-                    src='/images/penguin-img.png'
-                    alt='유저이미지'
-                  />
-                  <div className='sideBar_recommendation_user_list_id'>
-                    <p className='sideBar_recommendation_user_list_id_name'>
-                      김다람
-                    </p>
-                    <p className='sideBar_recommendation_user_list_id_time'>
-                      ...외 2명이 추천누름
-                    </p>
-                  </div>
-                  <button className='sideBar_recommendation_user_list_Fallow commonButton'>
-                    팔로우
-                  </button>
-                </li>
-                <li className='sideBar_recommendation_user_list'>
-                  <img
-                    className='sideBar_recommendation_user_list_image'
-                    src='/images/penguin-img.png'
-                    alt='유저이미지'
-                  />
-                  <div className='sideBar_recommendation_user_list_id'>
-                    <p className='sideBar_recommendation_user_list_id_name'>
-                      김다람
-                    </p>
-                    <p className='sideBar_recommendation_user_list_id_time'>
-                      ...외 2명이 추천누름
-                    </p>
-                  </div>
-                  <button className='sideBar_recommendation_user_list_Fallow commonButton'>
+                  <button className='sidebarFallowUserListButton commonButton'>
                     팔로우
                   </button>
                 </li>
               </ul>
             </div>
           </aside>
-          {/* footer */}
           <footer className='footer'>
-            <p className='footer_information'>위스타그램 정보입니다.</p>
-            <p className='footer_makeYear'>2022년도에 제작</p>
+            <p className='footerInformation'>위스타그램 정보입니다.</p>
+            <p className='footerMakeYear'>2022년도에 제작</p>
           </footer>
         </main>
       </div>
