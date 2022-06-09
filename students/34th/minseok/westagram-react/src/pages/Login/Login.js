066df914b98279cf4ce91d68e67aa38fe8469a60
conqueror-G <gitconquerorg@gmail.com>
2022-06-10 07:49:38 +0900

    FIX: 로그인 페이지, 인풋 값 남아있던 버그, 조건 만족하면 버튼 활성화하기

--- students/34th/minseok/westagram-react/src/pages/Login/Login.js
+++ students/34th/minseok/westagram-react/src/pages/Login/Login.js
@@ -4,38 +4,48 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 function Login() {
-  const [userName, setUserName] = useState();
-  const [userPw, setPw] = useState();
+  // main page move
   const navigate = useNavigate();
   const goToMain = () => {
-    userName.length > 6 && userPw.length > 5
-      ? navigate('/Main')
-      : alert('다시 입력하세요');
+    true ? navigate('/Main') : alert('다시 입력하세요');
   };
+
+  // input
+  const [userId, setUserId] = useState();
+  const idLength = event => {
+    setUserId(event.target.value.length);
+  };
+
+  const [userPw, setUserPw] = useState();
+  const pwLength = event => {
+    setUserPw(event.target.value.length);
+  };
+
+  const [enabled, setEnabled] = useState(true);
+  const compare = value => {
+    userId > 0 && userPw > 0 ? setEnabled(!value) : setEnabled(value);
+  };
+
   return (
     <div className='boundaryLine'>
       <section className='sectionWrapper'>
         <h1 className='loginTitle'>Westagram</h1>
         <article className='userInterface'>
           <input
-            onChange={e => {
-              setUserName(e.target.value);
-            }}
-            value={userName}
+            onChange={idLength}
+            onKeyUp={compare}
             className='userName'
             type='text'
             placeholder='전화번호, 사용자 이름 또는 이메일'
           />
           <input
-            onChange={e => {
-              setPw(e.target.value);
-            }}
-            value={userPw}
+            onChange={pwLength}
+            onKeyUp={compare}
             className='userPassword'
             type='password'
             placeholder='비밀번호'
           />
-          <button onClick={goToMain} className='loginButton' submit='#'>
+          <button disabled={enabled} onClick={goToMain} className='loginButton'>
             로그인
           </button>
         </article>
