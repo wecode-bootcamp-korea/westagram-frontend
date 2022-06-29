86a0457c3b21ca36405bcb239e56893695984010
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-06-29 18:08:44 +0900

    아이디엔터기능 추가

--- students/35th/eokhwa/js/main.js
+++ students/35th/eokhwa/js/main.js
@@ -4,18 +4,31 @@
 let newReple = document.querySelector(".reple_box");
 let repleButton = document.querySelector(".write_reple");
 let index = 0;
-repleButton.addEventListener('click', () => {
+
+
+const addComment = ()=>{
     let createEl =document.createElement("p")
     let repleVelue = document.getElementById('reple_input');    
     let newRepleTag =newReple.querySelectorAll("p");    
+    let repleLength = repleVelue.value.length;
+    
+    if(repleLength !== 0){
 
-    newReple.appendChild(createEl)  
+        newReple.appendChild(createEl)  
    
-    newRepleTag[index].innerHTML = `<strong>Wecode</strong>${repleVelue.value}`;
+        newRepleTag[index].innerHTML = `<strong>Wecode</strong>${repleVelue.value}`;
+    
+        index ++;
+        repleVelue.value =""
 
-    index ++;
-    repleVelue.value =""
-})
+    }else {
+
+        alert("댓글을 입력하세요")
+       
+    }
+
+
+}
 
 
 
@@ -33,3 +46,15 @@ if(document.querySelector('.article_container').classList.contains('more_active'
 
 
 }
+
+
+let inputbox = document.querySelector("#reple_input");
+
+
+
+inputbox.addEventListener('keyup', (event)=>{
+   
+    if(event.code === "NumpadEnter" || event.code === "Enter") {
+        addComment()
+    }
+});
