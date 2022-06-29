a3d2592d1374c5c83939399981633c3fe354f887
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-06-29 21:55:27 +0900

    추가기능

--- students/35th/eokhwa/js/login.js
+++ students/35th/eokhwa/js/login.js
@@ -1,45 +1,55 @@
 let idInput = document.querySelector("#id_input");
-
 let passInput = document.querySelector("#pass_input");
 let buttonOn = document.querySelector("button");
 let buttonActive = document.querySelector(".on");
+let inputAll = document.querySelectorAll("input_box")
 
-idInput.addEventListener('keyup',function(){
-    let idValue =idInput.value.length
-    let passValue = passInput.value.length
-   if(idValue >= 1 && passValue >=1){
+
+
+
+// 아이디, 비번 입력후 버튼 활성화 //
+const isCheckIdAndPass = ()=>{
+    let idValue =idInput.value.length;
+    let passValue = passInput.value.length;
+    (idValue && passValue)   ? isTure() : isFalse()
+  
+
+}
+
+const isTure = ()=>{
     buttonOn.classList.add("on");
     buttonOn.disabled =false;
-   }else{
+
+}
+
+
+const isFalse = ()=>{
     buttonOn.classList.remove("on")
     buttonOn.disabled = true;
-   }
-});
-
-passInput.addEventListener('keyup',function(){
-    let idValue =idInput.value.length
-    let passValue = passInput.value.length
-   if(idValue >= 1 && passValue >=1){
-    buttonOn.classList.add("on");
-    buttonOn.disabled =false;
-    
-   }else{
-    buttonOn.classList.remove("on")
-    buttonOn.disabled = true;
-   }
-});
+}
 
 
-buttonOn.addEventListener('click', function(){
+
+
+//아이디 비번 확인후 로그인//
+const isCheckLogin = ()=>{
     let idValue =idInput.value
     let passValue = passInput.value
     if(idValue === "jung3117" && passValue === "asdf1234"){
-
-        alert("로그인 성공")
-        location.href="../main.html"
+       goToMain();
     }else{
-
-        alert("로그인 실패")
+        goBackLogin();
     }
+}
 
-});
+//성공시//
+const goToMain = ()=>{
+    alert("로그인 성공");
+    location.href = "../main.html";    
+}
+
+//실패시//
+const goBackLogin = ()=>{
+
+    alert("로그인 실패");
+}
--- students/35th/eokhwa/js/main.js
+++ students/35th/eokhwa/js/main.js
@@ -1,60 +1,78 @@
-//댓글//
-
-
 let newReple = document.querySelector(".reple_box");
 let repleButton = document.querySelector(".write_reple");
+let inputbox = document.querySelector("#reple_input");
 let index = 0;
 
-
+//뎃글 입력여부//
 const addComment = ()=>{
+    let repleVelue = document.getElementById('reple_input');        
+    let repleLength = repleVelue.value.length;    
+    (repleLength !== 0)? commntUp() : alert("댓글을 입력하세요");   
+}
+
+//댓글 생성//
+const commntUp = ()=>{
     let createEl =document.createElement("p")
-    let repleVelue = document.getElementById('reple_input');    
-    let newRepleTag =newReple.querySelectorAll("p");    
-    let repleLength = repleVelue.value.length;
-    
-    if(repleLength !== 0){
-
-        newReple.appendChild(createEl)  
-   
-        newRepleTag[index].innerHTML = `<strong>Wecode</strong>${repleVelue.value}`;
-    
-        index ++;
-        repleVelue.value =""
-
-    }else {
-
-        alert("댓글을 입력하세요")
-       
-    }
-
+    let newRepleTag =newReple.querySelectorAll("p");   
+    let repleVelue = document.getElementById('reple_input');
+    newReple.appendChild(createEl);   
+    newRepleTag[index].innerHTML = `<strong>Wecode</strong>${repleVelue.value}`;
+    index ++;
+    repleVelue.value ="";
 
 }
 
 
-
-
+//오른쪽 더보기//
 const moreList = ()=>{
     let story = document.querySelector('.article_container')
-    
-if(document.querySelector('.article_container').classList.contains('more_active')) {
-    story.classList.remove('more_active')
-}else{
-    story.classList.add('more_active')
-}
-
-    
-
-
+    let checkClass = document.querySelector('.article_container').classList.contains('more_active')
+    if(checkClass) {
+        story.classList.remove('more_active')
+    }else{
+        story.classList.add('more_active')
+    }
 }
 
 
-let inputbox = document.querySelector("#reple_input");
 
 
 
+//엔터입력 확인//
 inputbox.addEventListener('keyup', (event)=>{
    
     if(event.code === "NumpadEnter" || event.code === "Enter") {
         addComment()
     }
 });
+
+
+
+
+//아이콘 클릭 .//
+
+let iconBox = document.querySelector(".icon_left");
+let iconIndex =  iconBox.querySelectorAll("span");
+let likeCount = 45;
+let viewCount = document.querySelector(".count")
+
+iconIndex[0].addEventListener('click', ()=>{
+    
+    let checkIconOn = iconIndex[0].classList.contains('on')
+    if(checkIconOn) {
+        iconIndex[0].classList.remove('on')
+        viewCount.innerHTML =`WeCode님 외 ${likeCount}명이 좋아합니다.`
+        
+        likeCount ++;
+        
+    }else{
+        iconIndex[0].classList.add('on')
+        viewCount.innerHTML =`WeCode님 외 ${likeCount}명이 좋아합니다.`
+        likeCount --;
+        
+    }
+
+})
+
+
+
