482f760ad7fc81e0254b8fa1473c2df6a131ec7e
hoonjo1 <hoonjo1@gmail.com>
2022-07-01 02:02:50 +0900

    login label 함수 수정

--- students/35th/jeonghoonjo/src/js/login.js
+++ students/35th/jeonghoonjo/src/js/login.js
@@ -14,6 +14,18 @@ const changePhoneImg = () => {
   phone.src = imgArr[randomIndex];
 };
 
+const switchingLabel = (value, label) => {
+  if (value) {
+    label.classList.remove("label-down");
+    label.classList.add("label-up");
+  }
+
+  if (!value) {
+    label.classList.remove("label-up");
+    label.classList.add("label-down");
+  }
+};
+
 setInterval(changePhoneImg, 3000);
 
 const validationAccount = (id, password) => {
@@ -31,34 +43,20 @@ const validationAccount = (id, password) => {
 const checkInput = (event) => {
   const targetName = event.target.name;
 
-  const getId = document.querySelector(".login__box__form__id");
-  const getPassword = document.querySelector(".login__box__form__pw");
+  const getId = document.querySelector(".login__box__form__id").value;
+  const getPassword = document.querySelector(".login__box__form__pw").value;
 
   const idLabel = document.querySelectorAll("label")[0];
   const passwordLabel = document.querySelectorAll("label")[1];
 
   if (targetName === "id") {
-    if (getId.value) {
-      idLabel.classList.remove("label-down");
-      idLabel.classList.add("label-up");
-    }
-    if (!getId.value) {
-      idLabel.classList.remove("label-up");
-      idLabel.classList.add("label-down");
-    }
+    switchingLabel(getId, idLabel);
+  }
+  if (targetName === "password") {
+    switchingLabel(getPassword, passwordLabel);
   }
 
-  if (targetName === "password") {
-    if (getPassword.value) {
-      passwordLabel.classList.remove("label-down");
-      passwordLabel.classList.add("label-up");
-    }
-    if (!getPassword.value) {
-      passwordLabel.classList.remove("label-up");
-      passwordLabel.classList.add("label-down");
-    }
-  }
-  validationAccount(getId.value, getPassword.value);
+  validationAccount(getId, getPassword);
 };
 
 const login = (event) => {
