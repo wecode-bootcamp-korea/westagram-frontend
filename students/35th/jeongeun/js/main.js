09818a146b04101f843448b10679fb60cac30806
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-06-30 15:32:17 +0900

    Refactor: 스스로 리팩토링-camelCase로 클래스명 변경, 불필요 코드드 제거

--- students/35th/jeongeun/js/main.js
+++ students/35th/jeongeun/js/main.js
@@ -1,23 +1,13 @@
-const input = document.querySelector('.input__comment');
-const submit = document.querySelector('.input__submit');
-
-// 댓글 input 창에 엔터치거나 "게시" 누르면 댓글 추가되도록 createElement로 요소 생성해서, input에 입력한 값이 추가 되어야합니다.
-// .classList.add('이름')
-// .classList.add('여러개','추가')
-// .classList.replace('변경전이름','변경후이름')
-// .classList.remove('삭제할 클래스명1','삭제할 클래스명2')
-
-const commentParent = document.querySelector('.contents__comments')
-// const smallheartIcon = document.querySelector('.smallheart')
-// const deleteIcon = document.querySelector('.delete')
-
+const input = document.querySelector('.inputComment');
+const submit = document.querySelector('.inputSubmit');
+const commentParent = document.querySelector('.contentsComments')
 
 function isEnter() {
   let div = document.createElement('div')
   div.classList.add('yourComment')
 
-  let comment1 = document.createElement('span')
-  comment1.classList.add('comment1')
+  let comment = document.createElement('span')
+  comment.classList.add('comment')
 
   let strongYourID = document.createElement('strong')
   strongYourID.innerHTML = 'yourID'
@@ -36,20 +26,15 @@ function isEnter() {
   deleteI.classList.add('delete', 'fa-solid', 'fa-eraser')
 
   commentParent.appendChild(div)
-  div.appendChild(comment1)
-  comment1.appendChild(strongYourID)
-  comment1.appendChild(realComment)
+  div.appendChild(comment)
+  comment.appendChild(strongYourID)
+  comment.appendChild(realComment)
   div.appendChild(smallheart_delete)
   smallheart_delete.appendChild(smallheartI)
   smallheart_delete.appendChild(deleteI)
 
-  // console.log(div)
   input.value = ''
 
-  // smallheartI를 누르면 하트가 빨간색으로 채워지게 하고 싶은데.. 
-  // smallheartI는 댓글을 추가해야 생기는 얘인데 얘를 어떻게 가져와? 아 생겼으니까 일단 가져오면 되나?
-  // 안되는뎅.. main.js:68 Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')이거 떠..
-  // 우왕.... 전역변수였다....
   smallheartI.addEventListener('click', () => {
     smallheartI.classList.replace('fa-regular', 'fa-solid')
     smallheartI.style.color = "red"
@@ -58,13 +43,8 @@ function isEnter() {
   //.remove()
   deleteI.addEventListener('click', () => {
     div.remove()
-
-
   })
-
 }
-
-
 
 
 input.addEventListener('keyup', (e) => {
