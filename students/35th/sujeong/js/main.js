aa4d3c5e6d43663c27cbdbe138fbc20547306f1a
1eo21eoe <1eo21eoe@gmail.com>
2022-06-30 16:38:03 +0900

    하트 아이콘 활성화

--- students/35th/sujeong/js/main.js
+++ students/35th/sujeong/js/main.js
@@ -1,39 +1,35 @@
 // 이미지 눌렀을 때, 해당 이미지 바뀌는 것
-const heart = document.getElementById("heart");
 
+const heart = document.querySelector("#heart");
+
+let toggleButton = false;
 const change = () => {
-  if(heart 이미지를 눌렀을 때){
-    이미지가 바뀐다.
-  }
-}
-
+  toggleButton = !toggleButton;
+  heart.src = toggleButton ? "img/heart2.png" : "img/heart.png";
+  // 불린 형태의 자바스크립트를 할 경우 위의 코드처럼 작성하도록 익숙해지기
+  // if (toggleButton) {
+  //   heart.src = "img/heart2.png";
+  // } else {
+  //   heart.src = "img/heart.png";
+  // }
+};
 heart.addEventListener("click", change);
 
 // 더보기 버튼 생성
 
-const btn = document.getElementById("btn");
-const contents = document.getElementById("contents");
+// const btn = document.getElementById("btn");
+// const contents = document.getElementById("contents");
 
-btn.addEventListener('click', ) => {
-  btn.classList.toggle('clicked');
-  contents.classList.toggle('clamp');
-};
+// btn.addEventListener('click', ) => {
+//   btn.classList.toggle('clicked');
+//   contents.classList.toggle('clamp');
+// };
 
 // 댓글창 작성
-const userId= document.getElementById("useId");
-const userWrite = document.getElementById("userWrite");
+// const dat = document.querySelector('.dat');
+// const div = d
+// const datgle = dat.value;
 
-
-const checkIdPw = () => {
-  if (id.value.length > 0 && pw.value.length > 0) {
-    submit.disabled = false;
-    submit.classList.add("focus");
-    //비활성화가 아니면 정보를 체크리스트에 더한다.
-  } else {
-    submit.disabled = true;
-    submit.classList.remove("focus");
-    //비활성화가 그대로면 체크리스트에서 지운다.
-  }
-};
-
-login.addEventListener("keyup", checkIdPw);
+// if(e.code == enter){
+//   div.innerHTML="user[i]"+datgle;
+// }
