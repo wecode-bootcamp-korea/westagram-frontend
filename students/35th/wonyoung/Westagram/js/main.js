bf1962eebaef25d3b7e07f9acc972756fb2dd399
foafls <>
2022-06-30 19:33:04 +0900

    modify: 댓글 기능 구현 완료

--- students/35th/wonyoung/Westagram/js/main.js
+++ students/35th/wonyoung/Westagram/js/main.js
@@ -1,7 +1,7 @@
 const cmntBox = document.getElementsByClassName('comment-box')[0];
-const cmntValue = cmntBox.value;
-
-
+const user = document.getElementsByClassName('userOfComment-container')[0];
+const cmnt = document.getElementsByClassName('commentOfUser-container')[0];
+const cmntPost = document.getElementsByClassName('comment-enter-button')[0];
 
 class Comment {
     constructor() {
@@ -12,8 +12,39 @@ class Comment {
         let cmntValue = cmntBox.value;
 
         this.cmntValue = cmntValue;
-        this.newcmnt = document.createElement('div');
-        this.newcmnt.classname = 'cmnt';
-        this.newcmnt.innerHTML = 
+        
+        this.myId = document.createElement('span');
+        this.myId.className = 'userOfComment';
+        this.myId.innerHTML = 'ore.zeno';
+        user.appendChild(this.myId);
+
+        this.newcmnt = document.createElement('span');
+        this.newcmnt.className = 'commentOfUser';
+        this.newcmnt.innerHTML = this.cmntValue;
+        cmnt.appendChild(this.newcmnt);
+
+        cmntBox.value = '';
     }
 }
+
+function activatePostButton() {
+    let cmntValue = cmntBox.value;
+    let cmntLength = cmntValue.length;
+
+    (cmntLength > 0) ? 
+    (cmntPost.disabled = false) :
+    (cmntPost.disabled = true)
+
+}
+
+cmntBox.addEventListener('keydown', activatePostButton);
+
+cmntPost.addEventListener('click', function () {
+    new Comment();
+})
+
+cmntBox.addEventListener('keypress', e => {
+    if (e.key == 'Enter'){
+        new Comment();
+    }
+})
