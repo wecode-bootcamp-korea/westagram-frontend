748edcc1f38df2231158921da14b46b668f1ad04
hoonjo1 <hoonjo1@gmail.com>
2022-06-30 14:46:57 +0900

    댓글 좋아요 모달창 구현

--- students/35th/jeonghoonjo/src/js/main.js
+++ students/35th/jeonghoonjo/src/js/main.js
@@ -1,8 +1,19 @@
 const commentForm = document.querySelector(".comment-form");
 const commentModify = document.querySelector(".comment-modify");
 const commentValue = document.querySelector(".comment-input");
+const LikeBtn = document.querySelector(".like-btn");
+const modal = document.querySelector("#modal");
+const moadlBtn = document.querySelector(".modal-btn");
+const modalOverLay = document.querySelector(".modal-overlay");
 
-const addComment = (event) => {
+let likecount = 10;
+
+const isCommentEvent = () => {
+  const btn = document.querySelector(".comment-submit");
+  commentValue.value ? (btn.disabled = false) : (btn.disabled = true);
+};
+
+const addCommentEvent = (event) => {
   const commentUl = document.querySelector(".comment-ul");
   const li = document.createElement("li");
   const div = document.createElement("div");
@@ -23,16 +34,29 @@ const addComment = (event) => {
   event.preventDefault();
 };
 
-const delComment = (event) => {
+const delCommentEvent = (event) => {
   event.preventDefault();
   event.path[2].remove();
 };
 
-const isComment = () => {
-  const btn = document.querySelector(".comment-submit");
-  commentValue.value ? (btn.disabled = false) : (btn.disabled = true);
+const handleLikeCheckEvent = () => {
+  const p = document.querySelector(".like-count");
+  LikeBtn.classList.toggle("is-like");
+  LikeBtn.classList.value == "like-btn" ? (likecount -= 1) : (likecount += 1);
+  p.innerHTML = `${likecount}`;
 };
 
-commentModify.addEventListener("click", delComment);
-commentForm.addEventListener("submit", addComment);
-commentForm.addEventListener("keyup", isComment);
+const openModal = () => {
+  modal.style.display = "flex";
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+commentForm.addEventListener("keyup", isCommentEvent);
+commentForm.addEventListener("submit", addCommentEvent);
+commentModify.addEventListener("click", delCommentEvent);
+LikeBtn.addEventListener("click", handleLikeCheckEvent);
+moadlBtn.addEventListener("click", openModal);
+modalOverLay.addEventListener("click", closeModal);
