57e5714db58007c432666f41d9b66b1ebf72fb30
최재홍 <choejaehong@choejaehong-ui-MacBookPro.local>
2022-07-01 13:11:06 +0900

    add: 로그인페이지  추가 조건, 메인페이지 댓글별 좋아요, 삭제 기능 추

--- students/35th/jaehong/westagram-login/westagram-login.js
+++ students/35th/jaehong/westagram-login/westagram-login.js
@@ -2,10 +2,7 @@ const thisIsId = document.getElementsByClassName('id')[0];
 const thisIsPw = document.getElementsByClassName('password')[0];
 const thisIsButton = document.getElementsByClassName('button')[0];
 
-document.addEventListener("keyup", function(e){
-    if(thisIsId.value && thisIsPw.value ) {
-        thisIsButton.style.backgroundColor = "#0095f6";
-    }else{
-        thisIsButton.style.backgroundColor = "#c4e1f7";
-    }
-}) 
+document.addEventListener("keyup", function(e) {
+    return (!thisIsId.value) || (thisIsPw.value.length < 5) || (thisIsId.value.indexOf("@") <0) ? thisIsButton.style.backgroundColor = "#c4e1f7"
+    :  thisIsButton.style.backgroundColor = "#0095f6"
+})
--- students/35th/jaehong/westagram-main/westagram-main.js
+++ students/35th/jaehong/westagram-main/westagram-main.js
@@ -1,38 +1,124 @@
-const clickBtn = document.getElementsByTagName('button')[0];
-const typeC = document.getElementsByTagName('input')[1];
+const commentInput = document.getElementsByClassName("replyContent")[0];
+const submitBtn = document.getElementsByClassName("replyBtn")[0];
 
-typeC.addEventListener("keydown", uploadComment);
-
-function uploadComment(e) {
-    if(e.keyCode === 13 ) {
-        let comment = document.getElementsByClassName('commentPlus')[0];
-        let makeDiv = document.createElement('div');
-        let makeUser = document.createElement('strong');
-        let makeMsg = document.createElement('span');
-        let makeBtn = document.createElement('BUTTON');
-
-        makeDiv.className = "commentDiv"
-        makeUser.className = "commentUser"
-        makeMsg.className = "commentMsg"
-        makeBtn.className = "deleteBtn"
-
-        makeMsg.innerHTML = typeC.value;
-        makeUser.innerHTML = "allong_sio";
-        makeBtn.innerHTML = "삭제";
-
-        comment.appendChild(makeDiv);
-        makeDiv.appendChild(makeUser);
-        makeDiv.appendChild(makeMsg);
-        makeDiv.appendChild(makeBtn);
-
-        typeC.value = "";
-
-
-        clickBtn.addEventListener("click", (event) => {
-
-            uploadComment()
-            typeC.value = "";
- 
-        });
-    }
+//댓글 게시 활성화 조건
+function checkInput() {
+  if (!commentInput.value) {
+    return;
+  }
+  submit();
 }
+function submit() {
+  //댓글이 생성될 div지정
+  const box = document.getElementsByClassName("commentPlus")[0];
+
+  //댓글의 형태를 구성할 element들 생성
+  const comments = document.createElement("div");
+  const userComment = document.createElement("div");
+  const likeDelete = document.createElement("div");
+  const userName = document.createElement("strong");
+  const mainText = document.createElement("p");
+  const likeButton = new Image();
+  likeButton.src = "emptyheart.png";
+  const deleteButton = document.createElement("button");
+
+  userName.innerHTML = "jaehong ";
+  mainText.innerText = commentInput.value;
+  deleteButton.innerHTML = "삭제";
+
+  //형성될 element들로 형태 구성
+  comments.appendChild(userComment);
+  comments.appendChild(likeDelete);
+  userComment.appendChild(userName);
+  userComment.appendChild(mainText);
+  likeDelete.appendChild(likeButton);
+  likeDelete.appendChild(deleteButton);
+  box.appendChild(comments);
+
+  //댓글을 구성하는 element들에 클래스명 부여
+  comments.classList.add("replyUserDiv");
+  userComment.classList.add("userComment");
+  userName.classList.add("replyUserName");
+  mainText.classList.add("replyMainText");
+  likeDelete.classList.add("likeDelete");
+  likeButton.classList.add("likeBtn");
+  deleteButton.classList.add("deleteBtn");
+
+  //생성된 버튼으로 게시된 댓글을 삭제하는 기능에 대한 함수
+  deleteButton.addEventListener("click", () => {
+    box.removeChild(comments);
+  });
+  //생성된 버튼으로 게시된 댓글에 좋아요하는 기능에 대한 함수
+  likeButton.addEventListener("click", () => {
+    likeDelete.removeChild(likeButton);
+    const filledHeart = new Image();
+    filledHeart.src = "heart.png";
+    likeDelete.appendChild(filledHeart);
+    filledHeart.classList.add("likeBtn");
+
+    filledHeart.addEventListener("click", () => {
+      likeDelete.removeChild(filledHeart);
+      likeDelete.appendChild(likeButton);
+    });
+  });
+}
+
+//게시버튼 클릭으로 인한 댓글 게시 함수
+submitBtn.addEventListener("click", (event) => {
+  checkInput();
+  commentInput.value = "";
+});
+
+//엔터버튼으로 인한 댓글 게시 함수
+commentInput.addEventListener("keypress", (event) => {
+  if (event.code === "Enter") {
+    checkInput();
+    commentInput.value = "";
+  }
+});
+
+//게시버튼 클릭으로 인한 댓글 게시 함수
+submitBtn.addEventListener("click", (event) => {
+  checkInput();
+  commentInput.value = "";
+});
+
+//엔터버튼으로 인한 댓글 게시 함수
+commentInput.addEventListener("keypress", (event) => {
+  if (event.code === "Enter") {
+    checkInput();
+    commentInput.value = "";
+  }
+});
+
+//상단 검색바에 대한 함수
+const searchBar = document.getElementsByClassName("searchBlank")[0];
+const search = document.getElementsByClassName("search")[0];
+const searchInput = document.getElementsByClassName("searchBlank")[0];
+const searchPlaceholder = searchInput.placeholder;
+
+//상단 검색바 클릭시 돋보기 모양 아이콘 사라짐
+searchBar.addEventListener("focusin", (event) => {
+  const searchBarMagni = document.getElementsByClassName(
+    "fa-magnifying-glass"
+  )[0];
+  search.removeChild(searchBarMagni);
+  //   searchInput.removeChild(searchPlaceholder);
+});
+//상단 검색바에서 포커스 아웃되면 돋보기 모양 아이콘 생겨남
+searchBar.addEventListener("focusout", (event) => {
+  document.createElement("i");
+  const searchBarIcon = document.createElement("i");
+  searchBarIcon.classList.add("fa-magnifying-glass");
+  searchBarIcon.classList.add("fa-solid");
+  search.appendChild(searchBarIcon);
+  //   document.createElement(searchPlaceholder);
+});
+
+searchBar.addEventListener("focusin", (event) => {
+  searchInput.setAttribute("placeholder", "");
+});
+
+searchBar.addEventListener("focusout", (event) => {
+  searchInput.setAttribute("placeholder", "검색");
+});
