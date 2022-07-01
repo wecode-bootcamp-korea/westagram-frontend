91cee65081b169b14d248d6dedd65192cd7414eb
김익현 <kimikhyun@gim-ighyeon-ui-MacBookAir.local>
2022-07-01 13:53:55 +0900

    필수,추가기능 전부 구현완료.

--- students/35th/ikhyun/js/search.js
+++ students/35th/ikhyun/js/search.js
@@ -9,8 +9,53 @@ let objectOfSearch = {
     nickname: "송은우 (Eun Woo Song)",
     img: '<img src="./guide/tobe/recommend3.jpg" alt="" />',
   },
-  // wonik: {},
+  wjddms4107: {
+    id: "wjddms4107",
+    nickname: "노정은 (Noh Jeong Eun)",
+    img: '<img src="./guide/tobe/wjddms4107.png" alt="" />',
+  },
+  pjyong01: {
+    id: "pjyong01",
+    nickname: "박정용 (Park Jeong Yong)",
+    img: '<img src="./guide/tobe/pjyong01.png" alt="" />',
+  },
+  "gongwon35.b": {
+    id: "gongwon35.b",
+    nickname: "박성은 (Park Seong Eun)",
+    img: '<img src="./guide/tobe/gongwon35.b.png" alt="" />',
+  },
+  minjee4281: {
+    id: "minjee428",
+    nickname: "손민지 (Son Min Jee)",
+    img: '<img src="./guide/tobe/minjee4281.png" alt="" />',
+  },
+  schk9611: {
+    id: "schk9611",
+    nickname: "손찬규 (Son Chan Kyu)",
+    img: '<img src="./guide/tobe/schk9611.png" alt="" />',
+  },
+  willy624777: {
+    id: "willy624777",
+    nickname: "이원재 (Lee Won Jae)",
+    img: '<img src="./guide/tobe/willy624777.png" alt="" />',
+  },
+  ljhun9012: {
+    id: "ljhun9012",
+    nickname: "이정훈 (Lee Jeong Hoon)",
+    img: '<img src="./guide/tobe/ljhun9012.png" alt="" />',
+  },
+  kangteuck: {
+    id: "kangteuck",
+    nickname: "이혜진 (Lee Hye Jin)",
+    img: '<img src="./guide/tobe/kangteuck.png" alt="" />',
+  },
+  yelihi19: {
+    id: "yelihi19",
+    nickname: "최원익 (Choi Won Ik)",
+    img: '<img src="./guide/tobe/yelihi19.png" alt="" />',
+  },
 };
+
 let searchBar = document.querySelector(".search-bar");
 let searchBarInfo = document.querySelector(".search-info");
 let searchBarInfoImg = document.querySelector(".search-info-imgbox");
@@ -58,6 +103,7 @@ function checkValue() {
       }
     });
   } else {
+    infobox.style.display = "flex";
     searchBarInfoImg.style.display = "";
     searchBarInfoFirst.innerHTML = "검색어를 입력해주세요";
   }
