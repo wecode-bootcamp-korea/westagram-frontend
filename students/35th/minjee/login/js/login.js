2b90cadd8598481af7ca5dc862c908f0aa1d7b2e
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-01 13:15:17 +0900

    Add: Mission 5 | ID, PW validation

--- students/35th/minjee/login/js/login.js
+++ students/35th/minjee/login/js/login.js
@@ -3,15 +3,19 @@ const thisIsPw = document.getElementById('loginPW');
 const thisIsSubmit = document.getElementById('loginSubmit');
 const thisIsForm = document.getElementById('loginForm');
 
-thisIsForm.addEventListener('keyup', () => {
-    if(thisIsId.value && thisIsPw.value){
-        thisIsSubmit.disabled = false;//활성화
-        thisIsSubmit.classList.add('active');//색바뀌는거
+// login
+// 로그인 버튼 활성화
+function login() {
+    if(thisIsId.value.includes('@') && thisIsPw.value.length >= 5){
+        thisIsSubmit.disabled = false;//버튼활성화
+        thisIsSubmit.classList.add('active');//color 투명도 높이는 class추가
     } else {
         thisIsSubmit.disabled = true;
         thisIsSubmit.classList.remove('active');
     }
-});
+}
+thisIsForm.addEventListener('keyup', login);
+
 
 function moveMain(){
     location.href="../main/main.html";
