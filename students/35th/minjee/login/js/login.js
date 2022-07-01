3080f1409103f727ce0dba7fc43c70659cacc1dc
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-01 17:47:40 +0900

    Add: 반응형 구현 / Refactor: 스스로 리팩토링

--- students/35th/minjee/login/js/login.js
+++ students/35th/minjee/login/js/login.js
@@ -1,22 +1,26 @@
+"use strict";
+
 const thisIsId = document.getElementById('loginId');
 const thisIsPw = document.getElementById('loginPW');
-const thisIsSubmit = document.getElementById('loginSubmit');
+const thisIsButton = document.getElementById('loginButton');
 const thisIsForm = document.getElementById('loginForm');
 
-// login
+// Login
 // 로그인 버튼 활성화
 function login() {
-    if(thisIsId.value.includes('@') && thisIsPw.value.length >= 5){
-        thisIsSubmit.disabled = false;//버튼활성화
-        thisIsSubmit.classList.add('active');//color 투명도 높이는 class추가
+    if (thisIsId.value.includes('@') && thisIsPw.value.length >= 5) {
+        thisIsButton.disabled = false;//버튼활성화
+        thisIsButton.classList.add('active');//color 투명도 높이는 class추가
     } else {
-        thisIsSubmit.disabled = true;
-        thisIsSubmit.classList.remove('active');
+        thisIsButton.disabled = true;
+        thisIsButton.classList.remove('active');
     }
 }
-thisIsForm.addEventListener('keyup', login);
 
-
-function moveMain(){
-    location.href="../main/main.html";
+//로그인 버튼 Click 시, main page로 이동.
+function moveMain() {
+    location.href = '../main/main.html';
 }
+
+thisIsForm.addEventListener('input', login);
+thisIsButton.addEventListener('click', moveMain);
--- students/35th/minjee/main/js/main.js
+++ students/35th/minjee/main/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //comments를 삽입할 부모노드
 let $bottom = document.querySelectorAll('.bottom');
 //comments를 삽입하고 싶은 위치 바로 뒤의 노드
@@ -7,36 +9,34 @@ let $input = document.querySelectorAll('.do-input');
 let $button = document.querySelectorAll('.input-button');
 let $commentsForm = document.querySelectorAll('.input-comments');
 
-//댓글이 피드의 갯수만큼 달아지도록 반복문을 사용함
-for(let n = 0; n < $commentsForm.length; n++){
+for (let n = 0; n < $commentsForm.length; n++) {
 
-    //댓글이 작성되면 '게시' 버튼 활성화 & 색 진하게 변화
-    $commentsForm[n].addEventListener('keyup', () => {
+    //댓글 '게시' 버튼 활성화
+    const activeCommentsBtn = () => {
 
         //댓글이 작성되면
-        if($input[n].value){
+        if ($input[n].value) {
             //'게시'버튼 활성화
             $button[n].disabled = false;
-            //색 진하게
+            //색 진하게 변경
             $button[n].classList.add('active');
-        } else if(!$input[n].value){
+        } else if (!$input[n].value) {
             $button[n].disabled = true;
             $button[n].classList.remove('active');
         }
-    });
+    }
 
-    //댓글 Form 작성 > 내용 작성 > 전송
-    $commentsForm[n].addEventListener('submit', (e) => {
-        //submit 후 새로고침 막기
-        e.preventDefault();
-
+    //댓글 기능 (Form 구성 > 입력 내용 받기 > 전송)
+    const sendComments = (e) => {
         // 댓글 Form 작성
         /*
         <div class="comments flex">
-            <div class="user-id">Yelihi</div>
-            <div class="details">대박이군요~!</div>
+            <span class="user-id">Yelihi</span>
+            <span class="details">대박이군요~!</span>
         </div> 
         */
+        e.preventDefault();
+
         const $comments = document.createElement('div');
         $comments.classList.add("comments");
         const $userId = document.createElement('span');
@@ -44,16 +44,20 @@ for(let n = 0; n < $commentsForm.length; n++){
         const $details = document.createElement('span');
         $details.classList.add('details');
 
-        //comments 내용 작성
-        $userId.innerHTML = "m.minzzy";
         $comments.appendChild($userId);
-        $details.innerHTML = $input[n].value;
         $comments.appendChild($details);
-
-        //comments노드를 bottom노드의 자식노드인 timeAgo노드 앞에 붙여라
+        //comments노드를 bottom노드의 자식노드인 timeAgo노드 앞에 붙이기
         $bottom[n].insertBefore($comments, $timeAgo[n]);
+
+        //comments 내용 작성
+        $userId.innerText = "m.minzzy";
+        $details.innerText = $input[n].value;
 
         //input창의 내용 삭제
         $input[n].value = "";
-    });
+    }
+
+    //피드가 여러개로 늘어나도 댓글 기능이 가능하도록 반복문 활용
+    $commentsForm[n].addEventListener('keyup', activeCommentsBtn);
+    $commentsForm[n].addEventListener('submit', sendComments);
 }
