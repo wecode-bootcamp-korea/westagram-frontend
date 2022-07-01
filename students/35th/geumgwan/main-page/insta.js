bd0b6e1656f7c63f7e5f9af9863f0c70b2da1281
이금관 <igeumgwan@eric.local>
2022-07-01 17:01:36 +0900

    Add: 댓글 좋아요 기능 추가 (미완성)

--- students/35th/geumgwan/main-page/insta.js
+++ students/35th/geumgwan/main-page/insta.js
@@ -3,6 +3,7 @@ const comment = document.getElementById('comment')
 const form = document.querySelector(".comment-write");
 const commentsContainer = document.querySelector("#comments")
 
+
 const commentPost = () => {
     comment.value.length > 0
         ? (submit.disabled = false)
@@ -12,20 +13,27 @@ const commentPost = () => {
 
 comment.addEventListener('keyup', commentPost);
 
-
+// 게시 버튼 활성화/비활성화
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const input = comment.value;
     comment.value = "";
-        const newComment = document.createElement("div");
-        
-        newComment.innerHTML = `<span class="textbold">Me_me  </span> ${input} <img src="../img/heart.png"> `;
-        commentsContainer.appendChild(newComment);
-        
+    const newComment = document.createElement("div");
+    newComment.setAttribute('class', 'newcomment')
+    newComment.innerHTML = `<span class="textbold">Me_me  </span> ${input}
+        <span id='heart'><i class="fa-regular fa-heart"></i> </span>`;
+    commentsContainer.appendChild(newComment);
+    const heart = document.getElementById('heart');
+
+    heart.addEventListener("click", (e) => {
+        e.target.remove();
+        // .parentElement.remove();
+        const redHeart = document.createElement("span");
+        redHeart.setAttribute('class','Red')
+        redHeart.innerHTML = `<i class="fa-solid fa-heart"></i>`
+        newComment.appendChild(redHeart);
+    })
 });
 
-
-
-// 게시 버튼 활성화/비활성화
-
+// 댓글기능
