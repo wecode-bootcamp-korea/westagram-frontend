ead11d873eac8e2df8d7847ea6094be6980977e6
Hyunjoo Lee <hyunjoo0572@gmail.com>
2022-07-02 09:26:37 +0900

    로그인 페이지 수정

--- students/35th/hyunjoo/westagram/js/login.js
+++ students/35th/hyunjoo/westagram/js/login.js
@@ -1,27 +1,9 @@
-// const id = document.getElementById("id");
-// const pw = document.getElementById("pw")
-// const submit = document.getElementById("click")
-
-// const checkIdPw =  function (){  
-//     console.log('sdfdsfd')
-//     console.log(id.value.length>=1 && pw.value.length >=1)
-//     if (id.value.length>=1 && pw.value.length >=1){
-//             submit.disabled = false
-//     }
-// }
-
-// id.addEventListener('keyup', checkIdPw)
-// pw.addEventListener('keyup', checkIdPw)
-
-
 const id = document.getElementById("id")
 const pw = document.getElementById("pw")
 const submit = document.getElementById("click")
 
 const checkIdPw=function (){
-    console.log('실행')
-    console.log(id.value.length>=1 && pw.value.length>=1)
-    if(id.value.length>=1 && pw.value.length>=1){
+    if(id.value.includes('@')&&id.value.length>=1 && pw.value.length>=1){
         submit.disabled=false
     }
 }
@@ -29,3 +11,4 @@ const checkIdPw=function (){
 
 id.addEventListener('keyup',checkIdPw)
 pw.addEventListener('keyup',checkIdPw)
+
