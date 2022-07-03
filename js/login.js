d6b2eca9a44cf6210c47914b897f7c241f067d28
dongkyusim <dongkyusim.aea@gmail.com>
2022-07-04 08:21:00 +0900

    스타일

--- js/login.js
+++ js/login.js
@@ -1,56 +1,11 @@
-"use strict";
-
-const input = document.getElementsByClassName("loginForm")[0];
-const loginBtn = document.getElementById("loginBtn");
-
-const handleInput = ( ) => {
-    const idValue = document. getElementById("id").value;
-    const pwValue = document. getElementById("pw").value;
-
-    const isValidId = checkValue(idValue);
-    const isValidpw = checkValue(pwValue);
-
-    const isValidValue = isValidId && isValidpw;
-
-    if(isValidValue){
-        handleBtn(true);
-    } else {
-        handleBtn(false);
-    }
+'use strict';
+const id = document.getElementById('id');
+const pw = document.getElementById('password');
+const submit = document.getElementById('submit');
+const checkIdPw = () => {
+  id.value.includes('@') > 0 && pw.value.length >= 5
+    ? (submit.disabled = false)
+    : (submit.disabled = true);
 };
-
-const checkValue = (value) => {
-    if (value.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-const handleBtn = (isBtnValid) => {
-    id(isBtnValid){
-        loginBtn.disabled = false;
-        loginBtn.style.opacity = 1;
-        loginBtn.style.cursor = "pointer";
-
-        if(window.event.code === "Enter"){
-            success();
-        }
-    } else{
-        loginBtn.disabled = true;
-        loginBtn.style.opacity = 0.5;
-        loginBtn.style.cursor="default";
-    }
-}
-
-const success = () => {
-    location.href = "http://127.0.0.1:5500/main.html";
-};
-
-const init = () => {
-input.addEventListener("input", handleInput);
-input.addEventListener("keyup", handleInput);
-loginBtn.addEventListener("click", success);
-};
-
-init();
+id.addEventListener('keyup', checkIdPw);
+pw.addEventListener('keyup', checkIdPw);
--- js/main.js
+++ js/main.js
@@ -1,28 +1,146 @@
-const inputText = document.getElementById('inputText');
-const submit = document.getElementById('submit');
-const commentBox = document.getElementById('commentBox');
+// 1. 버튼 활성화 구현
+const commentsId = document.getElementById('comments-id');
+const submitBtn = document.getElementById('comments-submit');
 
-inputText.addEventListener('keyup', function(e){
-    if(inputText.value.length > 0){
-        submit.style.color = "#0095F6";
-    }
-    else {
-        submit.style.color = "#B2DFFC"
-    }
-});
-
-function enter() {
-    if(window.event.keyCode == 13){
-        submitOnclick();
-    }
-}
-
-function submitOnclick() {
-    if(inputText.value.length > 0){
-        const create = document.createElement('div');
-        create.innerHTML = inputText.value;
-        commentBox.appendChild(create);
-        inputText.value = "";
-        submit.style.color = "#B2DFFC"
-        }
+const isDisabled = () => {
+  if (commentsId.value.length > 1) {
+    submitBtn.disabled = false;
+  } else if (commentsId.value.length == 0) {
+    submitBtn.disabled = true;
+  }
+  return;
 };
+
+commentsId.addEventListener('keyup', isDisabled);
+
+const commentSubmit = document.getElementById('comments-submit');
+const commentInput = document.getElementById('comments-id');
+
+// 01. 쿼리셀럭터로 가져왔을경우
+
+// let handleForm = (e) => {
+//   e.preventDefault();
+//   // 유저 입력 값
+//   let comment = commentInput.value;
+//   console.log(comment);
+//   // 빈 값 div를 가져옴
+//   let newComment = document.querySelector('.comment');
+
+//   if (commentInput.value.length > 0) {
+//     // 새로운 div를 만듬
+//     let commentText = document.createElement('div');
+//     // 새로운 div의 이름은 아래
+//     commentText.className = 'comment-text';
+//     // 새로운 div의 html text는 유저가 입력한 값
+//     commentText.innerHTML = commentInput.value;
+//     // 새로운 div가 출력되나 테스트
+//     console.log(commentText);
+//     // 가져온 부모div안에 차일드 맨앞에(새로만든 div를 넣겠다)
+//     newComment.appendChild(commentText);
+//     // commentText.innerText = `<div><span>canon_mj ${comment}</span></div>`;
+//     commentInput.value = '';
+//   }
+// };
+
+// 02. ClassName을 가져왔을때 경우
+// let handleForm = (e) => {
+//   e.preventDefault();
+//   // 유저 입력 값
+//   let comment = commentInput.value;
+//   console.log(comment);
+//   // 빈 값 div를 가져옴
+//   let newComment = document.getElementsByClassName('article-bottom-texts')[0];
+
+//   if (commentInput.value.length > 0) {
+//     // 새로운 div를 만듬
+//     let commentText = document.createElement('div');
+//     // 새로운 div의 이름은 아래
+//     commentText.className = 'comment-text';
+//     // 새로운 div의 html text는 유저가 입력한 값
+//     commentText.innerHTML = commentInput.value;
+//     // 새로운 div가 출력되나 테스트
+//     console.log(commentText);
+//     // 가져온 부모div안에 차일드 맨앞에(새로만든 div를 넣겠다)
+//     newComment.appendChild(commentText);
+//     // commentText.innerText = `<div><span>canon_mj ${comment}</span></div>`;
+//     commentInput.value = '';
+//   }
+// };
+
+// 03. div안에 span까지 만듬
+// let handleForm = (e) => {
+//   e.preventDefault();
+//   // 유저 입력 값
+//   let comment = commentInput.value;
+//   console.log(comment);
+//   // 빈 값 div를 가져옴
+//   let newComment = document.querySelector('.article-bottom-texts');
+
+//   if (commentInput.value.length > 0) {
+//     // 새로운 div를 만듬
+//     let commentText = document.createElement('div');
+//     let boldText = document.createElement('span');
+//     // 새로운 div의 이름은 아래
+//     commentText.className = 'comment-text';
+//     // 새로운 div의 html text는 유저가 입력한 값
+//     // commentText.innerHTML = commentInput.value;
+//     boldText.className = 'commnet-bold';
+//     commentText.appendChild(boldText);
+//     boldText.innerHTML = commentInput.value;
+//     // 새로운 div가 출력되나 테스트
+//     console.log(commentText);
+//     // 가져온 부모div안에 차일드 맨앞에(새로만든 div를 넣겠다)
+//     newComment.appendChild(commentText);
+//     // commentText.innerText = `<div><span>canon_mj ${comment}</span></div>`;
+//     commentInput.value = '';
+//   }
+// };
+
+// 04. 다른경우
+// let handleForm = (e) => {
+//   e.preventDefault();
+//   // 유저 입력 값
+//   let comment = commentInput.value;
+//   console.log(comment);
+//   // 빈 값 div를 가져옴
+//   let newComment = document.querySelector('.article-bottom-texts');
+
+//   if (commentInput.value.length > 0) {
+//     // 새로운 div를 만듬
+//     let commentText = document.createElement('div');
+//     let boldText = document.createElement('span');
+//     // 새로운 div의 이름은 아래
+//     commentText.className = 'comment-text';
+//     // 새로운 div의 html text는 유저가 입력한 값
+//     // commentText.innerHTML = commentInput.value;
+//     boldText.className = 'article-bold';
+//     commentText.appendChild(boldText);
+//     boldText.innerText = 'dfdsf';
+//     // boldText.innerText = commentInput.value;
+//     // boldText.innerHTML = `<div><span>canon_mj ${comment}</span></div>`;
+//     // 새로운 div가 출력되나 테스트
+//     console.log(commentText);
+//     // 가져온 부모div안에 차일드 맨앞에(새로만든 div를 넣겠다)
+//     newComment.appendChild(commentText);
+//     commentInput.value = '';
+//   }
+// };
+
+// 05. 다른경우
+let handleForm = (e) => {
+  e.preventDefault();
+  let comment = commentInput.value;
+  console.log(comment);
+  let newComment = document.querySelector('.article-bottom-texts');
+  if (commentInput.value.length > 0) {
+    let commentText = document.createElement('div');
+    commentText.innerHTML = `<div class="comment-text">
+    <span class="article-bold">canon_mj </span>
+    ${comment}</span></span></div>`;
+    newComment.appendChild(commentText);
+    console.log(newComment);
+    commentInput.value = '';
+  }
+};
+
+commentSubmit.addEventListener('click', handleForm);
