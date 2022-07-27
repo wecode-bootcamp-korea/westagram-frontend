1bbb8a8008bd7fcc7499135e040bab01738118c5
pmb087 <pmb087@gmail.com>
2022-07-27 22:21:49 +0900

    Fix: Refactoring version 1

--- students/36th/seungmin/js/login.js
+++ students/36th/seungmin/js/login.js
@@ -2,10 +2,12 @@ const login = document.querySelector(".loginSubmit");
 const id = document.querySelector("#idInput");
 const pw = document.querySelector("#pwInput");
 
-window.addEventListener("keyup", (e) => {
+// 아이디 비밀번호 Input 검증 후 Value가 있으면 Login 버튼 활성화 , 색변경으로 시인성 강조
+window.addEventListener("keyup", () => {
   id.value !== "" && pw.value !== ""
     ? (login.classList.add("loginAble"), (login.disabled = false))
     : (login.classList.remove("loginAble"), (login.disabled = true));
 });
 
+// Login 버튼 클릭시 Main 페이지로 연결
 const goToMain = () => (window.location.href = "main.html");
