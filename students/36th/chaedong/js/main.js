7c89680dec1d5aa87eed6f0d92be435f4d008d5f
Chaedie <chaedong.im.dev@gmail.com>
2022-07-27 18:30:17 +0900

    ADD : Mission 4 complete

--- students/36th/chaedong/js/main.js
+++ students/36th/chaedong/js/main.js
@@ -1,15 +1,21 @@
 const comments = document.querySelector('.articleComments');
 const inputComment = document.querySelector('.articleInputComment input');
 const userId = 'chaedong';
+const submitComment = document.querySelector('.articleInputComment button');
 
 inputComment.addEventListener('keydown', (e) => {
-  if (e.code === 'Enter') {
-    e.preventDefault();
-    const comment = document.querySelector('.articleComment');
-    const commentTime = document.querySelector('.articleCommentTime');
-    const newComment = comment.cloneNode(true);
-    newComment.children[0].innerHTML = userId;
-    newComment.children[1].innerHTML = e.target.value;
-    comments.insertBefore(newComment, commentTime);
+  if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+    addComment(e);
   }
 });
+submitComment.addEventListener('click', (e) => addComment(e));
+
+function addComment(e) {
+  const comment = document.querySelector('.articleComment');
+  const commentTime = document.querySelector('.articleCommentTime');
+  const newComment = comment.cloneNode(true);
+  newComment.children[0].innerHTML = userId;
+  newComment.children[1].innerHTML = inputComment.value;
+  comments.insertBefore(newComment, commentTime);
+  inputComment.value = '';
+}
