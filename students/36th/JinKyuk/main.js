f5659c3da031155ac96e2db500a9fd64781c5e73
HYUKSKEE <gin280833@gmail.com>
2022-07-27 22:12:33 +0900

    댓글 li style 추가

--- students/36th/JinKyuk/main.js
+++ students/36th/JinKyuk/main.js
@@ -1,20 +1,42 @@
 const commentInput = document.getElementById("comment-input");
 const commentUl = document.getElementById("comment-list");
 const uploadButton = document.getElementById("upload-button");
+const commentLi = document.getElementById("comment-li");
+const commentForm = document.querySelector(".comment-input-wrap");
+let newLis = [];
 
-function uploadComment(event) {
-  const createLi = document.createElement("li");
-  createLi.id = "comment-li";
-  let inputValue = commentInput.value;
-  const userId = "wecode bootcamp";
-  const keypressOrClick = event.key == "Enter" || event.target == uploadButton;
-
-  if (keypressOrClick && inputValue.length > 0) {
-    commentUl.appendChild(createLi);
-    createLi.innerText = `${userId} ${inputValue}`;
-    commentInput.value = "";
-  }
+function deleteList(event) {
+  const li = event.target.parentElement;
+  li.remove();
 }
 
-commentInput.addEventListener("keypress", uploadComment);
-uploadButton.addEventListener("click", uploadComment);
+function paintLi(newLi) {
+  const li = document.createElement("li");
+  li.id = newLi.id;
+  const span = document.createElement("span");
+  span.innerText = newLi.text;
+  const button = document.createElement("button");
+  button.id = "submit-button";
+  button.innerText = "X";
+  button.addEventListener("click", deleteList);
+  li.appendChild(span);
+  li.appendChild(button);
+  commentUl.appendChild(li);
+}
+
+function handleLiForm(event) {
+  event.preventDefault();
+  const newLi = commentInput.value;
+  const userId = "WECODE BOOTCAMP";
+  commentInput.value = "";
+
+  const newLiObj = {
+    text: `${userId} ${newLi}`,
+    id: new Date(),
+  };
+
+  newLis.push(newLiObj);
+  paintLi(newLiObj);
+}
+
+commentForm.addEventListener("submit", handleLiForm);
