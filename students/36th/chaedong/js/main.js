f76661af95747edb0dcc67f1468b3860531d5ac9
Chaedie <chaedong.im.dev@gmail.com>
2022-07-27 22:08:39 +0900

    FIX : Korean Character calls comment event twice

--- students/36th/chaedong/js/main.js
+++ students/36th/chaedong/js/main.js
@@ -3,19 +3,21 @@ const inputComment = document.querySelector('.articleInputComment input');
 const userId = 'chaedong';
 const submitComment = document.querySelector('.articleInputComment button');
 
-inputComment.addEventListener('keydown', (e) => {
-  if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-    addComment(e);
-  }
-});
-submitComment.addEventListener('click', (e) => addComment(e));
+inputComment.addEventListener('keydown', addComment);
+submitComment.addEventListener('click', addComment);
 
 function addComment(e) {
+  console.log(e.isComposing);
+  if (e.isComposing || !(e.code === 'Enter' || e.code === 'NumpadEnter') || e.target.value === '') {
+    return;
+  }
+
   const comment = document.querySelector('.articleComment');
   const commentTime = document.querySelector('.articleCommentTime');
   const newComment = comment.cloneNode(true);
-  newComment.children[0].innerHTML = userId;
-  newComment.children[1].innerHTML = inputComment.value;
+  console.log(newComment);
+  newComment.children[0].children[0].innerHTML = userId;
+  newComment.children[0].children[1].innerHTML = inputComment.value;
   comments.insertBefore(newComment, commentTime);
   inputComment.value = '';
 }
