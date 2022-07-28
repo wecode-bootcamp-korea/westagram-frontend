881dfa74bb9d9981225e2baac191e07eb4713729
Pulan <ekgus5095@gmail.com>
2022-07-28 19:26:07 +0900

    login page refactoring

--- students/36th/dahyun/js/login.js
+++ students/36th/dahyun/js/login.js
@@ -4,9 +4,7 @@ const $pw = document.querySelector('.password');
 const $loginBtn = document.querySelector('.loginBtn');
 
 $loginForm.addEventListener('keyup', () => {
-  if ($id.value.length >= 1 && $pw.value.length >= 1) {
-    $loginBtn.disabled = false;
-  } else {
-    $loginBtn.disabled = true;
-  }
+  $id.value.length >= 1 && $pw.value.length >= 1
+    ? ($loginBtn.disabled = false)
+    : ($loginBtn.disabled = true);
 });
