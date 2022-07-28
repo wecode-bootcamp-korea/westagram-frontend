03cc624c88fc20e5d7143c252ae9ffefd514ac3b
최슬기 <choisk09@gmail.com>
2022-07-28 22:06:28 +0900

    Refactor - self refactoring, removed duplicated codes.

--- students/36th/seulkiChoi/main.js
+++ students/36th/seulkiChoi/main.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const inputComments = document.querySelector('.input_comments');
-const postMark = document.querySelector('.post');
-const commentsLog = document.querySelector('.overflow');
+const postMark = document.querySelector('.post_button');
+const commentsLog = document.querySelector('.inputted_new_comment_overflow_js');
 
 
 
@@ -27,9 +27,9 @@ postMark.addEventListener('click', (e) => {
     commentsLog.appendChild(commentsDiv);
     commentsDiv.append(commentsID, commentsContent,image);
 
-    commentsDiv.classList.add('abc');
-    image.classList.add("btn_js");
-    commentsContent.classList.add("comment_js");
+    commentsDiv.classList.add('new_comment_wrapper');
+    image.classList.add("new_comment_like_btn_js");
+    commentsContent.classList.add("new_comment_content_js");
 
     image.setAttribute("src","image/heart_no_colour.png");
 
