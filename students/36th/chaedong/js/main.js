768e71e7dadc7043da204ad5d990bb6c00d272fe
Chaedie <chaedong.im.dev@gmail.com>
2022-07-28 10:07:35 +0900

    ADD : Comment like feature implement

--- students/36th/chaedong/js/main.js
+++ students/36th/chaedong/js/main.js
@@ -1,23 +1,36 @@
+// Comment
 const comments = document.querySelector('.articleComments');
 const inputComment = document.querySelector('.articleInputComment input');
-const userId = 'chaedong';
 const submitComment = document.querySelector('.articleInputComment button');
-
-inputComment.addEventListener('keydown', addComment);
-submitComment.addEventListener('click', addComment);
+const userId = 'chaedong';
 
 function addComment(e) {
-  console.log(e.isComposing);
   if (e.isComposing || !(e.code === 'Enter' || e.code === 'NumpadEnter') || e.target.value === '') {
     return;
   }
-
   const comment = document.querySelector('.articleComment');
   const commentTime = document.querySelector('.articleCommentTime');
   const newComment = comment.cloneNode(true);
-  console.log(newComment);
   newComment.children[0].children[0].innerHTML = userId;
   newComment.children[0].children[1].innerHTML = inputComment.value;
   comments.insertBefore(newComment, commentTime);
   inputComment.value = '';
 }
+
+// Comment EventListener
+inputComment.addEventListener('keydown', addComment);
+submitComment.addEventListener('click', addComment);
+
+// Like
+const commentHeart = document.querySelectorAll('.articleComment i');
+
+function handleClickLike(e) {
+  e.target.classList.toggle('colorRed');
+  e.target.classList.toggle('fa-regular');
+  e.target.classList.toggle('fas');
+}
+
+// Like EventListener
+commentHeart.forEach((heart) => {
+  heart.addEventListener('click', handleClickLike);
+});
