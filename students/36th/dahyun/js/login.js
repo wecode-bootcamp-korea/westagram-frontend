b6fd347c1cb97928578d6c263f954c87f3a2b758
Pulan <ekgus5095@gmail.com>
2022-07-29 20:10:35 +0900

    ADD: login - ID, PW validation

--- students/36th/dahyun/js/login.js
+++ students/36th/dahyun/js/login.js
@@ -3,8 +3,24 @@ const $id = document.querySelector('.id');
 const $pw = document.querySelector('.password');
 const $loginBtn = document.querySelector('.loginBtn');
 
+const isValidEmail = email => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
+
 $loginForm.addEventListener('keyup', () => {
-  $id.value.length >= 1 && $pw.value.length >= 1
+  $id.value.indexOf('@') !== -1 && $pw.value.length >= 5
     ? ($loginBtn.disabled = false)
     : ($loginBtn.disabled = true);
+});
+
+$loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  !isValidEmail($id.value)
+    ? alert('ID가 잘못된 형식입니다.')
+    : alert('로그인 성공!');
 });
