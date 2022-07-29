829c6f36bb2ed9a978ee35b5bfb8ae9f26a548d8
HYUKSKEE <gin280833@gmail.com>
2022-07-29 10:53:35 +0900

    activateLogin 함수명 변경 & if else문으로 변경

--- students/36th/JinKyuk/index.js
+++ students/36th/JinKyuk/index.js
@@ -5,16 +5,14 @@ const loginButton = document.getElementById("login-button");
 function activateLogin() {
   const idValue = inputForm[0].value;
   const passWordValue = inputForm[1].value;
-  const ableCondition =
-    idValue.indexOf("@") !== -1 && passWordValue.length >= 5;
-  const disableCondition = idValue.length === 0 || passWordValue.length === 0;
+  const isValidate = idValue.indexOf("@") !== -1 && passWordValue.length >= 5;
 
-  if (disableCondition) {
-    loginButton.disabled = true;
-    loginButton.style.cssText = "background: #a4dcff; cursor: no-drop;";
-  } else if (ableCondition) {
+  if (isValidate) {
     loginButton.disabled = false;
     loginButton.style.cssText = "background: #0095f6; cursor: pointer;";
+  } else {
+    loginButton.disabled = true;
+    loginButton.style.cssText = "background: #a4dcff; cursor: no-drop;";
   }
 }
 
