7005578408031d6b4426de50b9149df5ef59d6f1
seungmin lim <seungmin3238@gmail.com>
2022-07-29 12:26:30 +0900

    ADD:email login

--- students/36th/seungmin/js/login.js
+++ students/36th/seungmin/js/login.js
@@ -3,22 +3,24 @@ const id = document.getElementsByClassName('id')[0];
 const pw = document.getElementsByClassName('pw')[0];
 
 id.addEventListener('keyup',()=>{
-    (id.value.length>=5 && pw.value.length>0) ?
+    const idV = id.value;
+    (id.value.length > 0 && pw.value.length >= 5 && idV.indexOf('@')!==-1) ?
     (loginBtn.disabled = false) :
-    (id.value.length<5 || pw.value.length===0) ?
+    (id.value.length === 0 || pw.value.length < 5 || idV.indexOf('@')===-1) ?
     (loginBtn.disabled = true) : false
 });
 
 pw.addEventListener('keyup',()=>{
-    (id.value.length>=5 && pw.value.length>0) ?
+    const idV = id.value;
+    (id.value.length > 0 && pw.value.length >= 5 && idV.indexOf('@')!==-1) ?
     (loginBtn.disabled = false) :
-    (id.value.length<5 || pw.value.length===0) ?
+    (id.value.length === 0 || pw.value.length < 5 || idV.indexOf('@')===-1) ?
     (loginBtn.disabled = true) : false
 });
 
 //Login 확인
 loginBtn.addEventListener('click',()=>{
-    if(id.value==="1q2w3e4r"&&pw.value==="1q2w3e4r"){
+    if(id.value==="seungmin@gmail.com"&&pw.value==="1q2w3e4r"){
         console.log("okay");
         return window.location.href = 'main.html';
         
