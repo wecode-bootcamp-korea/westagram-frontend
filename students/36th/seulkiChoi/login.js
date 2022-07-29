a27a4afd8a9baac9e1978b94d9a4537bc78c3367
최슬기 <choisk09@gmail.com>
2022-07-29 13:41:36 +0900

    MODIFY: login javascript 삼항연산자 적용 / Conditional (ternary) operator

--- students/36th/seulkiChoi/login.js
+++ students/36th/seulkiChoi/login.js
@@ -5,10 +5,7 @@ const loginBtn = document.querySelector('.login_btn');
 addEventListener('keyup', login => {
     let idValue = id.value;
     let pwValue = pw.value;
-    if(idValue.length > 0 && pwValue.length > 0){
-        loginBtn.style.backgroundColor="#0095F6";
+    login = idValue.length > 0 && pwValue.length > 0 ? loginBtn.style.backgroundColor = "#0095F6"
+        : loginBtn.style.backgroundColor="#C4E1FB";
     }
-    else{
-        loginBtn.style.backgroundColor="#C4E1FB";
-    }
-})
+    )
