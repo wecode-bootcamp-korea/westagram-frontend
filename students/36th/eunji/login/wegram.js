df5d0376e28f64bc0a5ecbd85946885544878932
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-07-30 21:58:34 +0900

    sixth

--- students/36th/eunji/login/wegram.js
+++ students/36th/eunji/login/wegram.js
@@ -1,94 +1,47 @@
 
 
 
-const loginId = document.querySelector(".putLogin")
-const loginPw = document.querySelector(".putPw");
-const loginBtn = document.querySelector("button");
+const inputs = document.getElementsByClassName("put")[0]
+const loginBtn = document.getElementById("button")
 
 
-loginBtn.disabled = true;
+const handleInput = () => {
+    const idValue = document.getElementById("id").value;
+    const pwValue = document.getElementById("pw").value;
 
-function listener(){
-    loginId === "" && loginPw === "" ? loginBtn.disabled = true  : loginBtn.disabled = false;
+    const inValidId = checkValue(idValue)
+    const inValidpw = checkValue (pwValue)
+
+    const isValidValue = inValidId && inValidpw
+
+    handleBtn(isValidValue)
 }
 
 
-loginId.addEventListener("keyup",listener)
-loginPw.addEventListener("keyup",listener)
+const checkValue = (value) => {
+    if(value.length>0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+const handleBtn = (isBtnValid)=>{
 
+    if(isBtnValid){
+        loginBtn.disabled = false;
+        loginBtn.style.opacity = 1;
+    }
+    else {
+        loginBtn.disabled = true;
+        loginBtn.style.opacity = 0.5;
+    }
+}
 
-// loginId.addEventListener('keyup', function(){
-//     const idValue = loginId.value;
-//     const pwValue = loginPw.value;
+const init = () => {
+    inputs.addEventListener("input",handleInput)
+    inputs.addEventListener("keyup", handleInput)
+}
 
-//     if(idValue.length >0 && pwValue.length > 0){
-//     button.style.backgroundColor = '#0295f6'
-//     }
-//     else{}
-// })
-
-
-// loginPw.addEventListener('keyup', function(){
-//     const idValue = loginId.value;
-//     const pwValue = loginPw.value;
-
-//     if(idValue.length>0 && pwValue.length >= 0){
-//       //이제 .button 태그의 css 속성 중 background-color만 변경해주기!
-//     button.style.backgroundColor = '#0295f6'
-//     }
-//     else{}
-
-
-
-
-
-// function activeLogin (){
-
-//     let idvalue = loginId.value
-//     let pwvalue = loginPw.value
-
-//     (idvalue&&pwvalue)&&(pwvalue.length>4) ? loginBtnColored.add('login_active') : loginBtn.classList.remove('login_active')
-
-// }
-
-
-// loginId.addEventListener('keyup', activeLogin);
-// loginPw.addEventListener('keyup', activeLogin);
-
-
-
-
-
-
-
-
-
-
-
-// function listener(){
-//     loginId.length >0 && loginPw.length >0 ? loginBtn.disabled = false  : loginBtn.disabled = true;
-// }
-
-
-
-// function listenercolor(){
-//     loginId.value === "" && loginPw.value === "" ? loginBtnColored.disabled = true : loginBtnColored.disabled = false;
-// }
-
-
-// // function listener() {
-// //     if(loginId === "" && loginPw === ""){
-// //         loginBtn.disabled = true;
-// //     }
-// //     else{
-// //         loginBtn.disabled = false;
-// //     }
-// // }
-
-
-
-
-// loginId.addEventListener("keyup",listener)
-// loginPw.addEventListener("keyup",listener)
-// loginBtnColored.addEventListener("keyup",listenercolor)
+init()
--- students/36th/eunji/main/westagram_main.js
+++ students/36th/eunji/main/westagram_main.js
@@ -2,10 +2,8 @@
 
 
 
-const comment = document.querySelector("#comment"); 
 const commentList = document.querySelector('.commentlist');
 const input = document.querySelector(".comment-in input");
-const listItem = document.querySelector("li");
 const btn = document.querySelector("button");
 
 
@@ -13,14 +11,13 @@ const btn = document.querySelector("button");
 
 function addComment(event) {
 
-    event.preventDefault();
     const li = document.createElement("li");
     const commentUserId = document.createElement("span");
     const span = document.createElement("span");
 
 
     commentUserId.innerHTML = "<strong>eunji12</strong> ";
-    commentUserId.classList.add('commentUser');
+    commentUserId.classList.add('commentUser'); 
 
     span.innerHTML = input.value;
 
