e8a55ca000e58d08095b59e5b9228b13be1b2667
1211lys <hpppower111@gmail.com>
2022-08-24 23:52:46 +0900

    REFACTOR : 코드 보완

--- students/37th/leeyunseung/js/login.js
+++ students/37th/leeyunseung/js/login.js
@@ -1,25 +1,8 @@
-const thisInput = document.querySelector('#loginwrapper');
-const thisId = document.querySelector('.loginid');
-const thisPassword = document.querySelector('.loginpassword');
-const thisButton = document.querySelector('.loginbutton');
+const loginWrapperElem = document.querySelector('#loginWrapper');
+const inputIdElem = document.querySelector('.loginId');
+const inputPasswordElem = document.querySelector('.loginPassword');
+const loginButtonElem = document.querySelector('.loginButton');
 
-
-thisInput.addEventListener('keyup', () => {
-
-   if (thisId.value && thisPassword.value) {
-    thisButton.disabled = false;
-    thisButton.style.backgroundColor = '#0e71e1';
-   } else { 
-    thisButton.disabled = true; 
-    thisButton.style.backgroundColor = '#c0dcfb';
-   }
+loginWrapperElem.addEventListener('keyup', () => {
+  loginButtonElem.disabled = !!inputIdElem.value && !!inputPasswordElem.value;
 });
-
-// 1. 버튼의 스타일(disabled 일때와 아닐때) 정의가 있어야함
-// 2. 자바스크립트 코드
-// 첫번째 인풋, 두번째 인풋에 값을 읽어서
-// 둘중에 하나라도 값이 없을때
-// 버튼의 disabled 속성을 부여
-
-// 인풋에서 값 추출할때
-
