5cbb272d07f59bc0ea0a96c2fd7ab7addd20cc5b
ahn-sujin <asj9674@gmail.com>
2022-08-25 12:33:40 +0900

    Modify: 댓글 기능 구현 스크립트 주석 삭제

--- students/37th/sujin/js/main.js
+++ students/37th/sujin/js/main.js
@@ -9,23 +9,6 @@ const commentList = document.querySelector('.comment-list');
 input.addEventListener('keyup', activeButton);
 pushButton.addEventListener('click', createComment);
 moreButton.addEventListener('click', showAllPost);
-// commentList.addEventListener('click', (e) => {
-//     const likeTarget = e.target;
-//     if(likeTarget.classList.contains('fa-regular')){
-//         likeTarget.classList.replace('fa-regular', 'fa-solid');
-//     } else {
-//         likeTarget.classList.replace('fa-solid', 'fa-regular');
-//     }
-//     const name = likeTarget.getAttribute('class');
-
-//     if(name || 'fa-trash-can' || 'fa-regular'){
-//         likeTarget.parentNode.parentNode.remove();
-//         console.log(likeTarget.parentNode.parentNode);
-//     } 
-
-//     // console.log(typeof(name));    
-// });
-
 
 function activeButton(){
     const inputValue = input.value;
@@ -51,28 +34,8 @@ function showAllPost(){
 function createComment(e){
     e.preventDefault();
 
-    const li = document.createElement('li');
-    // const span = document.createElement('span');
-    // const p = document.createElement('p');
-    // const button1 = document.createElement('button');
-    // const button2 = document.createElement('button');
-    // const icon1 = document.createElement('i');
-    // const icon2 = document.createElement('i');
-
-    // span.classList.add('id');
-    // p.classList.add('comment-text');
-    // button1.classList.add('btn-like');
-    // button2.classList.add('btn-delete');
-    // icon1.classList.add('fa-regular', 'fa-heart');
-    // icon2.classList.add('fa-solid', 'fa-trash-can');
-
+    const li = document.createElement('li')
     commentList.appendChild(li);
-    // li.appendChild(span).innerText = 'test1234';
-    // li.appendChild(p).innerText = `<span class="sdnflsd">${input.value}`;
-    // li.appendChild(button1);
-    // li.appendChild(button2);
-    // button1.appendChild(icon1);
-    // button2.appendChild(icon2);
 
     li.innerHTML = `
     <span class="id">sujiny_829</span>
@@ -86,13 +49,6 @@ function createComment(e){
 
 function activeLikeButton(e) {
     const likeButton = e.target;
-    // const heartIcon = likeButton.childNodes;
-
-    // if((heartIcon.classList.contains('fa-regular'))){
-    //     heartIcon.classList.replace('fa-regular','fa-solid')
-    // } else{
-    //     heartIcon.classList.replace('fa-solid','fa-regular')
-    // }
 
     if(likeButton.classList.contains('fa-regular')){
         likeButton.classList.replace('fa-regular','fa-solid');
