f6ba2247e2a9c03a23ea17431115b5fb3879bf98
rayong <skdyds@gmail.com>
2022-08-26 08:15:20 +0900

    Add : 6번 댓글 좋아요, 삭제 기능 추가 - 좋아요 버튼은 toggle 기능 활용
    Modify : 함수명 및 전체 구조 수정
    - 가독성 고려, 각 변수명 및 함수명 정리
    - 동작하는 함수를 분리, submit 이벤트에 디폴트 옵션을 주는 함수 따로 분리
    - 그 왜 좋아요, 삭제 버튼 함수도 분리

--- students/37th/rayong/main/main.js
+++ students/37th/rayong/main/main.js
@@ -1,73 +1,46 @@
-const feedsForm = document.querySelector("#feedsForm");
-const feedsInput = document.querySelector("#feedsInput");
-const ul = document.querySelector("#feedsList");
-const searchInput = document.querySelector("#search-inputbox");
-
-// 상단 사용자 검색
-
-const idDataBase = [
-    {
-        name : "wecode_bootcamp",
-        desc : "위코드 부트캠프",
-    },
-    {
-        name : "wecode_founder",
-        desc : "송은우"
-    }
-]
-
-function searchId (e) {
-    let insertValue = e.target.value;
-    if (insertValue ) {
-
-    }
-}
-
-searchInput.addEventListener('keyup', searchId);
-
+const commentsForm = document.querySelector("#feedsForm");
+const commentsInput = document.querySelector("#feedsInput");
+const commentsListBox = document.querySelector("#feedsList");
 
 // 댓글 추가
 
-function addComments (event) {
-    event.preventDefault();
-    const feeds = feedsInput.value;
-    const li = document.createElement("li");
-    const iconLike = document.createElement("p");
-    const iconDelete = document.createElement("p");
-    const iconBox = document.createElement("div");
-    iconLike.classList.add("like");
-    iconDelete.classList.add("del");
-    li.innerText = feeds;
-    iconLike.innerText = '♥';
-    iconDelete.innerText = '✖';
-    iconBox.appendChild(iconLike);
-    iconBox.appendChild(iconDelete);
-    li.appendChild(iconBox);
-    ul.appendChild(li);
-    feedsInput.value = "";
+function paintComments (comments) {
 
- 
-    // 좋아요 버튼, 반복문 코드, 짝수만 토글 적용
-    const likeIcon = document.getElementsByClassName("like");
-    for (let i = 0; i < likeIcon.length; i++) {
-        likeIcon[i].addEventListener('click', changeIconColor)
-    }
-    function changeIconColor(e) {
-        console.log(e.target)
-        e.target.classList.toggle("icon-red");
-    }    
+    const commentsList = document.createElement("li");
+    const commentsText = document.createElement("span");
+    const commentsLikeBtn = document.createElement("i");
+    const commentsDelBtn = document.createElement("i");
+    const btnBox = document.createElement("div");
 
-    // 삭제 버튼,
-    const deleteIcon = document.getElementsByClassName("del");
-    for (let i = 0; i < deleteIcon.length; i++) {
-        deleteIcon[i].addEventListener('click', deleteComments);
-    }
-    function deleteComments(e) {
-        console.log(e.target)
-        this.parentElement.parentElement.remove();
-    }
+    commentsText.innerText = comments;
+    commentsLikeBtn.className = "fa-solid fa-heart"
+    commentsDelBtn.className = "fa-regular fa-circle-xmark";
+    
+    commentsDelBtn.addEventListener('click', delComments);
+    commentsLikeBtn.addEventListener('click', activateLikeBtn);
+
+    btnBox.appendChild(commentsLikeBtn);
+    btnBox.appendChild(commentsDelBtn);
+    commentsList.appendChild(commentsText);
+    commentsList.appendChild(btnBox);
+    commentsListBox.appendChild(commentsList);
+}
+
+function activateLikeBtn (e) {
+    e.target.classList.toggle("icon-red");
+}
+
+function delComments (e) {
+    e.target.parentElement.parentElement.remove();
+}
+
+function handleSubmit (e) {
+    e.preventDefault();
+    const comments = commentsInput.value;
+    paintComments(comments)
+    commentsInput.value = "";
 }
 
 
 
-feedsForm.addEventListener('submit', addComments)
+commentsForm.addEventListener('submit', handleSubmit);
