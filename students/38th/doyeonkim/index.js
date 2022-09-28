256faa1032614b080e3ea3ed97736180a6b74509
doyonnie <doyonni@gmail.com>
2022-09-28 14:55:11 +0900

    ADD : md 파일 업로드

---
+++ students/38th/doyeonkim/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () =>{
+ 
+  const inputId = document.querySelector('#id');
+  const inputPassword = document.querySelector('#pw');
+  const button = document.querySelector('#btn');
+
+
+  })
