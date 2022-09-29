269d6a0fadd56995397a98afb6d9b3cf1033f6ba
doyonnie <doyonni@gmail.com>
2022-09-29 18:02:10 +0900

    ADD : md 파일 업로드 (메인페이지 댓글 기능 추가)

--- students/38th/doyeonkim/main.js
+++ students/38th/doyeonkim/main.js
@@ -1,5 +1,29 @@
-function printName()  {
-  const name = document.getElementById('commentInput').value;
-  document.getElementById('result').innerText = name;
-}
+document.addEventListener('DOMContentLoaded', () =>{
 
+  const addText = document.getElementsByClassName("text-add")[0];
+  const addBtn = document.getElementsByClassName("btn-add")[0];
+  const inputReply = document.getElementsByClassName("input‑reply")[0];
+  
+  
+  //function to add tasks
+  const addTask = (task) => {
+    const listItem = document.createElement("li");
+    const showItem = inputReply.appendChild(listItem);
+    const taskElem = document.createElement("span");
+  
+    taskElem.innerHTML = task;
+    listItem.appendChild(taskElem);
+    listItem.classList.add("list‑item");
+  
+  };
+  
+  addBtn.addEventListener("click", () => {
+    const task = addText.value;
+    if (task == "") {
+      return;
+    }
+    addTask(task);
+    addText.value = "";
+  });
+  
+  })
