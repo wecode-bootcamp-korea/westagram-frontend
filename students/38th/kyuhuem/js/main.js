172deaa8e123932cf38f6fc58cc120e1766e29c1
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-09-29 19:30:49 +0900

    ADD : 댓글기능, 레이아웃 일부

--- students/38th/kyuhuem/js/main.js
+++ students/38th/kyuhuem/js/main.js
@@ -1,7 +1,8 @@
 const replyInput = document.getElementById('reply-input');
 const replyBtn = document.getElementById('reply-button');
 const replyContents = document.getElementById('reply-content');
-
+const replyComments = document.getElementById('reply-comments');
+const replyUser = document.getElementById('reply-user')
 const verifyReply = () => {
     const replyValue = replyInput.value.length;
     return replyValue > 0 ? true : false;
@@ -18,11 +19,23 @@ const activateBtn = () => {
 replyInput.addEventListener('keyup',activateBtn);
 
 
-const putInReply = () => {
-    replyContents.textContent = replyInput.value;
+const putInReply = (e) => {
+    const repl = document.createElement('div');
+    repl.className = 'reply-content';
+    repl.innerHTML = replyInput.value;
+    replyContents.appendChild(repl);
 }
 
-replyBtn.addEventListener('click',putInReply)
+const putInUser = (e) => {
+    const user = document.createElement('div');
+    user.className = 'reply-user';
+    user.innerHTML = 'Odor';
+    replyUser.appendChild(user);
+}
 
-/*onclick에 함수를 실행한다.
+
+replyBtn.addEventListener('click',putInReply);
+replyBtn.addEventListener('click',putInUser);
+
+/*click에 함수를 실행한다.
 함수 내용은 replyInput 안의 내용을 replyContents안에 집어 넣는 것.*/
