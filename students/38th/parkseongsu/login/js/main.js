7b8feac87f0560f5e54f15d528d99c0c671d6954
HolyWatter <qktjdtn97@icloud.com>
2022-09-29 16:40:45 +0900

    js에 필요없는 주석들을 지우고, 코드들을 기능별로 묶음

--- students/38th/parkseongsu/login/js/main.js
+++ students/38th/parkseongsu/login/js/main.js
@@ -5,21 +5,15 @@ const menuIcon = document.getElementById('menuIcon');
 const dropMenu = document.getElementById('dropMenu');
 const articleHeartIcon = document.querySelector('.likebox .heartIcon')
 
-// const commentarr = [];
+function writeComment(event){
+    event.preventDefault();
 
-// function appendLocalStorage(){
-//     localStorage.setItem('comment', JSON.stringify(commentarr));
-// }
-
-function writeComment(){
     const userId = sessionStorage.getItem('userId');
     const commentValue = commentInput.value;
     const li = document.createElement('li');
 
     commentList.appendChild(li);
-    // let newObj = {'id' : Date.now(), 'value' : commentValue};
-    // commentarr.push(newObj);
-    event.preventDefault();
+
     const userIdSpan = document.createElement('span');
     const textSpan = document.createElement('span');
     const heartBtn = document.createElement('span');
@@ -34,8 +28,6 @@ function writeComment(){
     userIdSpan.innerText = `${userId} `;
     textSpan.innerText = ` ${commentValue}`;
     
-
-    
     li.appendChild(userIdSpan);
     li.appendChild(textSpan);
     li.appendChild(heartBtn);
@@ -47,10 +39,12 @@ function writeComment(){
         console.log('it works')
         li.classList.add('displaynone')
     }
+
     function like(){
         heartBtn.classList.toggle('fullHeartSmall');
         heartBtn.classList.toggle('emptyHeart');
     }
+
     removeBtn.addEventListener('click',hidden);
     heartBtn.addEventListener('click', like);
 }
@@ -63,9 +57,6 @@ function articleHeart(){
     articleHeartIcon.classList.toggle('fullHeart');
 }
 
-
-
-// commentInput.addEventListener('change', writeComment);
 commentbtn.addEventListener('click', writeComment);
 menuIcon.addEventListener('click', createDropbar);
-articleHeartIcon.addEventListener('click', articleHeart)
+articleHeartIcon.addEventListener('click', articleHeart);
--- students/38th/parkseongsu/login/js/search.js
+++ students/38th/parkseongsu/login/js/search.js
@@ -1,48 +1,48 @@
 let userArr = [
-    {id : 'tjdtnxkrmfoa' ,
-    img : ''},
-    {id : 'WeCode' ,
-    img : ''},
-    {id : 'Instagram' ,
-    img : ''},
-    {id : 'Holywater' ,
-    img : ''},
-    {id : 'JustCode' ,
-    img : ''}
+    {id : 'tjdtnxkrmfoa'},
+    {id : 'WeCode'},
+    {id : 'Instagram'},
+    {id : 'Holywater'},
+    {id : 'JustCode'}
 ];
 
 const searchInput = document.querySelector('header>input');
 const searchBar = document.getElementById('searchBar');
+let newarr = [];
 
-function search(){
-    let newarr = []
+function search(){    
     if(searchInput.value != ""){
     newarr = userArr.filter(item=>
         item.id.includes(`${searchInput.value}`)
-    )
-    }else{ newarr = []
+    );
+    }else{ newarr = [];
     }
+    paintSearchList();
+}
 
+function paintSearchList(){
     searchBar.innerHTML = ""
     newarr.map(item=>{
         let li = document.createElement('li');
-        searchBar.appendChild(li);
         let span1 = document.createElement('span');
         let span2 = document.createElement('span');
+
         span1.innerText = `${item.id}`;
         span2.classList.add('userImg');
+
+        searchBar.appendChild(li);
         li.appendChild(span2);
-        li.appendChild(span1);
-        
+        li.appendChild(span1); 
     }
-        )
+    )
 }
+
 function searchbarShow(){
     searchBar.classList.remove('displaynone');
 }
 function searchBarHidden(){
     searchBar.classList.add('displaynone');
 }
-searchInput.addEventListener('input', search)
-searchInput.addEventListener('focus', searchbarShow)
-searchInput.addEventListener('blur', searchBarHidden)
+searchInput.addEventListener('input', search);
+searchInput.addEventListener('focus', searchbarShow);
+searchInput.addEventListener('blur', searchBarHidden);
