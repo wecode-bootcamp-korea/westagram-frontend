a23fbb2271a4dd500c2926ddc6fd791569a1bc34
doyonnie <doyonni@gmail.com>
2022-09-29 11:28:49 +0900

    ADD: 로그인 페이지 파일 업로드 - 로그인 스타일  파일 추가 - 로그인 버튼 활성화 기능 추가

---
+++ students/38th/doyeonkim/login.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () =>{
+ 
+  const inputId = document.querySelector('#id');
+  const inputPassword = document.querySelector('#pw');
+  const button = document.querySelector('#btn');
+ 
+  function loginBtn() {
+      let idValue = inputId.value;
+      let passwordValue = inputPassword.value;
+  
+      if(idValue.length > 0 && passwordValue.length > 0) {
+          button.disabled = false;
+          button.style.cursor = "pointer";
+          button.style.backgroundColor = "blue";
+      }else {
+          button.disabled = true;
+          button.style.cursor = "default";
+          button.style.backgroundColor = "#c4e1fb";
+      }
+  }
+  
+  inputId.addEventListener('keyup', loginBtn);
+  inputPassword.addEventListener('keyup', loginBtn);
+
+  })
