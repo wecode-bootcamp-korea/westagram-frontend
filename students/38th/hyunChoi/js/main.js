7edf67bbb970a76400c8b6ad640231972e4c42b8
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-10-01 14:36:26 +0900

    댓글 좋아요 기능 추가

--- students/38th/hyunChoi/js/main.js
+++ students/38th/hyunChoi/js/main.js
@@ -3,11 +3,13 @@
 //     3. comment 게시
 //     4. input에 입력된 comment 지우기
 //     5. 생성된 comment 지우기
+//     6. commen like 기능 
 
 let commentItem = document.querySelector("comment"); // 댓글 wrap
 let commentInput = document.getElementsByClassName("commentInput")[0]; // 입력요소
 let commentButton = document.getElementsByClassName("commentButton")[0]; // 입력 버튼
 let commentWrap = document.getElementsByClassName("feedDesc")[0] // 게시글 comment Wrap
+let commentLikeHeart = document.getElementsByClassName('like')[0]
 
 function buttonON() { // 1. 버튼 활성화
     commentButton.disabled = false;
@@ -25,6 +27,7 @@ function commentSubmit() { // 3. comment 게시
     let commentIdPtag = document.createElement('p')
     let commentContentPtag = document.createElement('p')
     let commentDeleteSpantag = document.createElement('span')
+    let commentLikeSpantag = document.createElement('span')
 
 
     commentIdPtag.className = 'ptagID'
@@ -32,9 +35,12 @@ function commentSubmit() { // 3. comment 게시
     commentContentPtag.innerHTML = comment
     commentDeleteSpantag.className = 'delete'
     commentDeleteSpantag.innerHTML = "X"
+    commentLikeSpantag.className = 'like'
+    commentLikeSpantag.innerHTML = "&#x2661"
     let newComment = document.getElementsByClassName('feedDesc')[0].appendChild(commentWrap)
     newComment.appendChild(commentIdPtag)
     newComment.appendChild(commentContentPtag)
+    newComment.appendChild(commentLikeSpantag)
     newComment.appendChild(commentDeleteSpantag)
 }
 
@@ -44,8 +50,20 @@ function initInputComment() { // 4. input에 입력된 comment 지우기
 }
 
 function commentDelete(event){
-  let deleteX = document.getElementsByClassName('delete')[0]; // delete X
+  // let deleteX = document.getElementsByClassName('delete')[0]; // delete X
   event.target.parentElement.remove();
+}
+
+function commentLike(event){
+  // let likeHeart = document.getElementsByClassName('like')[0]
+  event.target.className = "liked"
+  event.target.innerHTML = "&#x2665"
+}
+
+function commentUnLike(event){
+  // let likedHeart = document.getElementsByClassName('liked')[0]
+  event.target.className = "like"
+  event.target.innerHTML = "&#x2661"
 }
 
 // commentInput의 value에 따른 commentButton 활성화 이벤트
@@ -71,11 +89,18 @@ commentInput.addEventListener('keypress', function (event) {
 })
 
 commentWrap.addEventListener('click', function(event){
-  console.log(event.target)
-  console.log(document.getElementsByClassName('delete')[0])
   if(event.target.className === 'delete'){
     commentDelete(event)
   }else{
     return
+  }
+})
+
+commentWrap.addEventListener('click', function(event){
+  console.log(event.target)
+  if(event.target.className === 'like'){
+    commentLike(event)
+  }else if(event.target.className === 'liked'){
+    commentUnLike(event)
   }
 })
