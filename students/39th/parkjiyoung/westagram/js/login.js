96be83243f9859249e24ba3c8ac8e5549f315e82
ji <vyoungv7340naver.com>
2022-10-26 17:01:20 +0900

    MISSION 1~5,8,9 완료 MISSION 6,7 진행중

--- students/39th/parkjiyoung/westagram/js/login.js
+++ students/39th/parkjiyoung/westagram/js/login.js
@@ -3,14 +3,12 @@ const $pwInput = document.getElementById('pwInput')
 const $loginBtn = document.getElementById('loginBtn')
 
 function checkLogin(){
-    if(($idInput.value && $pwInput.value) && ($pwInput.value.length >= 5) && ($idInput.value.includes('@') || $pwInput.value.length >= 5))
-    {
-        $loginBtn.removeAttribute('disabled')
-    }
+    const validator = ($idInput.value && $pwInput.value) && ($pwInput.value.length >= 5) && ($idInput.value.includes('@') && $pwInput.value.length >= 5)
+    validator ? $loginBtn.removeAttribute('disabled') : $loginBtn.setAttribute('disabled')
 }
 
 const init = () => {
     $idInput.addEventListener('keyup',checkLogin)
     $pwInput.addEventListener('keyup',checkLogin)
 }
-init()
+
