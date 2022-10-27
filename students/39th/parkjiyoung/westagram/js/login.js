997bd7b00aa4a12e985687c0897cd2fac5d09269
ji <vyoungv7340naver.com>
2022-10-27 10:12:26 +0900

    MISSION 1~9 완료

--- students/39th/parkjiyoung/westagram/js/login.js
+++ students/39th/parkjiyoung/westagram/js/login.js
@@ -4,11 +4,11 @@ const $loginBtn = document.getElementById('loginBtn')
 
 function checkLogin(){
     const validator = ($idInput.value && $pwInput.value) && ($pwInput.value.length >= 5) && ($idInput.value.includes('@') && $pwInput.value.length >= 5)
-    validator ? $loginBtn.removeAttribute('disabled') : $loginBtn.setAttribute('disabled')
+    validator ? $loginBtn.removeAttribute('disabled') : $loginBtn.setAttribute('disabled','')
 }
 
 const init = () => {
     $idInput.addEventListener('keyup',checkLogin)
     $pwInput.addEventListener('keyup',checkLogin)
 }
-
+init()
--- students/39th/parkjiyoung/westagram/js/main.js
+++ students/39th/parkjiyoung/westagram/js/main.js
@@ -2,9 +2,13 @@
 const $commentAdd = document.getElementById('commentAdd')
 const $postBtn = document.querySelector('.postBtn')
 const $newComments = document.querySelector('.newComments')
+
 function postComment(e){    
     e.preventDefault()
+
+    // 사용자 입력 댓글
     const commentValue = $commentAdd.value
+    // 새로운 댓글 생성
     const newComment = document.createElement('p')
     newComment.className = 'comments'
     newComment.innerHTML = `<span>jyp_1234</span>${commentValue}`
@@ -16,26 +20,28 @@ function postComment(e){
     const newDelete = document.createElement('img')
     newDelete.src = 'img/delete.png'
     newDelete.className = 'commentDelete'
+
     $newComments.appendChild(newComment)
     newComment.appendChild(newLike)
     newComment.appendChild(newDelete)
     
-    newDelete.addEventListener('click',()=>{
-        const $comments = document.querySelector('.comments')
-        $newComments.removeChild($comments)
+    // delete click시 
+    newDelete.addEventListener('click',(e)=>{
+        const delComment = e.target.closest('.comments')
+        $newComments.removeChild(delComment) 
     })
 
-    // 구현안됨
+    // like 표시하기
     let cnt = 0
     newLike.addEventListener('click',()=>{
-        const $commentLike = document.querySelector('.commentLike')
         if (cnt % 2 == 0){
-            $commentLike.src = 'img/redheart.png'
+            newLike.src = 'img/redheart.png'
         } else{
-            $commentLike.src = 'img/heart.png'
+            newLike.src = 'img/heart.png'
         }
         cnt++
     })
+
     $commentAdd.value = '' 
 }
 $commentAdd.addEventListener('keyup',(e)=>{
@@ -48,14 +54,6 @@ $postBtn.addEventListener('click',function(e){
     postComment(e)
 })
 
-// 검색창 클릭시 빈칸
-const $searchInput = document.getElementById('searchInput')
-const $searchImg = document.getElementById('searchImg')
-function eraseSearchInPut(e){
-    e.target.removeAttribute('placeholder')
-    $searchImg.remove()
-}
-$searchInput.addEventListener('click',(e)=>{eraseSearchInPut(e)})
 
 // 검색창 입력시 아이디 찾기
 const idData = [
@@ -71,20 +69,52 @@ const idData = [
     { id : 'jyfav_baseball', profile : 'img/baseball.png'},
 ]
 function searchId(e){
+    // 사용자 입력값
     const idKeyup = e.target.value
     const idArray = idData.map(function(name){
-        return name.id
+        return [name.id, name.profile]
     })
-    const filterArray = idArray.filter(word => word.includes(idKeyup))
-
-    filterArray.forEach((el) => {
-        const listId = document.createElement('li')
-        listId.innerText = el
-        document.querySelector('.searchBox').appendChild(listId)
-    });
+    
+    // 사용자 입력갑과 id array에서 일치하는거 filtering
+    const filterArray = idArray.filter(word => word[0].includes(idKeyup))
+    const $filterBox = document.querySelector('.filterBox');
+    $filterBox.innerHTML = '';
+    $filterBox.style.display = 'block'
+    
+    if (filterArray.length !== 0){
+        filterArray.forEach((el) => {
+            const listId = document.createElement('div')
+            const listImg = document.createElement('img')
+            listId.className = 'filterId'
+            listImg.className = 'filterImg'
+            listId.innerText = el[0]
+            listImg.src = `${el[1]}`
+            $filterBox.appendChild(listId)
+            listId.appendChild(listImg)
+        });
+    } else{ // 해당 조건 없을 때
+        $filterBox.style.display = 'none'
+    }
 }
-$searchInput.addEventListener('keyup',(e)=>{searchId(e)})
-
+// 검색창 클릭시 빈칸
+const $searchInput = document.getElementById('searchInput')
+const $searchImg = document.getElementById('searchImg')
+function eraseSearchInPut(e){
+    e.target.removeAttribute('placeholder')
+    $searchImg.style.display = 'none'
+}
+$searchInput.addEventListener('click',(e)=>{
+    eraseSearchInPut(e)
+    if (!(e.target.value)){
+        e.target.addEventListener('keyup',(e)=>{searchId(e)})
+    }
+    e.target.addEventListener('focusout',function(e){
+        document.querySelector('.filterBox').style.display = 'none'
+        $searchInput.value = ''
+        $searchInput.setAttribute("placeholder","search")
+        $searchImg.style.display = 'block'
+    })
+})
 
 // 프로필 클릭시 메뉴 박스
 const $navProfile = document.querySelector('.navProfile')
