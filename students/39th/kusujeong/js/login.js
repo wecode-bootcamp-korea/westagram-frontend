0ee28b59fd19ada9af0b48b627fc397aa6df9e05
sujeong-dev <9sujeong.dev@gmail.com>
2022-10-27 11:23:25 +0900

    ADD: id, pw validation

--- students/39th/kusujeong/js/login.js
+++ students/39th/kusujeong/js/login.js
@@ -1,14 +1,22 @@
 // 문법이나 이상한 짓을 못하도록 선언하기
 "use strict";
 
+// id, pw 둘 다 입력시 로그인버튼 활성화
+// 221027 Id, pw validation 기능 추가
 const inputId = document.querySelector("#inputId");
 const inputPw = document.querySelector("#inputPw");
 const loginBtn = document.querySelector(".btn");
-inputId.addEventListener("keyup", onLoginBtn);
-inputPw.addEventListener("keyup", onLoginBtn);
+inputId.addEventListener("keyup", () => {
+  onLoginBtn();
+});
+inputPw.addEventListener("keyup", () => {
+  onLoginBtn();
+});
 
 function onLoginBtn() {
-  if (inputId.value && inputPw.value) {
+  let idValue = inputId.value;
+  let pwValue = inputPw.value;
+  if (idValue.includes("@") && pwValue.length >= 5) {
     loginBtn.style.opacity = 1;
     loginBtn.disabled = false;
   } else {
