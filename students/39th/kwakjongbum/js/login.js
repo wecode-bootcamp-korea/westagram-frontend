cd6b03237677ecade1f17c3090e95f6e36d5a023
jjongbum92 <jjongbum92@gmail.com>
2022-10-28 17:58:10 +0900

    소스 정리

--- students/39th/kwakjongbum/js/login.js
+++ students/39th/kwakjongbum/js/login.js
@@ -2,18 +2,21 @@ let inpId = document.querySelector('.input-id');
 let inpPw = document.querySelector('.input-pw');
 let lgBtn = document.querySelector('.btn-login');
 
-inpId.addEventListener('keyup', function(){
+function inpIdChk(){
     if(this.value && inpPw.value && (this.value).includes('@') && (inpPw.value).length >= 5){
         lgBtn.disabled = false;
     }else{
         lgBtn.disabled = true;
     }
-});
+};
 
-inpPw.addEventListener('keyup', function(){
+function inpPwChk(){
     if(inpId.value && this.value && (inpId.value).includes('@') && (this.value).length >= 5){
         lgBtn.disabled = false;
     }else{
         lgBtn.disabled = true;
     }
-});
+};
+
+inpId.addEventListener('input', inpIdChk);
+inpPw.addEventListener('input', inpPwChk);
