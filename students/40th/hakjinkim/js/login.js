a0ef7994d76883239ed46411294f3979972ff24f
metamro <en160305@gmail.com>
2022-11-16 19:33:35 +0900

    1차 기본

---
+++ students/40th/hakjinkim/js/login.js
@@ -0,0 +1,23 @@
+const loginButton = document.getElementById("button");
+
+loginButton.addEventListener("click", showConsole);
+function showConsole() {
+  alert("로그인 될까?");
+}
+
+function emaillog() {
+  const id = document.getElementById("email").value;
+  const pw = document.getElementById("password").value;
+  const target = document.getElementById("button");
+
+  if (id.length > 0 && pw.length > 0) {
+    target.disabled = false;
+    loginButton.classList.add("able");
+  }
+  if (id.length === 0 || pw.length === 0) {
+    target.disabled = true;
+    loginButton.classList.remove("able");
+  }
+  console.log(id);
+  console.log(pw.length);
+}
