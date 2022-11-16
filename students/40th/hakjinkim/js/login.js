428bff5f62173641a2f88f36963c710f46cdf352
metamro <en160305@gmail.com>
2022-11-16 19:57:53 +0900

    login.js의 중복 부분 삭제 login.html의 disabled중복 삭제

--- students/40th/hakjinkim/js/login.js
+++ students/40th/hakjinkim/js/login.js
@@ -8,14 +8,14 @@ function showConsole() {
 function emaillog() {
   const id = document.getElementById("email").value;
   const pw = document.getElementById("password").value;
-  const target = document.getElementById("button");
+  //const target = document.getElementById("button");
 
   if (id.length > 0 && pw.length > 0) {
-    target.disabled = false;
+    loginButton.disabled = false;
     loginButton.classList.add("able");
   }
   if (id.length === 0 || pw.length === 0) {
-    target.disabled = true;
+    loginButton.disabled = true;
     loginButton.classList.remove("able");
   }
   console.log(id);
