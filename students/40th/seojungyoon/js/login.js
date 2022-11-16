e9b6812b2fbf16d2753acd394a68516f4c0db023
sammiessi <115966514+sammiessi@users.noreply.github.com>
2022-11-16 17:05:35 +0900

    Add: 로그인 페이지 HTML/CSS/JS 구현, 메인 페이지 HTML 추가/CSS 구현중

---
+++ students/40th/seojungyoon/js/login.js
@@ -0,0 +1,22 @@
+const inputId = document.querySelector(".id");
+const inputPassword = document.querySelector(".password");
+const button = document.querySelector(".button");
+
+console.log(inputId, inputPassword, button);
+
+function loginBtnColor() {
+  let idValue = inputId.value;
+  let passwordValue = inputPassword.value;
+
+  if (idValue.length > 0 && passwordValue.length > 0) {
+    button.disabled = false;
+    button.style.backgroundColor = "#1877F2";
+    button.style.cursor = "pointer";
+  } else {
+    button.disabled = true;
+    button.style.backgroundColor = "#AAC9FF";
+  }
+}
+
+inputId.addEventListener("keyup", loginBtnColor);
+inputPassword.addEventListener("keyup", loginBtnColor);
