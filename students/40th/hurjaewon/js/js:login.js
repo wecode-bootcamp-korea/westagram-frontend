6a369733f56eee684e20db044ceb5b917d8f4735
hurjwon <jwh2700@gmail.com>
2022-11-16 21:02:05 +0900

    ADD: 파일 생성

---
+++ students/40th/hurjaewon/js/js:login.js
@@ -0,0 +1,3 @@
+const input_box = document.getElementsByClassName("id") [0];
+const password_box = document.getElementsByClassName("text")[0];
+const button = document.getElementsByClassName("button")[0];
