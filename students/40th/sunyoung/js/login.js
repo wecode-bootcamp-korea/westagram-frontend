96b48a25ca9055b916a72040fedd878f89580154
YOON <yoon@YOONui-MacBookPro.local>
2022-11-18 18:04:17 +0900

    Add: [40기 윤선영] Mission 1~4 구현완료

--- students/40th/sunyoung/js/login.js
+++ students/40th/sunyoung/js/login.js
@@ -1,3 +1,14 @@
+//이벤트 위임(+이벤트 버블링, 이벤트 캡쳐링)
+
+// 함수는 각 각 독립적인 역할을 해야함. (단일체계원칙) --> 확장성, 유지 보수 가능
+// Eventlistener들을 마지막에 init 안에 정리하여 코드 실행. - entry point가 될 수 있게
+// isValidId / isValidPw  - checkId/Pw  value.length > 0 ? true : false ;
+// isValidPw - value.includes("@")  ? ture : false;
+// boolean naming convention : 'is' or 'has'로 시작하는 name
+// 3항연산자 사용할 것 --> 리엑트에서 무조건 사용. 
+// 3항 연산자도 중첩으로 코드 작성이 가능하나 가독성이 떨어지기 때문에 if/else문을 쓰는 것이 더 깔끔.
+// not연산자 !isBtnValue
+
 const inputId = document.getElementById("input_id");
 const inputPwd = document.getElementById("input_pwd");
 const loginButton = document.getElementById("login_btn");
@@ -16,8 +27,17 @@ const validate = () => {
   } else {
     loginButton.disabled = true;
     loginButton.style.opacity = 0.3;
+    loginButton.style.cursor = "default";
   }
 };
 
-inputId.addEventListener("keydown", validate);
-inputPwd.addEventListener("keydown", validate);
+const isValidId 
+
+const isValidPw
+
+const init = () => {
+  inputId.addEventListener("keyup", validate);
+  inputPwd.addEventListener("keyup", validate);
+};
+
+init();
--- students/40th/sunyoung/js/main.js
+++ students/40th/sunyoung/js/main.js
@@ -1,50 +1,52 @@
-{
-  /* <div class="feed_comment_input">
-<input id="input_comment" type="text" placeholder="댓글 달기..." />
-<div id="button_comment">게시</div>
-</div> */
-}
-
-// keydown Enter or 게시 btn click
-
-{
-  /* <div class="comment">
-              <div class="comment_upper">
-                <div class="comment_content">
-                  <span>happy_sung</span>
-                  <span>~~~~~~~~</span>
-                </div>
-                <div class="comment_like_icon">
-                  <img
-                    alt="하트"
-                    src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-                  />
-                </div>
-              </div>
-              <div class="comment_bottom">42분전</div>
-            </div> */
-}
+// 입력한 내용이 없을 때 alert창
+// makeDeleteEvent - 댓글삭제 / 버튼을 눌렀을때 newcomment 를 없애버리는 동작.
+// 템플릿 literal 방식 : 백틱을 활용 ---> innerHTML
 
 const input_element = document.getElementById("input_comment");
 const btn_element = document.getElementById("button_comment");
 const comment_element = document.querySelector(".feed_comment");
-console.log(comment_element);
-console.log(btn_element);
 
-input_element.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    const p = document.createElement("div");
-    p.setAttribute("class", "comment");
+function submit_comment(value) {
+  const newComment = document.createElement("div");
+  const commentUser = "sunyoung";
+  newComment.innerHTML = `<div class="comment">
+            <div class="comment_upper">
+              <div class="comment_content">
+                <span id="userNickname" class="user_id">${commentUser}</span>
+                <span id="feed_comment" class="text"
+                  >${value}</span>
+              </div>
+              <div class="comment_like_icon">
+                <img
+                  alt="하트"
+                  src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                />
+                <img id="deleteBtn" alt="삭제" src="img/trash.png" />
+              </div>
+            </div>
+            <div class="comment_bottom">42분전</div>
+          </div>`;
 
-    p.innerText = input_element.value;
-    comment_element.appendChild(p);
+  comment_element.appendChild(newComment);
+  input_element.value = "";
+
+  //댓글 삭제 기능 구현
+
+  const delete_button = newComment.querySelector("#deleteBtn");
+
+  delete_button.addEventListener("click", function () {
+    comment_element.removeChild(newComment);
+  });
+}
+
+input_element.addEventListener("keyup", function (e) {
+  if (e.key === "Enter" && input_element.value.length > 0) {
+    return submit_comment(input_element.value);
   }
 });
 
-btn_element.addEventListener("click", function (e) {
-  const p = document.createElement("div");
-  p.setAttribute("class", "comment");
-
-  p.innerText = input_element.value;
-  comment_element.appendChild(p);
+btn_element.addEventListener("click", function () {
+  if (input_element.value.length > 0) {
+    return submit_comment(input_element.value);
+  }
 });
