22050b9f69e164cdcc3d37fc2d3b6ade82a4064e
문승훈 <moonseunghun@munseunghun-ui-MacBookPro.local>
2022-11-22 11:38:37 +0900

    위스타그램 구현

---
+++ students/40th/seuunghun/login.js
@@ -0,0 +1,30 @@
+//이벤트 위팀(+이벤트 버블링, 이벤트 캡쳐링)
+
+// const loginbox=document.getElementsByName("loginForm")("0")
+
+//function hemdleBtn[
+
+//]
+
+//function checkId(value) {}
+//return value.includes("@") && value.length >5;
+
+// //function handleInput() {
+//   const idValue = document.getElementById("id")
+// }
+
+//function checkPw(value) {} 
+
+
+// if (isBtnvalue) {
+//   loginBtn.disabled = false;
+//   loginBtn.style.opacity = 1;
+//   lpginBtn.style.cursor = "pointer
+
+//entry point
+// const init = () =>{
+//   loginform.addEvantlistener("input" , handleInput )
+//   loginform.addEvantlistener("keyup" , handleInput)
+//   loginform.addEvantlistener()
+
+// }
---
+++ students/40th/seuunghun/main.js
@@ -0,0 +1,7 @@
+// function  checkInput() {
+//   if(Input. value. length) ===
+
+//function makeDeleteEvent{new}
+
+//절차적 프로그래밍 방식
+
