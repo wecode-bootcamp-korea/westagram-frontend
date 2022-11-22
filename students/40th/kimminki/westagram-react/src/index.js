f181a1ceb6c59c37a35240155d05a125e941a230
minki3 <86992006+minki3@users.noreply.github.com>
2022-11-22 13:17:26 +0900

    westagram

--- students/40th/kimminki/westagram-react/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Login />);
