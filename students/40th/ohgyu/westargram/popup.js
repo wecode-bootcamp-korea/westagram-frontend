dd52b77834e54c34f4e3129f1dd8432cad59abf4
= <kog6148@gmail.com>
2022-11-27 21:23:27 +0900

    Add : comment

--- students/40th/ohgyu/westargram/popup.js
+++ students/40th/ohgyu/westargram/popup.js
@@ -4,3 +4,15 @@ const popupSection = document.getElementById("popupSection");
 popupBtn.addEventListener("click", function () {
   popupSection.classList.toggle("appear");
 });
+/*
+function windowRemove() {
+  if (popupSection.classList.value === "appear") {
+    console.log("appear");
+    //popupSection.classList.remove("appear");
+  }
+}
+
+window.addEventListener("click", function () {
+  windowRemove();
+});
+*/
--- students/40th/ohgyu/westargram/search.js
+++ students/40th/ohgyu/westargram/search.js
@@ -2,57 +2,31 @@ const navSearch = document.getElementById("navSearch");
 let searchSection = document.getElementById("searchSection");
 
 navSearch.addEventListener("input", function () {
-  test4();
+  searchFunc();
 });
 
-function test3() {
+function searchFunc() {
+  //검색창 초기화
   const searchSectionUl = searchSection.querySelector("ul");
   const searchSectionLi = searchSectionUl.children;
   let search1 = [...searchSectionLi];
   if (searchSectionLi) {
-    // console.log("ul있음");
-    // console.log(searchSectionLi);
-    // console.log(search1);
     search1.forEach((element) => {
       element.remove();
     });
   }
-  user.forEach((element) => {
-    let searchInput = navSearch.value;
-    const userId = element.id;
-    if (searchInput.length > 0 && userId.includes(`${searchInput}`)) {
-      // console.log(searchInput);
-      // console.log(element.id);
-      searchSectionUl.innerHTML += `<li>${element.id} ${element.name}</li>`;
-    }
-  });
-}
 
-function test4() {
-  const searchSectionUl = searchSection.querySelector("ul");
-  const searchSectionLi = searchSectionUl.children;
-  let search1 = [...searchSectionLi];
-  if (searchSectionLi) {
-    // console.log("ul있음");
-    // console.log(searchSectionLi);
-    // console.log(search1);
-    search1.forEach((element) => {
-      element.remove();
-    });
-  }
+  //검색창 요소 추출
   user.forEach((element) => {
     let searchInput = navSearch.value;
     const userId = element.id;
     const userName = element.name;
 
-    if (searchInput.length > 0 && userId.includes(`${searchInput}`)) {
-      // console.log(searchInput);
-      // console.log(element.id);
-      searchSectionUl.innerHTML += `<li>${element.id} ${element.name}</li>`;
-    }
-
-    if (searchInput.length > 0 && userName.includes(`${searchInput}`)) {
-      searchSectionUl.innerHTML += `<li>${element.id} ${element.name}</li>`;
+    if (
+      (searchInput.length > 0 && userId.includes(`${searchInput}`)) ||
+      (searchInput.length > 0 && userName.includes(`${searchInput}`))
+    ) {
+      searchSectionUl.innerHTML += `<li>${element.profile} ${element.id} ${element.name}</li>`;
     }
   });
 }
