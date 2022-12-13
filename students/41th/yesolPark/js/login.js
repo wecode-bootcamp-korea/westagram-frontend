06a6a97223343ff78b64f0b9c559755046447642
Hailey_ys <whassup_man@naver.com>
2022-12-13 17:21:06 +0900

    Add:login.JS
    - 로그인 Id,Password 입력시, 버튼 활성화
    - 버튼 클릭시, alert 발생

--- students/41th/yesolPark/js/login.js
+++ students/41th/yesolPark/js/login.js
@@ -1,4 +1,19 @@
-const $ = () => document.getElementById('');
-$.addEventListener('click');
+const $ = (selector) => document.querySelector(selector);
 
-function LoginButton() {}
+function App() {
+  $('#loginInput').addEventListener('keyup', (e) => {
+    if ($('#loginId').value !== '' && $('#loginPassword').value !== '') {
+      $('#loginBtn').style.backgroundColor = '#0095F6';
+      console.log('hi');
+    }
+  });
+
+  $('#loginBtn').addEventListener('click', (e) => {
+    if ($('#loginId').value !== '' && $('#loginPassword').value !== '') {
+      alert('로그인했습니다.');
+      console.log('hi');
+    }
+  });
+}
+
+App();
