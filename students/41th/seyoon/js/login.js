e4f4ed6d4b35013435c34a58e8fcabe2d92c69d5
Seyoon <seyoonleee@gmail.com>
2022-12-14 23:04:09 +0900

    Add: 로그인 버튼 활성화 기능 추가

---
+++ students/41th/seyoon/js/login.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector(".loginForm");
+console.log(loginForm);
+const idInput = document.querySelector(".user-id");
+const pwdInput = document.querySelector(".user-pw");
+const loginBtn = document.querySelector(".login-btn");
+
+loginForm.addEventListener("keyup", () => {
+  if (idInput.value && pwdInput.value) loginBtn.classList.add("btn-blue");
+  else loginBtn.classList.remove("btn-blue");
+});
