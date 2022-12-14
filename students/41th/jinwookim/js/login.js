58d759e1bd94c3a07e7e104418678a5e1116e71e
wecaners <wecaners@naver.com>
2022-12-14 23:42:44 +0900

    ADD: 로그인 페이지 레이아웃 구현 - 아이디 패스워드 입력시 버튼 활성화 구현 - 버튼 클릭시 메인페이지 이동 기능 구현 - 메인페이지 레이아웃 구현

---
+++ students/41th/jinwookim/js/login.js
@@ -0,0 +1,20 @@
+const target = document.getElementsByClassName('loginbtn')[0];
+const inputEmail = document.getElementsByClassName('email')[0];
+const inputPassword = document.getElementsByClassName('password')[0];
+// getElementsByClassName은 Elements 즉 복수이기때문에 하나만 가져오려면
+// 인덱스를 뒤에 명시 해 주여야 한다.
+
+inputEmail.addEventListener('keyup', activeEvent);
+inputPassword.addEventListener('keyup', activeEvent);
+
+function activeEvent() {
+    if(!(inputEmail.value && inputPassword.value)) {
+        target.disabled = true;
+        target.classList.remove('buttonDisabled');
+
+    } else {
+        target.disabled = false;
+        target.classList.add('buttonDisabled');
+
+    }
+}
