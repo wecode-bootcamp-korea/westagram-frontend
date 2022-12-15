16d320dbc175c3672255f12621447eb08b29e891
조윤환 <114235846+jyh2610@users.noreply.github.com>
2022-12-15 17:44:46 +0900

    댓글성공

--- students/41th/41-choyunhwan/js/main.js
+++ students/41th/41-choyunhwan/js/main.js
@@ -1,11 +1,22 @@
 const commentbtn = document.querySelector("#commentInput");
 const INPUT = document.querySelector(".commentinput");
 const commentWin = document.querySelector(".commentwindow");
-const createP = document.createElement("p");
+const createUl = document.querySelector(".commentList");
 
-commentbtn.addEventListener("click", function () {
-  commentWin.appendChild(createP);
-  createP.innerText = INPUT.value;
+function commentpush() {
+  const createli = document.createElement("li");
+
+  createUl.appendChild(createli);
+  createli.innerText = INPUT.value;
   INPUT.value = "";
+}
+
+commentbtn.addEventListener("click", commentpush);
+
+INPUT.addEventListener("keyup", (event) => {
+  if (event.keyCode == 13) {
+    commentpush();
+  }
 });
+
 // 댓글 리셋하고 다시 추가해야됨
