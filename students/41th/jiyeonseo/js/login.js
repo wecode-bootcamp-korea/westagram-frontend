3851b67f24ca57308c078954946779ad471bf244
Mia <tjwlfif@gmail.com>
2022-12-15 11:24:08 +0900

    check vaild

--- students/41th/jiyeonseo/js/login.js
+++ students/41th/jiyeonseo/js/login.js
@@ -2,11 +2,27 @@ const idInput = document.getElementById("idInput");
 const pwInput = document.getElementById("pwInput");
 const loginBtn = document.querySelector(".loginBtn");
 
-function onKeyUp() {
-  idInput.value !== "" && pwInput.value !== ""
+let idCheck = false;
+let pwCheck = false;
+
+function checkId() {
+  idInput.value !== "" && idInput.value.includes("@")
+    ? (idCheck = true)
+    : (idCheck = false);
+  activeButton();
+}
+
+function checkPw() {
+  pwInput.value.length >= 5 ? (pwCheck = true) : (pwCheck = false);
+  console.log(pwCheck);
+  activeButton();
+}
+
+function activeButton() {
+  idCheck === true && pwCheck === true
     ? (loginBtn.style.backgroundColor = "#033b71")
     : (loginBtn.style.backgroundColor = "#c5e1fb");
 }
 
-idInput.addEventListener("keyup", onKeyUp);
-pwInput.addEventListener("keyup", onKeyUp);
+idInput.addEventListener("keyup", checkId);
+pwInput.addEventListener("keyup", checkPw);
