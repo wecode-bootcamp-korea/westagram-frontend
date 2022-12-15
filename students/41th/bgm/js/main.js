114ddceff71116b3b89dd3485c543258ee643a13
baegyungmin <bjy6439@naver.com>
2022-12-15 17:12:46 +0900

    MODIFY : "javascript compite"

--- students/41th/bgm/js/main.js
+++ students/41th/bgm/js/main.js
@@ -1,1 +1,33 @@
-// 복붙
+const comentInput = document.querySelector(".comentInput");
+const comentBtn = document.querySelector(".comentBtn");
+const comentList = document.querySelector(".comentsList");
+
+function list() {
+  const li = document.createElement("li");
+  const spanName = document.createElement("span");
+  const spanText = document.createElement("span");
+  const img = document.createElement("img");
+
+  spanText.innerText = comentInput.value;
+  spanName.innerText = "junhn";
+  spanName.setAttribute("style", "font-weight: bold");
+  img.setAttribute("src", "./js/img/heart.png");
+
+  li.appendChild(spanName);
+  li.appendChild(spanText);
+  li.appendChild(img);
+
+  comentList.appendChild(li);
+
+  comentInput.value = "";
+}
+
+comentBtn.addEventListener("click", function () {
+  list();
+});
+
+comentInput.addEventListener("keypress", function (e) {
+  if (e.keycode === 13) {
+    list();
+  }
+});
