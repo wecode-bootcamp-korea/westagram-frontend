feb9ca4ffaba439593b2afadfd448c0d57c4977d
gimcheolho <sm708311@gmail.com>
2022-12-16 13:07:39 +0900

    Add : westagram 과제추 - 1. 로그인 화면 - 2. 메인 화면

---
+++ students/41th/cheolho/westrgram/js/login.js
@@ -0,0 +1,25 @@
+const inputId = document.querySelector("#id");
+const inputPassword = document.querySelector("#password");
+const button = document.querySelector(".btn");
+
+function loginBtn() {
+    let idValue = inputId.value;
+    let passwordValue = inputPassword.value;
+
+    if(idValue.length > 0 && passwordValue.length > 0) {
+        button.disabled = false;
+        button.style.cursor = "pointer";
+        button.style.backgroundColor = "#1c7ed6";
+    }else {
+        button.disabled = true;
+        button.style.cursor = "default";
+        button.style.backgroundColor = "#bfdffd";
+    }
+};
+
+inputId.addEventListener('keyup', loginBtn);
+inputPassword.addEventListener('keyup', loginBtn);
+
+
+
+
---
+++ students/41th/cheolho/westrgram/js/main.js
@@ -0,0 +1,56 @@
+const commentForm = document.querySelector('.comments_form');
+const comments = document.querySelector("#comment_input");
+const submitButton = document.querySelector(".btn span");
+
+function commentUpdate() {
+        const ulbox = document.querySelector(".comments_info.comment_list_ul")
+        const liBox =  document.createElement("li");
+        const pBox =  document.createElement("div");
+        const userBox = document.createElement("span");
+        const commentCtnBox = document.createElement("span");
+        let user ="ID";
+
+        userBox.setAttribute("class", "user_id");
+        commentCtnBox.setAttribute("class", "comment_contents");
+        pBox.setAttribute("class", "comments_tit");
+       
+        userBox.innerHTML = user;
+        commentCtnBox.innerHTML = comments.value;
+       
+        pBox.appendChild(userBox);
+        pBox.appendChild(commentCtnBox);
+        liBox.appendChild(pBox);
+        ulbox.appendChild(liBox);
+        comments.value = "";
+};
+
+
+// keydown시 page stop
+commentForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+});
+
+// 게시버튼 클릭 시 발생하는 함수
+submitButton.addEventListener("click",commentUpdate);
+
+// 게시글 작성 후 엔터를 쳤을 때 발생하는 함수
+comments.addEventListener("keydown", function(e) {
+    if(e.keyCode === 13) {
+        commentUpdate();
+    }
+});
+
+//댓글 버튼 disabled
+comments.addEventListener("keyup" , function(e) {
+    let commentValue = comments.value;
+
+    if(commentValue.length > 0) {
+        submitButton.disabled = false;
+        submitButton.style.cursor = "pointer";
+        submitButton.style.color = "#1c7ed6";
+    } else {
+        submitButton.disabled = true;
+        submitButton.style.cursor = "default";
+        submitButton.style.color = "#bfdffd";
+    }
+});
