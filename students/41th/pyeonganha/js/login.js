2ecf9e7d8a80930eb5e214f1a8bf808f1530a2f1
Ha-Pyeongan <96smilepeace@gmail.com>
2022-12-16 13:21:39 +0900

    2nd file

--- students/41th/pyeonganha/js/login.js
+++ students/41th/pyeonganha/js/login.js
@@ -7,8 +7,11 @@ pswText.addEventListener("keyup", switchOn);
 
 function switchOn() {
   if (idText.value && pswText.value) {
+    // btn.classList.add("loginbtncolorchange");
     btn.disabled = false;
-  } else btn.disabled = true;
+  }
+  // btn.classList.remove("loginbtncolorchange");
+  else btn.disabled = true;
 }
 
 btn.addEventListener("click", loginSuccess);
@@ -16,5 +19,9 @@ btn.addEventListener("click", loginSuccess);
 function loginSuccess() {
   if (idText.value == "wecode" && pswText.value == 1234) {
     alert("로그인 성공!");
+    location.href =
+      "http://127.0.0.1:5500/westagram-frontend/students/41th/pyeonganha/main.html";
+  } else {
+    alert("로그인 실패!");
   }
 }
--- students/41th/pyeonganha/js/main.js
+++ students/41th/pyeonganha/js/main.js
@@ -1,1 +1,41 @@
-const commentBox = document.querySelector("#commentjs");
+const inputTextnAndBtn = () => {
+  const commentInput = document.getElementById("commentjs");
+  const commentBtn = document.getElementById("btn");
+
+  const addNewComment = () => {
+    const addUl = document.getElementsByClassName("addcommentul")[0];
+    const addLi = document.createElement("li");
+
+    addLi.innerHTML = `<div class="pincomment">
+        <span>WecodeID : </span>
+        <span>${commentInput.value}</span><button id="delButton"><i class="fa-regular fa-circle-xmark"></i></button><button id="heartOnOff"><i class="fa-regular fa-heart"></i></button>
+      </div>`;
+
+    addUl.appendChild(addLi);
+    commentInput.value = "";
+
+    const btnDel = document.getElementById("delButton");
+    btnDel.addEventListener("click", () => {
+      addUl.removeChild(addLi);
+    });
+
+    const heartbtn = document.getElementById("heartOnOff");
+    heartbtn.addEventListener("click", (e) => {
+      if (e) {
+        console.log(e);
+        heartbtn.innerHTML = `<i class="fa-solid fa-heart"></i>`;
+        heartbtn.style.color = "red";
+      }
+    });
+  };
+
+  commentBtn.addEventListener("click", () => {
+    if (commentInput.value) {
+      addNewComment();
+    } else {
+      alert("댓글을 달아주세요.");
+    }
+  });
+};
+
+inputTextnAndBtn();
