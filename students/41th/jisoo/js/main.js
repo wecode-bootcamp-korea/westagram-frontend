77c6e2ac0372221a43b586086462fd5d14087467
윤지수 <songaji@yunjisuui-MacBookAir.local>
2022-12-16 13:11:44 +0900

    feature : 로그인 유효성, 메뉴 드롭박스

--- students/41th/jisoo/js/main.js
+++ students/41th/jisoo/js/main.js
@@ -1,8 +1,6 @@
 const commentInput = document.querySelector(".comment_input");
 const commentSection = document.getElementById("feeds_comment_section");
 const registerBtn = document.querySelector(".comment_btn_register");
-const myProfile = document.querySelector(".link_to_my_profile");
-const myMenuBox = document.getElementById("nav_my_menu_box");
 
 const disabledBtn = () => {
   commentInput.value.length >= 1
@@ -16,10 +14,6 @@ const onClickCommentBtn = () => {
   commentValue.innerHTML = commentInput.value;
   commentSection.appendChild(commentValue);
   commentInput.value = null;
-};
-
-const visualMyMenu = () => {
-  console.log(myMenuBox.style);
 };
 
 commentInput.addEventListener("keyup", disabledBtn);
