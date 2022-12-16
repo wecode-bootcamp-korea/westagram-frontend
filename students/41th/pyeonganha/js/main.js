798b2764ad8142be68afa3abb688d2964c04dd9f
Ha-Pyeongan <96smilepeace@gmail.com>
2022-12-16 17:28:26 +0900

    last files

--- students/41th/pyeonganha/js/main.js
+++ students/41th/pyeonganha/js/main.js
@@ -8,7 +8,7 @@ const inputTextnAndBtn = () => {
 
     addLi.innerHTML = `<div class="pincomment">
         <span>WecodeID : </span>
-        <span>${commentInput.value}</span><button id="delButton"><i class="fa-regular fa-circle-xmark"></i></button><button id="heartOnOff"><i class="fa-regular fa-heart"></i></button>
+        <span>${commentInput.value}</span><button id="delButton"><i class="fa-regular fa-circle-xmark"></i></button><button class="heartOnOff"><i class="fa-solid fa-heart"></i></button>
       </div>`;
 
     addUl.appendChild(addLi);
@@ -19,13 +19,9 @@ const inputTextnAndBtn = () => {
       addUl.removeChild(addLi);
     });
 
-    const heartbtn = document.getElementById("heartOnOff");
-    heartbtn.addEventListener("click", (e) => {
-      if (e) {
-        console.log(e);
-        heartbtn.innerHTML = `<i class="fa-solid fa-heart"></i>`;
-        heartbtn.style.color = "red";
-      }
+    const heartbtn = document.querySelector(".heartOnOff");
+    heartbtn.addEventListener("click", () => {
+      heartbtn.classList.toggle("hearttoggle");
     });
   };
 
@@ -36,6 +32,19 @@ const inputTextnAndBtn = () => {
       alert("댓글을 달아주세요.");
     }
   });
+
+  commentInput.addEventListener("keypress", (e) => {
+    if (commentInput.value.length !== 0 && e.key === "Enter") {
+      addNewComment();
+    }
+  });
 };
 
 inputTextnAndBtn();
+
+const popup = document.getElementsByClassName("headertextline")[0];
+
+popup.addEventListener("click", () => {
+  const popuppage1 = document.getElementsByClassName("searchpopuppage")[0];
+  popuppage1.classList.toggle("popuppage");
+});
