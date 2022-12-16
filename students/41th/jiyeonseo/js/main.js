35e007e64419b3f6d85220afe486bde2008ab5f0
Mia <tjwlfif@gmail.com>
2022-12-16 20:24:31 +0900

    검색창에 입력할 때 아이디 보여주기 구현

--- students/41th/jiyeonseo/js/main.js
+++ students/41th/jiyeonseo/js/main.js
@@ -6,26 +6,45 @@ const deleteBtn = document.querySelector(".delete");
 const commentBtn = document.getElementById("commentBtn");
 const myProfile = document.getElementById("myProfile");
 const myProfileBubble = document.getElementById("myProfileBubble");
+const searchInput = document.getElementById("search");
+const searchBubble = document.getElementById("searchBubble");
+const bubble = document.createElement("div");
+
+const idArray = [
+  "greenMon",
+  "cco_cco",
+  "queoqa",
+  "eeeeeasy",
+  "drink_lover",
+  "huhuhu",
+];
+
+const idObject = {
+  greenMon: "img/1.png",
+  cco_cco: "img/2.png",
+  queoqa: "img/3.png",
+  eeeeeasy: "img/4.png",
+  drink_lover: "img/5.png",
+  huhuhu: "img/6.png",
+};
 
 function addComment(input) {
   const li = document.createElement("li");
   const span_bold = document.createElement("span");
-  const span = document.createElement("span");
-  const img = document.createElement("img");
-  const span_gray = document.createElement("span");
-  const span_delete = document.createElement("span");
-
   span_bold.setAttribute("class", "bold");
-  span.setAttribute("class", "space");
-  img.setAttribute("class", "heart");
-  span_gray.setAttribute("class", "gray");
-  span_delete.setAttribute("class", "delete gray");
-  img.setAttribute("src", "img/heart.png");
-
   span_bold.innerText = "mia_seo";
+  const span = document.createElement("span");
+  span.setAttribute("class", "space");
   span.innerText = input;
-  span_delete.innerText = "삭제";
+  const img = document.createElement("img");
+  img.setAttribute("class", "heart");
+  img.setAttribute("src", "img/heart.png");
+  const span_gray = document.createElement("span");
+  span_gray.setAttribute("class", "gray");
   span_gray.innerText = "90분전";
+  const span_delete = document.createElement("span");
+  span_delete.setAttribute("class", "delete gray");
+  span_delete.innerText = "삭제";
 
   li.appendChild(span_bold);
   li.appendChild(span);
@@ -79,8 +98,37 @@ function onClickProfile() {
   }
 }
 
+function makeSearch(key) {
+  const bubble_inside = document.createElement("div");
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+
+  bubble.setAttribute("class", "bubble");
+  bubble_inside.setAttribute("class", "bubble-inside");
+  img.setAttribute("src", idObject[key]);
+  img.setAttribute("class", "profile-photo active");
+
+  p.innerText = key;
+
+  bubble_inside.appendChild(img);
+  bubble_inside.appendChild(p);
+  bubble.appendChild(bubble_inside);
+  searchBubble.appendChild(bubble);
+  deleteBubble(searchBubble);
+}
+
+function checkArray() {
+  if (searchInput !== "")
+    for (let i = 0; i < idArray.length; i++) {
+      if (idArray[i].startsWith(searchInput.value)) {
+        makeSearch(idArray[i]);
+      }
+    }
+}
+
 comment.addEventListener("submit", onSubmitInput);
 commentInput.addEventListener("keyup", onKeyUpInput);
 heart.addEventListener("click", onClickHeart);
 deleteBtn.addEventListener("click", onClickDelete);
 myProfile.addEventListener("click", onClickProfile);
+searchInput.addEventListener("keyup", checkArray);
