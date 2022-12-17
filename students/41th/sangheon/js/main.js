ba6b7a21bf54f2dd5e52a4123c35322e2134a999
sangheon <sangheon@sangheonui-MacBookAir.local>
2022-12-17 21:24:48 +0900

    Add : 메인 페이지 댓글 입력시 게시버튼 활성화 기능 추가

--- students/41th/sangheon/js/main.js
+++ students/41th/sangheon/js/main.js
@@ -1,21 +1,36 @@
 const listCmt = document.getElementById('listCmt');
 const inpCmt = document.getElementById('inpCmt');
 const btnSubmitCmt = document.getElementById('btnSubmitCmt');
+const btnRemoveCmts = document.querySelectorAll('.btn-remove');
 
 function createComment() {
   const liEl = document.createElement('li');
   liEl.innerHTML = `
-    <span class="cmt-user">canon_mj</span>
-    <span class="cmt-cont">${inpCmt.value}</span>
+    <span class="wrap-cmt">
+      <span class="cmt-user">canon_mj</span>
+      <span class="cmt-cont">${inpCmt.value}</span>
+    </span>
+    <span class="wrap-btn">
+      <button id="btnRemoveCmt" class="btn-remove">
+        <i class="fa fa-thin fa-xmark"></i>
+      </button>
+      <button class="btn-like">
+        <i class="fa fa-regular fa-heart"></i>
+      </button>
+    </span>
   `;
   listCmt.appendChild(liEl);
   inpCmt.value = '';
+  btnSubmitCmt.disabled = true;
 }
 
 btnSubmitCmt.addEventListener('click', createComment);
-inpCmt.addEventListener('keydown', (event) => {
-  console.log(event);
-  if (event.keyCode === 13) {
+inpCmt.addEventListener('keyup', (e) => {
+  e.target.value.length > 0
+    ? (btnSubmitCmt.disabled = false)
+    : (btnSubmitCmt.disabled = true);
+
+  if (e.target.value.length > 0 && e.keyCode === 13) {
     createComment();
   }
 });
