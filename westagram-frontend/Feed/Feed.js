9d7de03cdff38b9c175ee9010e4b0d419a6465db
Jangchan0 <jchy16@gmail.com>
2022-12-17 18:55:55 +0900

    ADD: Enter press

--- westagram-frontend/Feed/Feed.js
+++ westagram-frontend/Feed/Feed.js
@@ -3,6 +3,8 @@ const commentBtn = document.querySelector(".commentBtn");
 const newcomment = document.querySelector(".newComment");
 const user_id = document.getElementById("id_login");
 
+console.log(user_id);
+
 function comment() {
   if (commentMaker.value) {
     commentBtn.style.backgroundColor = "#68adfc";
@@ -16,19 +18,23 @@ function comment() {
 commentMaker.addEventListener("keyup", comment);
 
 function pushComment() {
-  const create_div = document.createElement("div");
-  create_div.innerHTML = `<div class= "Newcomment"> <div class= User_id>
+  if (commentMaker.value) {
+    const create_div = document.createElement("div");
+    create_div.innerHTML = `<div class= "Newcomment"> <div class= User_id>
   <span><b>${user_id}</b></span></div>        ${commentMaker.value} </div>`;
-  document.querySelector(".middle").appendChild(create_div);
+    document.querySelector(".middle").appendChild(create_div);
+    commentMaker.value = "";
+  }
 }
 commentBtn.addEventListener("click", pushComment);
 
 function commentEnter(e) {
-  if (e.KeyCode == 13) {
-    create_div = document.createElement("div");
+  if (e.keyCode == 13 && commentMaker.value) {
+    const create_div = document.createElement("div");
     create_div.innerHTML = `<div class= "Newcomment"> <div class= User_id>
-      <span>${user_id}</span></div>       ${commentMaker.value} </div>`;
+      <span>ChanHwi</span></div>       ${commentMaker.value} </div>`;
     document.querySelector(".middle").appendChild(create_div);
+    commentMaker.value = "";
   }
 }
 commentMaker.addEventListener("keypress", commentEnter);
--- westagram-frontend/login/js/login.js
+++ westagram-frontend/login/js/login.js
@@ -3,9 +3,10 @@ const user_pw = document.querySelector(".passward");
 const loginBtn = document.querySelector(".loginBtn");
 
 function input() {
-  if (user_id.value && user_pw.value) {
+  if (user_id.value.length >= 5 && user_pw.value.length >= 5) {
     loginBtn.disabled = false;
     loginBtn.style.backgroundColor = "#3e94f7";
+    loginBtn.style.cursor = "pointer";
   } else {
     loginBtn.disabled = true;
     loginBtn.style.backgroundColor = "#b2dffc";
@@ -15,6 +16,7 @@ user_pw.addEventListener("keyup", input);
 user_id.addEventListener("keyup", input);
 
 function newPage() {
+  alert(`환영합니다! ${user_id.value}님!`);
   window.location.href = "../Feed/Feed.html";
 }
 
