fe6b8b4e097d0c5104d36c3292540c04e8acea0c
Seyoon <seyoonleee@gmail.com>
2022-12-18 17:20:24 +0900

    Add: Dummy Data 추가, 피드 코멘트 전까지 완료 - 피드 max-height를 활용하여 동적으로 피드 사이즈 결정 - 더미데이터 추가로, js를 통한 피드관련 DOM 요소 추가

---
+++ students/41th/seyoon/data/data.js
@@ -0,0 +1,92 @@
+export const feedData = [
+  {
+    id: 1,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 2,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/902.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671229389033-8d301b1906c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Blanditiis ratione culpa voluptatem consequatur aut possimus.",
+  },
+  {
+    id: 3,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 4,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 5,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 6,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 7,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 8,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 9,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+  {
+    id: 10,
+    profileSrc:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
+    name: "Valentin",
+    postSrc:
+      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+  },
+];
--- students/41th/seyoon/js/main.js
+++ students/41th/seyoon/js/main.js
@@ -1,3 +1,19 @@
+// import feedData from "../data/feedData.json" assert { type: "json" };
+
+// console.log("asdf");
+// console.log(feedData);
+
+// const { default: feedData } = await import("./data/feedData.json", {
+//   assert: {
+//     type: "json",
+//   },
+// });
+
+// import { feedData } from "../data/data";
+// console.log(feedData);
+import { feedData } from "../data/data.js";
+console.log(feedData[0].name);
+
 const profileImgSrc =
   "https://lh3.googleusercontent.com/VAdRMJl2EHvlTGlSflGv23ztOVFMu8NLQqi26fK3tKBfFeyXGq3KNjjfIOPhyDf0rem0JiFRkBX1Cro1kovduUnLZuq6gwzGCeA8J_75fqpG9OmN_3G5MEdzRvBzl1XlJG_7JlMrQM80_BA1h5EjSJ-R50a1Mp2n3FfXbo-sc7x_YAOvmn_jdBH8rA0VOxN7dud4GiHJFDapW_WMWXWGZ9xn2X8L2g_BoXuF649lpjiw-rC9O2WlVtIQu_cU_2EkMirnBDEIp9sQDvNnVxJi4yY_xnVVzE5xln2mfcgPmyrn2Q7KvvdTBBdlh9G8Np5MUToWpxNm0PghDWDUJx1ZU45M5Osk7WgFL6NUv9WIQulrI3W3cDlKhA5ogcJPwFGhLU8PF9ZoBAXZDRqQAjyDCzhwmThUKQ0V10gSmQyx4NsQQf115e31qlnxf4bFA1tNMbStnumZrNg0v8VQfAPnfPZEvrOjJTNVHTdwXiisrLaqNrGgouij_xPufVGY-sPv2ER6YTpjoTnDf77VIKzLmYmsHqJizY1SwDt45BGRawhRCPWVrnLrV7bSAJRBulZ37259Zc5_R7_ZbEGtJeBwsO6BBjFD2CFrLgXi02k_5mUKIMq_t3NYdhBhHtC2DJYDXFZpXDRhGwwThUHH07ZTiFpncs5Ut0h6w0OxajadJc37KHUqSNCdPpnOEGJWiFUFKJ92jvLC4wCitpxCCCKoTvHgE5SKKmTOo28A91E3c7hHTFzgtxJvZ17l2ZL6podgmjQ_eoHOrMttSpB_RftmGtDoap9amIEKS9xLNm0nmOkVeZA4bc2GVLMTOsHB1xw9FgpE2vMM3LT0-Kfx9z6lWcOxToLia59CT_wnxCqbwGsZ1a2-7DRf66nlM5K5wKtkHMtRTFkwn86nPMPNURGQq8C2Sk3T-YXUMn0TXaKVFVx_=s2036-no?authuser=0";
 
@@ -40,3 +56,14 @@ const recommended = document.querySelector(".recommended-list");
 // ul li:not(:first-of-type) {
 //   color: red;
 // }
+
+const feedContainer = document.querySelector(".feed-container");
+console.dir(feedContainer);
+
+feedData.forEach((feed) => {
+  console.log("test");
+  const feedWrap = document.createElement("div");
+  document.body.appendChild(feedWrap);
+  feedContainer.innerHTML += `
+  `;
+});
