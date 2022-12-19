f7662655f51325f6cc4b65aa66877f4bad4b0290
Seyoon <seyoonleee@gmail.com>
2022-12-19 23:43:28 +0900

    Add: 댓글 추가 기능

--- students/41th/seyoon/data/data.js
+++ students/41th/seyoon/data/data.js
@@ -3,90 +3,313 @@ export const feedData = [
     id: 1,
     profileSrc:
       "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
+    nickname: "Valentin",
     postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+      "https://images.unsplash.com/photo-1621508654686-809f23efdabc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvcml6b258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    likePeople: [
+      {
+        nickname: "Ronaldo",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1019.jpg",
+      },
+      {
+        nickname: "Kevin",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1012.jpg",
+      },
+      {
+        nickname: "Jun",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/122.jpg",
+      },
+      {
+        nickname: "Hellen",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1030.jpg",
+      },
+      {
+        nickname: "Flavio",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1091.jpg",
+      },
+      {
+        nickname: "Toney",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1038.jpg",
+      },
+    ],
+    text: "",
+    comments: [
+      {
+        nickname: "Sydni",
+        comment: "Cool!!✨",
+      },
+      {
+        nickname: "Dusty",
+        comment: "hi!",
+      },
+      {
+        nickname: "Emma",
+        comment: "😎",
+      },
+      {
+        nickname: "Queenie",
+        comment: "만나서 반갑습니다!!!!!!👍",
+      },
+      {
+        nickname: "Federico",
+        comment: "react하라고!🙏",
+      },
+    ],
   },
   {
     id: 2,
     profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/902.jpg",
-    name: "Valentin",
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/91.jpg",
+    nickname: "Cheyanne",
     postSrc:
-      "https://images.unsplash.com/photo-1671229389033-8d301b1906c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Blanditiis ratione culpa voluptatem consequatur aut possimus.",
+      "https://images.unsplash.com/photo-1530236668220-b9c6c098c9aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHZlcnRpY2FsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    likePeople: [
+      {
+        nickname: "Matilda",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/814.jpg",
+      },
+    ],
+    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+    comments: [
+      {
+        nickname: "Deon",
+        comment: "Iste omnis amet animi in fugiat illum quisquam.",
+      },
+      {
+        nickname: "Duane",
+        comment: "Neque nam consequatur esse ipsa perspiciatis.",
+      },
+      {
+        nickname: "Horacio",
+        comment: "Tenetur possimus eos illum eum aut dolor aliquid aperiam.",
+      },
+      {
+        nickname: "Kelly",
+        comment: "Voluptas ducimus sapiente ut.",
+      },
+      {
+        nickname: "Lue",
+        comment: "Dolores omnis aut est itaque labore accusantium ipsa quo et.",
+      },
+    ],
   },
   {
     id: 3,
     profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/925.jpg",
+    nickname: "Will",
     postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+      "https://images.unsplash.com/photo-1671391079174-692392488026?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    likePeople: [
+      // {
+      //   nickname: "Danial",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/442.jpg",
+      // },
+      // {
+      //   nickname: "Carolyne",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/132.jpg",
+      // },
+      // {
+      //   nickname: "Vincenza",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/515.jpg",
+      // },
+      // {
+      //   nickname: "Abelardo",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/860.jpg",
+      // },
+      // {
+      //   nickname: "Leta",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/287.jpg",
+      // },
+      // {
+      //   nickname: "Tina",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/32.jpg",
+      // },
+    ],
+    text: "Nostrum neque sunt eligendi nam est dignissimos.",
+    comments: [
+      {
+        nickname: "Bailee",
+        comment:
+          "Blanditiis doloribus ut iusto sit voluptatem est et sed laboriosam.",
+      },
+      {
+        nickname: "Rhiannon",
+        comment: "Adipisci facere quidem et quos.",
+      },
+      {
+        nickname: "Christ",
+        comment:
+          "Sint repudiandae minus est dolor fuga consectetur debitis consequatur repellendus.",
+      },
+      {
+        nickname: "Mae",
+        comment: "Perferendis cumque error vel.",
+      },
+      {
+        nickname: "Raphael",
+        comment: "Porro quia modi incidunt nostrum qui.",
+      },
+    ],
   },
   {
     id: 4,
     profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/488.jpg",
+    nickname: "Elvis",
     postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+      "https://images.unsplash.com/photo-1655196601100-8bfb26cf99e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGpzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    likePeople: [
+      {
+        nickname: "Sandra",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/507.jpg",
+      },
+      {
+        nickname: "Madie",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1072.jpg",
+      },
+      // {
+      //   nickname: "Eudora",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/766.jpg",
+      // },
+      // {
+      //   nickname: "Cyrus",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/863.jpg",
+      // },
+      // {
+      //   nickname: "Retha",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/722.jpg",
+      // },
+      // {
+      //   nickname: "Erwin",
+      //   profileSrc:
+      //     "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/275.jpg",
+      // },
+    ],
     text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+    comments: [
+      {
+        nickname: "Constance",
+        comment: "Rerum perspiciatis et non.",
+      },
+      {
+        nickname: "Kellie",
+        comment: "Et ut ut neque non expedita.",
+      },
+      {
+        nickname: "Orlo",
+        comment:
+          "Officiis laboriosam voluptatem quo provident repellat dignissimos.",
+      },
+      {
+        nickname: "Arielle",
+        comment: "Doloremque et adipisci sed quae adipisci ea eum.",
+      },
+      {
+        nickname: "Herman",
+        comment: "Quos explicabo voluptate et aperiam autem rem.",
+      },
+    ],
   },
   {
     id: 5,
     profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1063.jpg",
+    nickname: "Priscilla",
     postSrc:
       "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    likePeople: [
+      {
+        nickname: "Ryder",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/53.jpg",
+      },
+      {
+        nickname: "Isabella",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/971.jpg",
+      },
+      {
+        nickname: "Alessia",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/712.jpg",
+      },
+      {
+        nickname: "Candelario",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/72.jpg",
+      },
+      {
+        nickname: "Cary",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/523.jpg",
+      },
+      {
+        nickname: "Thurman",
+        profileSrc:
+          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/221.jpg",
+      },
+    ],
     text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
-  },
-  {
-    id: 6,
-    profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
-    postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
-  },
-  {
-    id: 7,
-    profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
-    postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
-  },
-  {
-    id: 8,
-    profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
-    postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
-  },
-  {
-    id: 9,
-    profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
-    postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
-  },
-  {
-    id: 10,
-    profileSrc:
-      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/884.jpg",
-    name: "Valentin",
-    postSrc:
-      "https://images.unsplash.com/photo-1671253412347-f5c9437d067e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
-    text: "Temporibus et tenetur ut tenetur quam corrupti possimus.",
+    comments: [
+      {
+        nickname: "Kasey",
+        comment: "Nihil quos quae facere nisi.",
+      },
+      {
+        nickname: "Marlon",
+        comment: "Quis accusantium dolores sint.",
+      },
+      {
+        nickname: "Kristy",
+        comment: "Pariatur in illum iure in aperiam.",
+      },
+      {
+        nickname: "Giovanny",
+        comment: "Ut exercitationem vitae rerum est quisquam.",
+      },
+      // {
+      //   nickname: "Jed",
+      //   comment: "Sapiente deleniti autem sit omnis beatae voluptatem odio ut.",
+      // },
+    ],
   },
 ];
+
+{
+  /* <div class="like-wrap">
+            <div class="like-poeple-img-box">
+              <img
+                class="new-story-img"
+                src="${feed.likePeople[feed.likePeople.length - 1].profileSrc}"
+              />
+            </div>
+            <strong>${
+              feed.likePeople[feed.likePeople.length - 1].nickname
+            }</strong>
+            <span>님&nbsp</span>
+            <strong>${
+              feed.likePeople.length >= 2
+                ? " 외 " + feed.likePeople.length + "명"
+                : ""
+            }</strong>이 좋아합니다.
+          </div> */
+}
--- students/41th/seyoon/js/main.js
+++ students/41th/seyoon/js/main.js
@@ -12,7 +12,7 @@
 // import { feedData } from "../data/data";
 // console.log(feedData);
 import { feedData } from "../data/data.js";
-console.log(feedData[0].name);
+// console.log(feedData[0].name);
 
 const profileImgSrc =
   "https://lh3.googleusercontent.com/VAdRMJl2EHvlTGlSflGv23ztOVFMu8NLQqi26fK3tKBfFeyXGq3KNjjfIOPhyDf0rem0JiFRkBX1Cro1kovduUnLZuq6gwzGCeA8J_75fqpG9OmN_3G5MEdzRvBzl1XlJG_7JlMrQM80_BA1h5EjSJ-R50a1Mp2n3FfXbo-sc7x_YAOvmn_jdBH8rA0VOxN7dud4GiHJFDapW_WMWXWGZ9xn2X8L2g_BoXuF649lpjiw-rC9O2WlVtIQu_cU_2EkMirnBDEIp9sQDvNnVxJi4yY_xnVVzE5xln2mfcgPmyrn2Q7KvvdTBBdlh9G8Np5MUToWpxNm0PghDWDUJx1ZU45M5Osk7WgFL6NUv9WIQulrI3W3cDlKhA5ogcJPwFGhLU8PF9ZoBAXZDRqQAjyDCzhwmThUKQ0V10gSmQyx4NsQQf115e31qlnxf4bFA1tNMbStnumZrNg0v8VQfAPnfPZEvrOjJTNVHTdwXiisrLaqNrGgouij_xPufVGY-sPv2ER6YTpjoTnDf77VIKzLmYmsHqJizY1SwDt45BGRawhRCPWVrnLrV7bSAJRBulZ37259Zc5_R7_ZbEGtJeBwsO6BBjFD2CFrLgXi02k_5mUKIMq_t3NYdhBhHtC2DJYDXFZpXDRhGwwThUHH07ZTiFpncs5Ut0h6w0OxajadJc37KHUqSNCdPpnOEGJWiFUFKJ92jvLC4wCitpxCCCKoTvHgE5SKKmTOo28A91E3c7hHTFzgtxJvZ17l2ZL6podgmjQ_eoHOrMttSpB_RftmGtDoap9amIEKS9xLNm0nmOkVeZA4bc2GVLMTOsHB1xw9FgpE2vMM3LT0-Kfx9z6lWcOxToLia59CT_wnxCqbwGsZ1a2-7DRf66nlM5K5wKtkHMtRTFkwn86nPMPNURGQq8C2Sk3T-YXUMn0TXaKVFVx_=s2036-no?authuser=0";
@@ -58,12 +58,183 @@ const recommended = document.querySelector(".recommended-list");
 // }
 
 const feedContainer = document.querySelector(".feed-container");
-console.dir(feedContainer);
+// console.dir(feedContainer);
 
-feedData.forEach((feed) => {
-  console.log("test");
-  const feedWrap = document.createElement("div");
-  document.body.appendChild(feedWrap);
+feedData.forEach((feed, idx) => {
+  // console.log("test");
+  // const feedWrap = document.createElement("div");
+  // document.body.appendChild(feedWrap);
+  console.log(feed.id);
+  console.log(typeof feed.id);
+
   feedContainer.innerHTML += `
+    <article id=${feed.id}>
+      <div class="feed-header">
+        <div class="left">
+          <div class="new-recommended-img-box">
+            <img
+              class="new-story-img"
+              src="${feed.profileSrc}"
+            />
+          </div>
+          <div class="font-weight-600">${feed.nickname}</div>
+        </div>
+        <div class="right">
+          <i class="fa-solid fa-ellipsis"></i>
+        </div>
+      </div>
+      <div class="feed-img">
+        <img
+          src="${feed.postSrc}"
+          alt=""
+        />
+      </div>
+      <div class="section-wrap font-size-14">
+        <section class="feed-body">
+          <div class="left">
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-regular fa-comment"></i>
+            <i class="fa-regular fa-share-from-square"></i>
+          </div>
+          <div class="right">
+            <i class="fa-regular fa-bookmark"></i>
+          </div>
+        </section>
+
+        ${
+          feed.likePeople.length >= 1
+            ? `<div class="like-wrap">
+            <div class="like-poeple-img-box">
+              <img
+                class="new-story-img"
+                src="${feed.likePeople[feed.likePeople.length - 1].profileSrc}"
+              />
+            </div>
+            <strong>${
+              feed.likePeople[feed.likePeople.length - 1].nickname
+            }</strong>
+            <span>님&nbsp</span>
+            <strong>${
+              feed.likePeople.length >= 2
+                ? " 외 " + feed.likePeople.length + "명"
+                : ""
+            }</strong>이 좋아합니다.
+            </div>`
+            : ""
+        }
+
+        ${feed.text ? `<div class="text">${feed.text}</div>` : ""}
+
+        <div class="comments">
+        </div>
+
+        <form onsubmit="return false;" action="" class="new-comment">
+          <i class="fa-regular fa-face-smile"></i>
+          <input type="text" placeholder="댓글 달기..." />
+          <button class="posting font-blue font-weight-600 postingDeactivity">게시</button>
+        </form>
+      </div>
+    </article>
   `;
+
+  const comments = document.getElementsByClassName("comments");
+  // console.log(comments);
+
+  if (feed.comments) {
+    feed.comments.forEach((obj) => {
+      comments[idx].innerHTML += `
+        <div class='comment'>
+          <strong>${obj.nickname}</strong>
+          <span>${obj.comment}</span>
+        </div>
+      `;
+    });
+  }
 });
+
+const formInput = document.querySelectorAll("form input");
+const formBtn = document.querySelectorAll("form button");
+// console.log(formInput);
+// console.log(formBtn);
+
+formInput.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    // console.log(`${typeof e.key} ${e.key}`);
+    // console.log(e.path[2].children[3]);
+    // console.log(e.path[1][1]);
+
+    if (input.value) {
+      // console.log("진입 성공");
+      e.path[1][1].classList.add("postingActivity");
+      // console.log("진입 성공2");
+      // console.dir(e.path[1][1]);
+      activateBtn(e.path[1][1]);
+      // console.dir(e.path[1][1]);
+    } else {
+      e.path[1][1].classList.remove("postingActivity");
+      deactivateBtn(e.path[1][1]);
+    }
+
+    // console.log(e.path[1][1].classList);
+
+    if (e.key === "Enter" && input.value) {
+      e.path[2].children[3].innerHTML += `
+        <div class='comment'>
+          <strong>seyoonleee</strong>
+          <span>${input.value}</span>
+        </div>
+      `;
+      input.value = "";
+    }
+    // console.log(e.path.includes("article"));
+    // console.log(e.target.id);
+  });
+});
+
+formBtn.forEach((button) => {
+  // deactivateBtn(button);
+
+  button.addEventListener("click", (e) => {
+    // console.log((button.disabled = true));
+    console.dir(e);
+    // input
+    // console.log(e.path[1][0].value);
+    const comment = e.path[1][0].value;
+    const parent = e.path[2].children[3];
+    // console.log(parent);
+    // console.log(comment);
+
+    if (comment) {
+      parent.innerHTML += `
+      <div class='comment'>
+        <strong>seyoonleee</strong>
+        <span>${comment}</span>
+      </div>
+    `;
+      e.path[1][0].value = "";
+    }
+  });
+});
+
+const activateBtn = (button) => {
+  button.disabled = false;
+  console.log(`버튼 비활성화 : ${button.disabled}`);
+};
+
+const deactivateBtn = (button) => {
+  button.disabled = true;
+  console.log(`버튼 비활성화 : ${button.disabled}`);
+};
+
+const commentArr = document.querySelectorAll(".comment");
+console.log(commentArr);
+
+commentArr.forEach((comment) => {
+  comment.addEventListener("mouseover", (e) => {
+    console.log(e);
+  });
+});
+
+const targetRemove = (target) => {
+  target.remove();
+};
