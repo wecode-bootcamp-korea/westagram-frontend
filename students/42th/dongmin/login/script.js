58d3eb54b8296507eb266d3a08c5add5719548d3
gmgmgun <gmgmgun@gmail.com>
2023-01-11 22:03:21 +0900

    add: initial setting -add img dir -add login dir

---
+++ students/42th/dongmin/login/script.js
@@ -0,0 +1,25 @@
+class LoginBtn {
+  constructor(btn) {
+    this.btn = btn;
+    this.btn.style = btn.style;
+  }
+
+  loginBtnReady(inputId, inputPw) {
+    if (inputId.value !== "" && inputPw.value !== "") {
+      this.btn.style.backgroundColor = "rgb(80, 190, 250)";
+    }
+    if (inputId.value === "" || inputPw.value === "") {
+      this.btn.style.backgroundColor = "rgb(196, 225, 250)";
+    }
+  }
+
+  loginBtnClick(inputId, inputPw) {}
+}
+
+const loginBtn = new LoginBtn(document.getElementsByClassName("login-btn")[0]);
+
+document.addEventListener("keyup", function (e) {
+  const inputId = document.getElementsByClassName("input-id")[0];
+  const inputPw = document.getElementsByClassName("input-pw")[0];
+  loginBtn.loginBtnReady(inputId, inputPw);
+});
