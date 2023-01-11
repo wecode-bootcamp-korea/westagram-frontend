3626e80963165e38d57d0e752837f9f683bf0cd1
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-01-11 17:45:28 +0900

    add: login, main files

---
+++ students/42th/sooKim/script.js
@@ -0,0 +1,26 @@
+// "use strict";
+
+// const inputId = document.getElementById('id');
+const inputId = document.querySelector(".userId")
+const inputPw = document.querySelector(".userPw")
+const loginBtn = document.querySelector('#btn');
+
+
+function colorOnOff (){
+  if(inputId.value.length >= 1 && inputPw.value.length >= 1){
+    loginBtn.style.backgroundColor = '#3E7FDE';
+    loginBtn.disabled = false;
+  }
+  else{
+    loginBtn.style.backgroundColor = '#8CB4F0';
+    loginBtn.disabled = true;
+    console.log('failed')
+  }
+}
+
+inputPw.addEventListener('keyup', colorOnOff);
+
+function goMain(){
+  location.href = "main.html";
+}
+
