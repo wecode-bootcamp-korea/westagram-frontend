e254496064d753642c20502851dd4e418ef9324c
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-01-12 17:29:59 +0900

    ADD: main page

--- students/42th/hein_lee/login.js
+++ students/42th/hein_lee/login.js
@@ -3,5 +3,25 @@
  /*id ,pw에 각각 한 글자 이상써야 버튼이 활성화되도록, 
  원래 연한 파란색이었다가 활성화되면 파란색으로!>*/
 
- 
 
+
+const idInput = document.querySelector('.id');
+const pwInput = document.querySelector('.pw');
+const loginBtn = document.querySelector('.login');
+
+
+function buttonOnOff() {
+    if (idInput.value.length >= 1 && pwInput.value.length >= 1){
+        loginBtn.disabled = false;
+    } else {
+        loginBtn.disabled = true;
+    }
+}
+
+pwInput.addEventListener('keyup', buttonOnOff)
+
+function goMain(){
+   location.href = "main.html";
+  }
+
+loginBtn.addEventListener('click',goMain);
