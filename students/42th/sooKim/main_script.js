0be3516c2ff206da5bb8a5402d348279ccff424e
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-01-12 17:25:12 +0900

    add: comment delete function

--- students/42th/sooKim/main_script.js
+++ students/42th/sooKim/main_script.js
@@ -16,20 +16,58 @@ function commentBtnOnOff(){
 
 commentInput.addEventListener('keyup', commentBtnOnOff);
 
-//// 댓글 추가 //
+// 댓글 추가 //
 
-const commentBox = document.querySelector('.commentBox');
+const commentBox = document.querySelectorAll('.commentBox');
+console.log("commentBox",commentBox)
 
 function addNewComment() {
-  const newCommentLocation = document.querySelector('.commentBox');
+  const newCommentLocation = document.querySelector('.comments');
   const newComment = document.createElement('li');
 
-  newComment.innerHTML = `
-    <div class="newComment">
-      <b>blushcats</b>
-      <span>${commentInput.value}</span>
-    </div>
-  `;
+  // newComment.innerHTML = `
+  //   <div class="commentBox">
+  //     <b>blushcats</b>
+  //     <span class="commentContext">${commentInput.value}</span>
+  //     <img class ="commentDeleteIcon" alt="delete" src="./img/delete.png">
+  //   </div>
+  // `;
+  
+//1. div.commentBox 만들기
+
+const newDiv = document.createElement('div');
+newDiv.className = "commentBox";
+
+//2. b 태그 만들기 - blushcats라는 innerText 넣어주기
+const newB = document.createElement('b');
+newB.innerText = "blushcats"
+
+//3. span 태그 만들기 - class명 지어주기, innerText 넣어주기
+const newSpan = document.createElement('span');
+newSpan.innerText = commentInput.value;
+newSpan.className = "commentContext";
+
+//4. img 태그 만들기 - class, alt, src 경로 넣어주기 
+//4-1. img태그에 addEventListener함수 
+const newImg = document.createElement('img');
+newImg.className = "commentDeleteIcon";
+newImg.alt = "delete";
+newImg.src = "./img/delete.png";
+
+newImg.addEventListener('click', newCommentDelete );
+
+function newCommentDelete() {
+  newDiv.remove()
+}
+
+//5. div.commentBox -> b, span, img 태그 넣기
+
+newDiv.appendChild(newB);
+newDiv.appendChild(newSpan);
+newDiv.appendChild(newImg);
+//6. newComment -> div.commentBox 넣끼
+newComment.appendChild(newDiv);
+  
 
   newCommentLocation.appendChild(newComment);
   commentInput.value = '';
@@ -41,4 +79,16 @@ commentInput.addEventListener('keypress', function(){
   addNewComment();
   }
 });
+
+// 더보기 구현 //
+const moreBtn = document.querySelector("#postMore")
+const postContext = document.querySelector(".postContext")
+
+function postMore () {
+  postContext.style.whiteSpace = "normal";
+  moreBtn.style.marginTop = "auto";
+  moreBtn.remove();
+}
+
+moreBtn.addEventListener('click', postMore)
 
