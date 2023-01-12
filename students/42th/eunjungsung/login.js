1db0c8199547719dc193a0a861dd7bbeb22f022a
eejungee <sulau861@gmail.com>
2023-01-12 17:53:40 +0900

    modify: mission 1,2,3,4,5 완성

--- students/42th/eunjungsung/login.js
+++ students/42th/eunjungsung/login.js
@@ -3,9 +3,24 @@
 const userId = document.getElementById("user-id");
 const userPw = document.getElementById("user-pw");
 const loginBtn = document.getElementById("login-btn");
+const logo = document.getElementById("logo");
+
+// function loginCheck() {
+//   if (userId.value.length > 0 && userPw.value.length > 0) {
+//     loginBtn.style.backgroundColor = "#458eff";
+//     loginBtn.disabled = false;
+//   } else {
+//     loginBtn.style.backgroundColor = "#c4e1fb";
+//     loginBtn.disabled = true;
+//   }
+// }
 
 function loginCheck() {
-  if (userId.value.length > 0 && userPw.value.length > 0) {
+  if (
+    userId.value.length > 0 &&
+    userId.value.indexOf("@") > 0 &&
+    userPw.value.length >= 5
+  ) {
     loginBtn.style.backgroundColor = "#458eff";
     loginBtn.disabled = false;
   } else {
@@ -21,3 +36,4 @@ function moveToMain() {
 userId.addEventListener("keyup", loginCheck);
 userPw.addEventListener("keyup", loginCheck);
 loginBtn.addEventListener("click", moveToMain);
+logo.addEventListener("click", moveToMain);
