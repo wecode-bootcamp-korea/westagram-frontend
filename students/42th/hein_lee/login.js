c5f99fe26f7bcb47e3ef9a5feaa25ba0582fdb4b
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-01-13 20:00:18 +0900

    ADD: Main page HTML,CSS,JavaScript

--- students/42th/hein_lee/login.js
+++ students/42th/hein_lee/login.js
@@ -3,8 +3,6 @@
  /*id ,pw에 각각 한 글자 이상써야 버튼이 활성화되도록, 
  원래 연한 파란색이었다가 활성화되면 파란색으로!>*/
 
-
-
 const idInput = document.querySelector('.id');
 const pwInput = document.querySelector('.pw');
 const loginBtn = document.querySelector('.login');
@@ -19,7 +17,7 @@ function buttonOnOff() {
 }
 
 pwInput.addEventListener('keyup', buttonOnOff)
-
+//키업으로 하게되면 나중에 비밀번호를 붙여넣기 했을때 안될 수도 있음.
 function goMain(){
    location.href = "main.html";
 }
