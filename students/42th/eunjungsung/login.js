0c7630da8f10c69c4b36807b7845bbe2413b798e
eejungee <sulau861@gmail.com>
2023-01-13 13:14:28 +0900

    modify: 로그인 js에  삼항 조건 연산자 적용

--- students/42th/eunjungsung/login.js
+++ students/42th/eunjungsung/login.js
@@ -6,17 +6,24 @@ const loginBtn = document.getElementById("login-btn");
 const logo = document.getElementById("logo");
 
 function loginCheck() {
-  if (
-    userId.value.length > 0 &&
-    userId.value.indexOf("@") > 0 &&
-    userPw.value.length >= 5
-  ) {
-    loginBtn.style.backgroundColor = "#458eff";
-    loginBtn.disabled = false;
-  } else {
-    loginBtn.style.backgroundColor = "#c4e1fb";
-    loginBtn.disabled = true;
-  }
+  // if (
+  //   userId.value.length > 0 &&
+  //   userId.value.indexOf("@") > 0 &&
+  //   userPw.value.length >= 5
+  // ) {
+  //   loginBtn.style.backgroundColor = "#458eff";
+  //   loginBtn.disabled = false;
+  // } else {
+  //   loginBtn.style.backgroundColor = "#c4e1fb";
+  //   loginBtn.disabled = true;
+  // }
+  userId.value.length > 0 &&
+  userId.value.indexOf("@") > 0 &&
+  userPw.value.length >= 5
+    ? ((loginBtn.style.backgroundColor = "#458eff"),
+      (loginBtn.disabled = false))
+    : ((loginBtn.style.backgroundColor = "#c4e1fb"),
+      (loginBtn.disabled = true));
 }
 
 function moveToMain() {
