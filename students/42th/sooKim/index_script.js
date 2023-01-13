e4d0936229b08cbcbee16af2ae2df5b1fee358f1
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-01-13 13:47:00 +0900

    modify : comment heart on/off function

--- students/42th/sooKim/index_script.js
+++ students/42th/sooKim/index_script.js
@@ -6,12 +6,12 @@ const inputPw = document.querySelector(".userPw")
 const loginBtn = document.querySelector('#btn');
 
 
-function colorOnOff (){
-  if(inputId.value.indexOf('@') >= 0 && inputPw.value.length > 4){
+function colorOnOff() {
+  if (inputId.value.indexOf('@') >= 0 && inputPw.value.length > 4) {
     loginBtn.style.backgroundColor = '#3E7FDE';
     loginBtn.disabled = false;
   }
-  else{
+  else {
     loginBtn.style.backgroundColor = '#8CB4F0';
     loginBtn.disabled = true;
   }
@@ -19,7 +19,7 @@ function colorOnOff (){
 
 inputPw.addEventListener('keyup', colorOnOff);
 
-function goMain(){
+function goMain() {
   location.href = "main.html";
 }
 
--- students/42th/sooKim/main_script.js
+++ students/42th/sooKim/main_script.js
@@ -4,8 +4,8 @@ const commentBtn = document.querySelector(".commentWriteButton")
 
 // 댓글 버튼 활성화 //
 
-function commentBtnOnOff(){
-    if(commentInput.value.length !== 0){
+function commentBtnOnOff() {
+  if (commentInput.value.length !== 0) {
     commentBtn.style.color = '#3E7FDE';
     commentBtn.disabled = false;
   } else {
@@ -19,65 +19,91 @@ commentInput.addEventListener('keyup', commentBtnOnOff);
 // 댓글 추가 & 삭제 //
 
 const commentBox = document.querySelectorAll('.commentBox');
-console.log("commentBox",commentBox)
+console.log("commentBox", commentBox)
 
 function addNewComment() {
   const newCommentLocation = document.querySelector('.comments');
   const newComment = document.createElement('li');
 
 
-const newDiv = document.createElement('div');
-newDiv.className = "commentBox";
+  const newDiv = document.createElement('div');
+  newDiv.className = "commentBox";
 
 
-const newB = document.createElement('b');
-newB.innerText = "blushcats"
+  const newB = document.createElement('b');
+  newB.innerText = "blushcats"
 
 
-const newSpan = document.createElement('span');
-newSpan.innerText = commentInput.value;
-newSpan.className = "commentContext";
+  const newSpan = document.createElement('span');
+  newSpan.innerText = commentInput.value;
+  newSpan.className = "commentContext";
 
 
-const newImg = document.createElement('img');
-newImg.className = "commentDeleteIcon";
-newImg.alt = "delete";
-newImg.src = "./img/delete.png";
+  const newImg = document.createElement('img');
+  newImg.className = "commentDeleteIcon";
+  newImg.alt = "delete";
+  newImg.src = "./img/delete.png";
 
-const newHeart = document.createElement('img');
-newHeart.className = "commentHeartIcon";
-newHeart.alt = "heart";
-newHeart.src = "./img/loveOff.png";
+  const newHeart = document.createElement('img');
+  newHeart.className = "commentHeartIcon";
+  newHeart.alt = "heart";
+  newHeart.src = "./img/loveOff.png";
 
 
-//댓글 삭제//
-newImg.addEventListener('click', newCommentDelete );
+  //댓글 삭제//
+  newImg.addEventListener('click', newCommentDelete);
 
-function newCommentDelete() {
-  newDiv.remove()
-}
+  function newCommentDelete() {
+    newDiv.remove()
+  }
 
-function heartOnOff(){
-  newHeart.src = "./img/loveOn.png"
+  //하트 On Off//
+  function heartOnOff() {
+    let heartIcon = document.querySelector(".commentHeartIcon");
+    heartIcon.classList.toggle("fas");
+  }
+
+  newHeart.addEventListener('click', heartOnOff);
+/*
+function heartOnOff() {
+
+    switch (newHeart.src) {
+
+      case "./img/loveOn.png":
+        newHeart.src = "./img/loveOff.png";
+        console.log(1)
+        break;
+      case "./img/loveOff.png":
+        newHeart.src = "./img/loveOn.png";
+        console.log(2)
+        break;
+
+
     }
-  
-newHeart.addEventListener('click', heartOnOff);
+    console.log(3) 
+  }
 
-newDiv.appendChild(newB);
-newDiv.appendChild(newSpan);
-newDiv.appendChild(newHeart);
-newDiv.appendChild(newImg);
-newComment.appendChild(newDiv);
+  newHeart.addEventListener('click', heartOnOff);
+
+  */
   
 
-newCommentLocation.appendChild(newComment);
-commentInput.value = '';
+
+  newDiv.appendChild(newB);
+  newDiv.appendChild(newSpan);
+  newDiv.appendChild(newHeart);
+  newDiv.appendChild(newImg);
+  newComment.appendChild(newDiv);
+
+
+  newCommentLocation.appendChild(newComment);
+  commentInput.value = '';
 }
 
 commentBtn.addEventListener('click', addNewComment)
-commentInput.addEventListener('keypress', function(){
+commentInput.addEventListener('keypress', function () {
   if (window.event.keyCode == 13) {
-  addNewComment();
+    addNewComment();
   }
 });
 
@@ -86,10 +112,11 @@ commentInput.addEventListener('keypress', function(){
 
 
 // 더보기 구현 //
+
 const moreBtn = document.querySelector("#postMore")
 const postContext = document.querySelector(".postContext")
 
-function postMore () {
+function postMore() {
   postContext.style.whiteSpace = "normal";
   moreBtn.style.marginTop = "auto";
   moreBtn.remove();
@@ -106,8 +133,8 @@ const page = document.querySelector('body')
 
 userBtn.addEventListener('click', profileOnOff)
 
-function profileOnOff(){
-  if(userProfile.style.display =="block"){
+function profileOnOff() {
+  if (userProfile.style.display == "block") {
     userProfile.style.display = "none";
   } else {
     userProfile.style.display = "block";
