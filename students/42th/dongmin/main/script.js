3ebb10533b5a5863f5f67bb7af0d1225a698e3d8
gmgmgun <gmgmgun@gmail.com>
2023-01-16 01:02:57 +0900

    add: 댓글 추가 기능 구현

---
+++ students/42th/dongmin/main/script.js
@@ -0,0 +1,32 @@
+const btnSubmit = document.getElementsByClassName("submit")[0];
+const btnHeart = document.getElementsByClassName("heart-button")[0];
+const addList = document.createElement("li");
+
+console.log(btnHeart);
+// function addNewComment(comment) {
+//   addList.classList.add("comment-li");
+// }
+
+//console.log(listComment);
+
+btnSubmit.addEventListener("click", () => {
+  const listComment = document.getElementsByClassName("comment-li")[0];
+  const inputComment = document.getElementsByClassName("input-comment")[0];
+
+  const mkDiv = document.createElement("div");
+  const mkName = document.createElement("span");
+  const mkContent = document.createElement("span");
+
+  mkDiv.className = "name-content-cnt size-for-overflow-hidden";
+  mkName.className = "name";
+  mkContent.className = "content";
+
+  mkName.innerHTML = "gmgmgun";
+  mkContent.innerHTML = inputComment.value;
+
+  listComment.appendChild(mkDiv);
+  mkDiv.appendChild(mkName);
+  mkDiv.appendChild(mkContent);
+
+  inputComment.value = "";
+});
