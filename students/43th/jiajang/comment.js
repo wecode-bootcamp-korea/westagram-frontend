215dfb24ef06d5646a72bcfc8ba8f7b230ef4a81
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-17 02:16:29 +0900

    ADD : 댓글 삭제 기능 구현

--- students/43th/jiajang/comment.js
+++ students/43th/jiajang/comment.js
@@ -16,13 +16,23 @@ function addComment() {
     const newCommentInnerBox = document.createElement("div");
     const addedCommentName = document.createElement("span");
     const addedCommentContent = document.createElement("span");
+    const addedDeleteBtn = document.createElement("button");
 
     addedCommentName.innerHTML = "dazezd_z ";
     addedCommentName.style.fontWeight = "700";
     addedCommentContent.innerHTML = commentInput.value;
+    addedDeleteBtn.innerHTML = "❌";
+    addedDeleteBtn.style.background = "transparent";
+    addedDeleteBtn.style.border = "none";
 
     newCommentBox.appendChild(newCommentInnerBox);
     newCommentInnerBox.append(addedCommentName, addedCommentContent);
+    newCommentInnerBox.appendChild(addedDeleteBtn);
+
+    // 댓글 삭제 기능 (클릭)
+    addedDeleteBtn.addEventListener("click", function () {
+      newCommentBox.removeChild(newCommentInnerBox);
+    });
 
     commentInput.value = null;
   } else commentInput.value = null;
