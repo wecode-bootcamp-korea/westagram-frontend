1285e14eb81268ad3b70fa8a7ec4f1892c7ac525
Dante6327 <jtgk6327@hanmail.net>
2023-02-16 19:06:04 +0900

    MODIFY : 변수명 리팩토링 및 westagram 추가 기능 구현

--- students/43th/westagram/js/main.js
+++ students/43th/westagram/js/main.js
@@ -1,13 +1,24 @@
 const search = document.getElementById("search");
 const reply = document.getElementById("reply");
-const add = document.getElementById("add");
-const replyArea = document.getElementById("reply-area");
+const postReply = document.getElementById("post-reply");
+const replyContainer = document.getElementById("reply-area");
+const howManyReply = document.getElementById("how-many-reply");
 const storyBoard = document.getElementById("story-main");
 const recommendBoard = document.getElementById("recommend-main");
 const viewAllStroy = document.getElementById("view-all-story");
+const viewAllRecommend = document.getElementById("view-all-recommend");
 const id = document.getElementById("id");
+const navAccount = document.getElementById("nav-account");
 
 const userArr = [];
+
+navAccount.addEventListener("click", () => {
+  //account 이모지의 하단, 오른쪽 절대좌표
+  const menuBox = document.querySelector("#menu-box");
+  const menuBoxDisplay = window.getComputedStyle(menuBox).display;
+  if (menuBoxDisplay === "none") menuBox.style.display = "block";
+  else menuBox.style.display = "none";
+});
 
 //검색바 focus시 placeholder 사라지게
 search.addEventListener("focus", () => {
@@ -19,16 +30,41 @@ search.addEventListener("focus", () => {
 search.addEventListener("blur", () => {
   if (search.value === "") {
     search.setAttribute("placeholder", "검색");
-    search.style.backgroundImage = "url('/images/search.png')";
+    search.style.backgroundImage = "url('images/search.png')";
+  }
+});
+
+//검색시 아이디 찾기
+search.addEventListener("input", (e) => {
+  const searchUserArea = document.getElementById("searchUser");
+  searchUserArea.style.display = "none";
+  while (searchUserArea.hasChildNodes()) {
+    searchUserArea.removeChild(searchUserArea.firstChild);
+  }
+
+  if (e.target.value) {
+    const searchKeyword = e.target.value;
+    const newUserArr = [];
+    userArr.map((user) => {
+      user.includes(searchKeyword) && newUserArr.push(user);
+    });
+
+    for (userName of newUserArr) {
+      const userOne = document.createElement("p");
+      userOne.style.padding = "5px";
+      userOne.innerHTML = userName;
+      searchUserArea.append(userOne);
+      searchUserArea.style.display = "block";
+    }
   }
 });
 
 //댓글 입력시 "게시" 문구 활성화
 reply.addEventListener("input", () => {
   if (reply.value) {
-    add.style.display = "inline";
+    postReply.style.display = "inline";
   } else {
-    add.style.display = "none";
+    postReply.style.display = "none";
   }
 });
 
@@ -41,7 +77,7 @@ reply.addEventListener("keydown", (e) => {
 });
 
 //댓글 입력 후 게시 클릭이벤트
-add.addEventListener("click", () => {
+postReply.addEventListener("click", () => {
   if (reply.value) {
     createReply(reply.value);
   }
@@ -51,28 +87,81 @@ viewAllStroy.addEventListener("click", () => {});
 
 //댓글 생성하는 함수
 function createReply(value) {
-  const newReply = document.createElement("p");
-  const deleteReply = document.createElement("span");
+  const replyArea = document.createElement("p"); //댓글 달릴때마다 p태그 생성
+  const replyId = document.createElement("span"); //유저네임
+  const replyContents = document.createElement("span"); //댓글
+  const goodBtn = document.createElement("img"); //좋아요
+  const deleteReply = document.createElement("span"); //삭제
 
-  newReply.innerHTML = `${id.innerHTML} ${value}`;
-  newReply.style.position = "relative";
-  newReply.style.padding = "5px";
-  replyArea.append(newReply);
-  replyArea.style.display = "block";
-  reply.value = "";
+  //유저네임 설정
+  replyId.innerHTML = id.innerHTML;
+  replyId.style.fontWeight = "600";
+  replyId.style.fontSize = "14px";
+  replyArea.append(replyId);
 
-  newReply.append(deleteReply);
+  //댓글 설정
+  replyContents.innerHTML = value;
+  replyContents.style.fontSize = "12px";
+  replyContents.style.paddingLeft = "10px";
+  replyArea.append(replyContents);
 
+  //좋아요 설정
+  goodBtn.setAttribute("src", "images/emptyHeart.png");
+
+  goodBtn.setAttribute("alt", "좋아요");
+  goodBtn.style.width = "12px";
+  goodBtn.addEventListener("mouseover", () => {
+    goodBtn.style.cursor = "pointer";
+  });
+  goodBtn.addEventListener("click", () => {
+    if (goodBtn.getAttribute("src") === "images/emptyHeart.png") {
+      goodBtn.setAttribute("src", "images/fillHeart.png");
+    } else {
+      goodBtn.setAttribute("src", "images/emptyHeart.png");
+    }
+  });
+  goodBtn.style.position = "absolute";
+  goodBtn.style.right = "40px";
+  replyArea.append(goodBtn);
+
+  //삭제 설정
   deleteReply.style.position = "absolute";
-  deleteReply.style.right = "1%";
+  deleteReply.style.right = "1px";
   deleteReply.innerHTML = "삭제";
   deleteReply.style.fontSize = "14px";
-  deleteReply.onclick = (e) => {
-    newReply.remove();
-  };
+  deleteReply.addEventListener("click", () => {
+    replyArea.remove();
+    replyCnt();
+  });
+  deleteReply.addEventListener("mouseover", () => {
+    deleteReply.style.cursor = "pointer";
+  });
+  replyArea.append(deleteReply);
+
+  replyArea.style.position = "relative";
+  replyArea.style.padding = "5px";
+  replyArea.style.display = "flex";
+  replyArea.style.alignItems = "center";
+
+  replyContainer.append(replyArea);
+  replyContainer.style.display = "block";
+  replyCnt();
+
+  reply.value = ""; //댓글 추가 후 textarea 초기화
 }
 
-//랜덤 스토리 및 추천을 위한 class
+function replyCnt() {
+  let cnt = replyContainer.childElementCount;
+  const howManyReplyText = document.createElement("span");
+  howManyReplyText.innerHTML = `댓글 ${cnt}개 모두 보기`;
+  howManyReplyText.style.color = "gray";
+
+  howManyReply.append(howManyReplyText);
+  if (cnt > 0) howManyReply.style.display = "block";
+  else howManyReply.style.display = "none";
+}
+
+//랜덤 유저 생성 클래스
 class User {
   constructor() {
     let randomStr = Math.random().toString(36).substring(2, 12);
@@ -96,12 +185,14 @@ class User {
   }
 }
 
+//스토리 Area에 user 추가
 for (let i = 0; i < 5; i++) {
   const user = new User();
   storyBoard.append(user);
   userArr.push(user.innerText);
 }
 
+//추천 Area에 user 추가
 for (let i = 0; i < 5; i++) {
   const user = new User();
   recommendBoard.append(user);
