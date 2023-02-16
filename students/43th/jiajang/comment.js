a9429c4d9d1be47f9889265776b07cbce34dd179
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-16 19:50:24 +0900

    MODIFY : comment 코드 개선

--- students/43th/jiajang/comment.js
+++ students/43th/jiajang/comment.js
@@ -2,27 +2,19 @@ const commentInput = document.querySelector(".feeds-comment");
 const commentBox = document.querySelector(".feeds-new-comment-box");
 const newCommentBox = document.querySelector(".feeds-new-comment");
 const commentBtn = document.querySelector(".feeds-comment-upload");
-const mainRight = document.querySelector(".main-right");
 
-// 댓글 추가 기능 테스트중
+// 댓글 추가 기능 (엔터, 클릭)
 
 function addComment(e) {
   if (commentInput.value) {
-    e.preventDefault();
     const newCommentInnerBox = document.createElement("div");
-    newCommentBox.appendChild(newCommentInnerBox);
-
-    let addedCommentName = document.createElement("span");
-    addedCommentName.className = "added-comment-name";
+    const addedCommentName = document.createElement("span");
+    const addedCommentContent = document.createElement("span");
     addedCommentName.innerHTML = "dazezd_z ";
     addedCommentName.style.fontWeight = "700";
-
-    let addedCommentContent = document.createElement("span");
-    addedCommentContent.className = "added-comment-content";
     addedCommentContent.innerHTML = commentInput.value;
-
-    newCommentInnerBox.appendChild(addedCommentName);
-    newCommentInnerBox.appendChild(addedCommentContent);
+    newCommentBox.appendChild(newCommentInnerBox);
+    newCommentInnerBox.append(addedCommentName, addedCommentContent);
     commentInput.value = null;
   } else {
     commentInput.value = null;
@@ -30,9 +22,7 @@ function addComment(e) {
 }
 
 commentInput.addEventListener("keyup", function (e) {
-  if (e.key === "Enter") {
-    addComment(e);
-  } else {
-    commentBtn.addEventListener("click", addComment); // ()붙이지 말 것
-  }
+  e.key === "Enter" || e.keyCode === 13
+    ? addComment(e)
+    : commentBtn.addEventListener("click", addComment); // ()붙이지 말 것
 });
--- students/43th/jiajang/login.js
+++ students/43th/jiajang/login.js
@@ -2,8 +2,10 @@ let loginIdInput = document.querySelector(".id-input");
 let loginPwInput = document.querySelector(".pw-input");
 let loginBtn = document.querySelector(".login-btn");
 
+// 키 이벤트 감지 -> 버튼 활성화
+
 function checkInput() {
-  if (loginIdInput.value.length != 0 && loginPwInput.value.length != 0) {
+  if (loginIdInput.value && loginPwInput.value) {
     loginBtn.style.backgroundColor = "#5395E9";
     loginBtn.style.cursor = "pointer";
     loginBtn.disabled = false;
@@ -14,3 +16,19 @@ function checkInput() {
 
 loginIdInput.addEventListener("keyup", checkInput);
 loginPwInput.addEventListener("keyup", checkInput);
+
+// id, pw validation
+
+// 현재 id, pw 입력 시 무조건 로그인 버튼이 활성화 외어 있도록 구현이 되어 있습니다.
+// 실제 로그인 하는 경우를 생각하며 validation(유효성 검사) 기능을 추가해주세요.
+// ex) id >>> '@' 포함 / pw >>> 5글자 이상
+
+// const regex = /@/;
+// regex.test("jangjia01234@gmail.com"); //true
+
+// function checkId() {
+//   const regex = /\@/;
+//   regex.test(loginIdInput.value) ? console.log("성공") : console.log("실패");
+// }
+
+// checkId();
