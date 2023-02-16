888178ba9bcd2db11b2f21e385a18d4944d04678
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-16 15:55:32 +0900

    댓글 추가 기능 최종 구현 완료

--- students/43th/jiajang/comment.js
+++ students/43th/jiajang/comment.js
@@ -6,41 +6,33 @@ const mainRight = document.querySelector(".main-right");
 
 // 댓글 추가 기능 테스트중
 
-function addComment() {
-  if (commentInput.value.length != 0) {
+function addComment(e) {
+  if (commentInput.value) {
+    e.preventDefault();
+    const newCommentInnerBox = document.createElement("div");
+    newCommentBox.appendChild(newCommentInnerBox);
+
     let addedCommentName = document.createElement("span");
     addedCommentName.className = "added-comment-name";
-    addedCommentName.innerHTML = "dazezd_z";
+    addedCommentName.innerHTML = "dazezd_z ";
     addedCommentName.style.fontWeight = "700";
+
     let addedCommentContent = document.createElement("span");
     addedCommentContent.className = "added-comment-content";
     addedCommentContent.innerHTML = commentInput.value;
-    newCommentBox.appendChild(addedCommentName);
-    newCommentBox.appendChild(addedCommentContent);
+
+    newCommentInnerBox.appendChild(addedCommentName);
+    newCommentInnerBox.appendChild(addedCommentContent);
     commentInput.value = null;
-    commentBtn.disabled = false;
   } else {
-    commentBtn.disabled = true;
+    commentInput.value = null;
   }
 }
 
 commentInput.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
-    addComment();
+    addComment(e);
   } else {
     commentBtn.addEventListener("click", addComment); // ()붙이지 말 것
   }
 });
-
-// // function addComment() {
-// //   if (commentInput.value.length != 0) {
-// //     let addedComment = document.createElement("div");
-// //     addedComment.className = "added-comment";
-// //     addedComment.innerHTML = `dazezd_z ${commentInput.value}`;
-// //     commentBox.appendChild(addedComment);
-// //     commentInput.value = null;
-// //     commentBtn.disabled = false;
-// //   } else {
-// //     commentBtn.disabled = true;
-// //   }
-// // }
