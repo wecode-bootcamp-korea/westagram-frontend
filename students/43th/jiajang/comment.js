b682f310165b337c139ea3077157ecb8f16862a9
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-16 10:35:07 +0900

    ADD : 로그인, 메인 페이지 구현

---
+++ students/43th/jiajang/comment.js
@@ -0,0 +1,48 @@
+const commentInput = document.querySelector(".feeds-comment");
+const commentBox = document.querySelector(".feeds-new-comment-box");
+const newCommentBox = document.querySelector(".feeds-new-comment");
+const commentBtn = document.querySelector(".feeds-comment-upload");
+
+const bodyElement = document.querySelector("body");
+const mainRight = document.querySelector(".main-right");
+
+// 댓글 추가 기능 테스트중
+
+function addComment() {
+  if (commentInput.value.length != 0) {
+    let addedCommentName = document.createElement("span");
+    addedCommentName.className = "added-comment-name";
+    addedCommentName.innerHTML = "dazezd_z";
+    addedCommentName.style.fontWeight = "700";
+    let addedCommentContent = document.createElement("span");
+    addedCommentContent.className = "added-comment-content";
+    addedCommentContent.innerHTML = commentInput.value;
+    newCommentBox.appendChild(addedCommentName);
+    newCommentBox.appendChild(addedCommentContent);
+    commentInput.value = null;
+    commentBtn.disabled = false;
+  } else {
+    commentBtn.disabled = true;
+  }
+}
+
+commentInput.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    addComment();
+  } else {
+    commentBtn.addEventListener("click", addComment); // ()붙이지 말 것
+  }
+});
+
+// function addComment() {
+//   if (commentInput.value.length != 0) {
+//     let addedComment = document.createElement("div");
+//     addedComment.className = "added-comment";
+//     addedComment.innerHTML = `dazezd_z ${commentInput.value}`;
+//     commentBox.appendChild(addedComment);
+//     commentInput.value = null;
+//     commentBtn.disabled = false;
+//   } else {
+//     commentBtn.disabled = true;
+//   }
+// }
---
+++ students/43th/jiajang/login.js
@@ -0,0 +1,16 @@
+let loginIdInput = document.querySelector(".id-input");
+let loginPwInput = document.querySelector(".pw-input");
+let loginBtn = document.querySelector(".login-btn");
+
+function checkInput() {
+  if (loginIdInput.value.length != 0 && loginPwInput.value.length != 0) {
+    loginBtn.style.backgroundColor = "#5395E9";
+    loginBtn.style.cursor = "pointer";
+    loginBtn.disabled = false;
+  } else {
+    loginBtn.disabled = true;
+  }
+}
+
+loginIdInput.addEventListener("change", checkInput);
+loginPwInput.addEventListener("change", checkInput);
