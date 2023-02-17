2d6f389d5901db3e2854076c915cefd8eb3527f8
HongTaeHoon <kdn95374888@gmail.com>
2023-02-17 14:47:42 +0900

    ADD: main.html layout 수정

--- students/43th/홍태훈/login.js
+++ students/43th/홍태훈/login.js
@@ -1,6 +1,7 @@
 const id = document.getElementById("id");
 const pw = document.getElementById("password");
 const button = document.querySelector(".button");
+
 document.body.addEventListener("keyup", () => {
   if (id.value.length >= 1 && pw.value.length >= 1) {
     button.style.backgroundColor = "blue";
