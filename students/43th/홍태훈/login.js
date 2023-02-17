617b8a2fde14e99e9fdb5be47fcfc503a4453341
HongTaeHoon <kdn95374888@gmail.com>
2023-02-17 10:23:39 +0900

    ADD:login.js Refactoing

--- students/43th/홍태훈/login.js
+++ students/43th/홍태훈/login.js
@@ -1,11 +1,7 @@
 const id = document.getElementById("id");
 const pw = document.getElementById("password");
 const button = document.querySelector(".button");
-console.log(id.value);
-console.log(button);
 document.body.addEventListener("keyup", () => {
-  console.log(id.value);
-  console.log(pw.value);
   if (id.value.length >= 1 && pw.value.length >= 1) {
     button.style.backgroundColor = "blue";
   } else {
