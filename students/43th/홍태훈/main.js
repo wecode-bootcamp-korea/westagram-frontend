1afcda2c6b884c2b5c8285cbdee584ae3210d082
HongTaeHoon <kdn95374888@gmail.com>
2023-02-17 16:24:22 +0900

    현재진행상황 보고

---
+++ students/43th/홍태훈/main.js
@@ -0,0 +1,11 @@
+const addcomment = document.getElementById("comment");
+console.log(addcomment);
+const upload_comment = document.getElementById("upload-commen");
+console.log(upload_comment);
+const add_comment = document.getElementById("add-comment");
+console.log(add_comment);
+/*addcomment.addEventListener("submit", (e) => {
+  if (e.code === "Enter") {
+  }
+});
+*/
