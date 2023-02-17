2b436ed388c2b174a8a930241fae9214e74cb2c1
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-17 15:41:26 +0900

    ADD: 댓글기능추가완료

--- students/43th/yujeongin/js/main.js
+++ students/43th/yujeongin/js/main.js
@@ -1,1 +1,36 @@
-1
+let commentInput = document.getElementsByClassName("comment_text")[0];
+let submitBtn = document.getElementsByClassName("comment_submit")[0];
+
+function submit() {
+    const box = document.getElementsByClassName("add_user_comment")[0];
+
+    const comments = document.createElement("div");
+    const userName = document.createElement("span");
+    const userComment = document.createElement("span");
+
+    comments.classList.add("add_user_comment");
+    userName.classList.add("user_name");
+    userComment.classList.add("user_comment");
+
+    userName.innerHTML= "Youcode";
+    userComment.innerText = commentInput.value;
+
+    comments.appendChild(userName);
+    comments.appendChild(userComment);
+
+    box.appendChild(comments);
+}
+
+submitBtn.addEventListener("click", () => {
+
+    submit()
+    commentInput.value = "";
+
+});
+
+commentInput.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+        submit()
+        commentInput.value = "";
+    }
+});
