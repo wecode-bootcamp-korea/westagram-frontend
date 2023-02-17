49a5063f659d15a54fe0fb79a57693a2ccfef83d
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-17 15:21:10 +0900

    MODIFY : push 과정에서 충돌한 부분 수정

--- students/43th/jiajang/feed.js
+++ students/43th/jiajang/feed.js
@@ -4,7 +4,7 @@ const hideNavBtn = document.querySelector(".nav-bottom-icon");
 
 // 사이드바 토글
 
-function closeNavBar() {
+hideNavBtn.addEventListener("click", function () {
   navElement.classList.toggle("nav-closed");
   navWrapElement.classList.toggle("nav-closed-no-element");
   let closedNavElement = document.querySelector(".nav-closed");
@@ -12,6 +12,4 @@ function closeNavBar() {
   navElement.appendChild(navOpenBtn);
   navOpenBtn.classList.toggle("nav-open-btn");
   closedNavElement.style.transition = "all 0.5s";
-}
-
-hideNavBtn.addEventListener("click", closeNavBar);
+});
