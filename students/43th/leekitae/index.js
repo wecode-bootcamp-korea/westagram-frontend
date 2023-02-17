3660319a07a11fe481898abe4d1731fcc392f17b
kitae <kitae@kitaeui-MacBookPro.local>
2023-02-17 16:07:14 +0900

    로그인 창 활성화 구현

---
+++ students/43th/leekitae/index.js
@@ -0,0 +1,11 @@
+// 요소 가져오기
+const replInput = document.querySelector('.repl-input');
+const replBtn = document.querySelector('.repl-btn');
+// console.log(othersRepl);
+const othersRepl = document.querySelector('.others-repl');
+const replThreeDot = document.querySelector('.repl-hover-three-dot');
+
+const USER_ID = 'kitaegram';
+
+
+
