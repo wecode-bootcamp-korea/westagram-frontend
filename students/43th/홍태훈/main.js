9bd1ebdada729edd065dc41ff53c4408b74837ce
HongTaeHoon <kdn95374888@gmail.com>
2023-02-18 18:54:56 +0900

    ADD : main.js추가, 미션4 진행중

--- students/43th/홍태훈/main.js
+++ students/43th/홍태훈/main.js
@@ -1,11 +1,30 @@
+//const { text } = require("express");
+/*document.asdfa.addEventListener("keyup",()=>{
+});;*/
+/*function onKeyUp(e) {
+  if (e.keyCode == 13) {
+  }
+}*/
+/*const getComment = document.createElement("div");
+getComment.setAttribute("class", "add-comment");
+getComment.innerHTML = "addcomment.value";
+add_comment.appendChild(getComment);*/
+
+const upload_comment = document.getElementById("upload-comment");
 const addcomment = document.getElementById("comment");
-console.log(addcomment);
-const upload_comment = document.getElementById("upload-commen");
-console.log(upload_comment);
-const add_comment = document.getElementById("add-comment");
-console.log(add_comment);
-/*addcomment.addEventListener("submit", (e) => {
+const add_comment = document.querySelector(".add-comment");
+const getComment = document.createElement("div");
+
+function createNewComment() {
+  getComment.setAttribute("class", "add-comment");
+  getComment.innerHTML = addcomment.value;
+  add_comment.appendChild(getComment);
+  //addcomment.value ="";
+}
+
+upload_comment.addEventListener("keyup", function (e) {
+  console.log(e.key);
   if (e.code === "Enter") {
+    createNewComment;
   }
 });
-*/
