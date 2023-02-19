4b3fba5fd96a69e19db204a4ed6b80984aff6b36
HongTaeHoon <kdn95374888@gmail.com>
2023-02-19 18:25:04 +0900

    ADD: Misson4 완료

--- students/43th/홍태훈/main.js
+++ students/43th/홍태훈/main.js
@@ -1,30 +1,88 @@
-//const { text } = require("express");
+//console.log(addcomment);
+/*const upload_comment = document.getElementById("upload-comment");
+console.log(upload_comment);*/
+
+//console.log(add_comment);
+
+//console.log(addcomment.value);
 /*document.asdfa.addEventListener("keyup",()=>{
-});;*/
+
+});
+console.log(addcomment.value);*/
+
 /*function onKeyUp(e) {
   if (e.keyCode == 13) {
   }
 }*/
+
 /*const getComment = document.createElement("div");
+
 getComment.setAttribute("class", "add-comment");
 getComment.innerHTML = "addcomment.value";
 add_comment.appendChild(getComment);*/
 
-const upload_comment = document.getElementById("upload-comment");
+/*function onKeyUp(e) {
+  if (e.keyCode == 13) {
+  }
+
+
+  여기 물어볼거  getComment = document.createElement("div");
 const addcomment = document.getElementById("comment");
 const add_comment = document.querySelector(".add-comment");
-const getComment = document.createElement("div");
 
 function createNewComment() {
-  getComment.setAttribute("class", "add-comment");
+  const getComment = document.createElement("div");
+  const yourName = document.createElement("h6")
+
   getComment.innerHTML = addcomment.value;
+
   add_comment.appendChild(getComment);
-  //addcomment.value ="";
 }
 
-upload_comment.addEventListener("keyup", function (e) {
-  console.log(e.key);
-  if (e.code === "Enter") {
-    createNewComment;
+addcomment.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    createNewComment();
+    addcomment.value = "";
+  }
+});
+
+
+
+
+
+}*/
+const addcomment = document.getElementById("comment");
+const add_comment = document.querySelector(".add-comment");
+
+function nickName() {
+  //
+  //const span = document.createElement("span");
+  //// add_comment.innerHTML = `<span>Hong</span>`;
+  // yourName.style = "margin-right:10px";
+  //
+  //
+}
+
+function createNewComment() {
+  const getComment = document.createElement("span");
+  const yourName = document.createElement("strong");
+  yourName.className = "nickName";
+  const div = document.createElement("div");
+  add_comment.appendChild(div);
+
+  yourName.value = "Hong";
+
+  yourName.innerHTML = yourName.value;
+  getComment.innerHTML = addcomment.value;
+  div.appendChild(yourName);
+  div.appendChild(getComment);
+}
+
+addcomment.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    // nickName();
+    nickName() + createNewComment();
+    // createNewComment();
+    addcomment.value = "";
   }
 });
