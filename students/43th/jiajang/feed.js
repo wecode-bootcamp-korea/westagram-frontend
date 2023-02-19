0eb0234b3419a4c368d0fec7a55c452d8a096a92
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-19 14:24:26 +0900

    MODIFY : 사이드바 토글 및 css 완성

--- students/43th/jiajang/feed.js
+++ students/43th/jiajang/feed.js
@@ -1,12 +1,10 @@
 const navElement = document.querySelector("nav");
-const navWrapElement = document.querySelector(".nav-wrap");
-const hideNavBtn = document.querySelector(".nav-bottom-icon");
+const navWrapToggleElement = document.querySelector(".nav-wrap-toggle");
+const menuIcon = document.querySelector(".fa-bars");
 
 // 사이드바 토글
 
-hideNavBtn.addEventListener("click", function () {
-  navElement.className.includes("nav-open-btn")
-    ? navWrapElement.classList.remove("nav-closed-no-element")
-    : (navWrapElement.classList.add("nav-closed-no-element"),
-      navWrapElement.classList.add("nav-bottom-icon"));
+menuIcon.addEventListener("click", function () {
+  navElement.classList.toggle("nav-closed");
+  navWrapToggleElement.classList.toggle("hide-nav-elements");
 });
