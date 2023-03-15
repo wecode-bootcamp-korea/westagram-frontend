3514a9a2ec5a3d60abaa96bf5684746dd55b8436
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-15 21:48:21 +0900

    ADD : main-right의 스토리 프로필에 빨간원 추가

--- students/44th/HyominShin/js/login.js
+++ students/44th/HyominShin/js/login.js
@@ -1,2 +1,18 @@
+const userId = document.getElementById("loginBox");
+const userPassword = document.getElementById("passwordBox");
+
+let boolUserId = false;
+let boolUserPassword = false;
+
+userId.addEventListener('keypress', function(){
+    boolUserId = True;
+});
+userPassword.addEventListener('keypress', function(){
+    boolUserPassword = True;
+});
+
+if (boolUserId && boolUserPassword === True ) {
+    
+}
 
 
