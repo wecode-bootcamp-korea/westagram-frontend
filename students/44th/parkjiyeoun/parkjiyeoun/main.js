b1d0b688d19046c5aa5e5b7edaf3c47e62be66c7
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-03-16 10:15:43 +0900

    [44기 박지연]ADD : Mission4 댓글 기능 작업중

--- students/44th/parkjiyeoun/parkjiyeoun/main.js
+++ students/44th/parkjiyeoun/parkjiyeoun/main.js
@@ -1,21 +1,38 @@
 //구현할 기능 : 댓글 내용 입력 후 Enter press, 혹은 게시 버튼 클릭 시 댓글 추가 기능
-
 //1. 댓글창을 자바스크립트로 가져온다.
 const comment = document.getElementsByClassName("comment")[0];
 const commentBtn = document.getElementsByClassName("comment_upload_btn")[0];
-//2.댓글창을 keyup 했을 떄 글자수가 1개라도 넘어가면 게시버튼 활성화
-commentBtn.disabled = true;
 
 function commentValue() {
-  if (comment.value.length > 0) {
-    commentBtn.disabled = false;
-    commentBtn.style.color = "#4c9eee#";
-    //commentBtn.classList.remove("comment_upload_btn_off");
-  } else {
-    commentBtn.disabled = true;
-  }
+  //2.new_text_comment class 가져오고  그 안에 id 들어갈 span, txt 들어갈 span
+  const commentBox = document.getElementsByClassName("new_text_comment")[0];
+
+  const newComment = document.createElement("div");
+  const newId = document.createElement("span");
+  const newTxt = document.createElement("span");
+
+  newComment.classList.add("text_comment");
+  newId.classList.add("id");
+  newTxt.classList.add("txt");
+
+  newId.innerHTML = "parkjiyeoun";
+  newTxt.innerHTML = comment.value;
+
+  newComment.appendChild(newId);
+  newComment.appendChild(newTxt);
+
+  commentBox.appendChild(newComment);
 }
 
-comment.addEventListener("keyup", commentValue);
+//버튼 클릭하면 함수 실행
+commentBtn.addEventListener("click", (e) => {
+  commentValue();
+  comment.value = "";
+});
 
-//3. enter 키 누르거나 || 게시 버튼 누르면 댓글 추가
+comment.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    commentValue();
+    comment.value = "";
+  }
+});
