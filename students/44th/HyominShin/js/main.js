824a275bd5f3b45a0e54df659524b79fe90f28d3
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-16 21:51:00 +0900

    ADD : 댓글 삭제 기능 구현, 좋아요 기능 구현중

--- students/44th/HyominShin/js/main.js
+++ students/44th/HyominShin/js/main.js
@@ -1,30 +1,60 @@
-// 1. 댓글창에 댓글 달면 게시 버튼 활성화
-// 2. 댓글을 입력하면 피드에 댓글 요소 추가
-const listComment = document.getElementsByClassName('commentList');
-const replyButton = document.getElementById('commentReply');
-const inputComment = document.getElementById('commentInput');
+// 1. 댓글창에 댓글 달면 게시 버튼 활성화 - ok!
+// 2. 댓글을 입력하면 피드에 댓글 요소 추가 - ok!
+// 3. 댓글의 하트를 누르면 빨갛게 변하기 - 
+// 4. 댓글삭제 회색 문구를 누르면 삭제가 되게끔하기 -
 
-const deleteCommentUl = document.querySelector('ul.commentList');
-const deleteCommentLi = document.querySelector('li.comment');
+const listComment = document.querySelector('.commentList');
+const replyComment = document.querySelector('#commentReply');
+const inputComment = document.querySelector('#commentInput');
+
+const addComment = () => {
+    const text = inputComment.value;
+
+    const comment = document.createElement('li');
+    comment.setAttribute('class', 'comment');
+    
+    const commentText = document.createElement('span');
+    commentText.innerHTML = text;
+
+    const commentLike = document.createElement('img');
+    commentLike.setAttribute('src', './img/heart.png');
+    commentLike.setAttribute('class', 'smallHeart');
+    commentLike.addEventListener('click', function() {
+
+    })
+
+    const commentDel = document.createElement('span');
+    commentDel.setAttribute('class', 'miniSubGray');
+    commentDel.innerHTML = '댓글삭제';
+    commentDel.addEventListener('click', function() {
+        listComment.removeChild(comment);
+    })
+
+    comment.appendChild(commentText);
+    comment.appendChild(commentLike);
+    comment.appendChild(commentDel);
+    listComment.appendChild(comment);
+
+    console.log(text);
+};
+
+replyComment.addEventListener('click', () => {
+    addComment();
+    inputComment.value = '';
+});
 
 //댓글창에 키 입력시 게시 버튼 활성화
 inputComment.addEventListener("keyup", function(event) {
     if (inputComment.value) {
-        replyButton.disabled = false;
+        replyComment.disabled = false;
     } else {
-        replyButton.disabled = true;
+        replyComment.disabled = true;
     }
 });
 
 // 댓글창에 키 입력 후 게시버튼 클릭시 피드에 새로만든 댓글 생성
-replyButton.addEventListener('click', function(event) {
-    var newCommentLi = document.createElement('li');
-    // var newComment = document.createElement('span');
-    newCommentLi.innerHTML = '<span><span class="textBold";>BaCoffee</span>' + ' ' + inputComment.value + '<img class="smallHeart" onclick="changeLikeHeart()" src="./img/heart.png"><span class="miniSubGray" onclick="deleteComment()" ">댓글삭제</span></span>';
-
-
-    listComment[0].appendChild(newCommentLi);
-    inputComment.value = "";
+replyComment.addEventListener('click', function(event) {
+    
 })
 
 // 댓글의 하트를 클릭시 빨갛게 변경
@@ -35,8 +65,3 @@ function changeLikeHeart() {
 
 
 // 댓글 삭제를 누르면 삭제가 됨
-function deleteComment() {
-    // deleteCommentUl.removeChild(deleteCommentLi);    
-
-    console.log("삭제 좀 되봐바");
-}
