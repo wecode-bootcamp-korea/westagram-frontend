f17ec45b92ea6962d004d84c44f6a583cef69328
조건호 <geonho@jogeonhoui-MacBookPro.local>
2023-03-16 10:49:45 +0900

    ADD: 로그인 구현 완료

--- students/44th/chogeonho/front-kickoff1/index.js
+++ students/44th/chogeonho/front-kickoff1/index.js
@@ -1,32 +1,48 @@
 // const id = document.getElementById("id");
 // const pw = document.getElementById("pw");
-const login = document.getElementById("login");
+//const login = document.getElementById("login");
 
-login.addEventListener("click", function () {
-  const id = document.getElementById("id").value;
-  const password = document.getElementById("pw").value;
+// login.addEventListener("click", function () {
+//   const id = document.getElementById("id").value;
+//   const password = document.getElementById("pw").value;
 
-  if (!id) {
-    alert("아이디를 입력해주세요!");
-    return;
-  } else if (!password) {
-    alert("비밀번호를 입력해주세요!");
-    return;
-  } else {
-    alert("로그인 성공!");
-  }
-});
-
-// const loginBtn = document.querySelector(".login_btn");
-// const userId = document.querySelector(".user_id");
-
-// window.addEventListener("keyup", () => {
-//   const userPw = document.querySelector(".user_pw");
-//   if (userPw.value.length > 0 && userId.value.length > 0) {
-//     loginBtn.disabled = false;
-//     loginBtn.classList.add("active");
+//   if (!id) {
+//     alert("아이디를 입력해주세요!");
+//     return;
+//   } else if (!password) {
+//     alert("비밀번호를 입력해주세요!");
+//     return;
 //   } else {
-//     loginBtn.disabled = true;
-//     loginBtn.classList.remove("active");
+//     alert("로그인 성공!");
 //   }
 // });
+
+const loginId = document.getElementById("login_id");
+const loginPw = document.getElementById("login_pw");
+const loginBtn = document.getElementById("login_btn");
+
+const implement = () => {
+  if (
+    loginId.value.length > 0 &&
+    loginId.value.indexOf("@") !== -1 &&
+    loginPw.value.length >= 5
+  ) {
+    //loginBtn.style.backgroundColor = "#0095F6";
+    loginBtn.disabled = false;
+  } else {
+    loginBtn.style.backgroundColor = "#C0DFFD";
+    loginBtn.disabled = true;
+  }
+
+  // if (loginId.value.length < 30) {
+  //   alert("너무 짧습니다");
+  // }
+};
+
+const MoveToMain = () => {
+  location.replace("./main.html");
+};
+
+loginId.addEventListener("keyup", implement);
+loginPw.addEventListener("keyup", implement);
+loginBtn.addEventListener("click", MoveToMain);
