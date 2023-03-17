f65de0db7564f49aebcd0863e7f03608adb2249f
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-03-17 10:00:05 +0900

    ADD: 필수미션 완료-로그인 버튼 활성화 추가

--- students/44th/choisunyoung/로그인페이지/js/login.js
+++ students/44th/choisunyoung/로그인페이지/js/login.js
@@ -1,42 +1,37 @@
 //button disable의 값이 false면 활성화됨.
 //userId와 userPw 둘의 input에 글씨가 작성되면 버튼이 활성화가 됨.
 
+//필요한 버튼, 아이디, 비번의 class,id를 변수로 불러옴
+const loginButton = document.querySelector('.login-button');
+const loginId = document.querySelector('#user-id');
+const loginPw = document.querySelector('#user-pw');
 
-const contentWrap = document.getElementsByClassName("loginContent-wrap");
-const loginButton = document.getElementById("login-button");
-const userId = document.getElementsByid("user-id");
-const userPw = document.getElementsByid("user-pw");
+//로그인 할 때 필요한 설정 함수
+const loginSet = () => {
+  /*만약, 아이디(input)의 문자열 길이가 0보다 크고 &&
+  indexOf(@기호가 있으면 1리턴)로 , -1과 같지 않고 &&
+  비번(input)의 문자열 길이가 5랑 같거나 클 경우*/
+  if( 
+    loginId.value.length > 0 &&
+    loginId.value.indexOf("@")!== -1 &&
+    loginPw.value.length >= 5
+    ){
+  //버튼의 disabled값을 false(활성화)되고, 색상이 바뀜
+      loginButton.style.backgroundColor = "#0095F6";
+      loginButton.disabled = false;
+      console.log(loginButton.disabled);
+    } else {
+  //아닐 경우, disabled값이 ture(비활성화)되고, 색상이 바뀜
+      loginButton.style.backgroundColor = "#C0DFFD";
+      loginButton.disabled = true;
+      console.log(loginButton.disabled);
+    }
+}
+console.log(loginSet());
 
-userId.addEventListener('keydown', buttonEvent());
-userPw.addEventListener('keydown', buttonEvent());
+//id나 pw의 키보드키가 올라갈 때 함수가 실행 되게 설정
+loginId.addEventListener('keyup',loginSet);
+loginPw.addEventListener('keyup', loginSet);
 
-function buttonEvent(){
-  if(userId.value && userPw.value){
-    loginButton.disabled = false;
-    loginButton.className += 'keydown';
-  } else {
-    loginButton.disabled = true;
-  }
-};
-
-
-// contentWrap.keydownEvent('keydown', function(){
-//   let userId = true;
-//   let userPw = true;
-//   if(userId.value && userPw.value){
-//     userId.value = false;
-//     userPw.value = false;
-//   } 
-// })
-// function keydownEvent(){
-//   contentWrap.addEventListener('keydown', function(){
-//     if(userId.value && userPw.value){
-//       userId.disable = false;
-//       userPw.disable = false;
-//     } else {
-//       userId.disable = true;
-//       userPw.disable = true;
-//     }
-//   })
-// }
-// keydownEvent();
+// id.addEventListener('keyup', login);
+// pw.addEventListener('key', login);
