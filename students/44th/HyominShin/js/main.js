4d97a0ed2ce3f6f967d441079cc5cf2340da935a
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-17 15:51:27 +0900

    ADD : 댓글의 좋아요와 댓글 삭제 구현 완료

--- students/44th/HyominShin/js/main.js
+++ students/44th/HyominShin/js/main.js
@@ -1,12 +1,13 @@
 // 1. 댓글창에 댓글 달면 게시 버튼 활성화 - ok!
 // 2. 댓글을 입력하면 피드에 댓글 요소 추가 - ok!
 // 3. 댓글의 하트를 누르면 빨갛게 변하기 - 
-// 4. 댓글삭제 회색 문구를 누르면 삭제가 되게끔하기 -
+// 4. 댓글삭제 회색 문구를 누르면 삭제가 되게끔하기 - ok!
 
 const listComment = document.querySelector('.commentList');
 const replyComment = document.querySelector('#commentReply');
 const inputComment = document.querySelector('#commentInput');
 
+// 게시 버튼을 눌렀을 때 함수 발생
 const addComment = () => {
     const text = inputComment.value;
 
@@ -17,27 +18,36 @@ const addComment = () => {
     commentText.innerHTML = text;
 
     const commentLike = document.createElement('img');
+    let changeLike = 1;
     commentLike.setAttribute('src', './img/heart.png');
     commentLike.setAttribute('class', 'smallHeart');
+    
+    // 댓글의 하트를 누르면 빨갛게 변화
     commentLike.addEventListener('click', function() {
+        if (changeLike % 2 === 1) {
+            commentLike.setAttribute('src', './img/fillheart.png');
+            changeLike++;
+        } else {
+            commentLike.setAttribute('src', './img/heart.png');
+            changeLike++;
+        };
+    });
 
-    })
-
+    // 댓글의 회색 삭제를 누르면 해당 댓글 삭제
     const commentDel = document.createElement('span');
     commentDel.setAttribute('class', 'miniSubGray');
     commentDel.innerHTML = '댓글삭제';
     commentDel.addEventListener('click', function() {
         listComment.removeChild(comment);
-    })
+    });
 
     comment.appendChild(commentText);
     comment.appendChild(commentLike);
     comment.appendChild(commentDel);
     listComment.appendChild(comment);
-
-    console.log(text);
 };
 
+// 피드에 댓글 입력시 댓글창 clear
 replyComment.addEventListener('click', () => {
     addComment();
     inputComment.value = '';
@@ -51,17 +61,3 @@ inputComment.addEventListener("keyup", function(event) {
         replyComment.disabled = true;
     }
 });
-
-// 댓글창에 키 입력 후 게시버튼 클릭시 피드에 새로만든 댓글 생성
-replyComment.addEventListener('click', function(event) {
-    
-})
-
-// 댓글의 하트를 클릭시 빨갛게 변경
-function changeLikeHeart() {
-    var changeHeart = document.getElementsByClassName('smallheart');
-    console.log("빨갛게 좀 되봐바");
-};
-
-
-// 댓글 삭제를 누르면 삭제가 됨
