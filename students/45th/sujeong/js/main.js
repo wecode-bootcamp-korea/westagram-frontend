02bc12401545b57d9107cd75d66a47ce7127528b
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-13 20:03:26 +0900

    Add : main page 댓글 입력하고 엔터 또는 게시버튼 클릭 시 댓글 목록에 해당 댓글 추가되는 기능 구현

--- students/45th/sujeong/js/main.js
+++ students/45th/sujeong/js/main.js
@@ -1,1 +1,18 @@
 /** @format */
+const commentTxt = document.getElementsByClassName("comment_input")[ 0 ];
+const commentList = document.getElementsByClassName("comment_list")[ 0 ]
+
+commentTxt.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    outputValue()
+  }
+})
+
+function outputValue () {
+  const addCommentLi = document.createElement("li")
+  const commentInList = commentList.appendChild(addCommentLi)
+
+  return commentInList.innerText = commentTxt.value
+}
+
+
