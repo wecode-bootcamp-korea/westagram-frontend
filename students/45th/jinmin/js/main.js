203535d2e379f2a8a5fd76da1510740cbada9b98
min2oyo <min2u@outlook.com>
2023-04-13 22:18:41 +0900

    add: 댓글 엔터 입력 추가, 프사 1개 적용

--- students/45th/jinmin/js/main.js
+++ students/45th/jinmin/js/main.js
@@ -1,14 +1,22 @@
 // 변수
 const replies = document.querySelector(`#replies`);
-const myNewReply = document.querySelector(`#myNewReply`);
-const myReplyGo = document.querySelector(`#myReplyGo`);
+let replyBox = document.querySelectorAll(`.replyBox`);
+// let delReply = document.querySelectorAll(`.delReply`);
+const inputReply = document.querySelector(`#inputReply`);
+const inputReplyBtn = document.querySelector(`#inputReplyBtn`);
 
-// 실행
-myReplyGo.addEventListener(`click`, () => {
+// 함수
+const addReply = () => {
   replies.innerHTML += `
     <div class="replyBox">
-    <div class="userName">나당</div>
-    <div class="userTalk">${myNewReply.value}</div>
-  </div>
+      <div class="userName">나당</div>
+      <div class="userTalk">${inputReply.value}</div>
+      <div class="delReply">x</div>
+    </div>
   `;
-});
+  inputReply.value = ``;
+};
+
+// 실행
+inputReply.addEventListener(`keydown`, e => e.keyCode === 13 ? addReply() : null);
+inputReplyBtn.addEventListener(`click`, addReply);
