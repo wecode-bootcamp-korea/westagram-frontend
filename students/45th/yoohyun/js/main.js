525d6076b325104ca36796debfbce637bf81fb93
yoohmoon <ansekfzhd5455@naver.com>
2023-04-13 19:01:54 +0900

    MODIFY : main.js 댓글 기능 구현 완료 - innerHTML 활용

--- students/45th/yoohyun/js/main.js
+++ students/45th/yoohyun/js/main.js
@@ -21,14 +21,28 @@ console.log(commentForm);
 console.log(uploadedCommentsBox);
 
 const uploadComment = (e) => {
+  // 화면 새로고침 방지 코드
   e.preventDefault();
-  console.log("hello");
-  console.log("사용자 입력 댓글 내용 출력! ", commentInput.value);
+
+  // console.log("사용자 입력 댓글 내용 출력! ", commentInput.value);
+
+  // 댓글 내역 요소 생성
   const commentsList = document.createElement("ul");
   const newCommentItem = document.createElement("li");
-  newCommentItem.textContent = commentInput.value;
+
+  // 생성한 요소에 인풋창 입력값으로 텍스트 추가하기
+  // newCommentItem.textContent = commentInput.value;
+  const commentItemHtml = `<p><span style='margin-right:5px' class='bold'>tellmemoreaboutu</span>${commentInput.value}</p><i class="fa-regular fa-heart"></i>`;
+  newCommentItem.innerHTML = commentItemHtml;
+
+  // html에 append하기
   commentsList.append(newCommentItem);
-  console.log("추가한 건?? ", commentsList);
+  // console.log("추가한 건?? ", commentsList);
   uploadedCommentsBox.appendChild(commentsList);
+  console.log("li의 HTML 알아보기 ", newCommentItem.innerHTML);
+
+  // submit이 발생하는 순간, 사용자가 입력한 인풋창의 내용을 지워주는 코드
+  commentInput.value = "";
 };
+
 commentForm.addEventListener("submit", uploadComment);
