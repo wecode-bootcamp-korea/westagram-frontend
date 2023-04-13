c87819f5321d0354281ce97afd6e3f8acd863078
min2oyo <min2u@outlook.com>
2023-04-13 16:25:50 +0900

    Refactor: 두 개의 id, pw addEventListener를 form으로 병합

--- students/45th/jinmin/js/login.js
+++ students/45th/jinmin/js/login.js
@@ -4,8 +4,10 @@ const pw = document.querySelector(`#pw`);
 const form = document.querySelector(`form`);
 const loginButton = document.querySelector(`#loginButton`);
 
-// 함수
-const okLogin = () => {
+// 실행
+form.addEventListener(`click`, e => e.preventDefault());  // submit 새로고침 방지
+
+form.addEventListener(`keyup`, () => {  // id, pw 입력
   if (id.value.length !== 0 && pw.value.length !== 0) {
     loginButton.style.backgroundColor = `#3C9BF2`;
     loginButton.style.cursor = `pointer`;
@@ -13,14 +15,10 @@ const okLogin = () => {
     loginButton.style.backgroundColor = `#C4E1FB`;
     loginButton.style.cursor = null;
   }
-};
+});
 
-// 실행
-form.addEventListener(`click`, e => e.preventDefault());
-id.addEventListener(`keyup`, okLogin);
-pw.addEventListener(`keyup`, okLogin);
-loginButton.addEventListener(`click`, () => loginButton.style.cursor === `pointer` ? alert(`
-    로그인 전송
+loginButton.addEventListener(`click`, () => loginButton.style.cursor === `pointer` ? alert(`` + // 로그인 정보 확인
+  `로그인 전송
     id: ${id.value}
     pw: ${pw.value}
   `) : null
