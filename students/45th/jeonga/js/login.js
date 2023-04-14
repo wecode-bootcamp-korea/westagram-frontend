7a37c618e5a021bb0deb2ef627de7a30547677ed
joyfuldev <wings2132@naver.com>
2023-04-14 16:19:00 +0900

    ADD: ID, PW 유효성 검사

--- students/45th/jeonga/js/login.js
+++ students/45th/jeonga/js/login.js
@@ -1,6 +1,30 @@
 const loginId = document.querySelector(".login-id");
 const loginPassword = document.querySelector(".login-password");
 const loginBtn = document.querySelector(".login-button");
+const loginIdWrapper = document.querySelector(".login-id__wrapper");
+const loginPwWrapper = document.querySelector(".login-password__wrapper");
+
+const checkValid = () => {
+  const check = /\s+@\s+/;
+  let result = check.test(loginId.value);
+  if (result === false) {
+    const emailError = document.createElement("span");
+    emailError.className = "login-id__error";
+    emailError.innerHTML = "@를 포함해주세요";
+    emailError.style.color = "red";
+    emailError.style.fontSize = "12px";
+    loginIdWrapper.appendChild(emailError);
+  }
+  if (loginPassword.value.length < 5) {
+    const pwError = document.createElement("span");
+    pwError.className = "login-password__error";
+    pwError.innerHTML = "비밀번호는 5글자 이상이어야 합니다.";
+    pwError.style.color = "red";
+    pwError.style.fontSize = "12px";
+    loginPwWrapper.appendChild(pwError);
+  }
+  loginBtn.removeEventListener("click", checkValid);
+};
 
 const mouseEnter = () => {
   loginBtn.style.backgroundColor = "#1877f2";
@@ -15,6 +39,7 @@ const buttonOn = () => {
   loginBtn.classList.add("blue");
   loginBtn.addEventListener("mouseenter", mouseEnter);
   loginBtn.addEventListener("mouseleave", mouseLeave);
+  loginBtn.addEventListener("click", checkValid);
 };
 
 const buttonOff = () => {
