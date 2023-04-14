df93cef907e287789c1d3b51d256c7b271660e20
min2oyo <min2u@outlook.com>
2023-04-14 15:15:38 +0900

    ADD: Mission 8 구현 (개인 메뉴 표시닫기)

--- students/45th/jinmin/js/main.js
+++ students/45th/jinmin/js/main.js
@@ -1,4 +1,8 @@
 // 변수
+const myNavImg = document.querySelector(`#myNavImg`); // 상단바
+const navUserImg = document.querySelector(`#navUserImg`);
+const navMyMenu = document.querySelector(`#navMyMenu`);
+
 const replies = document.querySelector(`#replies`);
 let replyBox = document.querySelectorAll(`.replyBox`);
 let delReply = document.querySelectorAll(`.delReply`);
@@ -38,14 +42,25 @@ const addReply = () => {
 //   }
 // };
 
-for (let i = 0; i < delReply.length; i++) {
-  delReply[i].addEventListener(`click`, function () {
-    console.log(`hi`, i);
-  });
-}
+// for (let i = 0; i < delReply.length; i++) {
+//   delReply[i].addEventListener(`click`, function () {
+//     console.log(`hi`, i);
+//   });
+// }
 
 // 실행
-inputReply.addEventListener(`keydown`, e => e.keyCode === 13 ? addReply() : null);
+navUserImg.addEventListener(`click`, () => {  // 개인 메뉴 표시
+  navMyMenu.style.display = `flex`;
+});
+
+
+document.addEventListener('mouseup', e => { // 개인 메뉴 닫기
+  !navMyMenu.contains(e.target) ? navMyMenu.style.display = 'none' : null;
+});
+
+
+
+inputReply.addEventListener(`keydown`, e => e.keyCode === 13 ? addReply() : null);  // 댓글 입력
 inputReplyBtn.addEventListener(`click`, addReply);
 
 
