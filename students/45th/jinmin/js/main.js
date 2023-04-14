bd6ba4fc675f9c9f0e4a1ea566b50d690a9bf0a9
min2oyo <min2u@outlook.com>
2023-04-14 12:15:30 +0900

    댓글 삭제 보류

--- students/45th/jinmin/js/main.js
+++ students/45th/jinmin/js/main.js
@@ -1,17 +1,22 @@
 // 변수
 const replies = document.querySelector(`#replies`);
 let replyBox = document.querySelectorAll(`.replyBox`);
-// let delReply = document.querySelectorAll(`.delReply`);
+let delReply = document.querySelectorAll(`.delReply`);
 const inputReply = document.querySelector(`#inputReply`);
 const inputReplyBtn = document.querySelector(`#inputReplyBtn`);
 
 // 함수
 const addReply = () => {
-  console.log(inputReply.value.length);
+  const div = document.createElement(`div`);
+  const divClass = document.createAttribute(`class`);
+  divClass.value = `replyBox`;
+  div.innerHTML += `<p>1</p>`;
+  div.setAttributeNode(divClass);
+  console.log(div);
   if (inputReply.value.length) {
     replies.innerHTML += `
     <div class="replyBox">
-      <div class="userName">나당</div>
+      <div class="userName">나다</div>
       <div class="userTalk">${inputReply.value}</div>
       <div class="delReply">x</div>
     </div>
@@ -20,6 +25,31 @@ const addReply = () => {
   }
 };
 
+// const addReply = () => {
+//   if (inputReply.value.length) {
+//     replies.innerHTML += `
+//     <div class="replyBox">
+//       <div class="userName">나다</div>
+//       <div class="userTalk">${inputReply.value}</div>
+//       <div class="delReply">x</div>
+//     </div>
+//   `;
+//     inputReply.value = ``;
+//   }
+// };
+
+for (let i = 0; i < delReply.length; i++) {
+  delReply[i].addEventListener(`click`, function () {
+    console.log(`hi`, i);
+  });
+}
+
 // 실행
 inputReply.addEventListener(`keydown`, e => e.keyCode === 13 ? addReply() : null);
 inputReplyBtn.addEventListener(`click`, addReply);
+
+
+
+// test
+const check = document.querySelector(`#check`);
+check.addEventListener(`click`, () => console.log(delReply));
