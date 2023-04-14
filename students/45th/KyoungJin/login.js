b73c59c02be9932f777c8ff532e4544f0d979db0
Jinny-Jin <katie9796@naver.com>
2023-04-14 14:21:36 +0900

    ADD : login 페이지에 아이디와 비밀번호 유효성 검사 기능 추가
    - 아이디에 @ 미포함 시 버튼 비활성화
    - 비밀번호 길이가 5자 미만일 시 버튼 비활성화

--- students/45th/KyoungJin/login.js
+++ students/45th/KyoungJin/login.js
@@ -16,18 +16,52 @@ function login() {
   }
 }**/
 
-function loginbutton() {
+/*function loginbutton() {
   if (Id.value && Pw.value) {
-    Button.disabled = false;
-    Button.style.cursor = "pointer";
-    Button.classList.remove("no-login");
-    Button.classList.add("yes-login");
-  } else {
-    Button.disabled = true;
-    Button.classList.remove("yes-login");
-    Button.classList.add("no-login");
+    if (Id.value.indexOf("@") >= 1 && Pw.value.length >= 5) {
+      Button.disabled = false;
+      Button.style.cursor = "pointer";
+      Button.classList.remove("no-login");
+      Button.classList.add("yes-login");
+    } else {
+      Button.disabled = true;
+      Button.classList.remove("yes-login");
+      Button.classList.add("no-login");
+    }
   }
 }
 
 Id.addEventListener("keyup", loginbutton);
-Pw.addEventListener("keyup", loginbutton);
+Pw.addEventListener("keyup", loginbutton);*/
+
+//아이디에 @포함, 비밀번호 5글자 이상 기능 넣기
+// 1. 아이디랑 비밀번호 비어있으면 버튼 비활성화
+// 2. 아이디에@없고 비밀번호 5글자 미만이면 비활성화
+// 각각 input 아래에 올바른 형식으로 입력하세요 빨간 글씨 나타나게?
+// input id = ID 아래에 <p> 넣기
+// <p> 안에 innerHTML로 올바른~ 넣기
+// input id = Password도 똑같이
+
+// 3. 그 외에는 활성화
+
+function Login() {
+  if (!(Id.value && Pw.value)) {
+    Button.disabled = true;
+    Button.classList.remove("yes-login");
+    Button.classList.add("no-login");
+  }
+
+  if (Id.value.indexOf("@") == -1 || Pw.value.length <= 5) {
+    Button.disabled = true;
+    Button.classList.remove("yes-login");
+    Button.classList.add("no-login");
+  } else {
+    Button.disabled = false;
+    Button.style.cursor = "pointer";
+    Button.classList.remove("no-login");
+    Button.classList.add("yes-login");
+  }
+}
+
+Id.addEventListener("keyup", Login);
+Pw.addEventListener("keyup", Login);
