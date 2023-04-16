a83f0f3c8fbb3c557db3c4e6c3317e02d6fe121d
hyunah <qkrgusdk0303@naver.com>
2023-04-16 13:34:16 +0900

    Refactor: 리팩토링 수업처럼 코드 정리
    1. css는 속성 순서 정리
    2. js는 텍스트에 html 넣기보다는 함수를 통해 구조적으로 구성
    
    3. id 태그보다는 class 태그가 더 사용하기에 유용하다고 해서 그것에 관해
    정리
      (이전에는 js 코드 구현하기 급급해서 일단 id 태그를 통해 함수 구현했음)
      - login 페이지에서는 id 태그만 이용
      - main 페이지에서는 class 태그만 이용

--- students/45th/hyunah/westagram-js/loginPage_Script.js
+++ students/45th/hyunah/westagram-js/loginPage_Script.js
@@ -5,17 +5,12 @@ const pw =document.getElementById('password');
 //// id & pw input -> button validataion (blue)
 window.addEventListener('input', loginValid)
 
-id.addEventListener('input', function(){
-  console.log(id.value)
-})
+id.addEventListener('input', function(){})
 
-pw.addEventListener('input', function(){
-    console.log(pw.value)
-})
+pw.addEventListener('input', function(){})
 
 function loginValid(){
     if(id.value && pw.value) {
         btn.style.backgroundColor = '#4cb5f9';
     }
 }
-
--- students/45th/hyunah/westagram-js/mainPage_Scrip.js
+++ students/45th/hyunah/westagram-js/mainPage_Scrip.js
@@ -1,16 +1,41 @@
-const reply = document.getElementById('reply_txt');
-const showReply = document.getElementById('article_reply');
-const replyBtn = document.getElementById('reply_button');
+const reply = document.getElementsByClassName('reply_txt')[0];
+const showReply = document.getElementsByClassName('article_reply')[0];
+const replyBtn = document.getElementsByClassName('reply_button')[0];
 
 function addReply() {
     if (reply.value){
         const item = document.createElement('div');
 
-        var user = 'olive'
-        var heart = '<div><i class="fa-regular fa-heart"></i></div>';
+        // 1. content
+        const content = document.createElement('div')
 
-        item.innerHTML = '<div><b>'+user+'</b> '+ reply.value+'</div>' + heart;
-        item.id = 'article_reply_add';
+        
+        //// 1-1. user name
+        var name = 'olive'
+        const user_part = document.createElement('b')
+        user_part.innerText = name + ' '
+
+        content.appendChild(user_part)
+
+        //// 1-2. reply content
+        var reply_content = document.createTextNode(reply.value)
+        const reply_part = document.createElement('span')
+        reply_part.appendChild(reply_content)
+
+        content.appendChild(reply_part)
+
+        item.appendChild(content)
+
+
+        // 2. heart
+        var heart_mode = '<i class="fa-regular fa-heart"></i>'
+        const heart = document.createElement('div')
+        heart.innerHTML = heart_mode
+
+        item.appendChild(heart)
+
+        // show reply (attach 'item')
+        item.className = 'article_reply_add'; //for display:flex; justify-content:space-between;
 
         showReply.appendChild(item);
         reply.value = '';
@@ -18,7 +43,7 @@ function addReply() {
 }
 
 reply.addEventListener('keyup', function(event){
-    if (event.keyCode == 13){
+    if (event.keyCode == 13){ //'enter' key
         addReply()
     }
 })
