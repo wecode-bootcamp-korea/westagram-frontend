d574d4e55aaaaa93875fcbecc36643bb6d45170c
zosuzin <zosuzin@gmail.com>
2023-05-11 19:54:48 +0900

    ADD: 댓글창에 이모티콘과 삭제버튼추가

--- students/46th/chosujin/js/main.js
+++ students/46th/chosujin/js/main.js
@@ -1,17 +1,18 @@
 const inputBox = document.querySelector("#comment"); //input 변수 할당
-const buttonBox = document.querySelector("#commentBt"); //button 변수 할당
+const buttonBox = document.querySelector(".commentBt"); //button 변수 할당
+const commentHeart = document.querySelector(".fa-regular.fa-heart"); //하트에 변수할당
 
 //게시 버튼 클릭했을 때 댓글 기능 추가
 //1) 내가 쓴 input의 값을 가져온다.
 
-function commmentFun() {
+function commentFun() {
   const newCommentSpanId = document.createElement("span");
   const newCommentSpan = document.createElement("span"); // 2) 내가 입력한 값이 들어갈 공간만들어주기
   const newCommentDiv = document.createElement("div");
   const newCommentLi = document.createElement("li");
-  const newCommentImg = document.createElement("img");
+  const newCommentImg = document.createElement("i");
+  const newCommentBt = document.createElement("button");
   const commentBody = document.querySelector("ul");
-  newCommentImg.src = "/heart.png";
 
   if (inputBox.value !== "") {
     inputBox.value; //1) input에 내가 입력한 값 그 자체
@@ -20,22 +21,29 @@ function commmentFun() {
     newCommentDiv.appendChild(newCommentSpanId);
     newCommentDiv.appendChild(newCommentSpan);
     newCommentLi.appendChild(newCommentDiv);
+    newCommentLi.appendChild(newCommentDiv);
     newCommentLi.appendChild(newCommentImg);
+    newCommentLi.appendChild(newCommentBt);
     commentBody.appendChild(newCommentLi);
     newCommentSpanId.className = "id";
     newCommentSpanId.innerText = "btsSugar ";
-    newCommentImg.className = "commentHeart";
 
     inputBox.value = "";
   }
+}
+
+function heart() {
+  commentHeart.classList.add("fa-solid", "fa-heart");
+  commentHeart.style.color = "red";
 }
 
 function commentEnter(event) {
   if (event.keyCode === 13) {
-    commmentFun();
+    commentFun();
     inputBox.value = "";
   }
 }
 
-buttonBox.addEventListener("click", commmentFun);
+commentHeart.addEventListener("click", heart);
+buttonBox.addEventListener("click", commentFun);
 inputBox.addEventListener("keypress", commentEnter);
