dc9319eea6bcb50e6c39546e0a61044b1b160db3
shfkd123 <shfkd123@gmail.com>
2023-05-11 21:45:59 +0900

    ID, PW validation JS구현

--- students/46th/minji/js/login.js
+++ students/46th/minji/js/login.js
@@ -1,7 +1,7 @@
 function validate()  {
     const id = document.getElementById('id').value;
     const psword = document.getElementById('psword').value;
-   
+    
     if(!!(id && psword)){
         btnActive();
     }
@@ -12,9 +12,17 @@ function btnActive() {
     loginBtn.disabled = false;
     loginBtn.classList.remove("disableBtn");
     loginBtn.classList.add("ableBtn");
+
 }
 
 function login() {
-    alert("로그인 버튼 클릭!");
-    location.reload();
+    const id = document.getElementById('id').value;
+    const psword = document.getElementById('psword').value;
+    //id, pwd 유효성 검사
+    if(id.includes("@") && psword.length > 4){
+        location.href="./main.html";
+    }else {
+        alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+    }
 }
+    
