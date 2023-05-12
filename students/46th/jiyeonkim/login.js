502da046f19be2e02f43b2f505b8f0bd90d67708
yeonnnnn.k <yeonnnnn.k@gmail.com>
2023-05-12 15:57:08 +0900

    ADD:nav 프로필 사진 클릭 시 메뉴 박스 생성

--- students/46th/jiyeonkim/login.js
+++ students/46th/jiyeonkim/login.js
@@ -5,16 +5,6 @@ const loginBtn = document.querySelector("button");
 idBox.addEventListener("keyup", activeBtn);
 passwordBox.addEventListener("keyup", activeBtn);
 
-// function activeBtn() {
-//   if (idBox.value && passwordBox.value) {
-//     loginBtn.classList.add("activeBtn");
-//     loginBtn.disabled = false;
-//   } else {
-//     loginBtn.classList.remove("activeBtn");
-//     loginBtn.disabled = true;
-//   }
-// }
-
 function activeBtn() {
   // 변수 선언 함수 안에!! (새로운 값이 계속 업데이트 되어야 하는 경우**)
   const passwordValue = passwordBox.value;
--- students/46th/jiyeonkim/main.js
+++ students/46th/jiyeonkim/main.js
@@ -16,8 +16,12 @@ function getComment() {
   deleteBtn.classList.add("deleteBtn");
   deleteBtn.id = `${comment.value}`;
 
+  heartIcon.id = `${comment.value}`;
   heartIcon.classList.add("heartIcon");
   commentBox.classList.add("commentBox");
+
+  const commentValue = comment.value;
+
   userName.classList.add("userName");
   userName.innerText = "yeonnn";
 
@@ -32,31 +36,20 @@ function getComment() {
 
   //새 댓글 삭제
   deleteBtn.addEventListener("click", deleteComment);
+
+  //댓글 좋아요
+  heartIcon.addEventListener("click", likeBtnEvent);
 }
-
-//댓글 삭제 함수
-// function deleteComment() {
-//   const deleteBtn = document.getElementsByClassName("deleteBtn");
-//   const commentBox = document.getElementsByClassName("commentBox");
-
-//   commentBox.remove();
-// }
 
 const commentBtn = document.getElementById("commentBtn"); //게시 버튼
 
-//게시 버튼 class CSS
-comment.addEventListener("keypress", function () {
-  if (comment.v) {
-    commentBtn.classList.add(".active");
-  }
-});
-
-//click 댓글 구현
+//click 댓글 구
 commentBtn.addEventListener("click", function () {
   if (comment.value) {
     getComment();
     comment.value = null;
   }
+  console.log(typeof comment.value);
 });
 
 //enter 댓글 구현
@@ -73,8 +66,162 @@ const deleteBtn = document.querySelector(".deleteBtn");
 deleteBtn.addEventListener("click", deleteComment);
 
 function deleteComment(event) {
-  console.log(event);
   const deleteComment = event.target.parentNode;
 
   deleteComment.remove();
 }
+
+//댓글 좋아요
+const likeBtn = document.querySelector(".heartIcon");
+let likeBtnSwitch = false;
+
+likeBtn.addEventListener("click", likeBtnEvent);
+
+//댓글 좋아요 함수
+function likeBtnEvent(event) {
+  if (likeBtnSwitch === false) {
+    likeBtnSwitch = true;
+
+    const likeBtn = event.target;
+
+    likeBtn.classList.remove("likeIcon");
+    likeBtn.classList.add("solidHeartIcon");
+  } else {
+    likeBtnSwitch = false;
+
+    const likeBtn = event.target;
+
+    likeBtn.classList.remove("solidHeartIcon");
+    likeBtn.classList.add("likeIcon");
+  }
+}
+
+//아이디 검색 기능
+
+// 아이디 생성 class 실패!
+// class AccountObj {
+//   constructor(id, profile, details) {
+//     (this.id = id), (this.profile = profile), (this.details = details);
+//   }
+// }
+
+// const Wecode = new AccountObj(
+//   "Wecode",
+//   "./img/pin_drop_FILL0_wght400_GRAD0_opsz48.png",
+//   "강남구 테헤란로 427, 서울"
+// );
+
+// const wecode_korea = new AccountObj(
+//   "wecode_korea",
+//   "./img/account_circle_FILL0_wght400_GRAD0_opsz48.png"
+// );
+
+// const wecode_bootcamp = new AccountObj(
+//   "wecode_bootcamp",
+//   "./img/account_circle_FILL0_wght400_GRAD0_opsz48.png",
+//   ">wecode|위코드"
+// );
+
+// const yeonnn = new AccountObj(
+//   "yeonnn",
+//   "./img/more_horiz_FILL0_wght400_GRAD0_opsz48.png",
+//   "김지연(Ji Yeon Kim)"
+// );
+
+// const jiguuu = new AccountObj(
+//   "jiguuu",
+//   "img/KakaoTalk_20230509_143849516_02.jpg",
+//   "김지구(Ji Gu Kim)"
+// );
+
+// const AccountObjArr = [Wecode, wecode_korea, wecode_bootcamp, yeonnn, jiguuu];
+
+//아이디 생성 ---------------------------------------모르겟서ㅓㅓㅓㅓㅓㅓㅓ
+const account = [
+  {
+    id: "Wecode",
+    profile: "./img/pin_drop_FILL0_wght400_GRAD0_opsz48.png",
+    details: "강남구 테헤란로 427, 서울",
+  },
+  {
+    id: "wecode_korea",
+    profile: "./img/account_circle_FILL0_wght400_GRAD0_opsz48.png",
+  },
+  {
+    id: "wecode_bootcamp",
+    profile: "./img/account_circle_FILL0_wght400_GRAD0_opsz48.png",
+    details: ">wecode|위코드",
+  },
+  {
+    id: "yeonnn",
+    profile: "./img/more_horiz_FILL0_wght400_GRAD0_opsz48.png",
+    details: "김지연(Ji Yeon Kim)",
+  },
+  {
+    id: "jiguuu",
+    profile: "img/KakaoTalk_20230509_143849516_02.jpg",
+    details: "김지구(Ji Gu Kim)",
+  },
+];
+
+const accountID = account.map((item) => item.id); //accountObjArr -> IDarr
+
+//맞는 아이디 찾기
+const searchBar = document.getElementById("search"); // input
+const resultBox = document.getElementById("searchedID"); //ul
+
+function matchID(value) {
+  const serachBarValue = searchBar.value;
+  return accountID.indexOf(serachBarValue) != -1;
+}
+
+function paintMatchedID(element) {
+  const paintIDList = account.filter((x) => {
+    return x.id == element;
+  });
+
+  const resultID = document.createElement("li");
+  resultID.innerHTML = `<img
+  src=${paintIDList[0].profile}
+  alt="${paintIDList[0].id}님의 프로필 사진"
+/>
+<div>
+  <div class="bold">${paintIDList[0].id}</div>
+  <div class="grey">${paintIDList[0].details}</div>
+</div>`;
+  resultID.style.display = "display";
+  resultBox.appendChild(resultID);
+}
+
+function filterAccount() {
+  if (searchBar.value.length > 0) {
+    const filterId = accountID.filter((x) => matchID(x));
+    if (filterId.length > 0) {
+      resultBox.display = "flex";
+      filterId.forEach((id) => paintMatchedID(id)); //**forEach
+      resultBox.display = "none";
+      resultBox.innerHTML = "";
+    }
+  }
+}
+
+searchBar.addEventListener("keyup", filterAccount);
+
+// const result = idArray.filter((value) => value.indexOf("Wecode") !== -1);
+// -> value.indexOf('') 하면 해당하지 않는 거만 나옴!
+
+//profile menu
+const profile = document.getElementById("profile");
+const profileBox = document.getElementById("profileBox");
+
+let profileBoxSwitch = false;
+
+profile.addEventListener("click", function () {
+  if (profileBoxSwitch === false) {
+    profileBoxSwitch = true;
+    profileBox.classList.remove("displayNone");
+  } else {
+    profileBoxSwitch = false;
+    profileBox.classList.add("displayNone");
+  }
+});
