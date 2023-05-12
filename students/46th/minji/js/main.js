fc19f14a072e95180ae5e515a14f863a07d0582d
shfkd123 <shfkd123@gmail.com>
2023-05-12 11:57:58 +0900

    댓글 좋아요/삭제 기능 추가

--- students/46th/minji/js/main.js
+++ students/46th/minji/js/main.js
@@ -14,21 +14,47 @@ function btnActive() {
 }
 
 //태그 생성 공통 함수
-function createElementFunc(tag, select, name, input) {
+function createElementFunc(tag, attr, name, id, input) {
     const rtTag = document.createElement(tag);
-    rtTag.setAttribute(select, name);
-    rtTag.innerText = input;
+    rtTag.setAttribute(attr, name);
+    
+    if(tag === "img"){
+        rtTag.src = input;
+        rtTag.setAttribute("id", id);
+    }else {
+        rtTag.innerText = input;
+    }
+
     return rtTag;
+    
 }
 
 function chatEvent() { 
     const chatinput = document.getElementById('chatinput').value;
-    const li = createElementFunc("li" ,"class", "chat", chatinput);
-    const span = createElementFunc("span" ,"class", "bord-word", "neceosecius ");
+    const li = createElementFunc("li" ,"class", "chat", null, chatinput);
+    const span = createElementFunc("span" ,"class", "bord-word", null, "neceosecius ");
     
     const ul = document.querySelector('ul');
     ul.appendChild(li);
     li.prepend(span);
     chatinput.value = null;
 
+}
+
+// 좋아요 버튼 
+function Heart() {
+    const heartBtn = document.getElementById('heartBtn');
+    const nolike = document.getElementById("nolike");
+    const yeslike = document.getElementById("yeslike");
+
+    if(nolike){
+        nolike.remove();
+        const likeimg = createElementFunc("img", "class", "likeimg", "yeslike", "./images/red_heart.png");
+        heartBtn.appendChild(likeimg);
+    }else{
+        yeslike.remove();
+        const likeimg = createElementFunc("img", "class", "likeimg", "nolike","./images/heart.png");
+        heartBtn.appendChild(likeimg);
+    }
+    
 }
