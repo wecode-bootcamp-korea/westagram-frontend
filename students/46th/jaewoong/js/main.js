fdd9b72c1a9f63a87aee18f43d229a987f23a224
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-12 15:27:46 +0900

    STYLE : 주석제거,세미콜론 추가

--- students/46th/jaewoong/js/main.js
+++ students/46th/jaewoong/js/main.js
@@ -1,48 +1,34 @@
-// 엔터시 댓글 업로드
 document.querySelector('.reply').addEventListener('keydown',function(e){
     let reply = e.target.value;
     if( e.keyCode === 13){
-        // 댓글 추가
         createComment(reply);
 
-        // upload된 댓글에 하트 버튼 
         uploadHeart();
 
-        // upload된 댓글에 삭제 버튼
         uploadDelete();
     }
-})
+});
 
 
-// 게시 버튼 클릭시 댓글 업로드
 document.querySelector('.upload').addEventListener('click',function(){
     let reply = document.querySelector('.reply').value;
 
-    // 댓글 추가
     createComment(reply);
     
-    // upload된 댓글에 좋아요 버튼 
     uploadHeart();
 
-    // upload된 댓글에 삭제 버튼
     uploadDelete();
+});
 
-})
 
-
-// 기존 댓글 좋아요 버튼 
 uploadHeart();
 
-// 기존 댓글 삭제 버튼 
 uploadDelete();
 
-// 아이디 검색 
 searchId();
 
-// nav 프로필 사진 버튼
 profileSettings();
 
-// 댓글 추가 함수
 function createComment(reply){
     if(reply === ''){
         return;
@@ -55,11 +41,9 @@ function createComment(reply){
     <button class="setting-reply delete"">삭제</button>
     </div>
     </div>`;
-
-     document.querySelector('.content-reply').insertAdjacentHTML('beforeend',템플릿)
+    document.querySelector('.content-reply').insertAdjacentHTML('beforeend',템플릿);
 }
 
-// 댓글 하트 함수
 function uploadHeart(){
     let click = 0 ;
     for(let i = 0 ; i < $('.heart').length ; i++){
@@ -68,15 +52,12 @@ function uploadHeart(){
                 if(click%2 === 1){
                     $(e.target).addClass('red');
                 }else{
-                    $(e.target).removeClass('red')
+                    $(e.target).removeClass('red');
                 }
             })
-
-
     }
 }
 
-// 댓글 삭제 함수
 function uploadDelete(){
     for(let i = 0 ; i < $('.delete').length ; i++){
         $('.delete').eq(i).click(function(e){
@@ -85,18 +66,16 @@ function uploadDelete(){
     }
 }
 
-// 아이디 목록 배열
 const id = [{ nickname : 'wecode_bootcamp' , name : '>wecode' , img : 'img/wecode.png' },
           { nickname : 'wecode_founder' , name : '송은우' , img : 'img/zebra.jpg' },
           { nickname : 'wecode' , name : '강남구 테헤란로 427,서울', img : 'img/explore.png'},
           { nickname : '이건안되게' , name : '강남구 테헤란로 427,서울', img : 'img/explore.png'}
         ];
 
-// 아이디 검색 함수
 function searchId(){
     $('.search').on('keyup', e => {
-        $('.search-result').html('')
-        $('.search-result').addClass('show')
+        $('.search-result').html('');
+        $('.search-result').addClass('show');
         let search = $(e.target).val();
         if(search.length > 0){
             id.forEach( (a,i) => {
@@ -122,9 +101,8 @@ function searchId(){
     })
 }
 
-// 프로필 버튼 함수
 function profileSettings(){
     $('.nav-button').eq(2).click(function(){
-        $('.nav-menu').toggleClass('show')
+        $('.nav-menu').toggleClass('show');
     })
 }
