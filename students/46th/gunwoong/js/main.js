204b7779975e9ed7e1641ba480782a521087a313
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-12 22:55:39 +0900

    ADD : 댓글 추가기능  완료

--- students/46th/gunwoong/js/main.js
+++ students/46th/gunwoong/js/main.js
@@ -4,23 +4,29 @@ const feedsCommentButton = document.querySelector(".feeds__comment__button");
 const feedsCommentForm = document.querySelector("form");
 
 function CreateComment() {
-  const newComment = feedsCommentInput.value;
+  const newComment = [];
+  newComment.push(feedsCommentInput.value);
 
   const li = document.createElement("li");
+  li.className = "comment-list";
   feedsComment.appendChild(li);
 
   const span = document.createElement("span");
+  span.className = "comment-text";
   span.innerText = newComment;
   li.appendChild(span);
 
   const button = document.createElement("button");
-  button.innerText("❌");
+  button.className = "comment-delete";
+  button.innerText = "❌";
   li.appendChild(button);
+
+  feedsCommentInput.value = "";
 }
 
 feedsCommentButton.addEventListener("click", CreateComment);
 feedsCommentForm.addEventListener("submit", (event) => {
   event.preventDefault();
 });
-
+console.log(newComment);
 function DeleteComment() {}
