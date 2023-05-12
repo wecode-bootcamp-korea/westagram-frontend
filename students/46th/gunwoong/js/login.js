6d42a9cffdbd5bba3a51d0c437b3d3d547d3603d
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-12 21:41:13 +0900

    ADD: MISSION 5 완료

--- students/46th/gunwoong/js/login.js
+++ students/46th/gunwoong/js/login.js
@@ -1,16 +1,24 @@
 const loginIdInput = document.querySelector("#loginIdInput");
 const loginPwInput = document.querySelector("#loginPwInput");
 const loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.disabled = true;
 function btnActive() {
-  const loginValidationLength =
-    loginIdInput.value.length > 0 && loginPwInput.value.length > 0
-      ? (loginBtn.disabled = false)
-      : (loginBtn.disabled = true);
+  const loginId = loginIdInput.value;
+  const loginPw = loginPwInput.value;
+  const isValid = loginId.length > 0 && loginPw.length > 0;
+  isValid ? (loginBtn.disabled = false) : (loginBtn.disabled = true);
 }
+loginIdInput.addEventListener("input", btnActive);
+loginPwInput.addEventListener("input", btnActive);
+
 function loginValidation() {
-  const loginValidationIncludes =
-    loginIdInput.value.includes("@") && loginPwInput.value.length > 5
-      ? location.href("students/46th/gunwoong/main.html")
-      : alert("x");
+  const isValid =
+    loginIdInput.value.includes("@") && loginPwInput.value.length > 5;
+  if (isValid) {
+    location.href = "main.html";
+  } else {
+    alert("유효하지않음")
+  }
 }
 loginBtn.addEventListener("click", loginValidation);
