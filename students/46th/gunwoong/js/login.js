4fd768e9e54c77d5596a6837abaf4d2ec54f2a69
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-12 10:25:35 +0900

    ADD : MISSION 4 완료

--- students/46th/gunwoong/js/login.js
+++ students/46th/gunwoong/js/login.js
@@ -1,11 +1,16 @@
+const loginIdInput = document.querySelector("#loginIdInput");
+const loginPwInput = document.querySelector("#loginPwInput");
+const loginBtn = document.querySelector("#loginBtn");
 function btnActive() {
-  const loginIdInput = document.querySelector("#loginIdInput");
-  const loginPwInput = document.querySelector("#loginPwInput");
-  const loginBtn = document.querySelector("#loginBtn");
-
-  if (loginIdInput.value.length > 0 && loginPwInput.value.length > 0) {
-    loginBtn.disabled = false;
-  } else {
-    loginBtn.disabled = true;
-  }
+  const loginValidationLength =
+    loginIdInput.value.length > 0 && loginPwInput.value.length > 0
+      ? (loginBtn.disabled = false)
+      : (loginBtn.disabled = true);
 }
+function loginValidation() {
+  const loginValidationIncludes =
+    loginIdInput.value.includes("@") && loginPwInput.value.length > 5
+      ? location.href("students/46th/gunwoong/main.html")
+      : alert("x");
+}
+loginBtn.addEventListener("click", loginValidation);
--- students/46th/gunwoong/js/main.js
+++ students/46th/gunwoong/js/main.js
@@ -1,8 +1,26 @@
-const feedsComment = document.querySelector("feeds__comment");
-const feedsCommentInput = document.querySelector("feeds__comment__input");
+const feedsComment = document.querySelector(".feeds__comment");
+const feedsCommentInput = document.querySelector(".feeds__comment__input");
+const feedsCommentButton = document.querySelector(".feeds__comment__button");
+const feedsCommentForm = document.querySelector("form");
 
-function enterFn() {
-  if (window.event.keyCode == 13) {
-    feedsComment.innerHTML(`${feedsCommentInput}.value`);
-  }
+function CreateComment() {
+  const newComment = feedsCommentInput.value;
+
+  const li = document.createElement("li");
+  feedsComment.appendChild(li);
+
+  const span = document.createElement("span");
+  span.innerText = newComment;
+  li.appendChild(span);
+
+  const button = document.createElement("button");
+  button.innerText("❌");
+  li.appendChild(button);
 }
+
+feedsCommentButton.addEventListener("click", CreateComment);
+feedsCommentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+function DeleteComment() {}
