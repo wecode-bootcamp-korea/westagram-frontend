91bdd651ddca383e89a687034444cb11f041464d
Kaylla Son <kaylla@Kayllas-MacBook-Air.local>
2023-05-13 22:54:25 +0900

    ADD: 댓글 좋아요 기능, 댓글 삭제 기능 추가 - 댓글 옆 하트 아이콘을 누르면 아이콘의 색상이  변하고, 두번 클릭했을시 다시 원래 색상으로 돌아가는 기능 구현 - 내가 쓴  댓글 옆 삭제버튼을 누르면 댓글이 삭제되는 기능 구현

--- students/46th/sonjahyun/js/main.js
+++ students/46th/sonjahyun/js/main.js
@@ -1,32 +1,65 @@
+const commentNode = document.querySelector('.commentInput');
+const commentBtn = document.getElementsByClassName('commentBtn')[0];
+let callCount = 0;
+
 function postComment() {
-    const commentNode = document.querySelector('.commentInput');
+    ++callCount;
     const commentText = commentNode.value;
 
     if(commentText) {
-        const newComment = document.createElement('li');
+        const commentsListNode = document.querySelector('.commentsList'); 
+
+        const commentAndBtns = document.createElement('div');
+        commentAndBtns.className = "commentAndBtns";
+        commentsListNode.appendChild(commentAndBtns);
+
+        const newComment = document.createElement('div'); // creates a new comment
         newComment.textContent = commentText;
         newComment.className = "comment";
-
-        const commentsListNode = document.querySelector('.commentsList'); 
-        commentsListNode.appendChild(newComment);
+        commentAndBtns.appendChild(newComment);
   
-        const myUsername = document.createElement('span');
-        myUsername.className = 'bold';
+        const myUsername = document.createElement('span'); // creates a username for each comment
+        myUsername.classList.add('bold');
+        myUsername.classList.add('floatLeft');
         myUsername.textContent = 'wecode_bootcamp ';
-        newComment.insertBefore(myUsername,newComment.firstChild); // adds myUsername before the comment
+        newComment.insertBefore(myUsername,newComment.firstChild); 
 
-        const likeBtn = document.createElement('img');
-        likeBtn.src = '../img/heart.png';
-        likeBtn.className = 'smallIcon';
-        commentsListNode.appendChild(likeBtn);
+        const deleteBtn = document.createElement('button'); // creates a delete button
+        deleteBtn.className = "deleteBtn";
+        deleteBtn.classList.add("lighter");
+        deleteBtn.setAttribute('type', 'button');
+        deleteBtn.innerText = '삭제';
+        deleteBtn.setAttribute("id", "deleteBtn" + callCount);
+        commentAndBtns.appendChild(deleteBtn);
 
+        const likeBtn = document.createElement('button'); // creates a like button 
+        likeBtn.className = "likeBtn";
+        likeBtn.setAttribute('type', 'button');
+        likeBtn.setAttribute("id", "likeBtn" + callCount);
+        commentAndBtns.appendChild(likeBtn);
+
+        const heartIcon = document.createElement('i'); // adds a heart icon to the like button
+        heartIcon.className = "fa fa-heart";
+        heartIcon.setAttribute("id", "likeBtn" + callCount);
+        likeBtn.appendChild(heartIcon);
+    
         commentNode.value = ''; // clears the input box after comment added
     }
 }
 
-const commentBtn = document.getElementsByClassName('commentBtn')[0];
-commentBtn.addEventListener('click', postComment);
-
-document.querySelector('.commentInput').addEventListener('keypress', function(e) {
+commentBtn.addEventListener('click', postComment); // posts comment if clicked on comment button
+commentNode.addEventListener('keypress', function(e) { 
     e.key === 'Enter' ? postComment() : null;
-});
+}); // posts comment if the enter key was pressed after writing the comment
+
+document.addEventListener("click", function(event) {
+     let btnID = ''; 
+    if(event.target.id) {
+        btnID = event.target.id;
+        const clickedBtn = document.getElementById(btnID);
+        if (clickedBtn.classList.contains('likeBtn')) {
+            clickedBtn.classList.contains('liked') ? clickedBtn.classList.remove('liked') : clickedBtn.classList.add('liked');
+        }
+        clickedBtn.classList.contains('deleteBtn') ? clickedBtn.parentElement.remove() : null;   
+    }
+}); // changes color of the heart if clicked on the like button and deletes the comment if clicked on the delete button
